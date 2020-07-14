b3c42849c1b033f3294ea5cf762dfb725369182f
KiChang Shin <kc7851@gmail.com>
2020-07-14 23:58:13 +0900

    api postLogin 구현

--- src/services/api.js
+++ src/services/api.js
@@ -27,3 +27,13 @@ export async function fetchRestaurant({ restaurantId }) {
   const data = await response.json();
   return data;
 }
+
+export async function postLogin({ email, password }) {
+  const url = 'https://eatgo-login-api.ahastudio.com/session';
+  const response = await fetch(url, {
+    method: 'post',
+    body: JSON.stringify({ email, password }),
+  });
+  const { accessToken } = await response.json();
+  return accessToken;
+}
