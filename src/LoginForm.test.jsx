9c06addad5c9a2f4d6174e3176a4ae360aba522f
eonsang <2eonhworking@gmail.com>
2020-07-15 05:15:01 +0900

    Test: 로그인폼 test 분리

---
+++ src/LoginForm.test.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import LoginForm from './LoginForm';
+
+const renderLoginForm = () => render(<LoginForm />);
+
+describe('LoginForm', () => {
+  it('이메일과 비밀번호 폼이 있다.', () => {
+    const { getByLabelText } = renderLoginForm();
+
+    expect(getByLabelText('Email')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('이메일과 비밀번호가 변경된다.', () => {
+    const fields = [
+      {
+        label: 'Email',
+        value: 'test@test.com',
+      },
+      {
+        label: 'Password',
+        value: '1234',
+      },
+    ];
+
+    const { getByLabelText } = renderLoginForm();
+
+    fields.forEach((field) => {
+      const { label, value } = field;
+      const input = getByLabelText(label);
+
+      fireEvent.change(input, {
+        target: { value },
+      });
+
+      expect(input.value).toBe(value);
+    });
+  });
+});
