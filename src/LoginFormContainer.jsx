e6299be16233daf5887437b3fc5c91a08053a3b1
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-15 01:17:19 +0900

    Add dispatch to LoginFormContainer

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
+import { get } from './utils';
+
 export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
+  const { email, password } = { email: '', password: '' };
+
+  // TODO: useSelector(get('loginFields'));
+
+  function handleChange() {
+    dispatch(() => {});
+    // TODO: changeLoginField actions
+  }
+
   return (
     <>
       <div>
@@ -9,8 +24,10 @@ export default function LoginFormContainer() {
         </label>
         <input
           type="email"
-          id="login-password"
+          id="login-email"
           name="email"
+          value={email}
+          onChange={handleChange}
         />
       </div>
       <div>
@@ -21,6 +38,8 @@ export default function LoginFormContainer() {
           type="password"
           id="login-password"
           name="password"
+          value={password}
+          onChange={handleChange}
         />
       </div>
       <div>
