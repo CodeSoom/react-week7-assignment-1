f8045ba53cc339972e5709c89241925bb5fa2f77
eonsang <2eonhworking@gmail.com>
2020-07-15 05:04:48 +0900

    Test: 로그인 폼 변경 테스트

--- src/LoginContainer.test.jsx
+++ src/LoginContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginContainer from './LoginContainer';
 
@@ -10,5 +10,25 @@ describe('LoginContainer', () => {
 
     expect(getByLabelText('Email')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('이메일이 변경된다.', () => {
+    const { getByLabelText } = render(<LoginContainer />);
+
+    fireEvent.change(getByLabelText('Email'), {
+      target: { value: 'test@test.com' },
+    });
+
+    expect(getByLabelText('Email').value).toBe('test@test.com');
+  });
+
+  it('비밀번호가 변경된다.', () => {
+    const { getByLabelText } = render(<LoginContainer />);
+
+    fireEvent.change(getByLabelText('Password'), {
+      target: { value: '1234' },
+    });
+
+    expect(getByLabelText('Password').value).toBe('1234');
   });
 });
