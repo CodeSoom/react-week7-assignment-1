1ecb9f5d0b7622ef65dd7c1b084c16a877036ac0
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-15 00:27:46 +0900

    Add test for LoginPage

---
+++ src/LoginPage.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  const renderLoginPage = () => render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>,
+  );
+
+  it('renders title', () => {
+    const { container } = renderLoginPage();
+
+    expect(container).toHaveTextContent('Log In');
+  });
+
+  it('renders input controls', () => {
+    const { container } = renderLoginPage();
+
+    expect(container).toHaveTextContent('E-mail');
+    expect(container).toHaveTextContent('Password');
+  });
+});
