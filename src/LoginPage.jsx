71dbb988d9a46a1d45493c894db14734f587c61f
eonsang <2eonhworking@gmail.com>
2020-07-15 04:44:44 +0900

    Feat: 이메일과 비밀번호 폼 추가

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,6 +4,15 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
+
+      <div>
+        <label htmlFor="Email">Email</label>
+        <input type="email" id="Email" />
+      </div>
+      <div>
+        <label htmlFor="Password">Password</label>
+        <input type="password" id="Password" />
+      </div>
     </div>
   );
 }
