a2ec0c9e39c82ac13107a3b936b2c6dff92e5e46
eonsang <2eonhworking@gmail.com>
2020-07-15 05:07:53 +0900

    Test: 로그인 컨테이너 render 함수 분리

--- src/LoginContainer.test.jsx
+++ src/LoginContainer.test.jsx
@@ -4,16 +4,18 @@ import { render, fireEvent } from '@testing-library/react';
 
 import LoginContainer from './LoginContainer';
 
+const renderLoginContainer = () => render(<LoginContainer />);
+
 describe('LoginContainer', () => {
   it('이메일과 비밀번호 폼이 있다.', () => {
-    const { getByLabelText } = render(<LoginContainer />);
+    const { getByLabelText } = renderLoginContainer();
 
     expect(getByLabelText('Email')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
   });
 
   it('이메일이 변경된다.', () => {
-    const { getByLabelText } = render(<LoginContainer />);
+    const { getByLabelText } = renderLoginContainer();
 
     fireEvent.change(getByLabelText('Email'), {
       target: { value: 'test@test.com' },
@@ -23,7 +25,7 @@ describe('LoginContainer', () => {
   });
 
   it('비밀번호가 변경된다.', () => {
-    const { getByLabelText } = render(<LoginContainer />);
+    const { getByLabelText } = renderLoginContainer();
 
     fireEvent.change(getByLabelText('Password'), {
       target: { value: '1234' },
