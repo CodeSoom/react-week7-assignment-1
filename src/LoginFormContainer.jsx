d669d1694c8117c91e7eb923704178667ed8c736
KiChang Shin <kc7851@gmail.com>
2020-07-14 21:10:28 +0900

    LoginFormContainer test 구현

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+
+import {
+  changeLoginFields,
+} from './actions';
+
 export default function LoginPage() {
+  const dispatch = useDispatch();
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    dispatch(changeLoginFields({ name, value }));
+  }
+
   return (
     <>
       <p>Log in</p>
@@ -8,13 +22,13 @@ export default function LoginPage() {
         <label htmlFor="email">
           E-mail
         </label>
-        <input type="text" id="email" />
+        <input type="text" id="email" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="password">
           Password
         </label>
-        <input type="text" id="password" />
+        <input type="text" id="password" onChange={handleChange} />
       </div>
       <button
         type="button"
