9faac2654999ebe839352b4c109013a717461ad6
eonsang <2eonhworking@gmail.com>
2020-07-15 05:05:21 +0900

    Feat: 로그인 폼 상태관리 추가

--- src/LoginContainer.jsx
+++ src/LoginContainer.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LoginContainer() {
+  const [fields, setFields] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <h2>Log In</h2>
 
       <div>
         <label htmlFor="Email">Email</label>
-        <input type="email" id="Email" />
+        <input
+          type="email"
+          name="email"
+          id="Email"
+          value={fields.email}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="Password">Password</label>
-        <input type="password" id="Password" />
+        <input
+          type="password"
+          name="password"
+          id="Password"
+          value={fields.password}
+          onChange={handleChange}
+        />
       </div>
     </div>
   );
