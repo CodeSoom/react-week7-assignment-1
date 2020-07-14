fe661d323464eb2915244f8e92dffabb30276a48
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-14 09:20:59 +0900

    test(LoginPage): listen field change event
    - 이메일 필드 입력값이 변경되는 이벤트 추적 테스트

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
-test('LoginPage', () => {
-  const { getByLabelText } = render((
-    <LoginPage />
-  ));
+describe('LoginPage', () => {
+  it('renders the login form', () => {
+    const { getByLabelText } = render((
+      <LoginPage />
+    ));
 
-  expect(getByLabelText('E-mail')).not.toBeNull();
-  expect(getByLabelText('Password')).not.toBeNull();
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('listens form fields change event', () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render((
+      <LoginPage onChange={handleChange} />
+    ));
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'tester@example.com' },
+    });
+
+    expect(handleChange).toBeCalled();
+  });
 });
