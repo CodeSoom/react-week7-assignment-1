6fc6c0ae2a0f4150c56187bbe895f0f23198c27d
eonsang <2eonhworking@gmail.com>
2020-07-15 05:30:16 +0900

    Test: 로그인 폼 변경 테스트 추가

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
-const renderLoginForm = () => render(<LoginForm />);
-
 describe('LoginForm', () => {
+  const fields = {
+    email: '',
+    password: '',
+  };
+  const onChange = jest.fn();
+
+  const renderLoginForm = () =>
+    render(<LoginForm fields={fields} onChange={onChange} />);
+
   it('이메일과 비밀번호 폼이 있다.', () => {
     const { getByLabelText } = renderLoginForm();
 
     expect(getByLabelText('Email')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('폼이 변경되면 폼 변경함수가 호출된다.', () => {
+    const { getByLabelText } = renderLoginForm();
+
+    fireEvent.change(getByLabelText('Email'), {
+      target: { value: 'test' },
+    });
+
+    expect(onChange).toBeCalled();
   });
 });
