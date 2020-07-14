8ca1ec3ada79618b54e8f46763d47d2d94e86f27
KiChang Shin <kc7851@gmail.com>
2020-07-14 22:19:05 +0900

    LoginPage test 리팩토링
    useSelector를 mocking 하였습니다.

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
+import loginFields from '../fixtures/loginFields';
+
+jest.mock('react-redux');
+
+function renderLoginPage() {
+  return render(
+    <LoginPage />,
+  );
+}
+
 describe('LoginPage', () => {
-  function renderLoginPage() {
-    return render(
-      <LoginPage />,
-    );
-  }
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields,
+    }));
+  });
 
   it('renders input controls', () => {
     const { getByLabelText } = renderLoginPage();
