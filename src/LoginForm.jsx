a069a2656456081369cc22f6ab2db8b90f70336f
eonsang <2eonhworking@gmail.com>
2020-07-15 05:36:42 +0900

    Feat: 로그인 버튼 추가

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -7,26 +7,30 @@ export default function LoginForm({ fields, onChange }) {
     <div>
       <h2>Log In</h2>
 
-      <div>
-        <label htmlFor="Email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="Email"
-          value={email}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="Password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="Password"
-          value={password}
-          onChange={onChange}
-        />
-      </div>
+      <form action="">
+        <div>
+          <label htmlFor="Email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="Email"
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="Password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="Password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+
+        <button type="submit">로그인</button>
+      </form>
     </div>
   );
 }
