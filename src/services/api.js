e2dc4ff830a4c09e55d6492fb180debdec3f7eab
KiChang Shin <kc7851@gmail.com>
2020-07-15 00:32:43 +0900

    api 수정
    fetch에 headers 객체를 추가하였습니다.

--- src/services/api.js
+++ src/services/api.js
@@ -32,6 +32,9 @@ export async function postLogin({ email, password }) {
   const url = 'https://eatgo-login-api.ahastudio.com/session';
   const response = await fetch(url, {
     method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ email, password }),
   });
   const { accessToken } = await response.json();
