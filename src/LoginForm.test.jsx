bbb09c213d3a908588d7e744a8da704c0b2e11e1
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-15 15:46:19 +0900

    Test: LoginForm renders input controls and listens change events

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  it('renders input controls', () => {
+  it('renders input controls and listens change events', () => {
+    const handleChange = jest.fn();
+
+    const email = 'test@test';
+    const password = '1234';
     const { getByLabelText } = render((
-      <LoginForm />
+      <LoginForm
+        fields={{ email, password }}
+        onChange={handleChange}
+      />
     ));
 
-    expect(getByLabelText('E-mail')).not.toBeNull();
-    expect(getByLabelText('Password')).not.toBeNull();
-  })
+    const controls = [
+      { 
+        label: 'E-mail',
+        name: 'email',
+        origin: email,
+        value: 'tester@example.com'
+      },
+      { 
+        label: 'Password',
+        name: 'password',
+        origin: password,
+        value: 'test'
+      },
+    ];
+
+    controls.forEach(({label, name, origin, value}) => {
+      const input = getByLabelText(label)
+
+      expect(input.value).toBe(origin);
+
+      fireEvent.change(input, {
+        target: { value },
+      });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
+  });
 });
