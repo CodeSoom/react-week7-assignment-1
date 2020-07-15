75b82e86dfef539ccb96eb3c7f1e0916b48e90b2
daadaadaah <daadaadaah@gmail.com>
2020-07-16 05:37:34 +0900

    Update a LoginPage test for adding useSelector

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: '',
+        password: '',
+      },
+    }));
+  });
+
   it('renders LoginPage', () => {
     const { container, getByLabelText, queryByRole } = render(<LoginPage />);
 
