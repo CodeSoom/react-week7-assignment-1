36407fedc3309129d45f5b6759305fa95ae24484
Minwook Park <mwpark0975@gmail.com>
2020-07-15 22:35:02 +0900

    test - LoginForm

---
+++ src/LoginForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function LoginForm() {
+  function handleClick() {
+
+  }
+  return (
+    <>
+      <div>
+        <h2>Log In</h2>
+        <label htmlFor="login-email">E-mail</label>
+        <input type="email" id="login-email" />
+      </div>
+      <div>
+        <label htmlFor="login-password">Password</label>
+        <input type="password" id="login-password" />
+      </div>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Log In
+      </button>
+    </>
+  );
+}
---
+++ src/LoginForm.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import LoginForm from './LoginForm';
+
+describe('LoginForm', () => {
+  it('renders input control', () => {
+    const { getByLabelText } = render(<LoginForm />);
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+});
