c65187fb9e7e6ede4a78e45dd9390179b49cd50d
Minwook Park <mwpark0975@gmail.com>
2020-07-15 23:17:39 +0900

    implement LoginForm, refactor test

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -19,14 +19,23 @@ describe('LoginForm', () => {
       <LoginForm onChange={handleChange} />,
     );
 
-    expect(getByLabelText('E-mail')).not.toBeNull();
-    expect(getByLabelText('Password')).not.toBeNull();
+    const controls = [
+      { label: 'E-mail', name: 'email', value: 'tester@example.com' },
+      { label: 'Password', name: 'password', value: 'test' },
+    ];
 
-    fireEvent.change(getByLabelText('E-mail'), {
-      target: { value: 'tester@example.com' },
+    controls.forEach(({ label, name, value }) => {
+      const input = getByLabelText(label);
+
+      expect(input).not.toBeNull();
+
+      fireEvent.change(input, { target: { value } });
+
+      expect(handleChange).toBeCalledWith({ name, value });
     });
 
-    expect(handleChange).toBeCalled();
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
   });
 
   it('renders Log In button', () => {
