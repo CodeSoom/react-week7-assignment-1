edb930e9cadc12049af78bc20304025887dbfc07
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-16 08:27:31 +0900

    refactor(RoutePage): include header
    - 헤도 라우트 페이지에서 관리하도록 책임 이관

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 
-import {
-  Link,
-} from 'react-router-dom';
-
 import RoutePage from './RoutePage';
 
 export default function App() {
   return (
     <div>
-      <header>
-        <h1>
-          <Link to="/">헤더</Link>
-        </h1>
-      </header>
       <RoutePage />
     </div>
   );
--- src/RoutePage.jsx
+++ src/RoutePage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   Switch,
   Route,
+  Link,
 } from 'react-router-dom';
 
 import HomePage from './HomePage';
@@ -14,13 +15,20 @@ import LoginPage from './LoginPage';
 
 export default function RoutePage() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route exact path="/restaurants" component={RestaurantsPage} />
-      <Route path="/restaurants/:id" component={RestaurantPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <header>
+        <h1>
+          <Link to="/">헤더</Link>
+        </h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route exact path="/restaurants" component={RestaurantsPage} />
+        <Route path="/restaurants/:id" component={RestaurantPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   );
 }
