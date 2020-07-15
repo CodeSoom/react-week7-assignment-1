37c9eb24a0311eae599f70bb447b3c8bae3def4c
Paul An <anpaul0615@gmail.com>
2020-07-15 10:33:45 +0900

    LoginPage 리팩토링
    - 입력 관련 처리를 LoginFormContainer 에 위임

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
+import LoginFormContainer from '../containers/LoginFormContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
-      <div>
-        <label htmlFor="login-email">E-Mail</label>
-        <input type="email" name="login-email" id="login-email" />
-
-        <label htmlFor="login-password">Password</label>
-        <input type="password" name="login-password" id="login-password" />
-      </div>
+      <LoginFormContainer />
     </div>
   );
 }
