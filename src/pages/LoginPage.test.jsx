996bcddc2366015475abe87a05e1f800025a5202
morykwon <kwonmory@gmail.com>
2020-07-15 18:55:16 +0900

    [Blue] LoginPage 리팩토링한다
    - 중복 제거

--- src/pages/LoginPage.test.jsx
+++ src/pages/LoginPage.test.jsx
@@ -5,23 +5,21 @@ import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
+const renderLoginPage = () => render(
+  <MemoryRouter>
+    <LoginPage />
+  </MemoryRouter>,
+);
+
 describe('LoginPage', () => {
   it('renders Login title', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <LoginPage />
-      </MemoryRouter>,
-    );
+    const { container } = renderLoginPage();
 
     expect(container).toHaveTextContent('Log In');
   });
 
   it('redners Login input element', () => {
-    const { getByLabelText } = render(
-      <MemoryRouter>
-        <LoginPage />
-      </MemoryRouter>,
-    );
+    const { getByLabelText } = renderLoginPage();
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
