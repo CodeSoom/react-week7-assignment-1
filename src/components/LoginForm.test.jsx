20860b9e8f10f4a9b9fcf67fac14b9210d926b2d
morykwon <kwonmory@gmail.com>
2020-07-15 23:12:18 +0900

    [Blue] LoginForm 테스트 케이스 추가하기
    - context로 나누기
    - 로그인 버튼 눌렀을 때 이벤트 발생하는지 테스트 추가

--- src/components/LoginForm.test.jsx
+++ src/components/LoginForm.test.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  it('renders LoginForm', () => {
+  context('renders LoginForm', () => {
     const { getByLabelText, container } = render(<LoginForm />);
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
     expect(container.querySelector('button')).toHaveTextContent('Log In');
+  });
+
+  context('when click login button', () => {
+    it('request login', () => {
+      const handleSubmit = jest.fn();
+
+      const { container } = render(<LoginForm onSubmit={handleSubmit} />);
+
+      fireEvent.click(container.querySelector('button'));
+
+      expect(handleSubmit).toBeCalled();
+    });
   });
 });
