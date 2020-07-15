22167f96fb4a70a372e0f92843bdf65a0b5d1f84
sogoagain <imyong0@gmail.com>
2020-07-16 02:06:29 +0900

    Test - <LoginForm /> 입력 테스트 작성

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -6,29 +6,51 @@ import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   const handleSubmit = jest.fn();
+  const handleChange = jest.fn();
 
   beforeEach(() => {
     handleSubmit.mockClear();
+    handleChange.mockClear();
   });
 
-  function renderLoginForm(onSubmit) {
+  function renderLoginForm(email, password, onSubmit, onChange) {
     return render(
       <LoginForm
+        email={email}
+        password={password}
         onSubmit={onSubmit}
+        onChange={onChange}
       />,
     );
   }
 
   it('renders email and password input', () => {
-    const { getByLabelText } = renderLoginForm(handleSubmit);
+    const inputElements = [
+      { label: 'E-mail', name: 'email', values: { before: 'before@test.com', after: 'after@test.com' } },
+      { label: 'Password', name: 'password', values: { before: 'beforePassword1', after: 'afterPassword1' } },
+    ];
 
-    expect(getByLabelText('E-mail')).toBeInTheDocument();
-    expect(getByLabelText('Password')).toBeInTheDocument();
+    const { getByLabelText } = renderLoginForm(
+      inputElements[0].values.before,
+      inputElements[1].values.before,
+      handleSubmit,
+      handleChange,
+    );
+
+    inputElements.forEach(({ label, name, values: { before, after } }) => {
+      const inputElement = getByLabelText(label);
+      expect(inputElement.value).toBe(before);
+
+      fireEvent.change(inputElement, { target: { value: after } });
+      expect(handleChange).toBeCalledWith({ name, value: after });
+    });
   });
 
   it('renders login button', () => {
-    const { getByText } = renderLoginForm(handleSubmit);
+    const { getByText } = renderLoginForm('', '', handleSubmit, handleChange);
 
-    expect(getByText('Log In')).toBeInTheDocument();
+    fireEvent.click(getByText('Log In'));
+
+    expect(handleSubmit).toBeCalled();
   });
 });
