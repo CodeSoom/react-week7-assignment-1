eb8b16007bc5520c163d5b36e20a16f6a3482d14
morykwon <kwonmory@gmail.com>
2020-07-15 22:55:58 +0900

    [Red] LoginForm 테스트를 작성한다
    - input element 중 이메일과 패스워드가 있는지 체크
    - 로그인 버튼이 있는지 체크

---
+++ src/components/LoginForm.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import LoginForm from './LoginForm';
+
+describe('LoginForm', () => {
+  it('renders LoginForm', () => {
+    const { getByLabelText, container } = render(<LoginForm />);
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+    expect(container.querySelector('button')).toHaveTextContent('Log In');
+  });
+});
