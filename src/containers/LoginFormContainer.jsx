b23cfd979953016d8089f7fe942c0cb681224b5a
morykwon <kwonmory@gmail.com>
2020-07-15 22:58:07 +0900

    [Blue] LoginFormContainer를 수정한다
    - 기존에 뷰부분을 Component로 분리

--- src/containers/LoginFormContainer.jsx
+++ src/containers/LoginFormContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useDispatch } from 'react-redux';
+import LoginForm from '../components/LoginForm';
 
 import { requestLogin } from '../modules/actions';
 
@@ -8,24 +9,11 @@ export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   const onSubmit = () => {
-    // TODO dispatch
     dispatch(requestLogin());
   };
   return (
     <>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input type="email" id="login-email" />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          Password
-        </label>
-        <input type="password" id="login-password" />
-      </div>
-      <button type="button" onClick={onSubmit}>Log In</button>
+      <LoginForm onSubmit={onSubmit} />
     </>
   );
 }
