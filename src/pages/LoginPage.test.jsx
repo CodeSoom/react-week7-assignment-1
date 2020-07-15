2ea3af645c87e5aaa7c04961687ed3ae593eb039
morykwon <kwonmory@gmail.com>
2020-07-15 18:37:10 +0900

    [Red] LoginPage 테스트를 작성한다
    - renders login title

---
+++ src/pages/LoginPage.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  it('renders Login title', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>,
+    );
+
+    expect(container).toHaveTextContent('Log In');
+  });
+});
