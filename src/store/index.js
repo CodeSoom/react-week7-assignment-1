4cfd769828213cded5135564fd4473b8952fd2aa
morykwon <kwonmory@gmail.com>
2020-07-15 18:01:22 +0900

    add node package and apply
    - redux-devtools-extenstion

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import thunk from 'redux-thunk';
 
 import reducer from '../modules/reducer';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
