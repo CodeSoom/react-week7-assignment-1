f57da04407be7e0ad5c7bea1b6f1c4d7d1b0b768
Paul An <anpaul0615@gmail.com>
2020-07-15 10:36:11 +0900

    LoginFormContainer 리팩토링
    - 화면 처리를 LoginForm 에 위임

--- src/containers/LoginFormContainer.jsx
+++ src/containers/LoginFormContainer.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 
+import LoginForm from '../components/LoginForm';
+
 export default function LoginFormContainer() {
   return (
-    <div>
-      <div>
-        <label htmlFor="login-email">E-Mail</label>
-        <input type="email" name="login-email" id="login-email" />
-      </div>
-      <div>
-        <label htmlFor="login-password">Password</label>
-        <input type="password" name="login-password" id="login-password" />
-      </div>
-    </div>
+    <LoginForm />
   );
 }
