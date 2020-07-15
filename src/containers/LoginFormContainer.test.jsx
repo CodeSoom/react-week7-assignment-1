68ad310aa970e7bf08cee93cecece362d9712c69
morykwon <kwonmory@gmail.com>
2020-07-15 22:19:11 +0900

    [Red] LoginFormContainer 테스트 추가 작성
    - login button 클릭했을 때 테스트

--- src/containers/LoginFormContainer.test.jsx
+++ src/containers/LoginFormContainer.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
 import LoginFormContainer from './LoginFormContainer';
 
+jest.mock('react-redux');
+
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders input element', () => {
     const { getByLabelText } = render(
       <LoginFormContainer />,
@@ -12,5 +22,15 @@ describe('LoginFormContainer', () => {
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('renders login button', () => {
+    const { getByText } = render(
+      <LoginFormContainer />,
+    );
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
