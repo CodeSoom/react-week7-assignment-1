7b6b1c7b17b9518e10e6afed336c72ab197583c9
Minwook Park <mwpark0975@gmail.com>
2020-07-15 23:32:55 +0900

    refactor LoginFormContainer

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
+
+jest.mock('react-redux');
 
 describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
 
   beforeEach(() => {
+    dispatch.mockClear();
+
     useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: 'test@test',
+        password: '123',
+      },
+    }));
   });
 
   it('renders input control', () => {
