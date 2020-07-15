cce035eec2cc37f9d293f477a824cf3a74f8f27b
morykwon <kwonmory@gmail.com>
2020-07-15 23:29:42 +0900

    [Green] LoginForm를 수정한다
    - email, password 수정에 대한 테스트 적용

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
-export default function LoginForm({ onSubmit }) {
+export default function LoginForm({ onSubmit, onChange }) {
+  function handleChange(event) {
+    const { name, value } = event.target;
+    onChange({ name, value });
+  }
+
   return (
     <>
       <div>
         <label htmlFor="login-email">
           E-mail
         </label>
-        <input type="email" id="login-email" />
+        <input type="email" id="login-email" name="email" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="password" id="login-password" />
+        <input type="password" id="login-password" name="password" onChange={handleChange} />
       </div>
       <button type="button" onClick={onSubmit}>Log In</button>
     </>
