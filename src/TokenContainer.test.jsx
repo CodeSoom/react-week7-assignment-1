4d8c03f935b3f87dd3da50a8c6fee3d9a56fbea3
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-15 19:21:03 +0900

    test(TokenContainer): set accessToken
    - 로컬 스토리지에 토큰이 있는 경우 리덕스 상태를 업데이트 하는 테스트 추가

---
+++ src/TokenContainer.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import { setAccessToken } from './actions';
+
+import TokenContainer from './TokenContainer';
+
+import accessTokenFixture from '../fixtures/accessToken';
+
+jest.mock('react-redux');
+
+describe('TokenContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
+
+  context('with accessToken in localStorage', () => {
+    it('update redux state', () => {
+      localStorage.setItem('accessToken', accessTokenFixture);
+      useDispatch.mockImplementation(() => dispatch);
+
+      render((
+        <TokenContainer />
+      ));
+
+      expect(dispatch).toBeCalledWith(setAccessToken({ accessToken: accessTokenFixture }));
+    });
+  });
+});
