e96cbfa52152be0b80d64418defedc8830289bc0
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-15 15:46:47 +0900

    Implement: LoginForm renders input controls and listens change events

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 
-export default function LoginForm() {
-  function handleClick() {
+export default function LoginForm({ fields, onChange, onSubmit }) {
+  const { email, password } = fields;
+
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value })
     // TODO:
   }
-  
+
   return (
     <>
       <div>
         <label htmlFor="login-email">
           E-mail
         </label>
-        <input type="email" id="login-email" />
+        <input
+          type="email"
+          id="login-email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="email" id="login-password" />
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
       </div>
-      <button
-        type="button"
-        onClick={handleClick}
-      >
-        Log In
-      </button>
     </>
   );
 }
