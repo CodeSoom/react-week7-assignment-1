26c229ddbe52397d0156b0957d150c204097679a
KiChang Shin <kc7851@gmail.com>
2020-07-17 02:49:37 +0900

    LoginForm test 구현

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -4,40 +4,54 @@ export default function LoginForm({
   onChange,
   onClick,
   loginFields: { email, password },
+  accessToken,
 }) {
   return (
     <>
-      <p>Log in</p>
-      <div>
-        <label htmlFor="email">
-          E-mail
-        </label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          onChange={onChange}
-          value={email}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password
-        </label>
-        <input
-          type="text"
-          id="password"
-          name="password"
-          onChange={onChange}
-          value={password}
-        />
-      </div>
-      <button
-        type="button"
-        onClick={onClick}
-      >
-        Log In
-      </button>
+      <h2>Log in</h2>
+      {accessToken
+        ? (
+          <button
+            type="button"
+            onClick={onClick}
+          >
+            Log out
+          </button>
+        )
+        : (
+          <>
+            <div>
+              <label htmlFor="email">
+                E-mail
+              </label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                onChange={onChange}
+                value={email}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">
+                Password
+              </label>
+              <input
+                type="text"
+                id="password"
+                name="password"
+                onChange={onChange}
+                value={password}
+              />
+            </div>
+            <button
+              type="button"
+              onClick={onClick}
+            >
+              Log In
+            </button>
+          </>
+        )}
     </>
   );
 }
