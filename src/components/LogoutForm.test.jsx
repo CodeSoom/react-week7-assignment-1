f8ac199a2c4ee599754410d9076ed1a70c897241
morykwon <kwonmory@gmail.com>
2020-07-16 14:01:53 +0900

    [Red] LogoutForm 테스트를 추가한다
    - 단순하게 로그아웃 버튼만 테스트함

---
+++ src/components/LogoutForm.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import LogoutForm from './LogoutForm';
+
+describe('LogoutForm', () => {
+  const handleSubmit = jest.fn();
+
+  function renderLogoutForm() {
+    return render(
+      <LogoutForm
+        onSubmit={handleSubmit}
+      />,
+    );
+  }
+
+  beforeEach(() => {
+    handleSubmit.mockClear();
+  });
+
+  context('renders LogoutForm', () => {
+    const { container } = renderLogoutForm();
+
+    expect(container.querySelector('button')).toHaveTextContent('Log Out');
+  });
+
+  context('when click logout button', () => {
+    it('request logout', () => {
+      const { container } = renderLogoutForm();
+
+      fireEvent.click(container.querySelector('button'));
+
+      expect(handleSubmit).toBeCalled();
+    });
+  });
+});
