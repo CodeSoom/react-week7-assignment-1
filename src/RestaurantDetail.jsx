059b9c7e2fdd48f8992a6b86c09d6d51f0951b6a
daadaadaah <daadaadaah@gmail.com>
2020-07-17 08:26:46 +0900

    [Refactor] Refactor a RestaurantDetail for removeing ReviewForm

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -2,16 +2,8 @@ import React from 'react';
 
 import MenuItems from './MenuItems';
 
-export default function RestaurantDetail({
-  restaurant, reviewField, onChangeReviewField, onSubmitReviewField,
-}) {
-  const {
-    name, address, menuItems,
-  } = restaurant;
-
-  const {
-    score, description,
-  } = reviewField;
+export default function RestaurantDetail({ restaurant }) {
+  const { name, address, menuItems } = restaurant;
 
   return (
     <>
@@ -24,34 +16,6 @@ export default function RestaurantDetail({
         </p>
         <h3>메뉴</h3>
         <MenuItems menuItems={menuItems} />
-      </div>
-      <div>
-        <h3>리뷰 작성</h3>
-        <div>
-          <div>
-            <label htmlFor="review-score">평점</label>
-            <input
-              type="number"
-              id="review-score"
-              onChange={onChangeReviewField}
-              name="number"
-              value={score}
-            />
-          </div>
-          <div>
-            <label htmlFor="review-description">내용</label>
-            <input
-              type="text"
-              id="review-description"
-              onChange={onChangeReviewField}
-              name="text"
-              value={description}
-            />
-          </div>
-          <button type="button" onClick={onSubmitReviewField}>
-            리뷰 남기기
-          </button>
-        </div>
       </div>
     </>
   );
