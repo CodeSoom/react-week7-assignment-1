32349d734b4caa92cc407626a702464a4635db75
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-16 14:12:57 +0900

    feat(accessTokenRepository): delete, get
    - 현재 엑세스 토큰에 접근하거나 삭제하는 기능 구현

--- src/services/accessTokenRepository.js
+++ src/services/accessTokenRepository.js
@@ -1,7 +1,13 @@
+const ACCESS_TOKEN_KEY = 'accessToken';
+
 export function saveToken(token) {
-  localStorage.setItem('accessToken', token);
+  localStorage.setItem(ACCESS_TOKEN_KEY, token);
 }
 
-export function xx() {
+export function deleteToken() {
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+}
 
+export function getToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
 }
