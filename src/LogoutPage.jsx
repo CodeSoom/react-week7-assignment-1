1da5745a3f636fde1c0f1c6763388760d20830c9
daadaadaah <daadaadaah@gmail.com>
2020-07-17 02:50:38 +0900

    [Green] Implement a LogoutPage for when click logout button

--- src/LogoutPage.jsx
+++ src/LogoutPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
+import { logout } from './actions';
+
+import { get } from './utils';
+
 export default function LogoutPage() {
+  const dispatch = useDispatch();
+
+  const accessToken = useSelector(get('accessToken'));
+
+  function handleClickLogout() {
+    dispatch(logout());
+  }
+
   return (
     <>
       <h2>LogOut 페이지</h2>
-      <button type="button">LogOut</button>
+      {accessToken ? (
+        <button type="button" onClick={handleClickLogout}>
+          LogOut
+        </button>
+      ) : (
+        <p>LogOut 되었습니다.</p>
+      )}
     </>
   );
 }
