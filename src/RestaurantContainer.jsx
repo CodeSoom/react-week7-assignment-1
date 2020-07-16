274d6809df80bd7ef76a177d9beb8b6d9de0f5ff
KiChang Shin <kc7851@gmail.com>
2020-07-16 23:08:17 +0900

    RestaurantContainer 리팩토링

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -10,6 +10,21 @@ import {
 
 import { get } from './utils';
 
+function ReviewForm() {
+  return (
+    <>
+      <div>
+        <label htmlFor="review-score">평점</label>
+        <input type="text" id="review-score" />
+      </div>
+      <div>
+        <label htmlFor="review-description">리뷰 내용</label>
+        <input type="text" id="review-description" />
+      </div>
+    </>
+  );
+}
+
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
 
@@ -28,14 +43,7 @@ export default function RestaurantContainer({ restaurantId }) {
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
-      <div>
-        <label htmlFor="review-score">평점</label>
-        <input type="text" id="review-score" />
-      </div>
-      <div>
-        <label htmlFor="review-description">리뷰 내용</label>
-        <input type="text" id="review-description" />
-      </div>
+      <ReviewForm />
     </>
   );
 }
