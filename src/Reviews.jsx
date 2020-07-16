7fb4cdd1c346550d44cc17055e764176baeac908
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-16 15:32:20 +0900

    feat(Reviews): render without reviews
    - 리뷰가 없는 경우 '리뷰가 없어요ㅜ' 문구를 렌더링

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -4,15 +4,19 @@ export default function Reviews({ reviews }) {
   return (
     <>
       <h3>리뷰</h3>
-      <ul>
-        {reviews.map((review) => (
-          <li key={review.id}>
-            <p>{`리뷰어: ${review.name}`}</p>
-            <p>{`점수: ${review.score}`}</p>
-            <p>{`설명: ${review.description}`}</p>
-          </li>
-        ))}
-      </ul>
+      {(reviews.length === 0)
+        ? <p>리뷰가 없어요ㅜ</p>
+        : (
+          <ul>
+            {reviews.map((review) => (
+              <li key={review.id}>
+                <p>{`리뷰어: ${review.name}`}</p>
+                <p>{`점수: ${review.score}`}</p>
+                <p>{`설명: ${review.description}`}</p>
+              </li>
+            ))}
+          </ul>
+        )}
     </>
   );
 }
