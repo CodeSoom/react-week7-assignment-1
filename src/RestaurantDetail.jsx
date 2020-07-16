65e837e692fb82639430238c5171895a8276c2da
daadaadaah <daadaadaah@gmail.com>
2020-07-17 06:57:38 +0900

    [Green] Implement a RestaurantDetial for render Restaurant info for adding a review

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import MenuItems from './MenuItems';
 import ReviewItems from './ReviewItems';
 
-export default function RestaurantDetail({ restaurant }) {
+export default function RestaurantDetail({
+  restaurant, reviewField, onChangeReviewField, onSubmitReviewField,
+}) {
   const {
     name, address, menuItems, reviews,
   } = restaurant;
+
+  const {
+    score, description,
+  } = reviewField;
 
   return (
     <>
@@ -22,6 +28,31 @@ export default function RestaurantDetail({ restaurant }) {
       </div>
       <div>
         <h3>리뷰 작성</h3>
+        <div>
+          <div>
+            <label htmlFor="review-score">평점</label>
+            <input
+              type="number"
+              id="review-score"
+              onChange={onChangeReviewField}
+              name="number"
+              value={score}
+            />
+          </div>
+          <div>
+            <label htmlFor="review-description">내용</label>
+            <input
+              type="text"
+              id="review-description"
+              onChange={onChangeReviewField}
+              name="text"
+              value={description}
+            />
+          </div>
+          <button type="button" onClick={onSubmitReviewField}>
+            리뷰 남기기
+          </button>
+        </div>
       </div>
       <div>
         <h3>리뷰 목록</h3>
