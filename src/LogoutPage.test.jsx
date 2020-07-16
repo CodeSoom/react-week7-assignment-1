57acdeeb871c946eaf4484d2805a44ceae21b731
daadaadaah <daadaadaah@gmail.com>
2020-07-17 02:49:22 +0900

    Update a LogoutPage test for when click logout button

--- src/LogoutPage.test.jsx
+++ src/LogoutPage.test.jsx
@@ -1,24 +1,70 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import { render, fireEvent } from '@testing-library/react';
 
 import LogoutPage from './LogoutPage';
 
+jest.mock('react-redux');
+
 describe('LogoutPage', () => {
-  it('renders LogoutPage', () => {
-    const { container, queryByRole } = render(<LogoutPage />);
+  const dispatch = jest.fn();
 
-    expect(container).toHaveTextContent('LogOut 페이지');
-
-    expect(queryByRole('button', { name: 'LogOut' })).toBeInTheDocument();
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
   });
 
-  // TODO : 로그아웃 버튼 클릭시, 메인화면으로 넘어가기
-  it('click [LogOut] button ', () => {
-    const { container, getByRole } = render(<LogoutPage />);
+  describe('renders LogoutPage', () => {
+    context('with accessToken', () => {
+      beforeEach(() => {
+        useSelector.mockImplementation((selector) => selector({
+          accessToken: 'ACCESS_TOKEN',
+        }));
+      });
 
-    fireEvent.click(getByRole('button', { name: 'LogOut' }));
+      it('renders LogoutPage ', () => {
+        const { container, queryByRole } = render(<LogoutPage />);
 
-    expect(container).toHaveTextContent('LogOut 되었습니다.'); // 임시
+        expect(container).toHaveTextContent('LogOut 페이지');
+
+        expect(queryByRole('button', { name: 'LogOut' })).toBeInTheDocument();
+      });
+    });
+
+    // TODO : 로그아웃시, 메인화면으로 넘어가기
+    context('without accessToken', () => {
+      beforeEach(() => {
+        useSelector.mockImplementation((selector) => selector({
+          accessToken: '',
+        }));
+      });
+
+      it('show LogOut message', () => {
+        const { container } = render(<LogoutPage />);
+
+        expect(container).toHaveTextContent('LogOut 되었습니다.'); // 임시
+      });
+    });
+  });
+
+  context('when click [LogOut] button', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        accessToken: 'ACCESS_TOKEN',
+      }));
+    });
+
+    it('occur a logout action', () => {
+      const { getByRole } = render(<LogoutPage />);
+
+      fireEvent.click(getByRole('button', { name: 'LogOut' }));
+
+      expect(dispatch).toBeCalledWith({
+        type: 'logout',
+        payload: { accessToken: '' },
+      });
+    });
   });
 });
