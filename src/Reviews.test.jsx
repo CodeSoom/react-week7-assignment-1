7bbb8fe9a8287356ac6864751b6c9b347746bf1b
daadaadaah <daadaadaah@gmail.com>
2020-07-17 07:20:49 +0900

    [Red] Add a Reviews test

---
+++ src/Reviews.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Reviews from './Reviews';
+
+import restaurant from '../fixtures/restaurant';
+
+describe('Reviews', () => {
+  context('with review items', () => {
+    it('renders review items', () => {
+      const { reviews } = restaurant;
+
+      const { container } = render(<Reviews reviews={reviews} />);
+
+      expect(container).toHaveTextContent('테스터');
+    });
+  });
+
+  context('without review item', () => {
+    it('renders no items message', () => {
+      [[], null, undefined].forEach((item) => {
+        const { container } = render(<Reviews reviews={item} />);
+
+        expect(container).toHaveTextContent('리뷰가 없어요');
+      });
+    });
+  });
+});
