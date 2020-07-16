11a756b8a9321d1ad0624c410b60f3ddc6c8a3cc
Paul An <anpaul0615@gmail.com>
2020-07-16 18:39:11 +0900

    components/Button 에서 불필요한 props 기본값 제거

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Button({ name = '버튼', onClick }) {
+export default function Button({ name, onClick }) {
   return (
     <button type="button" onClick={onClick}>
       {name}
