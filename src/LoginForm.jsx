f25da1cecfc863e00e12b481b0cb64b4d7538a45
SeaFalcon <seafalcon323@gmail.com>
2020-07-16 15:54:00 +0900

    [Green] LoginForm 화면 렌더링 테스트 구현

---
+++ src/LoginForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LoginForm() {
+  return (
+    <div>
+      <label htmlFor="login-email">Email</label>
+      <input type="email" id="login-email" />
+    </div>
+  );
+}
