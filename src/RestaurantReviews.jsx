ce6a52c5bf032cbd681a88dd5df6abf73f3bae7f
KiChang Shin <kc7851@gmail.com>
2020-07-17 02:13:37 +0900

    RestaurantReviews test 구현

--- src/RestaurantReviews.jsx
+++ src/RestaurantReviews.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default function RestaurantReviews({ reviews }) {
+  if (!reviews || reviews.length === 0) {
+    return (
+      <p>리뷰가 없습니다.</p>
+    );
+  }
+
   return (
     <>
       <h2>리뷰</h2>
