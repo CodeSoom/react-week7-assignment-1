6c6eef4dfec622b1c31b099f5f3025fb5bc152a4
daadaadaah <daadaadaah@gmail.com>
2020-07-17 06:18:30 +0900

    [Green] Implement a RestaurantDetail for render Restaurant info

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import MenuItems from './MenuItems';
 
 export default function RestaurantDetail({ restaurant }) {
-  const { name, address, menuItems } = restaurant;
+  const {
+    name, address, menuItems, reviews,
+  } = restaurant;
 
   return (
     <>
@@ -22,6 +24,30 @@ export default function RestaurantDetail({ restaurant }) {
       </div>
       <div>
         <h3>리뷰 목록</h3>
+        <ul>
+          {reviews.length !== 0 ? (
+            reviews.map((review) => (
+              <li key={review.id}>
+                <h4>
+                  리뷰어 :
+                  {review.name}
+                </h4>
+                <h4>
+                  평점 :
+                  {review.score}
+                  {' '}
+                  점
+                </h4>
+                <h4>
+                  내용 :
+                  {review.description}
+                </h4>
+              </li>
+            ))
+          ) : (
+            <p>리뷰가 없습니다</p>
+          )}
+        </ul>
       </div>
     </>
   );
