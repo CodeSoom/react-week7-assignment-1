92df3c04530b0a3609fa9d644efcf6f177d32192
KiChang Shin <kc7851@gmail.com>
2020-07-17 01:17:39 +0900

    RestaurantContainer 리팩토링
    ReviewForm component를 사용하도록 변경하였습니다.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import ReviewForm from './ReviewForm';
 
 import {
   loadRestaurant,
@@ -11,48 +12,6 @@ import {
 } from './actions';
 
 import { get } from './utils';
-
-function ReviewForm({ reviewFields: { score, description }, onChange, onClick }) {
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    onChange({ name, value });
-  }
-  return (
-    <>
-      <div>
-        <label htmlFor="review-score">
-          평점
-        </label>
-        <input
-          type="number"
-          id="review-score"
-          name="score"
-          onChange={handleChange}
-          value={score}
-        />
-      </div>
-      <div>
-        <label htmlFor="review-description">
-          리뷰 내용
-        </label>
-        <input
-          type="text"
-          id="review-description"
-          name="description"
-          onChange={handleChange}
-          value={description}
-        />
-      </div>
-      <button
-        type="button"
-        onClick={onClick}
-      >
-        리뷰 남기기
-      </button>
-    </>
-  );
-}
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
