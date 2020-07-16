8aeb0ee19f0272326fea412706f24fdae5669936
daadaadaah <daadaadaah@gmail.com>
2020-07-17 08:31:00 +0900

    [Refactor] Refactor a RestaurantDetail teset for removeing ReviewForm

--- src/RestaurantDetail.test.jsx
+++ src/RestaurantDetail.test.jsx
@@ -7,22 +7,9 @@ import RestaurantDetail from './RestaurantDetail';
 import restaurant from '../fixtures/restaurant';
 
 describe('RestaurantDetail', () => {
-  const handleChangeReviewField = jest.fn();
-  const handleSubmitReviewField = jest.fn();
-
-  const reviewField = {
-    score: '',
-    description: '',
-  };
-
   it('renders Restaurant info', () => {
-    const { container, getByLabelText, getByRole } = render(
-      <RestaurantDetail
-        restaurant={restaurant}
-        reviewField={reviewField}
-        onChangeReviewField={handleChangeReviewField}
-        onSubmitReviewField={handleSubmitReviewField}
-      />,
+    const { container } = render(
+      <RestaurantDetail restaurant={restaurant} />,
     );
 
     expect(container).toHaveTextContent('양천주가');
@@ -31,10 +18,5 @@ describe('RestaurantDetail', () => {
 
     expect(container).toHaveTextContent('탕수육');
     expect(container).toHaveTextContent('팔보채');
-
-    expect(getByLabelText('평점')).toHaveAttribute('type', 'number');
-    expect(getByLabelText('내용')).toHaveAttribute('type', 'text');
-
-    expect(getByRole('button', { name: '리뷰 남기기' })).toBeInTheDocument();
   });
 });
