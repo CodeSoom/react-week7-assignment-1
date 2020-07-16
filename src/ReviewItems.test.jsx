1ec24b569250a95439fb51cd531cf2043c354970
daadaadaah <daadaadaah@gmail.com>
2020-07-17 06:29:25 +0900

    [Red+Green] Add a ReviewItems test and Implement it

---
+++ src/ReviewItems.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import ReviewItems from './ReviewItems';
+
+import restaurant from '../fixtures/restaurant';
+
+describe('ReviewItems', () => {
+  context('with review items', () => {
+    it('renders review items', () => {
+      const { reviews } = restaurant;
+
+      const { container } = render(<ReviewItems reviewItems={reviews} />);
+
+      expect(container).toHaveTextContent('테스터');
+    });
+  });
+
+  context('without review item', () => {
+    it('renders no items message', () => {
+      [[], null, undefined].forEach((reviews) => {
+        const { container } = render(<ReviewItems reviewItems={reviews} />);
+
+        expect(container).toHaveTextContent('리뷰가 없어요');
+      });
+    });
+  });
+});
