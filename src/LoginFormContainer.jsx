3d5542f6e4377206f8e6e3469f299c2645db5c55
SeaFalcon <seafalcon323@gmail.com>
2020-07-16 18:57:33 +0900

    [Green] LoginFormContainer input change event 테스트 구현

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+
+import { useDispatch } from 'react-redux';
+
 import LoginForm from './LoginForm';
 
+import { changeLoginField } from './actions';
+
 export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
   const email = '';
   const password = '';
 
+  function handleChange({ name, value }) {
+    dispatch(changeLoginField({ name, value }));
+  }
+
   return (
-    <LoginForm fields={{ email, password }} />
+    <LoginForm
+      fields={{ email, password }}
+      onChange={handleChange}
+    />
   );
 }
