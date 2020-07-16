244e26f8e2333f730f4907f59381aad30a30a257
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-16 14:09:54 +0900

    test(accessTokenRepository): delete, get
    - 현재 accessToken을 지우거나 가져오는 테스트 작성

--- src/services/accessTokenRepository.test.js
+++ src/services/accessTokenRepository.test.js
@@ -1,4 +1,8 @@
-import { saveToken } from './accessTokenRepository';
+import {
+  saveToken,
+  deleteToken,
+  getToken,
+} from './accessTokenRepository';
 
 describe('accessTokenRepository', () => {
   it('save the accessToken', () => {
@@ -7,6 +11,27 @@ describe('accessTokenRepository', () => {
     saveToken(accessToken);
 
     const savedToken = localStorage.getItem('accessToken');
+
+    expect(savedToken).toBe(accessToken);
+  });
+
+  it('delete current accessToken', () => {
+    const accessToken = 'TESTACCESSTOKEN';
+
+    saveToken(accessToken);
+    deleteToken();
+
+    const savedToken = localStorage.getItem('accessToken');
+
+    expect(savedToken).toBeNull();
+  });
+
+  it('get current accessToken', () => {
+    const accessToken = 'TESTACCESSTOKEN';
+
+    saveToken(accessToken);
+
+    const savedToken = getToken();
 
     expect(savedToken).toBe(accessToken);
   });
