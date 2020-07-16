b4d461dd406c344295a0e30cbd9bc23959712646
daadaadaah <daadaadaah@gmail.com>
2020-07-17 08:42:59 +0900

    [Red+Green] Update a RestaurantContainer test way for adding reviewField

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -19,21 +19,52 @@ describe('RestaurantContainer', () => {
 
     useSelector.mockImplementation((selector) => selector({
       restaurant: given.restaurant,
+      reviewField: given.reviewField,
     }));
   });
 
   it('dispatches action', () => {
+    beforeEach(() => {
+      given('restaurant', () => ({
+        id: 1,
+        name: '',
+        address: '',
+        reviews: [{
+          id: 1, restaurantId: 1, name: '테스터', score: 5, description: '훌륭하다 훌륭하다 지구인놈들',
+        }, {
+          id: 3, restaurantId: 1, name: '테스터', score: 3, description: 'Hi!',
+        }],
+      }));
+
+      given('reviewField', () => ({
+        score: '',
+        description: '',
+      }));
+    });
+
     renderRestaurantContainer();
 
     expect(dispatch).toBeCalled();
   });
 
   context('with restaurant', () => {
-    given('restaurant', () => ({
-      id: 1,
-      name: '마법사주방',
-      address: '서울시 강남구',
-    }));
+    beforeEach(() => {
+      given('restaurant', () => ({
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+        reviews: [{
+          id: 1, restaurantId: 1, name: '테스터', score: 5, description: '훌륭하다 훌륭하다 지구인놈들',
+        }, {
+          id: 3, restaurantId: 1, name: '테스터', score: 3, description: 'Hi!',
+        }],
+      }));
+
+      given('reviewField', () => ({
+        score: '',
+        description: '',
+      }));
+    });
 
     it('renders name and address', () => {
       const { container } = renderRestaurantContainer();
@@ -41,10 +72,14 @@ describe('RestaurantContainer', () => {
       expect(container).toHaveTextContent('마법사주방');
       expect(container).toHaveTextContent('서울시');
     });
-  })
+  });
 
   context('without restaurant', () => {
     given('restaurant', () => null);
+    given('reviewField', () => ({
+      score: '',
+      description: '',
+    }));
 
     it('renders loading', () => {
       const { container } = renderRestaurantContainer();
