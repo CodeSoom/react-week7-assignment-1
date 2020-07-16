2d0d0a9b286e9d1d259223ce52b56bfd865590fd
daadaadaah <daadaadaah@gmail.com>
2020-07-17 07:38:06 +0900

    [Refactor] Refactor a RestaurantContainer for adding a ReviewForm

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,14 +3,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import ReviewForm from './ReviewForm';
+import Reviews from './Reviews';
 
 import {
   loadRestaurant,
 } from './actions';
 
 import { get } from './utils';
-
-import Reviews from './Reviews';
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
@@ -20,6 +20,7 @@ export default function RestaurantContainer({ restaurantId }) {
   }, []);
 
   const restaurant = useSelector(get('restaurant'));
+  const reviewField = useSelector(get('reviewField'));
 
   const { reviews } = restaurant;
 
@@ -29,9 +30,22 @@ export default function RestaurantContainer({ restaurantId }) {
     );
   }
 
+  function handleChangeReviewField() {
+
+  }
+
+  function handleSubmitReviewField() {
+
+  }
+
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
+      <ReviewForm
+        reviewField={reviewField}
+        onChangeReviewField={handleChangeReviewField}
+        onSubmitReviewField={handleSubmitReviewField}
+      />
       <Reviews reviews={reviews} />
     </>
   );
