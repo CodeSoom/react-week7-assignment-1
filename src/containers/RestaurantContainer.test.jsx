e9dc3be7e6e0e19b68352f3ca371f660c5f29efd
morykwon <kwonmory@gmail.com>
2020-07-16 19:48:09 +0900

    [Red] RestaurantContainer 테스트 내용 추가하기
    - 평점과 리뷰 내용이 상태에 따라 바뀌는지 테스트하기

--- src/containers/RestaurantContainer.test.jsx
+++ src/containers/RestaurantContainer.test.jsx
@@ -28,11 +28,17 @@ describe('RestaurantContainer', () => {
       }));
     });
     it('renders review InputForm with value', () => {
-      const { getByLabelText, getByDisplayValue } = renderRestaurantContainer();
+      given('restaurant', () => ({
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+      }));
 
-      expect(getByLabelText('평점').value).toBe(5);
+      const { getByLabelText, getByText } = renderRestaurantContainer();
+
+      expect(getByLabelText('평점').value).toBe('5');
       expect(getByLabelText('리뷰 내용').value).toBe('바보들앙 이거 리뷰 아니지롱~');
-      expect(getByDisplayValue('리뷰 남기기')).not.toBeNull();
+      expect(getByText('리뷰 남기기')).not.toBeNull();
     });
   });
 
@@ -44,6 +50,10 @@ describe('RestaurantContainer', () => {
       useSelector.mockImplementation((selector) => selector({
         restaurant: given.restaurant,
         accessToken: '',
+        reviewFields: {
+          score: 5,
+          reviewContent: '바보들앙 이거 리뷰 아니지롱~',
+        },
       }));
     });
 
