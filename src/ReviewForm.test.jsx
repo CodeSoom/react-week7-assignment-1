41f3822ed0c4bc3cac8b61f34d08a20d44f97e44
daadaadaah <daadaadaah@gmail.com>
2020-07-17 07:35:53 +0900

    [Red] Add a ReviewForm test for when render ReviewForm

---
+++ src/ReviewForm.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import ReviewForm from './ReviewForm';
+
+describe('ReviewForm', () => {
+  const handleChangeReviewField = jest.fn();
+  const handleSubmitReviewField = jest.fn();
+
+  const reviewField = {
+    score: '',
+    description: '',
+  };
+
+  it('renders ReviewForm', () => {
+    const { getByLabelText, getByRole } = render(
+      <ReviewForm
+        reviewField={reviewField}
+        onChangeReviewField={handleChangeReviewField}
+        onSubmitReviewField={handleSubmitReviewField}
+      />,
+    );
+
+    expect(getByLabelText('평점')).toHaveAttribute('type', 'number');
+    expect(getByLabelText('내용')).toHaveAttribute('type', 'text');
+
+    expect(getByRole('button', { name: '리뷰 남기기' })).toBeInTheDocument();
+  });
+});
