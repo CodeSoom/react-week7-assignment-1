f7f9818d8ed03e81dcda385ae3733ebe03ee5837
daadaadaah <daadaadaah@gmail.com>
2020-07-17 07:36:10 +0900

    [Green] Implement a ReviewForm for when render ReviewForm

---
+++ src/ReviewForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function ReviewForm({
+  reviewField, onChangeReviewField, onSubmitReviewField,
+}) {
+  const {
+    score, description,
+  } = reviewField;
+
+  return (
+    <>
+      <h3>리뷰 작성</h3>
+      <div>
+        <label htmlFor="review-score">평점</label>
+        <input
+          type="number"
+          id="review-score"
+          onChange={onChangeReviewField}
+          name="number"
+          value={score}
+        />
+      </div>
+      <div>
+        <label htmlFor="review-description">내용</label>
+        <input
+          type="text"
+          id="review-description"
+          onChange={onChangeReviewField}
+          name="text"
+          value={description}
+        />
+      </div>
+      <button type="button" onClick={onSubmitReviewField}>
+        리뷰 남기기
+      </button>
+    </>
+  );
+}
