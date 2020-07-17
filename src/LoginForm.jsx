7f1e404c457b97a6a98f0293facf4527eca9d48e
KiChang Shin <kc7851@gmail.com>
2020-07-17 23:01:36 +0900

    LoginForm 리팩토링
    accessToken 사용하지 않으므로 제거하였습니다.

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -4,44 +4,39 @@ export default function LoginForm({
   onChange,
   onClick,
   loginFields: { email, password },
-  accessToken,
 }) {
   return (
     <>
-      {!accessToken && (
-        <>
-          <div>
-            <label htmlFor="email">
-              E-mail
-            </label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              onChange={onChange}
-              value={email}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">
-              Password
-            </label>
-            <input
-              type="text"
-              id="password"
-              name="password"
-              onChange={onChange}
-              value={password}
-            />
-          </div>
-          <button
-            type="button"
-            onClick={onClick}
-          >
-            Log In
-          </button>
-        </>
-      )}
+      <div>
+        <label htmlFor="email">
+          E-mail
+        </label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={onChange}
+          value={email}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">
+          Password
+        </label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          onChange={onChange}
+          value={password}
+        />
+      </div>
+      <button
+        type="button"
+        onClick={onClick}
+      >
+        Log In
+      </button>
     </>
   );
 }
--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -57,7 +57,6 @@ export default function LoginFormContainer() {
             onChange={handleChange}
             onClick={handleClick}
             loginFields={loginFields}
-            accessToken={accessToken}
           />
         )}
     </>
