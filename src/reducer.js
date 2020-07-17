0be159b9870013bb4253f71a6f2db1bbcfc480fb
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-18 02:38:08 +0900

    Add changeReviewField reducer

--- src/reducer.js
+++ src/reducer.js
@@ -12,6 +12,10 @@ const initialState = {
     email: '',
     password: '',
   },
+  reviewFields: {
+    score: '',
+    description: '',
+  },
 };
 
 const reducers = {
@@ -79,6 +83,17 @@ const reducers = {
       ...state,
       loginFields: {
         ...loginFields,
+        [name]: value,
+      },
+    };
+  },
+
+  changeReviewField(state, { payload: { name, value } }) {
+    const { reviewFields } = state;
+    return {
+      ...state,
+      reviewFields: {
+        ...reviewFields,
         [name]: value,
       },
     };
