2fde5630bf343faac1c09b73993ff5c52a8bd95d
Kwakcena <rhkrgudwh@gmail.com>
2020-07-17 20:55:03 +0900

    RED] handleChange called when input text changed
    handleChange 함수는 input box의 text가 바뀌면 호출된다.

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
@@ -12,5 +12,19 @@ describe('LoginForm', () => {
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  context('when change input text', () => {
+    it('should call handleChange', () => {
+      const handleChange = jest.fn();
+      const { getByLabelText } = render((
+        <LoginForm onChange={handleChange} />
+      ));
+
+      fireEvent.change(getByLabelText('E-mail'), {
+        target: { value: 'test@exam' },
+      });
+      expect(handleChange).toBeCalled();
+    });
   });
 });
