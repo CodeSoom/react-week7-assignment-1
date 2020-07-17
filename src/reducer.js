55956de2e6000c7d5fe5b5d26a9a5eb4b8dfe8c2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-17 13:15:11 +0900

    feat(reducer): changeReviewFields
    - 리뷰 필드가 변경됐을 때 리덕스 상태 변경하는 기능 구현

--- src/reducer.js
+++ src/reducer.js
@@ -79,6 +79,16 @@ const reducers = {
       accessToken,
     };
   },
+
+  changeReviewFields(state, { payload: { name, value } }) {
+    return {
+      ...state,
+      reviewFields: {
+        ...state.reviewFields,
+        [name]: value,
+      },
+    };
+  },
 };
 
 function defaultReducer(state) {
