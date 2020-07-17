9f89ae2ebc98dad02cd54385ae982889c7695ac3
daadaadaah <daadaadaah@gmail.com>
2020-07-18 06:10:47 +0900

    [Refactor] Refactor a RestaurantPage.test.jsx

--- src/Restaurant/RestaurantPage.test.jsx
+++ src/Restaurant/RestaurantPage.test.jsx
@@ -8,47 +8,26 @@ import { render } from '@testing-library/react';
 
 import RestaurantPage from './RestaurantPage';
 
-import REGIONS from '../../fixtures/regions';
-import CATEGORIES from '../../fixtures/categories';
-import RESTAURANTS from '../../fixtures/restaurants';
-import RESTAURANT from '../../fixtures/restaurant';
+import restaurant from '../../fixtures/restaurant';
 
 describe('RestaurantPage', () => {
+  const reviewField = {
+    score: '',
+    description: '',
+  };
+
   beforeEach(() => {
     const dispatch = jest.fn();
 
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((state) => state({
-      restaurant: {
-        id: 1,
-        name: '양천주가',
-        address: '서울 강남구',
-        reviews: [],
-      },
-      reviewField: {
-        score: '',
-        description: '',
-      },
+      restaurant,
+      reviewField,
     }));
   });
 
   context('with params props', () => {
-    beforeEach(() => {
-      useSelector.mockImplementation((state) => state({
-        restaurant: {
-          id: 1,
-          name: '양천주가',
-          address: '서울 강남구',
-          reviews: [],
-        },
-        reviewField: {
-          score: '',
-          description: '',
-        },
-      }));
-    });
-
     it('renders name', () => {
       const params = { id: '1' };
 
@@ -61,21 +40,6 @@ describe('RestaurantPage', () => {
   });
 
   context('without params props', () => {
-    beforeEach(() => {
-      useSelector.mockImplementation((state) => state({
-        restaurant: {
-          id: 1,
-          name: '양천주가',
-          address: '서울 강남구',
-          reviews: [],
-        },
-        reviewField: {
-          score: '',
-          description: '',
-        },
-      }));
-    });
-
     it('renders name', () => {
       const { container } = render(
         <MemoryRouter initialEntries={['/restaurants/1']}>
