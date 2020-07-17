a87faa4c7db133e3f480ef5adde744b42a87cf0d
sogoagain <imyong0@gmail.com>
2020-07-18 01:22:58 +0900

    Implement - Reviews 구현

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,3 +1,21 @@
-export default function Reviews() {
-  return null;
+import React from 'react';
+
+export default function Reviews({ reviews }) {
+  if (!(reviews || []).length) {
+    return (
+      <p>리뷰가 없어요!</p>
+    );
+  }
+
+  return (
+    <ul>
+      {reviews.map((review) => (
+        <li key={review.id}>
+          {review.name}
+          {`${review.score}점`}
+          {review.description}
+        </li>
+      ))}
+    </ul>
+  );
 }
