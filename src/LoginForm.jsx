114872f48aa22886f089b4a6f102e33b7f03d858
Kwakcena <rhkrgudwh@gmail.com>
2020-07-17 22:30:32 +0900

    GREEN] render "Log In" button
    테스트 통과

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -30,6 +30,11 @@ export default function LoginForm({ fields, onChange }) {
           onChange={handleChange}
         />
       </div>
+      <button
+        type="button"
+      >
+        Log In
+      </button>
     </>
   );
 }
