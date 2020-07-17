6123f79ac14565d14d74374dd7918aa57dc753e2
Kwakcena <rhkrgudwh@gmail.com>
2020-07-17 20:24:53 +0900

    Refactor] Using LoginForm
    컨테이너 컴포넌트 안에서 LoginForm 컴포넌트를 사용한다.

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
+import LoginForm from './LoginForm';
 
 export default function LoginFormContainer() {
   return (
     <>
-      <div>
-        <label htmlFor="login-email">E-mail</label>
-        <input type="email" id="login-email" />
-      </div>
-      <div>
-        <label htmlFor="login-password">Password</label>
-        <input type="password" id="login-password" />
-      </div>
+      <LoginForm />
     </>
   );
 }
