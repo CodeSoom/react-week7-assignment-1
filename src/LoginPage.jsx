66f341af50e87e5db1cc62776a57c218e4d14a1e
Kwakcena <rhkrgudwh@gmail.com>
2020-07-17 20:13:41 +0900

    GREEN] input controls render test pass
    테스트 통과

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,6 +4,14 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
+      <div>
+        <label htmlFor="login-email">E-mail</label>
+        <input type="email" id="login-email" />
+      </div>
+      <div>
+        <label htmlFor="login-password">Password</label>
+        <input type="password" id="login-password" />
+      </div>
     </div>
   );
 }
