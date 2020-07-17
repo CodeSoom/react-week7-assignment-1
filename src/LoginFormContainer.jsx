a9cdb4c375106f9fd4cec8436c31579f7e027c1a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-17 21:53:29 +0900

    GREEN] dispatch call test 구현

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { get } from './utils';
+
+import {
+  changeLoginFields,
+} from './actions';
 
 import LoginForm from './LoginForm';
 
 export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
   const { email, password } = useSelector(get('loginFields'));
 
+  function handleChange({ name, value }) {
+    dispatch(changeLoginFields({ name, value }));
+  }
+
   return (
-    <LoginForm fields={{ email, password }} />
+    <LoginForm
+      fields={{ email, password }}
+      onChange={handleChange}
+    />
   );
 }
