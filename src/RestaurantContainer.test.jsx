2189938b8de60f370e5fc79649b11a35a90f81b1
daadaadaah <daadaadaah@gmail.com>
2020-07-17 09:06:54 +0900

    [Red] Add a RestaurantContainer test for when change input

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -87,4 +87,41 @@ describe('RestaurantContainer', () => {
       expect(container).toHaveTextContent('Loading');
     });
   });
+
+  // TODO : control의 의미 파악 후 리팩토링 할 예정
+  context('when change inputs', () => {
+    it('change score input', () => {
+      const { getByLabelText } = renderRestaurantContainer();
+
+      fireEvent.change(getByLabelText('평점'), {
+        target: { value: 'newCore' },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeReviewField',
+        payload: { name: 'score', value: 'newCore' },
+      });
+    });
+
+    it('change description input', () => {
+      const { getByLabelText } = renderRestaurantContainer();
+
+      fireEvent.change(getByLabelText('내용'), {
+        target: { value: 'newDescription' },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeReviewField',
+        payload: { name: 'description', value: 'newDescription' },
+      });
+    });
+  });
+
+  // it('click button ', () => {
+  //   const { getByRole } = renderRestaurantContainer();;
+
+  //   fireEvent.click(getByRole('button', { name: '리뷰 남기기' }));
+
+  //   expect(dispatch).toBeCalledTimes(1);
+  // });
 });
