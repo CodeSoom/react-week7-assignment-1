617ee7ea160d93f71fb04c9a874337296e744fe6
sogoagain <imyong0@gmail.com>
2020-07-18 02:15:14 +0900

    Test - ReviewForm change

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
@@ -41,5 +41,34 @@ describe('ReviewForm', () => {
       const control = getByLabelText(label);
       expect(control.value).toBe(value);
     });
+  });
+
+  it('changes input controls', () => {
+    const controls = [
+      { label: '평점', name: 'score', values: { before: '0', after: '5' } },
+      { label: '리뷰 내용', name: 'description', values: { before: '별로예요.', after: '맛있어요.' } },
+    ];
+
+    const { getByLabelText } = renderReviewForm(
+      {
+        score: controls[0].values.before,
+        description: controls[1].values.before,
+      },
+    );
+
+    controls.forEach(({ label, name, values: { after } }) => {
+      const control = getByLabelText(label);
+      fireEvent.change(control, { target: { value: after } });
+
+      expect(handleChange).toBeCalledWith({ name, value: after });
+    });
+  });
+
+  it('renders register button', () => {
+    const { getByText } = renderReviewForm({});
+
+    fireEvent.submit(getByText('리뷰 남기기'));
+
+    expect(handleSubmit).toBeCalled();
   });
 });
