8b0239a49fcaac98d44a6124477f31d8e08fd5fe
KiChang Shin <kc7851@gmail.com>
2020-07-17 23:00:26 +0900

    LoginForm test 리팩토링

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -5,7 +5,6 @@ import { render, fireEvent } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 import loginFields from '../fixtures/loginFields';
-import ACCESS_TOKEN from '../fixtures/accessToken';
 
 jest.mock('react-redux');
 
@@ -13,69 +12,48 @@ describe('LoginForm', () => {
   const handleChange = jest.fn();
   const handleClick = jest.fn();
 
-  function renderLoginForm(accessToken) {
+  function renderLoginForm() {
     return render(
       <LoginForm
         onChange={handleChange}
         onClick={handleClick}
         loginFields={loginFields}
-        accessToken={accessToken}
       />,
     );
   }
 
-  context('without accessToken', () => {
-    it('renders input controls', () => {
+  it('renders input controls', () => {
+    const { getByLabelText } = renderLoginForm(null);
+
+    expect(getByLabelText('E-mail')).toHaveValue(loginFields.email);
+    expect(getByLabelText('Password')).toHaveValue(loginFields.password);
+  });
+
+  context('when changes value', () => {
+    it('call onChange', () => {
       const { getByLabelText } = renderLoginForm(null);
 
-      expect(getByLabelText('E-mail')).toHaveValue(loginFields.email);
-      expect(getByLabelText('Password')).toHaveValue(loginFields.password);
-    });
-
-    context('when changes value', () => {
-      it('call onChange', () => {
-        const { getByLabelText } = renderLoginForm(null);
-
-        fireEvent.change(getByLabelText('E-mail'), {
-          target: { value: 'tester@exmaple.com' },
-        });
-
-        expect(handleChange).toBeCalled();
+      fireEvent.change(getByLabelText('E-mail'), {
+        target: { value: 'tester@exmaple.com' },
       });
-    });
 
-    it('renders submit button', () => {
-      const { getByText } = renderLoginForm(null);
-
-      expect(getByText('Log In')).not.toBeNull();
-    });
-
-    context('when click button', () => {
-      it('call onClick', () => {
-        const { getByText } = renderLoginForm(null);
-
-        fireEvent.click(getByText('Log In'));
-
-        expect(handleClick).toBeCalled();
-      });
+      expect(handleChange).toBeCalled();
     });
   });
 
-  // context('with accessToken', () => {
-  //   it('renders logout', () => {
-  //     const { getByText } = renderLoginForm(ACCESS_TOKEN);
+  it('renders submit button', () => {
+    const { getByText } = renderLoginForm(null);
 
-  //     expect(getByText('Log out')).not.toBeNull();
-  //   });
+    expect(getByText('Log In')).not.toBeNull();
+  });
 
-  //   context('when “Log out” click event', () => {
-  //     it('listens click event', () => {
-  //       const { getByText } = renderLoginForm(ACCESS_TOKEN);
+  context('when click button', () => {
+    it('call onClick', () => {
+      const { getByText } = renderLoginForm(null);
 
-  //       fireEvent.click(getByText('Log out'));
+      fireEvent.click(getByText('Log In'));
 
-  //       expect(handleClick).toBeCalledWith('Log-out');
-  //     });
-  //   });
-  // });
+      expect(handleClick).toBeCalled();
+    });
+  });
 });
