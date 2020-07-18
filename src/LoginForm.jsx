efc9a674ce04768d0b41835e295505f18f99f451
jiunkoo <jiunkoo0106@gmail.com>
2020-07-18 18:06:48 +0900

    [구현] LoginForm, LoginFormContainer에 onChange 테스트 구현

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 
-export default function LoginForm({ onSubmit }) {
+export default function LoginForm({ onChange, onSubmit }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
+
   return (
     <>
       <div>
         <label htmlFor="login-email">
           Email
         </label>
-        <input type="email" id="login-email" />
+        <input
+          type="email"
+          id="login-email"
+          name="email"
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="password" id="login-password" />
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+          onChange={handleChange}
+        />
       </div>
       <button
         type="button"
--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import { requestLogin } from './actions';
+import {
+  requestLogin,
+  changeLoginField,
+} from './actions';
 
 import LoginForm from './LoginForm';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
+  function handleChange({ name, value }) {
+    dispatch(changeLoginField({ name, value }));
+  }
+
   function handleSubmit() {
     dispatch(requestLogin());
   }
 
   return (
-    <LoginForm onSubmit={handleSubmit} />
+    <LoginForm
+      onChange={handleChange}
+      onSubmit={handleSubmit}
+    />
   );
 }
