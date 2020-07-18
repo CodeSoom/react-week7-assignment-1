631c4cd3cc890b044a5cd082f4bfa072fb9b03c6
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 19:39:41 +0900

    Refactor] prop 추가

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -6,8 +6,14 @@ import TextField from './TextField';
 
 describe('TextField', () => {
   it('renders review input controls', () => {
+    const onChange = jest.fn();
     const { queryByLabelText } = render((
-      <TextField />
+      <TextField
+        label="평점"
+        type="number"
+        name="score"
+        onChange={onChange}
+      />
     ));
 
     expect(queryByLabelText('평점')).not.toBeNull();
