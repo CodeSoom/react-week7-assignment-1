2cc47e30b5daf77289801ba09190d68bcfdbca60
sogoagain <imyong0@gmail.com>
2020-07-18 09:41:17 +0900

    Refactor - LoginForm에서 InputControl을 사용하도록 수정

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,39 +1,26 @@
 import React from 'react';
 
+import InputControl from './InputControl';
+
 export default function LoginForm({
   email, password, onSubmit, onChange,
 }) {
-  const handleChange = (e) => {
-    const { target: { name, value } } = e;
-    onChange({ name, value });
-  };
-
   return (
     <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="email">
-          E-mail
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password
-        </label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </div>
+      <InputControl
+        label="E-mail"
+        name="email"
+        type="email"
+        value={email}
+        onChange={onChange}
+      />
+      <InputControl
+        label="Password"
+        name="password"
+        type="password"
+        value={password}
+        onChange={onChange}
+      />
       <button type="submit">Log In</button>
     </form>
   );
