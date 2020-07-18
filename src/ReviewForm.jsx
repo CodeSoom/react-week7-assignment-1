7e3b13c413cd17d5fb9ef498098ef00a6bc9498e
SeaFalcon <seafalcon323@gmail.com>
2020-07-18 12:39:44 +0900

    [Red+Green] ReviewForm control 렌더 테스트 구현

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 export default function ReviewForm() {
   return (
     <>
-      <label htmlFor="input-score">평점</label>
-      <input type="text" id="input-score" />
+      <div>
+        <label htmlFor="input-score">평점</label>
+        <input type="number" id="input-score" />
+      </div>
+      <div>
+        <label htmlFor="input-description">리뷰 내용</label>
+        <input type="text" id="input-description" />
+      </div>
+      <button type="button">리뷰 남기기</button>
     </>
   );
 }
--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -5,9 +5,12 @@ import { render } from '@testing-library/react';
 import ReviewForm from './ReviewForm';
 
 describe('ReviewForm', () => {
-  it('renders review input form', () => {
-    const { container } = render(<ReviewForm />);
+  it('renders review input form and submit button', () => {
+    const { getByLabelText, getByText } = render(<ReviewForm />);
 
-    expect(container).toHaveTextContent('평점');
+    expect(getByLabelText('평점')).toBeInTheDocument();
+    expect(getByLabelText('리뷰 내용')).toBeInTheDocument();
+
+    expect(getByText('리뷰 남기기')).toBeInTheDocument();
   });
 });
