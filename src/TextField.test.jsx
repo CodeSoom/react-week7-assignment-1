df03fe7e955bbe2acdbd93b62bcca23a26d8e59a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 19:44:05 +0900

    Refactor] TextField type 있을때와 없을때 테스트
    type의 기본값을 정해놓고 type이 있을때와 없을때로 테스트를 나눈다.

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -5,17 +5,35 @@ import { render } from '@testing-library/react';
 import TextField from './TextField';
 
 describe('TextField', () => {
-  it('renders review input controls', () => {
-    const onChange = jest.fn();
-    const { queryByLabelText } = render((
-      <TextField
-        label="평점"
-        type="number"
-        name="score"
-        onChange={onChange}
-      />
-    ));
+  context('with type', () => {
+    it('renders score input control', () => {
+      const onChange = jest.fn();
+      const { queryByLabelText } = render((
+        <TextField
+          label="평점"
+          type="number"
+          name="score"
+          onChange={onChange}
+        />
+      ));
 
-    expect(queryByLabelText('평점')).not.toBeNull();
+      expect(queryByLabelText('평점')).not.toBeNull();
+    });
+  });
+
+  context('without type', () => {
+    it('renders description input controls', () => {
+      const onChange = jest.fn();
+
+      const { queryByLabelText } = render((
+        <TextField
+          label="리뷰 내용"
+          name="description"
+          onChange={onChange}
+        />
+      ));
+
+      expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+    });
   });
 });
