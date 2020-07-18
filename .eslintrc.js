ec5d682d1ac780419847dbe572ea93eab702ca6d
jiunkoo <jiunkoo0106@gmail.com>
2020-07-18 16:11:45 +0900

    eslint 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,5 +51,6 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
     'react/prop-types': 'off',
     'linebreak-style': 'off',
+    "jsx-a11y/anchor-is-valid": 0,
   },
 };
