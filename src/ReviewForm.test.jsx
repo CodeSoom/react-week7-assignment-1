0dcbc907cc32f8fd1e00bb6cb37d95145c0cbf9b
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 17:16:19 +0900

    Refactor] test refactoring
    - 중복 제거
    - beforeEach 사용
    - mockClear 사용

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -5,21 +5,27 @@ import { render, fireEvent } from '@testing-library/react';
 import ReviewForm from './ReviewForm';
 
 describe('ReviewForm', () => {
-  it('renders review input controls', () => {
-    const { queryByLabelText } = render((
-      <ReviewForm />
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+  });
+
+  function renderReviewForm() {
+    return render((
+      <ReviewForm onChange={handleChange} />
     ));
+  }
+
+  it('renders review input controls', () => {
+    const { queryByLabelText } = renderReviewForm();
 
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 남기기')).not.toBeNull();
   });
 
   it('listens change event', () => {
-    const handleChange = jest.fn();
-
-    const { getByLabelText } = render((
-      <ReviewForm onChange={handleChange} />
-    ));
+    const { getByLabelText } = renderReviewForm();
 
     fireEvent.change(getByLabelText('평점'), {
       target: { value: '5' },
