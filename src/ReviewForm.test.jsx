5b3d3d472c64ba57a95ec94e1f0cb7d79be15ae0
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 17:11:48 +0900

    RED] ReviewForm listens change event test
    input controls 에 텍스트를 입력하면 onChange 이벤트가 호출됨을 테스트
    한다.

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
@@ -12,5 +12,22 @@ describe('ReviewForm', () => {
 
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 남기기')).not.toBeNull();
+  });
+
+  it('listens change event', () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render((
+      <ReviewForm onChange={handleChange} />
+    ));
+
+    fireEvent.change(getByLabelText('평점'), {
+      target: { value: '5' },
+    });
+
+    expect(handleChange).toBeCalledWith({
+      name: 'score',
+      value: '5',
+    });
   });
 });
