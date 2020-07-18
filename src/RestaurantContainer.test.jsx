e4f85f3b1273b2597c6503d6fcb8ccbe883ce328
SeaFalcon <seafalcon323@gmail.com>
2020-07-18 12:12:33 +0900

    [Red] RestaurantContainer 로그인시 input 렌더링 테스트 추가

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -41,7 +41,15 @@ describe('RestaurantContainer', () => {
       expect(container).toHaveTextContent('마법사주방');
       expect(container).toHaveTextContent('서울시');
     });
-  })
+
+    context('with logged in', () => {
+      it('renders review input form', () => {
+        const { container } = renderRestaurantContainer();
+
+        expect(container).toHaveTextContent('평점');
+      });
+    });
+  });
 
   context('without restaurant', () => {
     given('restaurant', () => null);
