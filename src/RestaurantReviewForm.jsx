a0073cde69f4a4228ed71f732d5d225f962f80d6
sogoagain <imyong0@gmail.com>
2020-07-18 09:43:25 +0900

    Refactor - RestaurantReviewForm에서 InputControl을 사용하도록 수정

--- src/RestaurantReviewForm.jsx
+++ src/RestaurantReviewForm.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
 
+import InputControl from './InputControl';
+
 export default function RestaurantReviewForm({
   score, description, onSubmit, onChange,
 }) {
-  const handleChange = (e) => {
-    const { target: { name, value } } = e;
-    onChange({ name, value });
-  };
-
   return (
     <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="score">
-          평점
-        </label>
-        <input
-          id="score"
-          name="score"
-          type="number"
-          min="1"
-          max="5"
-          value={score}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="description">
-          리뷰 내용
-        </label>
-        <input
-          id="description"
-          name="description"
-          type="description"
-          value={description}
-          onChange={handleChange}
-        />
-      </div>
+      <InputControl
+        label="평점"
+        name="score"
+        type="number"
+        value={score}
+        onChange={onChange}
+      />
+      <InputControl
+        label="리뷰 내용"
+        name="description"
+        type="text"
+        value={description}
+        onChange={onChange}
+      />
       <button type="submit">리뷰 남기기</button>
     </form>
   );
