7126324e00446c749c9db92fdb218752281111d5
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 19:41:14 +0900

    Refactor] TextField 사용

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,35 +1,21 @@
 import React from 'react';
+import TextField from './TextField';
 
 export default function ReviewForm({ onChange, onSubmit }) {
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
   return (
     <>
-      <div>
-        <label htmlFor="review-score">
-          평점
-        </label>
-        <input
-          type="number"
-          id="review-score"
-          name="score"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="review-description">
-          리뷰 내용
-        </label>
-        <input
-          type="text"
-          id="review-description"
-          name="description"
-          onChange={handleChange}
-        />
-      </div>
+      <TextField
+        label="평점"
+        type="number"
+        name="score"
+        onChange={onChange}
+      />
+      <TextField
+        label="리뷰 내용"
+        type="text"
+        name="description"
+        onChange={onChange}
+      />
       <button
         type="button"
         onClick={onSubmit}
