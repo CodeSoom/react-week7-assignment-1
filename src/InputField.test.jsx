12b1cf464ca00bf19a3ae1486f4adf8915d834cc
SeaFalcon <seafalcon323@gmail.com>
2020-07-18 12:58:22 +0900

    [Red+Green] InputField change event  테스트 추가, 구현

--- src/InputField.test.jsx
+++ src/InputField.test.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import InputField from './InputField';
 
 describe('InputField', () => {
-  it('renders input fields', () => {
-    const handleChange = jest.fn();
+  const handleChange = jest.fn();
 
-    const label = '평점';
-    const type = 'number';
-    const name = 'score';
-
-    const { getByLabelText } = render((
+  function renderInputField({ label, type, name }) {
+    return render((
       <InputField
         label={label}
         type={type}
@@ -20,6 +16,14 @@ describe('InputField', () => {
         onChange={handleChange}
       />
     ));
+  }
+
+  it('renders input fields', () => {
+    const label = '평점';
+    const type = 'number';
+    const name = 'score';
+
+    const { getByLabelText } = renderInputField({ label, type, name });
 
     const attributes = [
       { attribute: 'type', value: type },
@@ -30,5 +34,19 @@ describe('InputField', () => {
     attributes.forEach(({ attribute, value }) => {
       expect(getByLabelText(label)).toHaveAttribute(attribute, value);
     });
+  });
+
+  it('listens change events', () => {
+    const label = '평점';
+    const type = 'number';
+    const name = 'score';
+
+    const { getByLabelText } = renderInputField({ label, type, name });
+
+    fireEvent.change(getByLabelText('평점'), {
+      target: { value: '5' },
+    });
+
+    expect(handleChange).toBeCalledWith({ name, value: '5' });
   });
 });
