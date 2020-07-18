f042eab51866aa650292abd2e7ce63aaa092b226
jiunkoo <jiunkoo0106@gmail.com>
2020-07-18 18:04:29 +0900

    [추가] LoginForm에 onChange 테스트 추가

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -4,6 +4,31 @@ import { render, fireEvent } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  it('renders input controls and listens change events', () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render((
+      <LoginForm
+        onChange={handleChange}
+      />
+    ));
+
+    const controls = [
+      { label: 'Email', name: 'email', value: 'test@email.com' },
+      { label: 'Password', name: 'password', value: 'test' },
+    ];
+
+    controls.forEach(({ label, name, value }) => {
+      const input = getByLabelText(label);
+
+      expect(input).not.toBeNull();
+
+      fireEvent.change(input, { target: { value } });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
+  });
+
   it('renders Login button', () => {
     const handleSubmit = jest.fn();
 
