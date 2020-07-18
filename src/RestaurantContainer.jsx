709dae96711d5e447a2af8845f731edec866c659
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 16:55:38 +0900

    GREEN] render review input controls 구현

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -28,6 +28,18 @@ export default function RestaurantContainer({ restaurantId }) {
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
+      <div>
+        <label htmlFor="review-score">
+          평점
+        </label>
+        <input type="number" id="review-score" />
+      </div>
+      <div>
+        <label htmlFor="review-description">
+          리뷰 남기기
+        </label>
+        <input type="text" id="review-description" />
+      </div>
     </>
   );
 }
