e4dc10572ea207c20d9a8afa5a0b1410ebd0c5c1
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 19:20:52 +0900

    GREEN] postReview api 구현

--- src/services/api.js
+++ src/services/api.js
@@ -44,5 +44,15 @@ export async function postLogin({ email, password }) {
 export async function postReview({
   accessToken, restaurantId, score, description,
 }) {
-  // TODO ...
+  const url = 'https://eatgo-customer-api.ahastudio.com/'
+   + `restaurants/${restaurantId}/reviews`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ score, description }),
+  });
+  await response.json();
 }
