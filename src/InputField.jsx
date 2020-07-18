f52544c6263bec72fee177dc8c0c99759bae8ca5
SeaFalcon <seafalcon323@gmail.com>
2020-07-18 12:46:18 +0900

    [Green] InputField inputFields 렌더 테스트 구현

--- src/InputField.jsx
+++ src/InputField.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
 
+export default function InputField({ label, name, onChange }) {
+  return (
+    <div>
+      <label
+        htmlFor="input-score"
+      >
+        평점
+      </label>
+      <input
+        type="number"
+        id="input-score"
+      />
+    </div>
+  );
+}
