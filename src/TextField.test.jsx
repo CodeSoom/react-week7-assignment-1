7694ee1120cfa97106b09e31726ce3d8853c7c71
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 19:55:31 +0900

    Refactor] TextField 테스트 구체화

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -5,35 +5,59 @@ import { render } from '@testing-library/react';
 import TextField from './TextField';
 
 describe('TextField', () => {
+  const onChange = jest.fn();
+  beforeEach(() => {
+    onChange.mockClear();
+  });
+
   context('with type', () => {
-    it('renders score input control', () => {
-      const onChange = jest.fn();
-      const { queryByLabelText } = render((
+    function renderTextField() {
+      const handleChange = jest.fn();
+      return render((
         <TextField
           label="평점"
           type="number"
           name="score"
-          onChange={onChange}
+          onChange={handleChange}
         />
       ));
+    }
+
+    it('renders label and input control', () => {
+      const { queryByLabelText } = renderTextField();
 
       expect(queryByLabelText('평점')).not.toBeNull();
+    });
+
+    it('renders "number" input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="number"');
     });
   });
 
   context('without type', () => {
-    it('renders description input controls', () => {
-      const onChange = jest.fn();
-
-      const { queryByLabelText } = render((
+    function renderTextField() {
+      const handleChange = jest.fn();
+      return render((
         <TextField
           label="리뷰 내용"
           name="description"
-          onChange={onChange}
+          onChange={handleChange}
         />
       ));
+    }
+
+    it('renders label and input control', () => {
+      const { queryByLabelText } = renderTextField();
 
       expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+    });
+
+    it('renders "text" input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="text"');
     });
   });
 });
