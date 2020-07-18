9ca5181f24482bdd76bce46199ca3afdf16e03b6
Minwook Park <mwpark0975@gmail.com>
2020-07-18 16:46:50 +0900

    implement - RestaurantContainer for ReviewForm

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -11,6 +11,44 @@ import {
 import { get } from './utils';
 
 export default function RestaurantContainer({ restaurantId }) {
+  function ReviewForm() {
+    return (
+      <>
+        <div>
+          <label htmlFor="review-score">평점</label>
+          <input
+            type="number"
+            id="review-score"
+            name="score"
+          // value={score}
+          // onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="review-description">리뷰 내용</label>
+          <input
+            type="text"
+            id="review-description"
+            name="description"
+          // value={description}
+          // onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button
+            type="button"
+            id="review-post"
+            name="post"
+            // value={post}
+            // onChange={handleChange}
+          >
+            리뷰 남기기
+          </button>
+        </div>
+      </>
+    );
+  }
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -28,6 +66,7 @@ export default function RestaurantContainer({ restaurantId }) {
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
+      <ReviewForm />
     </>
   );
 }
