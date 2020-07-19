101f714af7a4d38206405e3a910161bf8450cb8f
Minwook Park <mwpark0975@gmail.com>
2020-07-19 18:24:27 +0900

    imeplement LogoutPage

---
+++ src/LogoutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LogoutPage() {
+  return (
+    <div>
+      <button
+        type="button"
+      >
+        Log out
+      </button>
+    </div>
+  );
+}
--- src/LogoutPage.test.jsx
+++ src/LogoutPage.test.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
-
 import { render } from '@testing-library/react';
 
 import LogoutPage from './LogoutPage';
