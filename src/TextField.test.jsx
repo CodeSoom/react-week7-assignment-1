6c5eba56dae7a6f1845b8133d294c25f77d0cbf8
jiunkoo <jiunkoo0106@gmail.com>
2020-07-19 10:50:18 +0900

    [추가] TextField에 onChange 테스트 추가

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -20,4 +20,26 @@ describe('TextField', () => {
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(container).toContainHTML('type="number"');
   });
+
+  it('renders review write fields', () => {
+    const name = 'score';
+    const value = '5';
+
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render((
+      <TextField
+        label="평점"
+        name={name}
+        onChange={handleChange}
+      />
+    ));
+
+    fireEvent.change(
+      getByLabelText('평점'),
+      { target: { value } },
+    );
+
+    expect(handleChange).toBeCalledWith({ name, value });
+  });
 });
