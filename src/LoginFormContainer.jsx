76edb90646e5afe7bdf95b3729fefe7f97915a95
jiunkoo <jiunkoo0106@gmail.com>
2020-07-19 15:29:02 +0900

    [구현] LoginFormContainer에 LogoutForm 컴포넌트 테스트 붙이기

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -5,30 +5,44 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   requestLogin,
   changeLoginField,
+  setAccessToken,
 } from './actions';
 
 import { get } from './utils';
 
 import LoginForm from './LoginForm';
+import LogoutForm from './LogoutForm';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   const { email, password } = useSelector(get('loginFields'));
+  const accessToken = useSelector(get('accessToken'));
 
   function handleChange({ name, value }) {
     dispatch(changeLoginField({ name, value }));
   }
 
-  function handleSubmit() {
+  function handleSubmitLogin() {
     dispatch(requestLogin());
   }
 
+  function handleSubmitLogout() {
+    dispatch(setAccessToken(''));
+  }
+
   return (
-    <LoginForm
-      fields={{ email, password }}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-    />
+    (accessToken) ? (
+      <LogoutForm
+        onClick={handleSubmitLogout}
+      />
+    )
+      : (
+        <LoginForm
+          fields={{ email, password }}
+          onChange={handleChange}
+          onSubmit={handleSubmitLogin}
+        />
+      )
   );
 }
