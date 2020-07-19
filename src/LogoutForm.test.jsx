0801466c5d5bd268ba579a6b6404795bfea1a698
Kwakcena <rhkrgudwh@gmail.com>
2020-07-20 00:13:37 +0900

    RED] LogoutForm 테스트
    log out 버튼이 렌더링 되는지 테스트합니다.

---
+++ src/LogoutForm.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import LogoutForm from './LogoutForm';
+
+describe('LogoutForm', () => {
+  it('renders "Log out" button', () => {
+    const { getByText } = render(<LogoutForm />);
+
+    expect(getByText('Log out')).toBeInTheDocument();
+  });
+});
