882a3593e1dff204375b82b03f97f61487647e25
Joylish <yeonzzu2@gmail.com>
2020-07-19 22:35:47 +0900

    delete codes related ReviewForm in RestaurantContainer and its test

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import ReviewForm from './ReviewForm';
 
 import {
   loadRestaurant,
@@ -11,39 +12,6 @@ import {
 
 import { get } from './utils';
 
-function ReviewForm({ onChange }) {
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
-  return (
-    <>
-      <div>
-        <label htmlFor="review-score">
-          score
-        </label>
-        <input
-          type="number"
-          id="review-score"
-          name="score"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="review-description">
-          리뷰 내용
-        </label>
-        <input
-          type="text"
-          id="review-description"
-          name="description"
-          onChange={handleChange}
-        />
-      </div>
-    </>
-  );
-}
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
 
--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -50,44 +50,6 @@ describe('RestaurantContainer', () => {
       const { container } = renderRestaurantContainer();
 
       expect(container).toHaveTextContent('Loading');
-    });
-  });
-
-  it('renders review write form', () => {
-    const { queryByLabelText } = render((
-      <RestaurantContainer restaurantId="1" />
-    ));
-    expect(queryByLabelText('평점')).not.toBeNull();
-    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
-  });
-
-  it('listen score change event', () => {
-    const { getByLabelText } = render((
-      <RestaurantContainer restaurantId="1" />
-    ));
-
-    fireEvent.change(getByLabelText('score'), {
-      target: { value: '5' },
-    });
-
-    expect(dispatch).toBeCalledWith({
-      type: 'changeReviewField',
-      payload: { name: 'score', value: '5' },
-    });
-  });
-
-  it('listen description change event', () => {
-    const { getByLabelText } = render((
-      <RestaurantContainer restaurantId="1" />
-    ));
-
-    fireEvent.change(getByLabelText('리뷰 내용'), {
-      target: { value: '진짜진짜진짜진짜맛있는집이에요' },
-    });
-
-    expect(dispatch).toBeCalledWith({
-      type: 'changeReviewField',
-      payload: { name: 'description', value: '진짜진짜진짜진짜맛있는집이에요' },
     });
   });
 });
