7ac11fa89273660cdb4ad582e606638ad2b2221f
Kwakcena <rhkrgudwh@gmail.com>
2020-07-20 00:17:18 +0900

    GREEN] LogoutForm click event 테스트 구현

--- src/LogoutForm.jsx
+++ src/LogoutForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function LogoutForm() {
+export default function LogoutForm({ onClick }) {
   return (
     <div>
       <button
         type="button"
+        onClick={onClick}
       >
         Log out
       </button>
