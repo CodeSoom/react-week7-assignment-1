6657f327fef6998c532ac6cbebd2220eb046e90b
jiunkoo <jiunkoo0106@gmail.com>
2020-07-19 10:24:16 +0900

    [구현] ReviewForm에 onChange 테스트 구현

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-export default function ReviewForm() {
+export default function ReviewForm({ onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
+
   return (
     <div>
       <label htmlFor="review-score">
@@ -9,6 +14,8 @@ export default function ReviewForm() {
       <input 
        type="text"
        id="review-score"
+       name="score"
+       onChange={handleChange}
       />
       <label htmlFor="review-description">
         리뷰 내용
@@ -16,6 +23,8 @@ export default function ReviewForm() {
       <input 
        type="text"
        id="review-description"
+       name="description"
+       onChange={handleChange}
       />
     </div>
   );
