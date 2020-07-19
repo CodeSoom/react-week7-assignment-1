069c856dd29abdc64dc243f81567d17b107f7570
Minwook Park <mwpark0975@gmail.com>
2020-07-19 18:22:40 +0900

    test - LogoutPage

---
+++ src/LogoutPage.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import LogoutPage from './LogoutPage';
+
+jest.mock('react-redux');
+
+describe('LogoutPage', () => {
+  beforeEach(() => {
+  });
+
+  const renderLogoutPage = () => render(<LogoutPage />);
+
+  it('renders log out', () => {
+    const { container } = renderLogoutPage();
+
+    expect(container).toHaveTextContent('Log out');
+  });
+});
