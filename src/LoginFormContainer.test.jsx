1d422b5444492ac415024159ce3d61910083b0e7
Joylish <yeonzzu2@gmail.com>
2020-07-19 20:19:22 +0900

    divide from 1 tests to 2 tests in LoginFormContainer.test.jsx 1. only test renders input controls 2. test listens change events about input controls; email and password

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -10,6 +10,7 @@ jest.mock('react-redux');
 
 describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
+  const handleChange = jest.fn();
 
   // beforeEach는 모든 it 테스트 이전에 실행된다
   // 만약에 A라는 it 테스트에서 dispatch를 사용하고 테스트를 종료했을 때
@@ -17,6 +18,7 @@ describe('LoginFormContainer', () => {
   // 테스트를 잘 못 수행하게 된다
   beforeEach(() => {
     dispatch.mockClear();
+    handleChange.mockClear();
 
     useDispatch.mockImplementation(() => dispatch);
 
@@ -28,20 +30,28 @@ describe('LoginFormContainer', () => {
     }));
   });
 
-  it('renders input controls and listens change events', () => {
-    const handleChange = jest.fn();
-
+  it('renders input controls', () => {
     const { getByLabelText } = render(
       <LoginFormContainer onChange={handleChange} />,
     );
+
     expect(getByLabelText('Email').value).toBe('test@test.com');
     expect(getByLabelText('Password').value).toBe('test');
+  });
+
+  it('listens change events', () => {
+    const { getByLabelText } = render(
+      <LoginFormContainer onChange={handleChange} />,
+    );
 
     fireEvent.change(getByLabelText('Email'), {
-      target: { value: 'tester@example.com' },
+      target: { value: 'new@email' },
     });
 
-    expect(handleChange).toBeCalled();
+    expect(dispatch).toBeCalledWith({
+      type: 'changeLoginField',
+      payload: { name: 'email', value: 'new@email' },
+    });
   });
 
   it('renders "Login" Button', () => {
