1f09eedb680f3e02d90428d9964544f0e8262d21
Kwakcena <rhkrgudwh@gmail.com>
2020-07-19 19:54:21 +0900

    GREEN] ReviewForm 통과
    ReviewForm에 fields prop을 추가하여 통과시킨다.
    
    하지만 ReviewForm을 사용하는 RestaurantContainer에서는 전달하고 있지
    않으므로 테스트가 깨진다.

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import TextField from './TextField';
 
-export default function ReviewForm({ onChange, onSubmit }) {
+export default function ReviewForm({ fields, onChange, onSubmit }) {
+  const { score, description } = fields;
+
   return (
     <>
       <TextField
         label="평점"
         type="number"
         name="score"
+        inputValue={score}
         onChange={onChange}
       />
       <TextField
         label="리뷰 내용"
         name="description"
+        inputValue={description}
         onChange={onChange}
       />
       <button
