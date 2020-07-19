501af7d746c884b68a68eed7ae2042d81c9bdc9f
Joylish <yeonzzu2@gmail.com>
2020-07-20 01:22:21 +0900

    add reviewFields and accessToken when useSelector called

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -8,6 +8,7 @@ import ReviewForm from './ReviewForm';
 import {
   loadRestaurant,
   changeReviewField,
+  sendReview,
 } from './actions';
 
 import { get } from './utils';
@@ -20,6 +21,7 @@ export default function RestaurantContainer({ restaurantId }) {
   }, []);
 
   const restaurant = useSelector(get('restaurant'));
+  const accessToken = useSelector(get('accessToken'));
 
   if (!restaurant) {
     return (
@@ -30,13 +32,21 @@ export default function RestaurantContainer({ restaurantId }) {
   function handleChange({ name, value }) {
     dispatch(changeReviewField({ name, value }));
   }
+  function handleSubmit() {
+    dispatch(sendReview({ restaurantId }));
+  }
 
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
-      <ReviewForm
-        onChange={handleChange}
-      />
+      {/* 로그인했을 때만 리뷰가 보인다. */}
+      {accessToken ? (
+        <ReviewForm
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        />
+      ) : null}
+
     </>
   );
 }
--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -20,6 +20,11 @@ describe('RestaurantPage', () => {
         name: '마법사주방',
         address: '서울시 강남구',
       },
+      reviewFields: {
+        score: '',
+        description: '',
+      },
+      accessToken: 'ACCESS_TOKEN',
     }));
   });
 
