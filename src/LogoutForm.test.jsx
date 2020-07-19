cff6b95aeb769391fe4dcd7e6fbd31630548e0e3
Kwakcena <rhkrgudwh@gmail.com>
2020-07-20 00:16:26 +0900

    RED] LogoutForm click event 테스트
    log out 버튼을 누르면 click event가 호출되는 테스트를 수행합니다.

--- src/LogoutForm.test.jsx
+++ src/LogoutForm.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LogoutForm from './LogoutForm';
 
@@ -9,5 +9,17 @@ describe('LogoutForm', () => {
     const { getByText } = render(<LogoutForm />);
 
     expect(getByText('Log out')).toBeInTheDocument();
+  });
+
+  it('listens click event', () => {
+    const handleClick = jest.fn();
+
+    const { getByText } = render((
+      <LogoutForm onClick={handleClick} />
+    ));
+
+    fireEvent.click(getByText('Log out'));
+
+    expect(handleClick).toBeCalled();
   });
 });
