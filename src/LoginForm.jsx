fc3fa71f7778fea27e9bbb251b7de45a005cf76e
Kwakcena <rhkrgudwh@gmail.com>
2020-07-19 20:32:10 +0900

    Refactor] LoginForm에서 TextFields 사용하기

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
 
+import TextField from './TextField';
+
 export default function LoginForm({ fields, onChange, onSubmit }) {
   const { email, password } = fields;
 
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
   return (
     <>
-      <div>
-        <label htmlFor="login-email">E-mail</label>
-        <input
-          type="email"
-          id="login-email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="login-password">Password</label>
-        <input
-          type="password"
-          id="login-password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </div>
+      <TextField
+        id="login-email"
+        label="E-mail"
+        type="email"
+        name="email"
+        inputValue={email}
+        onChange={onChange}
+      />
+      <TextField
+        id="login-password"
+        label="Password"
+        type="password"
+        name="password"
+        inputValue={password}
+        onChange={onChange}
+      />
       <button
         type="button"
         onClick={onSubmit}
