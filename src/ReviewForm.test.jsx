44b289874456b097211c9682f92f126a38f08630
Joylish <yeonzzu2@gmail.com>
2020-07-20 00:42:51 +0900

    updates ReviewForm.test.jsx 1. add test codes which check 'send' button rendered 2. initialize 2 mock functions using beforeEach() ; handleChange and handleSubmit 3. split out renderReviewForm()

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -4,21 +4,32 @@ import { render, fireEvent } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
-describe('ReviewForm', () => {
+describe('TextField', () => {
   const handleChange = jest.fn();
+  const handleSubmit = jest.fn();
 
-  it('renders review write fields', () => {
-    const { queryByLabelText } = render((
-      <ReviewForm onChange={handleChange} />
+  beforeEach(() => {
+    handleChange.mockClear();
+    handleSubmit.mockClear();
+  });
+
+  function renderReviewForm() {
+    return render((
+      <ReviewForm
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     ));
+  }
+  it('renders review write fields', () => {
+    const { queryByLabelText } = renderReviewForm();
+
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 내용')).not.toBeNull();
   });
 
   it('listen change events', () => {
-    const { getByLabelText } = render((
-      <ReviewForm onChange={handleChange} />
-    ));
+    const { getByLabelText } = renderReviewForm();
 
     const controls = [
       { label: '평점', name: 'score', value: '5' },
@@ -30,5 +41,13 @@ describe('ReviewForm', () => {
 
       expect(handleChange).toBeCalledWith({ name, value });
     });
+  });
+
+  it('renders "Send" Button', () => {
+    const { getByText } = renderReviewForm();
+
+    fireEvent.click(getByText('리뷰 남기기'));
+
+    expect(handleSubmit).toBeCalled();
   });
 });
