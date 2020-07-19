c1134b9e0d6083bdb8d5db499a37aa21f11c4cb3
Joylish <yeonzzu2@gmail.com>
2020-07-19 20:23:01 +0900

    delete unnecessary codes in LoginPage.test.jsx

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -4,20 +4,14 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import LoginPage from './LoginPage';
 
 jest.mock('react-redux');
 
 describe('LoginPage', () => {
-  const dispatch = jest.fn();
-
   beforeEach(() => {
-    dispatch.mockClear();
-
-    useDispatch.mockImplementation(() => dispatch);
-
     useSelector.mockImplementation((selector) => selector({
       loginFields: {
         email: 'test@test.com',
@@ -33,13 +27,5 @@ describe('LoginPage', () => {
       </MemoryRouter>,
     );
     expect(container).toHaveTextContent('Log In');
-  });
-  it('renders Login title', () => {
-    const { getByLabelText } = render(
-      <MemoryRouter>
-        <LoginPage />
-      </MemoryRouter>,
-    );
-    expect(getByLabelText('Email')).not.toBeNull();
   });
 });
