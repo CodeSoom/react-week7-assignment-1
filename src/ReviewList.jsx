e137a5cfef06950b4f5c2831672142ba2bdc6c33
Kwakcena <rhkrgudwh@gmail.com>
2020-07-20 02:38:38 +0900

    Refactor] 리뷰 title 추가

--- src/ReviewList.jsx
+++ src/ReviewList.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function ReviewList({ reviews }) {
   return (
     <div>
+      <h3>리뷰</h3>
       <ul>
         {reviews.map(({
           id, name, score, description,
