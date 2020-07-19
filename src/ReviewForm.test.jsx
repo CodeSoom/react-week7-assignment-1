50d1cccae8e9ade79908b6c0e239c0120ec7060e
jiunkoo <jiunkoo0106@gmail.com>
2020-07-19 10:24:08 +0900

    [추가] ReviewForm에 onChange 테스트 추가

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -17,4 +17,25 @@ describe('ReviewForm', () => {
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 내용')).not.toBeNull();
   });
+
+  it('renders review write fields', () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render((
+      <ReviewForm
+        onChange={handleChange}
+      />
+    ));
+
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: '정말 최고 :)' },
+    ];
+
+    controls.forEach(({ label, name, value }) => {
+      fireEvent.change(getByLabelText(label), { target: { value } });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
+  });
 });
