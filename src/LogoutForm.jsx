672bba5f415392b0badca8527cc267d89a3c3c06
Kwakcena <rhkrgudwh@gmail.com>
2020-07-20 00:14:40 +0900

    GREEN] LogoutForm 테스트 구현

---
+++ src/LogoutForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LogoutForm() {
+  return (
+    <div>
+      <button
+        type="button"
+      >
+        Log out
+      </button>
+    </div>
+  );
+}
