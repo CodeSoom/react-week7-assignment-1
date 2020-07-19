c47a62c166819677d29f029efebab13ad47a0513
Kwakcena <rhkrgudwh@gmail.com>
2020-07-20 02:16:12 +0900

    Refactor] add fixtures

--- src/ReviewList.test.jsx
+++ src/ReviewList.test.jsx
@@ -4,24 +4,9 @@ import { render } from '@testing-library/react';
 
 import ReviewList from './ReviewList';
 
-describe('ReviewList', () => {
-  const reviews = [
-    {
-      id: 1,
-      restaurantId: 1,
-      name: '형조',
-      score: 5,
-      description: '훌륭하다 훌륭하다 지구인놈들',
-    },
-    {
-      id: 3,
-      restaurantId: 1,
-      name: '행쥬',
-      score: 3,
-      description: 'Hi!',
-    },
-  ];
+import reviews from '../fixtures/reviews';
 
+describe('ReviewList', () => {
   context('with reviews', () => {
     it('render reviews', () => {
       const { container } = render((
