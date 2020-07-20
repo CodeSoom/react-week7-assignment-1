d84b7ea7819ee7271b0fec5e9854dbd1dea5015a
Minwook Park <minwook.park@enernoc.com>
2020-07-20 11:56:06 +0900

    refactor LoginFormContainer to use Logout

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -7,11 +7,13 @@ import { requestLogin, changeLoginField } from './actions';
 import { get } from './utils';
 
 import LoginForm from './LoginForm';
+import LogoutForm from './LogoutForm';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   const loginFields = useSelector(get('loginFields'));
+  const accessToken = useSelector(get('accessToken'));
 
   function handleChange({ name, value }) {
     dispatch(changeLoginField({ name, value }));
@@ -23,11 +25,15 @@ export default function LoginFormContainer() {
 
   return (
     <>
-      <LoginForm
-        fields={loginFields}
-        onChange={handleChange}
-        onSubmit={handleSubmit}
-      />
+      {accessToken ? (
+        <LogoutForm />)
+        : (
+          <LoginForm
+            fields={loginFields}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        )}
     </>
   );
 }
