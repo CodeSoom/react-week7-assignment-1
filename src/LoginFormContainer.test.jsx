1f1263571a3014559b446b927eb78a3680d5fefa
Joylish <yeonzzu2@gmail.com>
2020-07-20 17:44:46 +0900

    divde context 'whether logged in or not' 1. when not logged in - describe there is not accessToken using given() 2. when logged in - describe theres is accessToken using given() 2-1. test Logout button rendered 2-2. test whether Logout button event is listened

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -10,15 +10,9 @@ jest.mock('react-redux');
 
 describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
-  const handleChange = jest.fn();
 
-  // beforeEach는 모든 it 테스트 이전에 실행된다
-  // 만약에 A라는 it 테스트에서 dispatch를 사용하고 테스트를 종료했을 때
-  // 다른 B라는 it 테스트에서 dispatch는 이미 사용했다고 판단하여
-  // 테스트를 잘 못 수행하게 된다
   beforeEach(() => {
     dispatch.mockClear();
-    handleChange.mockClear();
 
     useDispatch.mockImplementation(() => dispatch);
 
@@ -27,40 +21,77 @@ describe('LoginFormContainer', () => {
         email: 'test@test.com',
         password: 'test',
       },
+      accessToken: given.accessToken,
     }));
   });
 
-  it('renders input controls', () => {
-    const { getByLabelText } = render(
-      <LoginFormContainer onChange={handleChange} />,
-    );
+  context('when not logged in', () => {
+    given('accessToken', () => '');
 
-    expect(getByLabelText('Email').value).toBe('test@test.com');
-    expect(getByLabelText('Password').value).toBe('test');
-  });
+    it('renders input controls', () => {
+      const { getByLabelText } = render(
+        <LoginFormContainer />,
+      );
 
-  it('listens change events', () => {
-    const { getByLabelText } = render(
-      <LoginFormContainer onChange={handleChange} />,
-    );
-
-    fireEvent.change(getByLabelText('Email'), {
-      target: { value: 'new@email' },
+      expect(getByLabelText('Email').value).toBe('test@test.com');
+      expect(getByLabelText('Password').value).toBe('test');
     });
 
-    expect(dispatch).toBeCalledWith({
-      type: 'changeLoginField',
-      payload: { name: 'email', value: 'new@email' },
+    it('listens change events', () => {
+      const handleChange = jest.fn();
+
+      const { getByLabelText } = render(
+        <LoginFormContainer onChange={handleChange} />,
+      );
+
+      fireEvent.change(getByLabelText('Email'), {
+        target: { value: 'new@email' },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeLoginField',
+        payload: { name: 'email', value: 'new@email' },
+      });
+    });
+
+    it('renders "Login" Button', () => {
+      const { container } = render(
+        <LoginFormContainer />,
+      );
+
+      expect(container).toHaveTextContent('Login');
+    });
+
+    it('listens "Login" Button click event', () => {
+      const { getByText } = render(
+        <LoginFormContainer />,
+      );
+
+      fireEvent.click(getByText('Login'));
+
+      expect(dispatch).toBeCalled();
     });
   });
 
-  it('renders "Login" Button', () => {
-    const { getByText } = render(
-      <LoginFormContainer />,
-    );
+  context('when logged in', () => {
+    given('accessToken', () => 'ACCESS_TOKEN');
 
-    fireEvent.click(getByText('Login'));
+    it('renders "Logout" Button', () => {
+      const { container } = render(
+        <LoginFormContainer />,
+      );
 
-    expect(dispatch).toBeCalled();
+      expect(container).toHaveTextContent('Logout');
+    });
+
+    it('listens "Logout" Button click event', () => {
+      const { getByText } = render(
+        <LoginFormContainer />,
+      );
+
+      fireEvent.click(getByText('Logout'));
+
+      expect(dispatch).toBeCalledWith({ type: 'logout' });
+    });
   });
 });
