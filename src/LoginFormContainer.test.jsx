f1597e7170b750d5b88097410c8884949a7e42a2
saseungmin <dbd02169@naver.com>
2020-11-02 20:59:50 +0900

    LoginFormContainer 테스트를 추가한다.

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+
+import { fireEvent, render } from '@testing-library/react';
 
 import LoginFormContainer from './LoginFormContainer';
 
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   const renderLoginPage = () => render(<LoginFormContainer />);
 
   it('renders input controls', () => {
@@ -12,5 +22,13 @@ describe('LoginFormContainer', () => {
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('renders "Log In" button', () => {
+    const { getByText } = renderLoginPage();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
