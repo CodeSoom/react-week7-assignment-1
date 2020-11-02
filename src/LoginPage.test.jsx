18ef0cb59810b5564a445bb2c6ac7f2086a3c780
saseungmin <dbd02169@naver.com>
2020-11-02 22:39:38 +0900

    LoginPage 테스트를 수정한다.

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
 import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
 describe('LoginPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: 'tester@example.com',
+        password: 'test',
+      },
+    }));
+  });
+
   const renderLoginPage = () => render(<LoginPage />);
 
   it('renders input controls', () => {
