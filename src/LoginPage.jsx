c84391ee6ad23f888830fbc6746409d00d419b3d
davidyang2149 <davidyang2149@gmail.com>
2020-11-02 22:30:21 +0900

    [Green] Add LoginPage.jsx
    7강 로그인 - 7주 차 과제 1
    - LoginPage 추가 및 UI 구현.

---
+++ src/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h2>Log In</h2>
+      <p>Email</p>
+      <input type="text" />
+      <p>Password</p>
+      <input type="text" />
+      <button type="button">Log In</button>
+    </div>
+  );
+}
