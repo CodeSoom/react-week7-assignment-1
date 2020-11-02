bd5d343391df0e8240dc9a41654d0e17f7de529e
saseungmin <dbd02169@naver.com>
2020-11-02 20:54:06 +0900

    Fixed LoginPage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,11 +4,11 @@ import LoginFormContainer from './LoginFormContainer';
 
 export default function LoginPage() {
   return (
-    <form>
+    <>
       <div>
         <h2>Log In</h2>
       </div>
       <LoginFormContainer />
-    </form>
+    </>
   );
 }
