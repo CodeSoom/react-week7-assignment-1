82eb4671ea40fc6ae2a3f7e06bcd9b4dc35e074c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-02 18:01:51 +0900

    [refactor] Refactoring HomePage.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <h2>Home</h2>
       <ul>
         <li><Link to="/about">About</Link></li>
-        <li>Log In</li>
+        <li><Link to="/login">Log In</Link></li>
         <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </div>
