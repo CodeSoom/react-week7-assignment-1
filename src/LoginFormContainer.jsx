8a16d52c6d4072d33753365f41ad2f2d3dbb2948
saseungmin <dbd02169@naver.com>
2020-11-02 22:38:43 +0900

    LoginFormContainer를 구현한다.

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { changeLoginField } from './actions';
+import { get } from './utils';
 import LoginForm from './LoginForm';
 
 export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
+
   function handleSubmit() {
     // TODO: form dispatch 작성
   }
 
+  function handleChange({ name, value }) {
+    dispatch(changeLoginField({ name, value }));
+  }
+
   return (
     <>
-      <LoginForm onSubmit={handleSubmit} />
+      <LoginForm
+        fields={{ email, password }}
+        onSubmit={handleSubmit}
+        onChange={handleChange}
+      />
     </>
   );
 }
