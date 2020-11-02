fddcbebd05719047dbcdf488093e0d398b68c77a
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-02 18:11:22 +0900

    [refactor] Apply LoginFormContainer to LoginPage.

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function LoginFormContainer() {
   return (
     <div>
-      <h2>Log In</h2>
       <div>
         <label htmlFor="email-input">
           E-mail
--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
+import LoginFormContainer from './LoginFormContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
-      <div>
-        <label htmlFor="email-input">
-          E-mail
-        </label>
-        <input
-          type="text"
-          id="email-input"
-        />
-      </div>
-      <div>
-        <label htmlFor="password-input">
-          Password
-        </label>
-        <input
-          type="text"
-          id="password-input"
-        />
-      </div>
+      <LoginFormContainer />
     </div>
   );
 }
