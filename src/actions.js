9efaf3383e2c5870614289984fab72bec0b3bb89
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-02 19:02:51 +0900

    [green] Implement requestLogin in actions.

--- src/actions.js
+++ src/actions.js
@@ -87,9 +87,12 @@ export function loadRestaurant({ restaurantId }) {
 }
 
 export function requestLogin() {
-  return async (dispatch, getState) => ({
-// TODO : getState로 {email, password} 받아오기
-// Fetch({email, password})으로 accessTocken 얻기.
-// disptach(setAccessToken(accessTocken));
-  });
+  return async (dispatch, getState) => {
+    // TODO : getState로 {email, password} 받아오기
+    // Fetch({email, password})으로 accessTocken 얻기.
+    dispatch({
+      type: 'setAccessTocken',
+      payload: {},
+    });
+  };
 }
