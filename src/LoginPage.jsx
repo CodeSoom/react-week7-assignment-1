51cdda6806c09906bf4809f3427d3012e72f3193
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-02 17:45:34 +0900

    [green] Implement LoginPage.

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,6 +4,13 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
+      <label htmlFor="email-input">
+        E-mail
+      </label>
+      <input
+      type="text"
+      id="email-input"
+      />
     </div>
   );
 }
