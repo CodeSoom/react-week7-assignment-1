f6acb722969b5378447bfddeae9b19332f1b6a2a
oceanide <oceanide@gmail.com>
2020-11-03 08:50:54 +0900

    [REFACTOR] use LoginFormContainer in LoginPage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 
+import LoginFormContainer from './LoginFormContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input type="text" id="login-email" />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          password
-        </label>
-        <input type="text" id="login-password" />
-      </div>
+      <LoginFormContainer />
     </div>
   );
 }
