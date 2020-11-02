4dfe40ad23cc9e0b6ce70fc80bc4944865ab545e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-02 23:39:56 +0900

    [refactor] Add state to App test and LoginPage test to pass tests.

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
+
 test('LoginPAge', () => {
+  useSelector.mockImplementation((selector) => selector({
+    loginFields: {
+      email: '',
+      password: '',
+    },
+  }));
+
   const { container, getByLabelText } = render(<LoginPage />);
 
   expect(container).toHaveTextContent('Log In');
