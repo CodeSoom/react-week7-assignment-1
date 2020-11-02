bdd96a8dc5655ae6a34e8af6e7635cc31b4e1613
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-02 17:44:54 +0900

    [red] Add test for LoginPage.

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -5,7 +5,9 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 test('LoginPAge', () => {
-  const { container } = render(<LoginPage />);
+  const { container, getByLabelText } = render(<LoginPage />);
 
   expect(container).toHaveTextContent('Log In');
+
+  expect(getByLabelText('E-mail')).not.toBeNull();
 });
