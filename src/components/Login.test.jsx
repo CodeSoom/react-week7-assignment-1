f624a1e28ac65b0cccf135c45d2b8ab164000352
juunini <juuni.ni.i@gmail.com>
2020-11-04 00:31:37 +0900

    Add Login component [Red]

--- src/components/Login.test.jsx
+++ src/components/Login.test.jsx
@@ -1,30 +1,53 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Login from './Login';
 
 describe('Login', () => {
+  const handleChange = jest.fn();
+
+  const renderLogin = ({ id, password }) => render(
+    <Login
+      id={id}
+      password={password}
+      onChange={handleChange}
+    />,
+  );
   context('with all fields', () => {
     it('all green and clickable button', () => {
+      renderLogin({ id: 'id', password: 'passowrd' });
 
+      expect(screen.getAllByText('green')).toHaveLength(2);
+      expect(screen.getByRole('button')).toBeEnabled();
     });
   });
 
   context('without id', () => {
     it('id field is red. disabled button', () => {
+      renderLogin({ id: 'id' });
 
+      expect(screen.getByText('green')).toBeInTheDocument();
+      expect(screen.getByText('red')).toBeInTheDocument();
+      expect(screen.getByRole('button')).toBeDisabled();
     });
   });
 
   context('without password', () => {
     it('password field is red. disabled button', () => {
+      renderLogin({ password: 'password' });
 
+      expect(screen.getByText('green')).toBeInTheDocument();
+      expect(screen.getByText('red')).toBeInTheDocument();
+      expect(screen.getByRole('button')).toBeDisabled();
     });
   });
 
   context('without all fields', () => {
     it('all field is red. disabled button', () => {
+      renderLogin({});
 
+      expect(screen.getAllByText('red')).toHaveLength(2);
+      expect(screen.getByRole('button')).toBeDisabled();
     });
   });
 });
