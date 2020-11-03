c15d1f6806ed90310eac593b1673a1540e9ad25f
thkim <thkim2017@gmail.com>
2020-11-03 19:09:27 +0900

    [red] Add a test for LoginPage : input control

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -13,6 +13,18 @@ describe('LoginPage', () => {
         <LoginPage />
       </MemoryRouter>
     ));
+
     expect(container).toHaveTextContent('Log In');
+  });
+
+  it('renders input control', () => {
+    const { getByLabelText } = render((
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    ));
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
   });
 });
