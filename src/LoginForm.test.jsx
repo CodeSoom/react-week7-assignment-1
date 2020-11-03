a685a392dc8d646ebc46bd5c8a9e34aa535a345b
oceanide <oceanide@gmail.com>
2020-11-03 22:30:23 +0900

    [REFACTOR] change matcher from toBeCalled to toBeCalledWith

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -45,7 +45,10 @@ describe('LoginForm', () => {
         target: { value: 'tester@example.com' },
       });
 
-      expect(handleChange).toBeCalled();
+      expect(handleChange).toBeCalledWith({
+        name: 'email',
+        value: 'tester@example.com',
+      });
     });
   });
 });
