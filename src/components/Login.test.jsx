9ff03a6fd061f69745e5dc6e181a1794d827b966
juunini <juuni.ni.i@gmail.com>
2020-11-03 23:32:24 +0900

    Add Login component [Red]

---
+++ src/components/Login.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Login from './Login';
+
+describe('Login', () => {
+  context('with all fields', () => {
+    it('all green and clickable button', () => {
+
+    });
+  });
+
+  context('without id', () => {
+    it('id field is red. disabled button', () => {
+
+    });
+  });
+
+  context('without password', () => {
+    it('password field is red. disabled button', () => {
+
+    });
+  });
+
+  context('without all fields', () => {
+    it('all field is red. disabled button', () => {
+
+    });
+  });
+});
