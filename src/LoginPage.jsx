49fe2b4b2bab993c235f7d7cf4c61b19ada4c7dc
thkim <thkim2017@gmail.com>
2020-11-03 19:10:38 +0900

    [green] Apply LoginFormContainer in LoginPage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import LoginFormContainer from './LoginFormContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <p>Log In</p>
+      <LoginFormContainer />
     </div>
   );
 }
