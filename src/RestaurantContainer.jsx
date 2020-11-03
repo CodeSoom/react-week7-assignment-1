a74dcb228cfcc9f6c31fa753679e40516f5db3ac
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-03 18:56:17 +0900

    [refactor] Apply RevieForm to RestaurantContainer.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,57 +3,13 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import ReviewForm from './ReviewForm';
 
 import {
   loadRestaurant, changeReviewField,
 } from './actions';
 
 import { get } from './utils';
-
-function ReviewForm({ onChange, onSubmit }) {
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    onChange({ name, value });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const { name, value } = event.target;
-
-    onSubmit({ name, value });
-  }
-
-  return (
-    <>
-      <label htmlFor="input-score">
-        평점
-      </label>
-      <input
-        type="number"
-        id="input-score"
-        name="score"
-        onChange={handleChange}
-      />
-      <label htmlFor="input-description">
-        리뷰 내용
-      </label>
-      <input
-        type="text"
-        id="input-description"
-        name="description"
-        onChange={handleChange}
-      />
-      <button
-        type="submit"
-        onSubmit={handleSubmit}
-      >
-        리뷰 남기기
-      </button>
-    </>
-  );
-}
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
@@ -74,7 +30,7 @@ export default function RestaurantContainer({ restaurantId }) {
     dispatch(changeReviewField({ name, value }));
   }
 
-  function handleSubmit({ name, value }) {
+  function handleSubmit() {
     dispatch();
   }
 
