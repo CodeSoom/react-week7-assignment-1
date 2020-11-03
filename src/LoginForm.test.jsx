3d3fb070bb9844c6df99704ae13ae405467af991
saseungmin <dbd02169@naver.com>
2020-11-03 22:22:46 +0900

    LoginForm에 대한 테스트를 추가한다.
    - Add Error

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -8,55 +8,76 @@ describe('LoginForm', () => {
   const handleSubmit = jest.fn();
   const handleChange = jest.fn();
 
-  const renderLoginForm = ({ email, password }) => render((
+  const renderLoginForm = ({ email, password, error }) => render((
     <LoginForm
       fields={{ email, password }}
       onSubmit={handleSubmit}
       onChange={handleChange}
+      error={error}
     />
   ));
 
-  it('renders input controls', () => {
-    const email = 'tester@example.com';
-    const password = 'test';
+  context('without error', () => {
+    const error = false;
 
-    const { getByLabelText } = renderLoginForm({ email, password });
+    it('renders input controls', () => {
+      const email = 'tester@example.com';
+      const password = 'test';
 
-    const controls = [
-      { label: 'E-mail', value: email },
-      { label: 'Password', value: password },
-    ];
+      const { getByLabelText } = renderLoginForm({ email, password, error });
 
-    controls.forEach(({ label, value }) => {
-      const input = getByLabelText(label);
-      expect(input.value).toBe(value);
-    });
-  });
+      const controls = [
+        { label: 'E-mail', value: email },
+        { label: 'Password', value: password },
+      ];
 
-  it('listens change events', () => {
-    const { getByLabelText } = renderLoginForm({});
-
-    const controls = [
-      { label: 'E-mail', name: 'email', value: 'testers@example.com' },
-      { label: 'Password', name: 'password', value: 'tests' },
-    ];
-
-    controls.forEach(({ label, value, name }) => {
-      const input = getByLabelText(label);
-
-      fireEvent.change(input, {
-        target: { value, name },
+      controls.forEach(({ label, value }) => {
+        const input = getByLabelText(label);
+        expect(input.value).toBe(value);
       });
+    });
 
-      expect(handleChange).toBeCalledWith({ name, value });
+    it('listens change events', () => {
+      const { getByLabelText } = renderLoginForm({ error });
+
+      const controls = [
+        { label: 'E-mail', name: 'email', value: 'testers@example.com' },
+        { label: 'Password', name: 'password', value: 'tests' },
+      ];
+
+      controls.forEach(({ label, value, name }) => {
+        const input = getByLabelText(label);
+
+        fireEvent.change(input, {
+          target: { value, name },
+        });
+
+        expect(handleChange).toBeCalledWith({ name, value });
+      });
+    });
+
+    it('renders "Log In" button', () => {
+      const { getByText } = renderLoginForm({ error });
+
+      fireEvent.click(getByText('Log In'));
+
+      expect(handleSubmit).toBeCalled();
+    });
+
+    it("doesn't renders error message", () => {
+      const { container } = renderLoginForm({ error });
+
+      expect(container).not.toHaveTextContent('입력이 안된 사항이 있습니다.');
     });
   });
 
-  it('renders "Log In" button', () => {
-    const { getByText } = renderLoginForm({});
+  context('with error', () => {
+    const error = true;
 
-    fireEvent.click(getByText('Log In'));
+    it('renders Error message', () => {
+      const { getByText } = renderLoginForm({ error });
 
-    expect(handleSubmit).toBeCalled();
+      expect(getByText('입력이 안된 사항이 있습니다.')).not.toBeNull();
+    });
   });
 });
