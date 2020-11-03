6ac86ed3277df59650ac7ef5d8124b44409c5c99
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-03 20:23:33 +0900

    [Green] Implement rendering login-form

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,6 +4,12 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+          <input type="email" id="login-email" />
+        </label>
+      </div>
     </div>
   );
 }
