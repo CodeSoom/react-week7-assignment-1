cd8102d4de8a5c7d9fd3e04bda1eee7dc09006a8
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-03 15:07:23 +0900

    [refactor] Refactoring LoginFormContainer.

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -14,6 +14,8 @@ describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
 
   beforeEach(() => {
+    dispatch.mockClear();
+
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((selector) => selector({
