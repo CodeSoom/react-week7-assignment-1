68b65a1631b217eaf0d6060fbcb7115780e64085
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-03 22:50:01 +0900

    [Green] Fix LoginPage test to pass

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
+
 describe('LoginPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {},
+    }));
+  });
+
   it('renders title', () => {
     const { container } = render(
       <LoginPage />,
