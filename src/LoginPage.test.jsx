ebec62493dbf5c2ecfd975627103f2c2de447a56
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-11-03 23:20:05 +0900

    [Red] LoginPage에 대한 테스트 작성.
    LoginPage 구현을 위한 간단한 테스트 작성.
    LoginPage 컴포넌트 생성.

---
+++ src/LoginPage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  const renderHomePage = () => render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>,
+  );
+
+  it('renders title', () => {
+    const { container } = renderHomePage();
+
+    expect(container).toHaveTextContent('Login');
+  });
+});
