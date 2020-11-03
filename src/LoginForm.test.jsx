af42598d4aad6f3f34e520f875fbcf2c73b6314a
oceanide <oceanide@gmail.com>
2020-11-03 15:56:47 +0900

    [RED] add testcase to check to click 'Log In' button

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  const handleSubmit = jest.fn();
+
+  beforeEach(() => {
+    handleSubmit.mockClear();
+  });
+
   it('renders input controls', () => {
-    const { getByLabelText } = render(<LoginForm />);
+    const { getByLabelText } = render(<LoginForm onSubmit={handleSubmit} />);
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('password')).not.toBeNull();
+  });
+
+  context('with "Log In" button is clicked', () => {
+    it('called handleSubmit', () => {
+      const { getByText } = render(<LoginForm onSubmit={handleSubmit} />);
+
+      fireEvent.click(getByText('Log In'));
+
+      expect(handleSubmit).toBeCalled();
+    });
   });
 });
