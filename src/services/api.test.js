a65635c1308558a1aa72e079a380ae2c3ff10904
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-03 12:30:03 +0900

    [refactor] Refactoring test for api to pass the test.

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -71,7 +71,7 @@ describe('api', () => {
 
   describe('postLogin', () => {
     beforeEach(() => {
-      mockFetch('qwer!!');
+      mockFetch({accessToken: 'qwer!!'});
     });
 
     it('returns accessTocken', async () => {
