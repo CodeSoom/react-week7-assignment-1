7eeb27e22fe5dff56d76a0f8a1b242cbd772f42c
oceanide <oceanide@gmail.com>
2020-11-03 13:35:57 +0900

    [RED] add testcase to check to call dispatch

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
+jest.mock('react-redux');
+
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders input controls', () => {
     const { getByLabelText } = render(<LoginFormContainer />);
 
@@ -16,5 +28,15 @@ describe('LoginFormContainer', () => {
     const { container } = render(<LoginFormContainer />);
 
     expect(container).toHaveTextContent('Log In');
+  });
+
+  context('when "Log In" button is clicked', () => {
+    it('called dispatch', () => {
+      const { getByText } = render(<LoginFormContainer />);
+
+      fireEvent.click(getByText('Log In'));
+
+      expect(dispatch).toBeCalled();
+    });
   });
 });
