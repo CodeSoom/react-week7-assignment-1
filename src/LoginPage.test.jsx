e233d47cadd16dbf037d5558826692f8ed5866a8
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-03 20:10:45 +0900

    [Red] Add LoginPage and its test

---
+++ src/LoginPage.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  it('renders title', () => {
+    const { container } = render(
+      <LoginPage />,
+    );
+
+    expect(container).toHaveTextContent('Home');
+  });
+});
