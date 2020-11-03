a4ea6e5ee810a13180f7acf4199b50371a5b4bf7
thkim <thkim2017@gmail.com>
2020-11-03 19:31:44 +0900

    [red] Add a test for Log In Button in LoginFormContainer

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders input controls', () => {
     const { getByLabelText } = render((
       <LoginFormContainer />
@@ -12,5 +21,15 @@ describe('LoginFormContainer', () => {
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('renders "Log In" button', () => {
+    const { getByText } = render((
+      <LoginFormContainer />
+    ));
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
