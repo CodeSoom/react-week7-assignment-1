1d8db5a40fd81e8b1c4bc503a0174ad1f4eff413
juunini <juuni.ni.i@gmail.com>
2020-11-04 22:21:15 +0900

    Set contents in LoginContainer

--- src/containers/LoginContainer.jsx
+++ src/containers/LoginContainer.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { get } from '../utils';
 import LoginForm from '../components/LoginForm';
+import {
+  setId,
+  setPassword,
+  setUser,
+} from '../redux/actions';
 
 export default function LoginContainer() {
-  return <LoginForm />;
+  const dispatch = useDispatch();
+
+  const { id, password } = useSelector(get('user'));
+
+  function handleChangeId(value) {
+    dispatch(setId(value));
+  }
+
+  function handleChangePassword(value) {
+    dispatch(setPassword(value));
+  }
+
+  function handleClick() {
+    dispatch(setUser());
+  }
+
+  return (
+    <LoginForm
+      id={id}
+      password={password}
+      onChangeId={handleChangeId}
+      onChangePassword={handleChangePassword}
+      onClick={handleClick}
+    />
+  );
 }
