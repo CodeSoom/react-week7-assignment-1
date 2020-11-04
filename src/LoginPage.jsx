93e1e52241ffad80128f109d5e7ef96bac2d71de
Dong A Kim <dongakim90@gmail.com>
2020-11-04 10:37:43 +0900

    LoginPage 리팩토링
    id에 login의 field라는 의미로 login prefix를 추가함

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -6,13 +6,13 @@ export default function LoginPage() {
   return (
     <>
       <InputField
-        id="email"
+        id="login-email"
         label="E-mail"
         type="email"
         name="email"
       />
       <InputField
-        id="password"
+        id="login-password"
         label="Password"
         type="password"
         name="password"
