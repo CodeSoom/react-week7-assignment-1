6a6625877dad6685bd84e36a3c5a0132e4a97247
saseungmin <dbd02169@naver.com>
2020-11-04 23:15:47 +0900

    ReviewForm에 대한 테스트를 구현한다.

---
+++ src/ReviewForm.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import ReviewForm from './ReviewForm';
+
+describe('ReviewForm', () => {
+  const handleChange = jest.fn();
+
+  const renderReviewForm = () => render((
+    <ReviewForm onChange={handleChange} />
+  ));
+
+  it('renders review inputs', () => {
+    const { queryByLabelText } = renderReviewForm();
+
+    expect(queryByLabelText('평점')).not.toBeNull();
+    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('change input value', () => {
+    const reviewInput = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: 'Good!' },
+    ];
+
+    const { getByLabelText } = renderReviewForm();
+
+    reviewInput.forEach(({ label, name, value }) => {
+      fireEvent.change(getByLabelText(label), {
+        target: { name, value },
+      });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
+  });
+});
