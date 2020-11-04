60c39666dafd8ffe4b97056cf54f24839677d760
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-04 20:52:08 +0900

    [Refactor] Refactor TextField

--- src/TextField.jsx
+++ src/TextField.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-export default function TextField({ label, text, name, onChange }) {
+export default function TextField({
+  label, type, text, name, onChange,
+}) {
+  const id = `input-${name}`;
+
   const handleChange = (event) => {
-    const { target: { name, value } } = event;
+    const { value } = event.target;
 
     onChange({ name, value });
   };
 
   return (
-    <label>
+    <label htmlFor={id}>
       { label }
       <input
+        id={id}
+        type={type}
         name={name}
         value={text}
         onChange={handleChange}
