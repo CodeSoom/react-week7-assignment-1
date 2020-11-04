50e27cccbf026c6d50e9e134f1ddfa367f1722e6
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-04 21:38:34 +0900

    [Green] Implement ReviewFormContainer

--- src/ReviewFormContainer.jsx
+++ src/ReviewFormContainer.jsx
@@ -7,6 +7,9 @@ export default function ReviewFormContainer() {
         평점
         <input type="number" id="review-rate" />
       </label>
+      <button type="button">
+        리뷰 남기기
+      </button>
     </div>
   );
 }
