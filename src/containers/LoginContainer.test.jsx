59853deabcb9aabccd855cca532712f17f5749b9
juunini <juuni.ni.i@gmail.com>
2020-11-04 22:28:08 +0900

    Add tests in LoginContainer

--- src/containers/LoginContainer.test.jsx
+++ src/containers/LoginContainer.test.jsx
@@ -4,27 +4,61 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 import LoginContainer from './LoginContainer';
 
-test('LoginContainer', () => {
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('LoginContainer', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector({
-    login: {
-      id: '',
-      password: '',
-    },
-  }));
+  context('when change id', () => {
+    it('calls action setId', () => {
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        login: { id: '', password: '' },
+      }));
 
-  render(<LoginContainer />);
+      render(<LoginContainer />);
 
-  expect(screen.getByPlaceholderText('ID')).toBeInTheDocument();
-  expect(screen.getByPlaceholderText('PASSWORD')).toBeInTheDocument();
-  expect(screen.getByRole('button')).toBeInTheDocument();
+      fireEvent.change(
+        screen.getByPlaceholderText('ID'),
+        { target: { value: 'test@test.com' } },
+      );
 
-  fireEvent.change(
-    screen.getByPlaceholderText('ID'),
-    { target: { value: 'test@test.com' } },
-  );
+      expect(dispatch).toEqual({ type: 'setId', payload: 'test@test.com' });
+    });
+  });
 
-  expect(dispatch).toEqual({ type: 'setId', payload: 'test@test.com' });
+  context('when change password', () => {
+    it('calls action setPassword', () => {
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        login: { id: '', password: '' },
+      }));
+
+      render(<LoginContainer />);
+
+      fireEvent.change(
+        screen.getByPlaceholderText('PASSWORD'),
+        { target: { value: 'password' } },
+      );
+
+      expect(dispatch).toEqual({ type: 'setPassword', payload: 'password' });
+    });
+  });
+
+  context('when click button', () => {
+    it('calls action setUser', () => {
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        login: { id: 'test@test.com', password: 'password' },
+      }));
+
+      render(<LoginContainer />);
+
+      fireEvent.click(screen.getByRole('button'));
+
+      expect(dispatch).toEqual({ type: 'setUser' });
+    });
+  });
 });
