04b10050ee9c6b9f2eeb71c8c513e035dc6aaa30
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-04 21:20:16 +0900

    [Green] Implement ReviewForm

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -4,10 +4,22 @@ import { useParams } from 'react-router-dom';
 
 import RestaurantContainer from './RestaurantContainer';
 
+function ReviewForm() {
+  return (
+    <label>
+      평점
+      <input type="number" />
+    </label>
+  );
+}
+
 export default function RestaurantPage({ params }) {
   const { id } = params || useParams();
 
   return (
-    <RestaurantContainer restaurantId={id} />
+    <>
+      <RestaurantContainer restaurantId={id} />
+      <ReviewForm />
+    </>
   );
 }
