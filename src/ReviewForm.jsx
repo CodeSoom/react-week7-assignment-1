2fb2502a33598046a02971a8a39307b88efca351
saseungmin <dbd02169@naver.com>
2020-11-04 23:15:55 +0900

    ReviewForm을 구현한다.

---
+++ src/ReviewForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function ReviewForm({ onChange }) {
+  const handleChange = (event) => {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="review-score">
+          평점
+        </label>
+        <input
+          type="number"
+          id="review-score"
+          name="score"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="review-description">
+          리뷰 내용
+        </label>
+        <input
+          type="text"
+          id="review-description"
+          name="description"
+          onChange={handleChange}
+        />
+      </div>
+    </>
+  );
+}
+
+export default ReviewForm;
