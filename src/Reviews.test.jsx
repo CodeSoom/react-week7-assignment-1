2634dc635f631fe9766d6da969db3a2052824f32
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-04 16:57:52 +0900

    [red] Add without reviews test for Reviews.

--- src/Reviews.test.jsx
+++ src/Reviews.test.jsx
@@ -4,27 +4,41 @@ import { render } from '@testing-library/react';
 
 import Reviews from './Reviews';
 
-test('Reviews', () => {
-  const reviews = [
-    {
-      name: '테스터',
-      score: 5,
-      description: '훌륭하다 훌륭하다 지구인놈들',
-    },
-    {
-      name: '테스터',
-      score: 3,
-      description: '맛있네요!',
-    },
-  ];
+describe('Reviews', () => {
+  context('with reviews', () => {
+    it('renders reviews', () => {
+      const reviews = [
+        {
+          name: '테스터',
+          score: 5,
+          description: '훌륭하다 훌륭하다 지구인놈들',
+        },
+        {
+          name: '테스터',
+          score: 3,
+          description: '맛있네요!',
+        },
+      ];
 
-  const { container } = render(<Reviews reviews={reviews} />);
+      const { container } = render(<Reviews reviews={reviews} />);
+      expect(container).toHaveTextContent('리뷰');
 
-  expect(container).toHaveTextContent('리뷰');
+      reviews.forEach(({ name, score, description }) => {
+        expect(container).toHaveTextContent(name);
+        expect(container).toHaveTextContent(score);
+        expect(container).toHaveTextContent(description);
+      });
+    });
+  });
 
-  reviews.forEach(({ name, score, description }) => {
-    expect(container).toHaveTextContent(name);
-    expect(container).toHaveTextContent(score);
-    expect(container).toHaveTextContent(description);
+  context('without reviews', () => {
+    it('render no review message', () => {
+      const reviews = [];
+
+      const { container } = render(<Reviews reviews={reviews} />);
+      expect(container).toHaveTextContent('리뷰');
+
+      expect(container).toHaveTextContent('리뷰가 없어요!');
+    });
   });
 });
