b098b11977ecfe85d8c4fc46f72b96ec95f9ed30
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-04 17:53:20 +0900

    [refactor] Refactoring Reviews.

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
 
-function ReviewItems({ reviews }) {
-  if (reviews.length === 0) {
-    return (
-      <p>리뷰가 없어요!</p>
-    );
-  }
-
-  return (
-    <ul>
-      {reviews.map(({
-        id, name, score, description,
-      }) => (
-        <li key={id}>
-          <p>{name}</p>
-          <p>
-            {score}
-            점
-          </p>
-          <p>{description}</p>
-        </li>
-      ))}
-    </ul>
-  );
-}
+import ReviewItems from './ReviewItems';
 
 export default function Reviews({ reviews }) {
   return (
     <>
       <h2>리뷰</h2>
-      <ReviewItems reviews={reviews} />
+      {
+        !(reviews || []).length
+          ? (<p>리뷰가 없어요!</p>)
+          : (<ReviewItems reviews={reviews} />)
+      }
     </>
   );
 }
