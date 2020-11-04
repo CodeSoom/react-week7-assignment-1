be33b68785cbc436a7e374f910d13c7b1a79246c
Dong A Kim <dongakim90@gmail.com>
2020-11-04 10:39:24 +0900

    LoginPage 리팩토링
    요구사항에 맞게 field를 block 처리

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -5,18 +5,22 @@ import InputField from './InputField';
 export default function LoginPage() {
   return (
     <>
-      <InputField
-        id="login-email"
-        label="E-mail"
-        type="email"
-        name="email"
-      />
-      <InputField
-        id="login-password"
-        label="Password"
-        type="password"
-        name="password"
-      />
+      <div>
+        <InputField
+          id="login-email"
+          label="E-mail"
+          type="email"
+          name="email"
+        />
+      </div>
+      <div>
+        <InputField
+          id="login-password"
+          label="Password"
+          type="password"
+          name="password"
+        />
+      </div>
       <button type="button">Log In</button>
     </>
   );
