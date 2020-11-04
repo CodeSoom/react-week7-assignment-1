11725756b370bd5fdeac0d6fa752f635f691389c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-04 17:49:30 +0900

    [green] Copy ReviewItems from Reviews.

---
+++ src/ReviewItems.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function ReviewItems({ reviews }) {
+  return (
+    <ul>
+      {reviews.map(({
+        id, name, score, description,
+      }) => (
+        <li key={id}>
+          <p>{name}</p>
+          <p>
+            {score}
+            점
+          </p>
+          <p>{description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
