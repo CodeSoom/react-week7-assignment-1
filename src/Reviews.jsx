b0ffc21441be15a41fdfce1a73267705a3c9900e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-04 11:53:54 +0900

    [refactor] Refactoring Reviews

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function Reviews() {
-  return '리뷰 테스터 5점 훌륭하다 훌륭하다 지구인놈들';
+  return (
+    <>
+      <h2>리뷰</h2>
+      <ul>
+        <li>
+          <p>
+            테스터
+            <br />
+            5점
+            <br />
+            훌륭하다 훌륭하다 지구인놈들
+          </p>
+        </li>
+      </ul>
+    </>
+  );
 }
