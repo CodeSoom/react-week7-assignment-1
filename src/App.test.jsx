5ceaa5652504b689cc2375ccb08c4eceeec5a55c
JamieShin0201 <jamieshin1450@gmail.com>
2020-11-05 14:20:39 +0900

    [Green] Implement login path in App.jsx, Fix test in App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -27,6 +27,7 @@ describe('App', () => {
       categories: [],
       restaurants: [],
       restaurant: { id: 1, name: '마녀주방' },
+      loginFields: { email: '', password: '' },
     }));
   });
 
