f61ac2c668a2f2923c9b939d773bef2b18f4b2de
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-05 23:17:48 +0900

    [Green] Implment App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import {
   Switch,
   Route,
   Link,
 } from 'react-router-dom';
+
+import { loadLoginStatus } from './actions';
 
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
@@ -14,6 +18,12 @@ import LoginPage from './LoginPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadLoginStatus());
+  }, []);
+
   return (
     <div>
       <header>
--- src/actions.js
+++ src/actions.js
@@ -148,3 +148,7 @@ export function sendReview() {
     dispatch(addReview(review));
   };
 }
+
+export function loadLoginStatus() {
+
+}
