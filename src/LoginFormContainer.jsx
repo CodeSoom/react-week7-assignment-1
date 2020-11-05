deaac21c6d4cd1fd2f8299d96806533d706afde9
JamieShin0201 <jamieshin1450@gmail.com>
2020-11-05 11:00:08 +0900

    [Red] Refactor LoginFormContainer.jsx

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  changeLoginField,
+} from './actions';
+
+import { get } from './utils';
 
 export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
+
+  function handleChangeLoginField(event) {
+    const { target: { name, value } } = event;
+    dispatch(changeLoginField({ name, value }));
+  }
+
   return (
     <div>
       <div>
         <label htmlFor="login-email">
           E-mail
         </label>
-        <input type="email" id="login-email" />
+        <input
+          type="email"
+          id="login-email"
+          name="email"
+          value={email}
+          onChange={handleChangeLoginField}
+        />
       </div>
       <div>
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="password" id="login-password" />
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+          value={password}
+          onChange={handleChangeLoginField}
+        />
       </div>
       <button type="button">로그인</button>
     </div>
