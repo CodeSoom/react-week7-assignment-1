f8b8a91d3a7fc2de16b494194ab13ff5d02f178f
Dong A Kim <dongakim90@gmail.com>
2020-11-05 10:37:00 +0900

    LoginPage 테스트 추가

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
 describe('<LoginPage />', () => {
+  const handleChange = jest.fn();
+
   it('renders email and password fields', () => {
     // When
     const { getByLabelText } = render(<LoginPage />);
@@ -20,5 +22,18 @@ describe('<LoginPage />', () => {
 
     // Then
     expect(getByRole('button')).toHaveTextContent('Log In');
+  });
+
+  it('call onChange', () => {
+    const { getAllByRole } = render(<LoginPage onChange={handleChange} />);
+
+    const inputs = getAllByRole('textbox');
+    inputs.forEach((input) => {
+      fireEvent.change(input, {
+        target: { value: '인풋 작성' },
+      });
+
+      expect(handleChange).toBeCalled();
+    });
   });
 });
