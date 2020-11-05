601185c153986b060c2334621d3c12c7947056ae
Dong A Kim <dongakim90@gmail.com>
2020-11-05 10:23:20 +0900

    InputField 테스트 추가

--- src/InputField.test.jsx
+++ src/InputField.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import InputField from './InputField';
 
 describe('<InputField />', () => {
+  const handleChange = jest.fn();
+
   it('renders input fields', () => {
     // Given
     const inputs = [
@@ -35,5 +37,23 @@ describe('<InputField />', () => {
       // Then
       expect(getByLabelText(label)).toBeInTheDocument();
     });
+  });
+
+  it('calls onChange', () => {
+    const { getByRole } = render((
+      <InputField
+        id="test"
+        label="test"
+        name="test"
+        type="text"
+        onChange={handleChange}
+      />
+    ));
+
+    fireEvent.change(getByRole('textbox'), {
+      target: { value: '인풋 작성' },
+    });
+
+    expect(handleChange).toBeCalled();
   });
 });
