a027e17dec3f434c295b73b329e1e209f00151c6
JamieShin0201 <jamieshin1450@gmail.com>
2020-11-05 11:13:13 +0900

    [Green] Fix LoginPage.test.jsx

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: '',
+        password: '',
+      },
+    }));
+  });
+
   it('renders LoginPage', () => {
-    const { container } = render(<LoginPage />);
+    const { container, queryByText } = render(<LoginPage />);
 
     expect(container).toHaveTextContent('LogIn');
     expect(container).toHaveTextContent('E-mail');
     expect(container).toHaveTextContent('Password');
+    expect(queryByText('로그인')).not.toBeNull();
   });
 });
