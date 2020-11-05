b37b925e3f100f7b3814533d646b348a2509176b
JamieShin0201 <jamieshin1450@gmail.com>
2020-11-05 10:49:56 +0900

    [Green] Implement LoginFormContainer

---
+++ src/LoginFormContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function LoginFormContainer() {
+  return (
+    <div>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input type="email" id="login-email" />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input type="password" id="login-password" />
+      </div>
+      <button type="button">로그인</button>
+    </div>
+  );
+}
