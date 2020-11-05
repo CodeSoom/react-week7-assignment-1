e3da49e8c053c94c8b568b747548be45a8c99869
Dong A Kim <dongakim90@gmail.com>
2020-11-05 10:57:31 +0900

    LoginPage 구현

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import InputField from './InputField';
 
 export default function LoginPage({
+  email,
+  password,
   onChange,
 }) {
   return (
@@ -13,6 +15,7 @@ export default function LoginPage({
           label="E-mail"
           type="email"
           name="email"
+          value={email}
           onChange={onChange}
         />
       </div>
@@ -22,6 +25,7 @@ export default function LoginPage({
           label="Password"
           type="password"
           name="password"
+          value={password}
           onChange={onChange}
         />
       </div>
