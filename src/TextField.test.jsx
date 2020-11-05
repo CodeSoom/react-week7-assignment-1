6afc20bd1b6d852371a8d73c5e2daa193b38fda5
oceanide <oceanide@gmail.com>
2020-11-06 00:37:59 +0900

    [REFACTOR] add testcase of TextField change event

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import TextField from './TextField';
 
@@ -19,24 +19,42 @@ describe('TextField', () => {
     );
   }
 
+  const inputTextFields = [
+    {
+      label: '평점', type: 'number', name: 'score', value: '',
+    },
+    {
+      label: '리뷰 내용', type: 'text', name: 'description', value: '',
+    },
+  ];
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('renders label and input control', () => {
-    const inputTextFields = [
-      {
-        label: '평점', type: 'number', name: 'score', value: '',
-      },
-      {
-        label: '리뷰 내용', type: 'text', name: 'description', value: '',
-      },
-    ];
-
     inputTextFields.forEach(({ label, type, name }) => {
       const { getByLabelText } = renderTextField(label, type, name);
 
       expect(getByLabelText(label)).not.toBeNull();
+    });
+  });
+
+  context('with input change event', () => {
+    it('calls handleChange', () => {
+      const score = '5';
+
+      const { label, type, name } = inputTextFields[0];
+
+      const { getByLabelText } = renderTextField(label, type, name);
+
+      const input = getByLabelText(label);
+
+      fireEvent.change(input, {
+        target: { value: score },
+      });
+
+      expect(handleChange).toBeCalled();
     });
   });
 });
