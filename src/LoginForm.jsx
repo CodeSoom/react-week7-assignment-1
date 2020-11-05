b99f604ce5ee6bf2f3083a2e15ce0030f8aad154
oceanide <oceanide@gmail.com>
2020-11-06 01:06:13 +0900

    [REFACTOR] use TextField in LoginForm

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,39 +1,26 @@
 import React from 'react';
 
+import TextField from './TextField';
+
 export default function LoginForm({ fields, onChange, onSubmit }) {
   const { email, password } = fields;
 
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
   return (
     <>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input
-          type="email"
-          id="login-email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          password
-        </label>
-        <input
-          type="password"
-          id="login-password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </div>
+      <TextField
+        label="E-mail"
+        type="email"
+        name="email"
+        inputValue={email}
+        onChange={onChange}
+      />
+      <TextField
+        label="password"
+        type="password"
+        name="password"
+        inputValue={password}
+        onChange={onChange}
+      />
       <button
         type="button"
         onClick={onSubmit}
