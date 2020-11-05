6fd9cdd43d0fc5eed6d5efa4337b54c73c23f4f5
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-05 16:19:31 +0900

    [refactor] Refactoring TextField test.

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -11,74 +11,49 @@ describe('TextField', () => {
     handleChange.mockClear();
   });
 
+  function renderTextField(type) {
+    return render(
+      <TextField
+        label="E-mail"
+        name="email"
+        textValue="test@test"
+        type={type}
+        onChange={handleChange}
+      />,
+    );
+  }
+
+  it('renders label and input control', () => {
+    const { getByLabelText } = renderTextField();
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+  });
+
+  it('listen change events', () => {
+    const { getByLabelText } = renderTextField();
+
+    const input = getByLabelText('E-mail');
+
+    fireEvent.change(input, { target: { value: 'test@test.com' } });
+
+    expect(handleChange).toBeCalled();
+  });
+
   context('without type', () => {
-    function renderTextField() {
-      return render(
-        <TextField
-          label="E-mail"
-          name="email"
-          textValue="test@test"
-          onChange={handleChange}
-        />,
-      );
-    }
-
-    it('renders label and input control', () => {
-      const { getByLabelText } = renderTextField();
-
-      expect(getByLabelText('E-mail')).not.toBeNull();
-    });
-
     it('renders "text" input control', () => {
       const { container } = renderTextField();
 
       expect(container).toContainHTML('type="text"');
     });
-
-    it('listen change events', () => {
-      const { getByLabelText } = renderTextField();
-
-      const input = getByLabelText('E-mail');
-
-      fireEvent.change(input, { target: { value: 'test@test.com' } });
-
-      expect(handleChange).toBeCalled();
-    });
   });
 
   context('with type', () => {
-    function renderTextField() {
-      return render(
-        <TextField
-          label="E-mail"
-          name="email"
-          textValue="test@test"
-          type="number"
-          onChange={handleChange}
-        />,
-      );
-    }
+    const type = 'number';
 
-    it('renders label and input control', () => {
-      const { getByLabelText } = renderTextField();
+    it('renders input control with type', () => {
+      const { container } = renderTextField(type);
 
-      expect(getByLabelText('E-mail')).not.toBeNull();
-    });
-
-    it('renders "number" input control', () => {
-      const { container } = renderTextField();
-
-      expect(container).toContainHTML('type="number"');
-    });
-
-    it('listen change events', () => {
-      const { getByLabelText } = renderTextField();
-
-      const input = getByLabelText('E-mail');
-
-      fireEvent.change(input, { target: { value: 'test@test.com' } });
-
-      expect(handleChange).toBeCalled();
+      expect(container).toContainHTML(`type="${type}"`);
     });
   });
 });
