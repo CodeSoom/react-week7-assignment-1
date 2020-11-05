e5f3113d49743906dbaa9ab236418c7943787884
oceanide <oceanide@gmail.com>
2020-11-05 17:28:35 +0900

    [REFACTOR] add testcase of review change event in ReviewForm

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
 describe('ReviewForm', () => {
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders review write form', () => {
     const { queryByText } = render(<ReviewForm />);
 
     expect(queryByText('평점')).not.toBeNull();
     expect(queryByText('리뷰 내용')).not.toBeNull();
+  });
+
+  context('with review change event', () => {
+    it('calls handleChange', () => {
+      const inputs = [
+        { label: '평점', name: 'score', value: '5' },
+        { label: '리뷰 내용', name: 'description', value: '정말 최고!' },
+      ];
+
+      const { getByLabelText } = render(<ReviewForm onChange={handleChange} />);
+
+      inputs.forEach(({ label, name, value }) => {
+        fireEvent.change(getByLabelText(label), {
+          target: { value },
+        });
+
+        expect(handleChange).toBeCalledWith({ name, value });
+      });
+    });
   });
 });
