b742eba0414d9ba5e90fa6ec6827dfe92f2580ee
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-05 15:55:42 +0900

    [Green] Implement renders input control in ReviewFormContainer

--- src/ReviewFormContainer.jsx
+++ src/ReviewFormContainer.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 import ReviewForm from './ReviewForm';
 
-import get from './utils';
+import { get } from './utils';
 
 export default function ReviewFormContainer() {
   const reviewFields = useSelector(get('reviewFields'));
