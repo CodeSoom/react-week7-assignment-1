1cfcff82f06f8c39004f69a2b30f7d43e6a09266
oceanide <oceanide@gmail.com>
2020-11-06 00:13:17 +0900

    [REFACTOR] refactor TextField.jsx

--- src/TextField.jsx
+++ src/TextField.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-export default function TextField({ onChange }) {
+export default function TextField({
+  label, type = 'text', name, onChange,
+}) {
+  const id = `input-${name}`;
+
   function handleChange(event) {
-    const { target: { name, value } } = event;
+    const { target: { value } } = event;
 
     onChange({ name, value });
   }
 
   return (
     <div>
-      <label htmlFor="review-score">
-        평점
+      <label htmlFor={id}>
+        {label}
       </label>
       <input
-        type="number"
-        id="review-score"
-        name="score"
+        type={type}
+        id={id}
+        name={name}
         onChange={handleChange}
       />
     </div>
