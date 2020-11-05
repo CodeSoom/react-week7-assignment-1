ce3c5249f73c002b0a7bb47b6a6b188d18220027
oceanide <oceanide@gmail.com>
2020-11-06 00:25:56 +0900

    [REFACTOR] add value props

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -7,12 +7,13 @@ import TextField from './TextField';
 describe('TextField', () => {
   const handleChange = jest.fn();
 
-  function renderTextField(label, type, name) {
+  function renderTextField(label, type, name, value) {
     return render(
       <TextField
         label={label}
         type={type}
         name={name}
+        value={value}
         onChange={handleChange}
       />,
     );
@@ -24,8 +25,12 @@ describe('TextField', () => {
 
   it('renders label and input control', () => {
     const inputTextFields = [
-      { label: '평점', type: 'number', name: 'score' },
-      { label: '리뷰 내용', type: 'text', name: 'description' },
+      {
+        label: '평점', type: 'number', name: 'score', value: '',
+      },
+      {
+        label: '리뷰 내용', type: 'text', name: 'description', value: '',
+      },
     ];
 
     inputTextFields.forEach(({ label, type, name }) => {
