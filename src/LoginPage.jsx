9a647387c352d33db636fd9cfc494e6ac51f0c8c
JamieShin0201 <jamieshin1450@gmail.com>
2020-11-05 10:51:26 +0900

    [Refactor] Refactor LoginPage.jsx

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
 
-export function LoginFormContainer() {
-  return (
-    <div>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input type="email" id="login-email" />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          Password
-        </label>
-        <input type="password" id="login-password" />
-      </div>
-      <button type="button">로그인</button>
-    </div>
-  );
-}
+import LoginFormContainer from './LoginFormContainer';
 
 export default function LoginPage() {
   return (
