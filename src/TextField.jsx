c91b6bed8813078c335990bec6c52cda02b61782
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-05 15:36:31 +0900

    [green] Implement TextField.

---
+++ src/TextField.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function TextField() {
+  return (
+    <>
+      <label htmlFor="id">
+        E-mail
+      </label>
+      <input
+        type="number"
+        id="id"
+      />
+    </>
+  );
+}
