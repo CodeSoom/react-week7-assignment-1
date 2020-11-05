15a427dd283a07eb2cfcc22abf544ad308791621
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-05 15:50:23 +0900

    [red] Add with & without type test for Textfield.

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -5,19 +5,52 @@ import { render } from '@testing-library/react';
 import TextField from './TextField';
 
 describe('TextField', () => {
-  function renderTextField() {
-    return render(
-      <TextField
-        label="E-mail"
-        name="email"
-        value="test@test"
-      />,
-    );
-  }
+  context('without type', () => {
+    function renderTextField() {
+      return render(
+        <TextField
+          label="E-mail"
+          name="email"
+          value="test@test"
+        />,
+      );
+    }
 
-  it('renders label and input control', () => {
-    const { getByLabelText } = renderTextField();
+    it('renders label and input control', () => {
+      const { getByLabelText } = renderTextField();
 
-    expect(getByLabelText('E-mail')).not.toBeNull();
+      expect(getByLabelText('E-mail')).not.toBeNull();
+    });
+
+    it('renders "text" input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="text"');
+    });
+  });
+
+  context('with type', () => {
+    function renderTextField() {
+      return render(
+        <TextField
+          label="E-mail"
+          name="email"
+          value="test@test"
+          type="number"
+        />,
+      );
+    }
+
+    it('renders label and input control', () => {
+      const { getByLabelText } = renderTextField();
+
+      expect(getByLabelText('E-mail')).not.toBeNull();
+    });
+
+    it('renders "number" input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="number"');
+    });
   });
 });
