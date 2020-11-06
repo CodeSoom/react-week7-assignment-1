bb410b03d75dc6aeeb43292ebc0f03ec9a787189
hyeonhyejin <dev.hyejin04@gmail.com>
2020-11-06 22:29:05 +0900

    [Red] Add test case about dispatch reviewFieldChange action into ReviewFormContainer component

--- src/container/ReviewFormContainer.test.jsx
+++ src/container/ReviewFormContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch } from 'react-redux';
 import ReviewFormContainer from './ReviewFormContainer';
@@ -30,9 +30,35 @@ describe('ReviewFormContainer', () => {
     expect(container).toHaveTextContent('리뷰 남기기');
   });
 
-  //   describe('change review fields', () => {
-  //     it('call review field change action', () => {
+  describe('change review fields', () => {
+    it('calls review field change action when score is changed', () => {
+      const { getByLabelText } = renderReviewFormContainer();
+      const value = 10;
 
-//     });
-//   });
+      fireEvent.change(getByLabelText('평점'), { target: { value } });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeReviewField',
+        paylod: {
+          name: 'score',
+          value,
+        },
+      });
+    });
+
+    it('calls review field change action when review is changed', () => {
+      const { getByLabelText } = renderReviewFormContainer();
+      const value = '리뷰내용이다';
+
+      fireEvent.change(getByLabelText('리뷰 내용'), { target: { value } });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeReviewField',
+        paylod: {
+          name: 'description',
+          value,
+        },
+      });
+    });
+  });
 });
