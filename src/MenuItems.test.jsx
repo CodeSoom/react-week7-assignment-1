829c382f0df13c642dcb98522b7e24f778cffefb
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-06 09:24:39 +0900

    [refactor] Refactoring MenuItems.

--- src/MenuItems.test.jsx
+++ src/MenuItems.test.jsx
@@ -5,13 +5,19 @@ import { render } from '@testing-library/react';
 import MenuItems from './MenuItems';
 
 describe('MenuItems', () => {
+  function renderMenuItems(menuItems) {
+    return render(
+      <MenuItems menuItems={menuItems} />,
+    );
+  }
+
   context('with menu items', () => {
     it('renders menu items', () => {
       const menuItems = [
         { id: 1, name: '공기밥' },
       ];
 
-      const { container } = render(<MenuItems menuItems={menuItems} />);
+      const { container } = renderMenuItems(menuItems);
 
       expect(container).toHaveTextContent('공기밥');
     });
@@ -20,7 +26,7 @@ describe('MenuItems', () => {
   context('without menu item', () => {
     it('renders no items message', () => {
       [[], null, undefined].forEach((menuItems) => {
-        const { container } = render(<MenuItems menuItems={menuItems} />);
+        const { container } = renderMenuItems(menuItems);
 
         expect(container).toHaveTextContent('메뉴가 없어요');
       });
