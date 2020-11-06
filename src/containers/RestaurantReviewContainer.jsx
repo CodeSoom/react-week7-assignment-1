db4301f1f911d45732ab0be97d3671632f0b04aa
juunini <juuni.ni.i@gmail.com>
2020-11-06 22:36:42 +0900

    Add RestaurantReviewContainer[Refactoring Red]

--- src/containers/RestaurantReviewContainer.jsx
+++ src/containers/RestaurantReviewContainer.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantReview from '../components/RestaurantReview';
+import { get } from '../utils';
+import {
+  setReview,
+  postReview,
+} from '../redux/actions';
 
 export default function RestaurantReviewContainer() {
-  return (<RestaurantReview />);
+  const review = useSelector(get('review'));
+
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    dispatch(setReview());
+  }
+
+  function handleChange() {
+    dispatch(postReview());
+  }
+
+  return (
+    <RestaurantReview
+      review={review}
+      onClick={handleClick}
+      onChange={handleChange}
+    />
+  );
 }
--- src/containers/RestaurantReviewContainer.test.jsx
+++ src/containers/RestaurantReviewContainer.test.jsx
@@ -1,17 +1,60 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import RestaurantReviewContainer from './RestaurantReviewContainer';
 
 describe('RestaurantReviewContainer', () => {
+  const renderRestaurantReviewContainer = () => render(<RestaurantReviewContainer />);
+
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders component', () => {
     useSelector.mockImplementation((selector) => selector({
       review: 'review',
     }));
 
-    render(<RestaurantReviewContainer />);
+    renderRestaurantReviewContainer();
 
     expect(screen.getByDisplayValue('review')).toBeInTheDocument();
+  });
+
+  context('when changes review', () => {
+    it('call onChange', () => {
+      useSelector.mockImplementation((selector) => selector({
+        review: '',
+      }));
+
+      renderRestaurantReviewContainer();
+
+      fireEvent.change(
+        screen.getByPlaceholderText('리뷰를 작성해 주세요!'),
+        'r',
+      );
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  context('when click button', () => {
+    it('call onClick', () => {
+      useSelector.mockImplementation((selector) => selector({
+        review: 'review',
+      }));
+
+      renderRestaurantReviewContainer();
+
+      fireEvent.click(
+        screen.getByRole('button', { name: '작성하기' }),
+      );
+
+      expect(dispatch).toBeCalled();
+    });
   });
 });
