17dd42c09bf4e98c464847f0f2c4b0fef552a830
Noah <mike1124@naver.com>
2020-11-06 21:07:52 +0900

    [Green] Implement Inputs in LoginPage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,22 +4,32 @@ export default function LoginPage() {
   return (
     <>
       <h2>Log In</h2>
-      <label htmlFor="login-email">
-        E-mail
-      </label>
-      <input
-        type="email"
-        id="login-email"
-        name="email"
-      />
-      <label htmlFor="login-password">
-        Password
-      </label>
-      <input
-        type="password"
-        id="login-password"
-        name="password"
-      />
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="login-email"
+          name="email"
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+        />
+      </div>
+      <button
+        type="button"
+        onClick={onsubmit}
+      >
+        Log In
+      </button>
     </>
   );
 }
