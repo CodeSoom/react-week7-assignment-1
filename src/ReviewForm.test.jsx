bbdb708ef1715efe639118f2ff41f824200389e3
saseungmin <dbd02169@naver.com>
2020-11-07 02:24:18 +0900

    ReviewForm 테스트를 수정한다.

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -8,42 +8,54 @@ describe('ReviewForm', () => {
   const handleChange = jest.fn();
   const handleSubmit = jest.fn();
 
-  const renderReviewForm = () => render((
+  const renderReviewForm = (error = false) => render((
     <ReviewForm
       onChange={handleChange}
       onSubmit={handleSubmit}
+      fields={{ score: '', description: '' }}
+      error={error}
     />
   ));
 
-  it('renders review inputs', () => {
-    const { queryByLabelText } = renderReviewForm();
+  context('with isError', () => {
+    it('renders error message', () => {
+      const { container } = renderReviewForm(true);
 
-    expect(queryByLabelText('평점')).not.toBeNull();
-    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
-  });
-
-  it('change input value', () => {
-    const reviewInput = [
-      { label: '평점', name: 'score', value: '5' },
-      { label: '리뷰 내용', name: 'description', value: 'Good!' },
-    ];
-
-    reviewInput.forEach(({ label, name, value }) => {
-      const { getByLabelText } = renderReviewForm();
-
-      fireEvent.change(getByLabelText(label), {
-        target: { value },
-      });
-
-      expect(handleChange).toBeCalledWith({ name, value });
+      expect(container).toHaveTextContent('입력이 안된 사항이 있습니다.');
     });
   });
 
-  it('Click button to send', () => {
-    const { getByText } = renderReviewForm();
+  context('without isError', () => {
+    it('renders review inputs', () => {
+      const { queryByLabelText } = renderReviewForm();
 
-    fireEvent.click(getByText('리뷰 남기기'));
+      expect(queryByLabelText('평점')).not.toBeNull();
+      expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+    });
 
-    expect(handleSubmit).toBeCalled();
+    it('change input value', () => {
+      const reviewInput = [
+        { label: '평점', name: 'score', value: '5' },
+        { label: '리뷰 내용', name: 'description', value: 'Good!' },
+      ];
+
+      reviewInput.forEach(({ label, name, value }) => {
+        const { getByLabelText } = renderReviewForm();
+
+        fireEvent.change(getByLabelText(label), {
+          target: { value },
+        });
+
+        expect(handleChange).toBeCalledWith({ name, value });
+      });
+    });
+
+    it('Click button to send', () => {
+      const { getByText } = renderReviewForm();
+
+      fireEvent.click(getByText('리뷰 남기기'));
+
+      expect(handleSubmit).toBeCalled();
+    });
   });
 });
