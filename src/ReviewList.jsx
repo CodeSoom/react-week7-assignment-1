ec7c7c427a493301c674966c95cf7d6713685ae1
saseungmin <dbd02169@naver.com>
2020-11-07 00:44:28 +0900

    Refactoring ReviewList

--- src/ReviewList.jsx
+++ src/ReviewList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReviewItem from './ReviewItem';
 
 const ReviewList = ({ reviews }) => {
   const NO_REVIEW_TEXT = '리뷰가 존재하지 않습니다.';
@@ -8,14 +9,11 @@ const ReviewList = ({ reviews }) => {
       <div>리뷰</div>
       {reviews && reviews.length ? (
         <ul>
-          {reviews.reverse().map(({
-            id, name, score, description,
-          }) => (
-            <li key={id}>
-              <div>{name}</div>
-              <div>{score}</div>
-              <div>{description}</div>
-            </li>
+          {reviews.reverse().map((review) => (
+            <ReviewItem
+              key={review.id}
+              review={review}
+            />
           ))}
         </ul>
       ) : (
