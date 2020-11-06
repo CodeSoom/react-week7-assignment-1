6e9bd7b961e219e0cd133128434909d1fd2dfd39
juunini <juuni.ni.i@gmail.com>
2020-11-06 22:19:05 +0900

    Add RestaurantReview component [Refactoring]

--- src/components/RestaurantReview.jsx
+++ src/components/RestaurantReview.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-export default function RestaurantReview() {
+export default function RestaurantReview({ review, onChange, onClick }) {
   return (
     <form>
       <input
         type="text"
         placeholder="리뷰를 작성해 주세요!"
+        value={review}
+        onChange={onChange}
       />
-      <button type="button">작성하기</button>
+      <button
+        type="button"
+        onClick={onClick}
+        disabled={!review}
+      >
+        작성하기
+      </button>
     </form>
   );
 }
--- src/components/RestaurantReview.test.jsx
+++ src/components/RestaurantReview.test.jsx
@@ -1,15 +1,68 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import RestaurantReview from './RestaurantReview';
 
 describe('RestaurantReview', () => {
-  const renderRestaurantReview = () => render(<RestaurantReview />);
+  const handleClick = jest.fn();
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleClick.mockClear();
+    handleChange.mockClear();
+  });
+
+  const renderRestaurantReview = ({ review }) => render(
+    <RestaurantReview
+      review={review}
+      onClick={handleClick}
+      onChange={handleChange}
+    />,
+  );
 
   it('renders input and button', () => {
-    renderRestaurantReview();
+    renderRestaurantReview({});
 
     expect(screen.getByPlaceholderText('리뷰를 작성해 주세요!')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: '작성하기' })).toBeInTheDocument();
+  });
+
+  context('when review is changed', () => {
+    it('call onChange', () => {
+      renderRestaurantReview({});
+
+      expect(handleChange).not.toBeCalled();
+
+      fireEvent.change(
+        screen.getByPlaceholderText('리뷰를 작성해 주세요!'),
+        { target: { value: 'review' } },
+      );
+
+      expect(handleChange).toBeCalled();
+    });
+  });
+
+  context('without review value', () => {
+    it('can not click button', () => {
+      renderRestaurantReview({});
+
+      expect(handleClick).not.toBeCalled();
+
+      fireEvent.click(screen.getByRole('button', { name: '작성하기' }));
+
+      expect(handleClick).not.toBeCalled();
+    });
+  });
+
+  context('with review value', () => {
+    it('can click button', () => {
+      renderRestaurantReview({ review: 'review' });
+
+      expect(handleClick).not.toBeCalled();
+
+      fireEvent.click(screen.getByRole('button', { name: '작성하기' }));
+
+      expect(handleClick).toBeCalled();
+    });
   });
 });
