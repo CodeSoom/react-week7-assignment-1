875903915f3d26d31c17c857aa1139852064a126
juunini <juuni.ni.i@gmail.com>
2020-11-06 22:11:26 +0900

    Add RestaurantReview component [Green]

--- src/components/RestaurantReview.jsx
+++ src/components/RestaurantReview.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function RestaurantReview() {
   return (
-    <></>
+    <form>
+      <input
+        type="text"
+        placeholder="리뷰를 작성해 주세요!"
+      />
+      <button type="button">작성하기</button>
+    </form>
   );
 }
