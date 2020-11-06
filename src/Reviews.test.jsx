f78acf71db9c5eca2a6965afa69e518fbefc07d5
oceanide <oceanide@gmail.com>
2020-11-06 19:36:06 +0900

    [REFACTOR] add testcase to check name in Reviews

--- src/Reviews.test.jsx
+++ src/Reviews.test.jsx
@@ -7,13 +7,18 @@ import Reviews from './Reviews';
 describe('Reviews', () => {
   it('renders reviews', () => {
     const reviews = [
-      { id: 1, score: '5', description: '정말 최고!' },
-      { id: 2, score: '10', description: '정말 맛집!' },
+      {
+        id: 1, name: '테스트', score: '5', description: '정말 최고!',
+      },
+      {
+        id: 2, name: '테스트', score: '10', description: '정말 맛집!',
+      },
     ];
 
     const { container } = render(<Reviews reviews={reviews} />);
 
-    reviews.forEach(({ score, description }) => {
+    reviews.forEach(({ name, score, description }) => {
+      expect(container).toHaveTextContent(name);
       expect(container).toHaveTextContent(score);
       expect(container).toHaveTextContent(description);
     });
