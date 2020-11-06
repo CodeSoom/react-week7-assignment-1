78ef9816fae224bbefad8c8cb7310ced52d54817
hyeonhyejin <dev.hyejin04@gmail.com>
2020-11-06 19:24:28 +0900

    [Green] Implement Reviews component

--- src/presentational/Reviews.jsx
+++ src/presentational/Reviews.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 export default function Reviews({ reviews }) {
   return (
-    <>
-    </>
+    <ul>
+      {
+        reviews.map(({ name, score, description }) => (
+          <li>
+            <p>{name}</p>
+            <p>{score}</p>
+            <p>{description}</p>
+          </li>
+        ))
+      }
+    </ul>
   );
 }
