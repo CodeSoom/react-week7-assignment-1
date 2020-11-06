c6d9d01350be5ed6adc211017b5861582d39125e
oceanide <oceanide@gmail.com>
2020-11-06 19:25:19 +0900

    [REFACTOR] add name props in Reviews

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 export default function Reviews({ reviews }) {
   return (
     <ul>
-      {reviews.map(({ id, score, description }) => (
-        <li key={id}>
-          <p>{score}</p>
-          <p>{description}</p>
+      {reviews.map((review) => (
+        <li key={review.id}>
+          <p>{review.name}</p>
+          <p>{review.score}</p>
+          <p>{review.description}</p>
         </li>
       ))}
     </ul>
