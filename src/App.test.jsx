d42861955aad17eda362e27d04885df0ec2aa474
hyeonhyejin <dev.hyejin04@gmail.com>
2020-11-06 19:11:10 +0900

    Modify the code about reviews test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -26,7 +26,7 @@ describe('App', () => {
       ],
       categories: [],
       restaurants: [],
-      restaurant: { id: 1, name: '마녀주방' },
+      restaurant: { id: 1, name: '마녀주방', reviews :[] },
       loginField: {
         email: '',
         password: '',
