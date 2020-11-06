6e7be48192d28a5515bb54dfe817e5e1a894176b
thkim <thkim2017@gmail.com>
2020-11-06 10:03:23 +0900

    [green] Implement a password control

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -23,7 +23,12 @@ export default function LoginForm({ onChange, onSubmit }) {
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="password" id="login-password" />
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+          onChange={handleChange}
+        />
       </div>
       <button
         type="button"
