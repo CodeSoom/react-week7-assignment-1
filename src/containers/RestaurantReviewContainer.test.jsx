9822f72ebedc5c92401bb3948294b5c241a65a5e
juunini <juuni.ni.i@gmail.com>
2020-11-06 22:24:04 +0900

    Add RestaurantReviewContainer[Red]

--- src/containers/RestaurantReviewContainer.test.jsx
+++ src/containers/RestaurantReviewContainer.test.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
 import RestaurantReviewContainer from './RestaurantReviewContainer';
 
 describe('RestaurantReviewContainer', () => {
   it('renders component', () => {
+    useSelector.mockImplementation((selector) => selector({
+      review: 'review',
+    }));
+
     render(<RestaurantReviewContainer />);
+
+    expect(screen.getByDisplayValue('review')).toBeInTheDocument();
   });
 });
