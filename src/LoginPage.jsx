f29ee3ddc3258c71c069d7efa5853b8707abfd1a
Noah <mike1124@naver.com>
2020-11-06 19:45:02 +0900

    [Green] Implement Input controls

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,6 +4,22 @@ export default function LoginPage() {
   return (
     <>
       <h2>Log In</h2>
+      <label htmlFor="login-email">
+        E-mail
+      </label>
+      <input
+        type="email"
+        id="login-email"
+        name="email"
+      />
+      <label htmlFor="login-password">
+        Password
+      </label>
+      <input
+        type="password"
+        id="login-password"
+        name="password"
+      />
     </>
   );
 }
