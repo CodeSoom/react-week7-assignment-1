e85c37ed70ef129e8ae03aad54306075e45c2e67
hyeonhyejin <dev.hyejin04@gmail.com>
2020-11-06 20:05:02 +0900

    Add a test case into ReviewsForm

--- src/presentational/ReviewForm.test.jsx
+++ src/presentational/ReviewForm.test.jsx
@@ -5,8 +5,15 @@ import { fireEvent, render } from '@testing-library/react';
 import ReviewForm from './ReviewForm';
 
 describe('ReviewForm', () => {
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
   const renderReviewForm = () => render(
-    <ReviewForm />,
+    <ReviewForm
+      score={1}
+      description="this is review"
+      onChange={handleChange}
+      onClick={handleClick}
+    />,
   );
 
   beforeEach(() => {
@@ -24,5 +31,27 @@ describe('ReviewForm', () => {
     const { getByText } = renderReviewForm();
 
     expect(getByText('리뷰 남기기')).not.toBeNull();
+  });
+
+  describe('review field change', () => {
+    it('calls field change action', () => {
+      const { getByLabelText } = renderReviewForm();
+
+      fireEvent.change(getByLabelText('평점'), { target: { value: '10' } });
+
+      fireEvent.change(getByLabelText('리뷰 내용'), { target: { value: '이것은 리뷰다' } });
+
+      expect(handleChange).toBeCalledTimes(2);
+    });
+  });
+
+  describe('login button click', () => {
+    it('calls request login action', () => {
+      const { getByText } = renderReviewForm();
+
+      fireEvent.click(getByText('리뷰 남기기'));
+
+      expect(handleClick).toBeCalled();
+    });
   });
 });
