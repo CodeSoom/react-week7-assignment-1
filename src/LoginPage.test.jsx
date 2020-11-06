38e8a237a6dd8b627d6ef8bd1e3544f27679c2b1
thkim <thkim2017@gmail.com>
2020-11-06 20:27:20 +0900

    [delayed red] Add a useSelector test in LoginPage.test

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -4,9 +4,28 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
+
 describe('LoginPage', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: 'test@test',
+        password: '1234',
+      },
+    }));
+  });
+
   it('renders log-in title', () => {
     const { container } = render((
       <MemoryRouter>
