1dd83bd98e7f345938b60734eb720403eb885ddd
saseungmin <dbd02169@naver.com>
2020-11-07 02:24:27 +0900

    ReviewForm을 수정한다.

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import ErrorMessage from './ErrorMessage';
 
 import TextField from './TextField';
 
-function ReviewForm({ onChange, onSubmit }) {
+function ReviewForm({
+  onChange, onSubmit, fields, error,
+}) {
+  const ERROR_MESSAGE = '입력이 안된 사항이 있습니다.';
+
+  const { score, description } = fields;
+
   return (
     <>
       <TextField
         label="평점"
         name="score"
         type="number"
+        inputValue={score}
         onChange={onChange}
       />
       <TextField
         label="리뷰 내용"
         name="description"
+        inputValue={description}
         onChange={onChange}
       />
+      {error && <ErrorMessage message={ERROR_MESSAGE} />}
       <button
         type="button"
         onClick={onSubmit}
