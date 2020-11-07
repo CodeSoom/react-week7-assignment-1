856ce632c4e88b53a7f136e0e4b38fa45ffc0516
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-11-08 00:47:03 +0900

    [Green] Make '리뷰가 없어요' message in Reviews
    Make message when no reviews.

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default function Reviews({ reviews = [] }) {
+  if (reviews.length === 0) {
+    return <p>리뷰가 없어요!</p>;
+  }
+
   return (
     <div>
       <ul>
