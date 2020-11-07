a6a33ec7f32287d318af3dbfed89037e631b7b1e
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-11-07 21:20:58 +0900

    Fix storage.test.js

--- src/services/storage.test.js
+++ src/services/storage.test.js
@@ -1,35 +1,11 @@
 import { loadItem, saveItem, deleteItem } from './storage';
 
-describe('storage', () => {
-  describe('loadItem', () => {
-    beforeEach(() => {
-      localStorage.accessToken = 'ACCESS_TOKEN';
-    });
+test('storage', () => {
+  saveItem('accessToken', 'ACCESS_TOKEN');
 
-    it('returns item', () => {
-      const accessToken = loadItem('accessToken');
+  expect(loadItem('accessToken')).toBe('ACCESS_TOKEN');
 
-      expect(accessToken).toBe('ACCESS_TOKEN');
-    });
-  });
+  deleteItem('accessToken');
 
-  describe('saveItem', () => {
-    it('saves item', () => {
-      saveItem('accessToken', 'ACCESS_TOKEN');
-
-      expect(localStorage.accessToken).toBe('ACCESS_TOKEN');
-    });
-  });
-
-  describe('deleteItem', () => {
-    beforeEach(() => {
-      localStorage.accessToken = 'ACCESS_TOKEN';
-    });
-
-    it('deletes item', () => {
-      deleteItem('accessToken');
-
-      expect(localStorage.accessToken).toBeUndefined();
-    });
-  });
+  expect(loadItem('accessToken')).toBeNull();
 });
