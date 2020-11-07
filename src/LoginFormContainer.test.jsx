1adb3620c0ef211832b47b2764fd581e80fbaa15
Noah <mike1124@naver.com>
2020-11-07 19:52:57 +0900

    [Red] Add test code in LoginFormContainer

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -22,12 +22,51 @@ describe('LoginFormContainer', () => {
     }));
   });
 
-  it('renders input controls', () => {
-    const { getByLabelText } = render(
-      <LoginFormContainer />,
-    );
+  describe('inputs', () => {
+    it('renders input controls', () => {
+      const { getByLabelText } = render(
+        <LoginFormContainer />,
+      );
 
-    expect(getByLabelText('E-mail').value).toBe('test@test');
-    expect(getByLabelText('Password').value).toBe('1234');
+      expect(getByLabelText('E-mail').value).toBe('test@test');
+      expect(getByLabelText('Password').value).toBe('1234');
+    });
+
+    context('when typed in', () => {
+      it('calls change fields dispatch', () => {
+        const { getByLabelText } = render(
+          <LoginFormContainer />,
+        );
+
+        fireEvent.change(getByLabelText('E-mail'), { target: { value: 'new test@test' } });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'changeLoginField',
+          payload: { name: 'email', value: 'new test@test' },
+        });
+      });
+    });
+  });
+
+  describe('"Log in"button', () => {
+    it('renders "Log in" button', () => {
+      const { getByText } = render(
+        <LoginFormContainer />,
+      );
+
+      expect(getByText('Log In')).not.toBeNull();
+    });
+
+    context('when clicked', () => {
+      it('calls request login dispatch', () => {
+        const { getByText } = render(
+          <LoginFormContainer />,
+        );
+
+        fireEvent.click(getByText('Log In'));
+
+        expect(dispatch).toBeCalled();
+      });
+    });
   });
 });
