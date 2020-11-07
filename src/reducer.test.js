a1951cd53d55714b83cd8d07c9fefa4723ce5634
thkim <thkim2017@gmail.com>
2020-11-07 17:08:31 +0900

    [red] Add a test for reducer: changeLoginField

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,6 +7,7 @@ import {
   setRestaurant,
   selectRegion,
   selectCategory,
+  changeLoginField,
 } from './actions';
 
 describe('reducer', () => {
@@ -124,5 +125,21 @@ describe('reducer', () => {
         name: '한식',
       });
     });
+  });
+
+  describe('changeLoginField', () => {
+    const initialState = {
+      loginFields: {
+        email: '',
+        password: '',
+      },
+    };
+
+    const state = reducer(
+      initialState,
+      changeLoginField({ name: 'email', value: 'tester@example.com' }),
+    );
+
+    expect(state.loginFields.value).toBe('tester@example.com');
   });
 });
