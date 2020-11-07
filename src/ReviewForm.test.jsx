1e1cfaa7ec5a09bf6ffc9ce2316d3c79fcf1d7ba
goodzzong <goodzzong@gmail.com>
2020-11-08 01:12:24 +0900

    ReviewForm 에 대한 테스트 코드 추가

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
 describe('ReviewForm', () => {
+  const handleChange = jest.fn();
+
+  function renderReviewForm() {
+    return render((
+      <ReviewForm
+        onChange={handleChange}
+      />
+    ));
+  }
+
   it('renders input fields', () => {
-    const { container } = render(
-      <ReviewForm />,
-    );
+    const { container } = renderReviewForm();
 
     expect(container).toHaveTextContent('평점');
     expect(container).toHaveTextContent('리뷰 내용');
+  });
+
+  describe('Change input fields', () => {
+    it('calls onChange handler', () => {
+      const { getByLabelText } = renderReviewForm();
+
+      fireEvent.change(getByLabelText('평점'), {
+        target: { value: 5 },
+      });
+
+      expect(handleChange).toBeCalledWith({
+        name: 'score',
+        value: '5',
+      });
+    });
   });
 });
