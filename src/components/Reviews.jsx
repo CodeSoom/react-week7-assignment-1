2ecf414f0c99f7524110839a4466a0621b5d5f8f
juunini <juuni.ni.i@gmail.com>
2020-11-08 00:08:16 +0900

    Sort reviews from id

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,23 +4,25 @@ export default function Reviews({ reviews = [] }) {
   return (
     <ul>
       {
-        reviews.map(({
-          id, name, score, description,
-        }) => {
-          const fixedScore = score > 5 ? 5 : score;
+        reviews
+          .sort((a, b) => a.id - b.id)
+          .map(({
+            id, name, score, description,
+          }) => {
+            const fixedScore = score > 5 ? 5 : score;
 
-          return (
-            <li
-              key={id}
-            >
-              <p>{`닉네임 : ${name}`}</p>
-              <p>
-                {`점수 : ${'★'.repeat(fixedScore)}${'☆'.repeat(5 - fixedScore)}`}
-              </p>
-              <p>{`평가 : ${description}`}</p>
-            </li>
-          );
-        })
+            return (
+              <li
+                key={id}
+              >
+                <p>{`닉네임 : ${name}`}</p>
+                <p>
+                  {`점수 : ${'★'.repeat(fixedScore)}${'☆'.repeat(5 - fixedScore)}`}
+                </p>
+                <p>{`평가 : ${description}`}</p>
+              </li>
+            );
+          })
       }
     </ul>
   );
