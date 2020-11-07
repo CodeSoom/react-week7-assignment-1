7e8dd22328dfbbc2bb09ee07d257c4ecd849364d
goodzzong <goodzzong@gmail.com>
2020-11-08 01:51:38 +0900

    TextField 에 대한 테스트 코드 추가

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import TextField from './TextField';
 
 describe('TextField', () => {
   const handleChange = jest.fn();
 
-  it('renders input fields', () => {
-    const { container } = render((
+  function renderTextField(label, name, type = 'text') {
+    return render((
       <TextField
-        label="평점"
-        name="score"
-        type="number"
+        label={label}
+        name={name}
+        type={type}
         onChange={handleChange}
       />
     ));
+  }
+  context('with type', () => {
+    it('renders input fields', () => {
+      const label = '평점';
+      const name = 'score';
+      const type = 'number';
+      const value = '5';
 
-    expect(container).toHaveTextContent('평점');
+      const { container, getByLabelText } = renderTextField(label, name, type);
+
+      expect(container).toHaveTextContent('평점');
+
+      fireEvent.change(getByLabelText(label), {
+        target: { value },
+      });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
+  });
+
+  context('without type', () => {
+    it('renders input fields', () => {
+      const label = '리뷰내용';
+      const name = 'description';
+      const value = '굳뜨!';
+
+      const { container, getByLabelText } = renderTextField(label, name);
+
+      expect(container).toHaveTextContent(label);
+
+      fireEvent.change(getByLabelText(label), {
+        target: { value },
+      });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
   });
 });
