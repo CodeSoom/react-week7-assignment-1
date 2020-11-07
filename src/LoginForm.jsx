7c3f38120e009695cfa78f07d08ac8a242bf3153
Dong A Kim <dongakim90@gmail.com>
2020-11-07 18:40:44 +0900

    LoginForm 구현

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -6,6 +6,7 @@ export default function LoginForm({
   email,
   password,
   onChange,
+  onSubmit,
 }) {
   const handleChange = (event) => {
     const { target: { name, value } } = event;
@@ -34,7 +35,12 @@ export default function LoginForm({
           onChange={handleChange}
         />
       </div>
-      <button type="button">Log In</button>
+      <button
+        type="button"
+        onClick={onSubmit}
+      >
+        Log In
+      </button>
     </>
   );
 }
