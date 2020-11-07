70eb518d8a08f27bbbbe9517fea6d3a8f3a1844f
Noah <mike1124@naver.com>
2020-11-07 20:35:36 +0900

    [Green] Implement Log out Form

---
+++ src/LogoutForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LogoutForm() {
+  return (
+    <>
+      <button
+        type="button"
+      >
+        Log Out
+      </button>
+    </>
+  );
+}
