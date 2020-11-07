d0d496eaeadf33f655467b0828c979fe8ab2cc3e
goodzzong <goodzzong@gmail.com>
2020-11-08 04:01:24 +0900

    ReviewItems 에 대한 테스트 코드 추가

---
+++ src/ReviewItems.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import ReviewItems from './ReviewItems';
+
+describe('ReviewItems', () => {
+  context('with review items', () => {
+    it('renders review items', () => {
+      const reviewtems = [
+        {
+          id: 1,
+          restaurantId: 1,
+          name: '테스터',
+          score: 5,
+          description: '멋',
+        },
+      ];
+
+      const { container } = render(<ReviewItems reviewtems={reviewtems} />);
+
+      expect(container).toHaveTextContent('멋');
+    });
+  });
+
+  context('without review item', () => {
+    it('renders no review message', () => {
+      [[], null, undefined].forEach((reviewtems) => {
+        const { container } = render(<ReviewItems reviewtems={reviewtems} />);
+
+        expect(container).toHaveTextContent('리뷰가 없어요');
+      });
+    });
+  });
+});
