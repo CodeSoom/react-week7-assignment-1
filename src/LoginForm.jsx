a5db09a5fc26423bf80a32f22f316ca6c73a781b
ParkDongJo <pdj10211@gmail.com>
2020-11-07 14:11:02 +0900

    [week7-1][Green] LoginForm 구현하기

---
+++ src/LoginForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { requestLogin } from './actions';
+
+export default function LoginForm({ onChange, onSubmit }) {
+  function handleChange(event) {
+    const { target: { name, value } } = evnet;
+    onChange({ name, value});
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input 
+          type="email" 
+          id="login-email"
+          name="email"
+          onChange={handleChange}/>
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input 
+          type="password" 
+          id="login-password"
+          name="password"
+          onChange={handleChange}
+          />
+      </div>
+      <button
+        type='button'
+        onClick={onSubmit}
+      >
+        Log In
+      </button>
+    </>
+  );
+}
