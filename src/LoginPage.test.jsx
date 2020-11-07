684648b37eccf7017687971bcd1122a285804a8a
ParkDongJo <pdj10211@gmail.com>
2020-11-07 15:44:18 +0900

    [week7-1][Green] LoginPage.test에 userSelector 테스트 코드작성

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import {MemoryRouter} from 'react-router-dom';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
+
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: 'test@test',
+        password: '1234'
+      }
+    }))
+  })
 
   function loginPageRender() {
     return render(
