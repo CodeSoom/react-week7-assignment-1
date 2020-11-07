be6ffec1b182ef281c38975ad9e38f844b503241
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-07 15:45:03 +0900

    [refactor] Refactoring storage test.

--- src/services/storage.test.js
+++ src/services/storage.test.js
@@ -1,32 +1,14 @@
 import { loadItem, saveItem, deleteItem } from './storage';
 
-describe('storage', () => {
+test('storage', () => {
   const key = 'accessToken';
   const value = 'ACCESSTOKEN';
 
-  const getItem = jest.fn(() => value);
-  const setItem = jest.fn();
-  const removeItem = jest.fn();
+  saveItem(key, value);
 
-  beforeEach(() => {
-    Storage.prototype.getItem = getItem;
-    Storage.prototype.setItem = setItem;
-    Storage.prototype.removeItem = removeItem;
-  });
+  expect(loadItem(key)).toBe(value);
 
-  it('loadItem', () => {
-    expect(loadItem(key)).toBe(value);
-  });
+  deleteItem(key);
 
-  it('saveItem', () => {
-    saveItem(key, value);
-
-    expect(setItem).toBeCalledWith(key, value);
-  });
-
-  it('deleteItem', () => {
-    deleteItem(key);
-
-    expect(removeItem).toBeCalledWith(key);
-  });
+  expect(loadItem(key)).toBeNull();
 });
