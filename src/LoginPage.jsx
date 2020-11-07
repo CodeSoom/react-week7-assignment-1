76deed0693e835f581da24a40b733c96e4943bc9
Dong A Kim <dongakim90@gmail.com>
2020-11-07 16:10:39 +0900

    LoginPage 수정
    LoginContainer 적용

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,36 +1,12 @@
 import React from 'react';
 
-import InputField from './InputField';
+import LoginContainer from './LoginContainer';
 
-export default function LoginPage({
-  email,
-  password,
-  onChange,
-}) {
+export default function LoginPage() {
   return (
     <>
       <h2>Log In</h2>
-      <div>
-        <InputField
-          id="login-email"
-          label="E-mail"
-          type="email"
-          name="email"
-          value={email}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <InputField
-          id="login-password"
-          label="Password"
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChange}
-        />
-      </div>
-      <button type="button">Log In</button>
+      <LoginContainer />
     </>
   );
 }
