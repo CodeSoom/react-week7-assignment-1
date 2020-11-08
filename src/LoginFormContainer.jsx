82a1ac62bcb38b594648f9171beec3616c835fc6
ParkDongJo <pdj10211@gmail.com>
2020-11-09 00:48:46 +0900

    [week7-1][Green] LoginForm login/logout 에 대한 로직 분리하기

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { requestLogin , changeLoginField } from './actions';
+import { requestLogin , changeLoginFields, setAccessToken, logout } from './actions';
 
 import { get } from './utils';
 
 import LoginForm from './LoginForm';
 
+import LogoutForm from './LogoutForm';
+
+import { deleteItem } from './services/storage';
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   const { email, password } = useSelector(get('loginFields'));
+  const accessToken = useSelector(get('accessToken'));
 
   function handleChange({ name, value }) {
-    dispatch(changeLoginField({ name, value }));
+    dispatch(changeLoginFields({ name, value }));
   }
 
   function handleSubmit() {
@@ -21,11 +26,23 @@ export default function LoginFormContainer() {
     dispatch(requestLogin());
   }
 
+  function handleClickLogout() {
+    dispatch(logout());
+
+    deleteItem('accessToken');
+  }
+
   return (
-    <LoginForm 
-      fields={{ email, password}}
-      onSubmit={handleSubmit}
-      onChange={handleChange} 
-    />
+    <>
+      {accessToken ? (
+        <LogoutForm onClick={handleClickLogout}/>
+      ) : (
+        <LoginForm 
+          fields={{ email, password}}
+          onSubmit={handleSubmit}
+          onChange={handleChange} 
+        />
+      )} 
+    </>
   );
 }
