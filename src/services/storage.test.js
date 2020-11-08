146ca078cdfdcbd54a923a3f804b69bbadf20a64
wjy5446 <wjy5446@gmail.com>
2020-11-08 13:54:38 +0900

    refac) loadItem 테스트 코드 수정

--- src/services/storage.test.js
+++ src/services/storage.test.js
@@ -1,14 +1,12 @@
-import { loadItem } from './storage';
+import { saveItem, loadItem } from './storage';
 
 describe('loadItem', () => {
-  beforeEach(() => {
-    const localStorage = { accessToken: 'ACCESSTOKEN' };
-    Storage.prototype.getItem = jest.fn((key) => localStorage[key]);
-  });
-
   it('입력한 key 값을 가져옵니다.', () => {
-    const accessToken = loadItem('accessToken');
+    const key = 'accessToken';
+    const value = 'ACCESSTOKEN';
 
-    expect(accessToken).toEqual('ACCESSTOKEN');
+    expect(loadItem(key)).toBeNull();
+    saveItem(key, value);
+    expect(loadItem(key)).toBe(value);
   });
 });
