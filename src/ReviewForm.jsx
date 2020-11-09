bb9969cf5750aa0d304c9442e0554e457d79d30f
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-11-09 15:12:11 +0900

    [Refector] ReviewForm component
    Add, const { score, description } = fields;
    - for More clearly!

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -3,19 +3,21 @@ import React from 'react';
 import TextField from './TextField';
 
 export default function ReviewForm({ fields, onChange, onSubmit }) {
+  const { score, description } = fields;
+
   return (
     <>
       <TextField
         label="평점"
         type="number"
         name="score"
-        value={fields.score}
+        value={score}
         onChange={onChange}
       />
       <TextField
         label="리뷰 내용"
         name="description"
-        value={fields.description}
+        value={description}
         onChange={onChange}
       />
       <button
