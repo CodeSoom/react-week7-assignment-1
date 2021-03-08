5c0a05c80780df6ec2a8378340eb0a375f6df34b
EL <ickyicky@gmail.com>
2021-03-08 20:44:31 +0900

    Green: 테스트 통과

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,6 +4,21 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Log in</h2>
+      <div>
+        <label htmlFor="loginEmail">
+          E-mail
+        </label>
+        <input type="email" id="loginEmail" />
+      </div>
+      <div>
+        <label htmlFor="loginPassword">
+          Password
+        </label>
+        <input type="password" id="loginPassword" />
+      </div>
+      <button type="button">
+        Log In
+      </button>
     </div>
   );
 }
