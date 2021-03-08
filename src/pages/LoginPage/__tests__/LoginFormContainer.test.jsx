566923d2b3fd9f3089ccf5f730d74cbd8ca5335e
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-08 20:30:34 +0900

    LoginFormContainer test 작성

---
+++ src/pages/LoginPage/__tests__/LoginFormContainer.test.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import LoginFormContainer from '../LoginFormContainer';
+
+jest.mock('react-redux');
+
+describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      userLoginInputs: { email: 'test@naver', password: 'te' },
+    }));
+  });
+
+  it('input을 입력하면 변경된 입력을 반영하는 dispatch함수가 실행된다.', () => {
+    const { queryByLabelText } = render(<LoginFormContainer />);
+    const controls = [
+      { label: 'E-mail', origin: 'test@naver', value: 'test@naver.com' },
+      { label: 'Password', origin: 'te', value: 'test' },
+    ];
+
+    controls.forEach(({ label, origin, value }) => {
+      expect(queryByLabelText(label).value).toBe(origin);
+
+      fireEvent.change(queryByLabelText(label), { target: { value } });
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  it('Login 버튼을 클릭하면 로그인하는 dispatch함수가 실행된다.', () => {
+    const { queryAllByText } = render(<LoginFormContainer />);
+
+    fireEvent.click(queryAllByText('Log In')[1]);
+
+    expect(dispatch).toBeCalled();
+  });
+});
