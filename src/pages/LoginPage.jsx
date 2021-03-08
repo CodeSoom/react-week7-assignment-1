576f03e432f4975fb5aa654a7b4d991545b4dc1a
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-08 22:51:01 +0900

    :construction: Green-Working: LoginPage
    - email, password input 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,14 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
+      <label htmlFor="email">
+        email
+      </label>
+      <input type="email" id="email" />
+      <label htmlFor="password">
+        password
+      </label>
+      <input type="password" id="password" />
     </div>
   );
 }
