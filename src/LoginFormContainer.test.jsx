ae95b159966807d69bdc0bbb851dc8a4a0a5b4f5
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-08 21:16:54 +0900

    Red) LoginFormContainer.test.jsx: 테스트 케이스 작성

---
+++ src/LoginFormContainer.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import LoginFormContainer from './LoginFormContainer';
+
+describe('LoginFormContainer', () => {
+  it('renders input controls', () => {
+    const { getByLabelText } = render((
+      <LoginFormContainer />
+    ));
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+});
