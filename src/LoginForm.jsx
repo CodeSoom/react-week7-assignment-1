1b6599408a315de0e284538bb5391a63dc489ba3
datalater <the7mincheol@gmail.com>
2021-03-08 19:17:22 +0900

    LoginForm에서 input change 이벤트를 리스닝하도록 구현하라

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-export default function LoginForm({ onClick }) {
+export default function LoginForm({ onChange, onClick }) {
   return (
     <>
       <div>
         <label htmlFor="login-email">Email</label>
-        <input type="email" id="login-email" />
+        <input
+          type="email"
+          id="login-email"
+          onChange={onChange}
+        />
       </div>
       <div>
         <label htmlFor="login-password">Password</label>
-        <input type="password" id="login-password" />
+        <input
+          type="password"
+          id="login-password"
+          onChange={onChange}
+        />
       </div>
       <button
         type="button"
