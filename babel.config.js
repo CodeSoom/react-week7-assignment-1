a8824bddcd636533c84df81206a732197e6c2474
dodoongtak <themighty.tak@gmail.com>
2021-03-08 19:42:07 +0900

    🔧 configure: set alias for actions

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = {
         alias: {
           '@fixtures': './fixtures',
           '@utils': './src/utils',
+          '@actions': './src/redux/actions',
         },
       },
     ],
