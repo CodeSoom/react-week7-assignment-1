9eaf5e35a80788c908d409c10fff03e13e1ae98e
datalater <the7mincheol@gmail.com>
2021-03-08 19:04:01 +0900

    로그인 폼에 로그인 버튼이 나오는지 테스트하라

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
+jest.mock('react-redux');
+
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplemenetation(() => dispatch);
+  });
+
   it('renders login form', () => {
-    const { getByLabelText } = render(<LoginFormContainer />);
+    const { getByLabelText, getByText } = render(<LoginFormContainer />);
 
     expect(getByLabelText('Email')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
