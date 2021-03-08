34e25115a60b460cccf160756585590ccf573228
datalater <the7mincheol@gmail.com>
2021-03-08 19:16:10 +0900

    LoginForm에서 input change 이벤트를 리스닝하는지 테스트하라

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -5,14 +5,25 @@ import { render, fireEvent } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  it('renders login form', () => {
-    const handleClick = jest.fn();
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-    const { getByLabelText, getByText } = render((
+  beforeEach(() => {
+    handleChange.mockClear();
+    handleClick.mockClear();
+  });
+
+  function renderLoginForm() {
+    return render((
       <LoginForm
+        onChange={handleChange}
         onClick={handleClick}
       />
     ));
+  }
+
+  it('renders login form', () => {
+    const { getByLabelText, getByText } = renderLoginForm();
 
     expect(getByLabelText('Email')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
@@ -20,5 +31,15 @@ describe('LoginForm', () => {
     fireEvent.click(getByText('Log In'));
 
     expect(handleClick).toBeCalled();
+  });
+
+  it('listens to input change events', () => {
+    const { getByLabelText } = renderLoginForm();
+
+    fireEvent.change(getByLabelText('Email'), {
+      target: { value: 'test@test.com' },
+    });
+
+    expect(handleChange).toBeCalled();
   });
 });
