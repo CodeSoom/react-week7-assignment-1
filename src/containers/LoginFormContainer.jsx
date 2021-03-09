21eca475d5ac0f2944e947a0e5868ec519975b0e
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 12:06:43 +0900

    :construction: Green-Working:LoginFormContainer
    - dispatch changeLoginFields

--- src/containers/LoginFormContainer.jsx
+++ src/containers/LoginFormContainer.jsx
@@ -4,11 +4,13 @@ import { useDispatch } from 'react-redux';
 
 import LoginForm from '@components/LoginForm';
 
+import { changeLoginFields } from 'src/actions';
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   function handleChange({ target: { name, value } }) {
-    dispatch();
+    dispatch(changeLoginFields({ name, value }));
   }
   return (
     <LoginForm onChange={handleChange} />
