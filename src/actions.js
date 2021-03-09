c1e0f900184382c9b7b9cc6a1c69ab0b7aaee26a
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-09 19:47:21 +0900

    Red) test: LoginForm 테스트 추가

--- src/actions.js
+++ src/actions.js
@@ -85,3 +85,8 @@ export function loadRestaurant({ restaurantId }) {
     dispatch(setRestaurant(restaurant));
   };
 }
+
+export function requestLogin() {
+  return async (dispatch, getState) => {
+  };
+}
