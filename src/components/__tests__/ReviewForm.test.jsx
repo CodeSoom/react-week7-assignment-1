04cf62b1dfb5c4073e5e69b4ac4c0e66cd4f3c79
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 23:23:14 +0900

    :white_check_mark: Red-Test: ReviewForm
    - onSubmit 을 form 에 적용

--- src/components/__tests__/ReviewForm.test.jsx
+++ src/components/__tests__/ReviewForm.test.jsx
@@ -6,9 +6,10 @@ import ReviewForm from '@components/ReviewForm';
 
 describe('ReviewForm', () => {
   const onChange = jest.fn();
+  const onSubmit = jest.fn();
 
-  it('renders input fields and button', () => {
-    const { getByLabelText, getByRole } = render(<ReviewForm onChange={onChange} />);
+  it('renders input fields', () => {
+    const { getByLabelText } = render(<ReviewForm onChange={onChange} />);
 
     const scoreInput = getByLabelText('평점');
     const reviewInput = getByLabelText('리뷰');
@@ -22,7 +23,19 @@ describe('ReviewForm', () => {
     fireEvent.change(scoreInput, { target: { value: '3' } });
     fireEvent.change(reviewInput, { target: { value: '그만큼 맜있다는 거지' } });
     expect(onChange).toHaveBeenCalledTimes(2);
+  });
 
-    expect(getByRole('button', { name: '리뷰 남기기' })).toBeInTheDocument();
+  it('renders button ', () => {
+    onSubmit.mockImplementationOnce((event) => {
+      event.preventDefault();
+    });
+    const { getByRole } = render(<ReviewForm onSubmit={onSubmit} />);
+
+    const postReviewButton = getByRole('button', { name: '리뷰 남기기' });
+
+    expect(postReviewButton).toBeInTheDocument();
+
+    fireEvent.click(postReviewButton);
+    expect(onSubmit).toHaveBeenCalled();
   });
 });
