02f45d4c9501fe88e2c5520decbcb472acefe3c4
canary4651 <canary4651@gmail.com>
2021-03-09 09:23:39 +0900

    🔴 Create LoginPage test

---
+++ src/LoginPage.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  const renderLoginPage = () => render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>,
+  );
+
+  it('renders title', () => {
+    const { container } = renderLoginPage();
+
+    expect(container).toHaveTextContent('Login');
+  });
+
+  it('renders input controls', () => {
+    const { getByLabelText } = renderLoginPage();
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+});
