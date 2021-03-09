e640f8341add6697951f2448004f70b6ec80bc11
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-09 09:56:40 +0900

    화면 레이아웃 수정

--- src/pages/LoginPage/LoginForm.jsx
+++ src/pages/LoginPage/LoginForm.jsx
@@ -10,23 +10,27 @@ export default function LoginForm({ userLoginInputs, onChange, onSubmit }) {
 
   return (
     <>
-      <label htmlFor="login-email">E-mail</label>
-      <input
-        value={email}
-        type="email"
-        id="login-email"
-        name="email"
-        onChange={handleChange}
-      />
-      <label htmlFor="login-password">Password</label>
-      <input
-        value={password}
-        type="password"
-        id="login-password"
-        name="password"
-        onChange={handleChange}
-      />
-      <button type="button" onClick={onSubmit}>Log In</button>
+      <div>
+        <label htmlFor="login-email">E-mail</label>
+        <input
+          value={email}
+          type="email"
+          id="login-email"
+          name="email"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">Password</label>
+        <input
+          value={password}
+          type="password"
+          id="login-password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button type="button" onClick={onSubmit}>Log In</button>
+      </div>
     </>
   );
 }
