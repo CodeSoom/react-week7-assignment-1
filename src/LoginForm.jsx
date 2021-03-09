c7a176dc3be30c8f513696e1a8d605eaf1d57c4c
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-09 21:17:59 +0900

    LoginForm을 구현하여 elements들을 구성합니다.

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,6 +1,14 @@
-export default function LoginForm(){
-    return (
-        <>
-        </>
-    )
+import React from 'react';
+
+export default function LoginForm() {
+  return (
+    <>
+      <h1>Log in</h1>
+      <label htmlFor="login-email">E-mail</label>
+      <input type="email" id="login-email" />
+
+      <label htmlFor="login-password">Password</label>
+      <input type="password" id="login-password" />
+    </>
+  );
 }
