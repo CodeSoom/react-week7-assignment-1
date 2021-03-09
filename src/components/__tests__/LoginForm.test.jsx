a57f741b68589a2fd38d703c298134a7c5c389a1
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 13:20:14 +0900

    :white_check_mark: Red-Test: LoginForm
    - LoginForm input value 확인

--- src/components/__tests__/LoginForm.test.jsx
+++ src/components/__tests__/LoginForm.test.jsx
@@ -5,8 +5,10 @@ import { render } from '@testing-library/react';
 import LoginForm from '@components/LoginForm';
 
 describe('LoginForm', () => {
+  const loginFields = { email: 'tester@example.com', password: 'test' };
+
   it('renders input fields', () => {
-    const { getByLabelText } = render(<LoginForm />);
+    const { getByLabelText } = render(<LoginForm loginFields={loginFields} />);
 
     const emailInput = getByLabelText('email');
     const passwordInput = getByLabelText('password');
@@ -19,10 +21,13 @@ describe('LoginForm', () => {
 
     expect(emailInput).toHaveAttribute('type', 'email');
     expect(passwordInput).toHaveAttribute('type', 'password');
+
+    expect(emailInput.value).toBe(loginFields.email);
+    expect(passwordInput.value).toBe(loginFields.password);
   });
 
   it('renders "Log In" button', () => {
-    const { getByRole } = render(<LoginForm />);
+    const { getByRole } = render(<LoginForm loginFields={loginFields} />);
 
     expect(getByRole('button', { name: 'Log In' })).toBeInTheDocument();
   });
