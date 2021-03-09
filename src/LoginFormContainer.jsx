72e96602817672fb741560daf2f07158f5cf5606
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-09 20:51:38 +0900

    LoginFormContainer 구현

---
+++ src/LoginFormContainer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import LoginForm from './LoginForm'
+
+import {
+  requestLogin,
+  changeLoginField,
+} from './actions';
+
+import { get } from './utils';
+
+export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+  
+  const { email, password } = useSelector(get('loginFields'));
+
+  function handleSubmit() {
+    dispatch(requestLogin());
+  }
+  function handleChange({ name, value }) {
+    dispatch(changeLoginField({ name, value }));
+  }
+
+  return (
+    <LoginForm 
+      fields={ email, password }
+      onChange={handleChange}  
+      onSubmit={handleSubmit}
+    />
+  );
+}
