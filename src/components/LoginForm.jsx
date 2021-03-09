9bcb0e65fa35c46ca718a8457491c19c220912e1
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 10:16:24 +0900

    :construction: Green-Working: LoginForm
    - email, password input

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function LoginForm() {
+  return (
+    <form>
+      <label htmlFor="email">
+        email
+      </label>
+      <input type="email" id="email" />
+      <label htmlFor="password">
+        password
+      </label>
+      <input type="password" id="password" />
+    </form>
+  );
+}
