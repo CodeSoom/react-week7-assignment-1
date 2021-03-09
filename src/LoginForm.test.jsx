196b33858e6235ea69774ffb3e8106b20c39d9dc
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-09 21:22:27 +0900

    LoginForm을 테스트하여 버튼을 확인합니다.

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -5,11 +5,14 @@ import { render } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  it('제목, 이메일 입력창, 암호 입력창을 보여줍니다.', () => {
-    const { getByText, queryByLabelText } = render(<LoginForm />);
+  it('제목, 이메일 입력창, 암호 입력창, 버튼을 보여줍니다.', () => {
+    const { queryByText, queryByLabelText } = render(<LoginForm />);
 
-    expect(getByText(/Log in/)).not.toBeNull();
+    expect(queryByText(/Log in/)).not.toBeNull();
+
     expect(queryByLabelText(/E-mail/)).not.toBeNull();
     expect(queryByLabelText(/Password/)).not.toBeNull();
+
+    expect(queryByText(/Login/)).not.toBeNull();
   });
 });
