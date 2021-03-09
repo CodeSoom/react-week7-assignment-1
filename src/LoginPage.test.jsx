094ac4b36da4fe651bfeac8342628213ea3b3054
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-09 16:35:56 +0900

    LoginPage에 로그인 폼 추가

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -5,10 +5,15 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
-  it('renders title', () => {
+  it('renders log-in title', () => {
     const { container } = render(<LoginPage />);
 
     expect(container).toHaveTextContent('Log In');
+  });
+
+  it('renders log-in form', () => {
+    const { container } = render(<LoginPage />);
+
     expect(container).toHaveTextContent('E-mail');
     expect(container).toHaveTextContent('Password');
   });
