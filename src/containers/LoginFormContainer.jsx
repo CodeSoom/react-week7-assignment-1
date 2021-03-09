4aa0acc981b7a7fa0ea15e5ebbdb5ff76b4460af
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 16:19:11 +0900

    :construction: Green-Working: LoginFormContainer
    - accessToken 있을 때 로그아웃 버튼 출력

--- src/containers/LoginFormContainer.jsx
+++ src/containers/LoginFormContainer.jsx
@@ -8,8 +8,16 @@ import { changeLoginFields, postLoginFields } from 'src/actions';
 
 import { get } from 'src/utils';
 
+import LogoutContainer from './LogoutContainer';
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
+
+  const accessToken = useSelector(get('accessToken'));
+
+  if (accessToken) {
+    return <LogoutContainer />;
+  }
 
   const loginFields = useSelector(get('loginFields'));
 
