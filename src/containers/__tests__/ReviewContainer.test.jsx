18e26f77c7e39dd4235a1bfa7d7315132c5ab9a2
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 23:07:38 +0900

    :white_check_mark: Red-Test: ReviewContainer
    - 리뷰 input test

---
+++ src/containers/__tests__/ReviewContainer.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
+
+import ReviewContainer from '@containers/ReviewContainer';
+
+describe('ReviewContainer', () => {
+  const dispatch = jest.fn();
+
+  context('when logged in ', () => {
+    beforeEach(() => {
+      dispatch.mockClear();
+      useDispatch.mockImplementation(() => dispatch);
+    });
+
+    it('changes input fields value', () => {
+      const { getByLabelText } = render(<ReviewContainer />);
+
+      const scoreInput = getByLabelText('평점');
+      const reviewInput = getByLabelText('리뷰');
+
+      fireEvent.change(scoreInput, { target: { value: '3' } });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'changeReviewFields',
+        payload: { name: 'score', value: '3' },
+      });
+
+      fireEvent.change(reviewInput, { target: { value: '그만큼 맜있다는 거지' } });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'changeReviewFields',
+        payload: { name: 'description', value: '그만큼 맜있다는 거지' },
+      });
+    });
+  });
+});
