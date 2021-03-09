aa45e1695748843ac1b913bd40a9889d38249d99
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-09 20:09:19 +0900

    ReviewForm test 통과

---
+++ src/pages/RestaurantPage/ReviewForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function ReviewForm({
+  userReviewInputs,
+  onChange,
+  onSubmit,
+}) {
+  const { score, description } = userReviewInputs;
+
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="review-score">평점</label>
+        <input
+          value={score}
+          type="number"
+          id="review-score"
+          name="score"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="review-description">리뷰 내용</label>
+        <input
+          value={description}
+          type="text"
+          id="review-description"
+          name="description"
+          onChange={handleChange}
+        />
+        <button type="button" onClick={onSubmit}>리뷰 남기기</button>
+      </div>
+    </>
+  );
+}
