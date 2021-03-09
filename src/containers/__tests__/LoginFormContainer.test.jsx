c47c44213c509a9a37bfa8edb13469b171b1edbf
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 13:26:42 +0900

    :white_check_mark: Red-Test: LoginFormContainer
    - useSelector 로 email,password 가져오는지 확인

--- src/containers/__tests__/LoginFormContainer.test.jsx
+++ src/containers/__tests__/LoginFormContainer.test.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 import { fireEvent, render } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import given from 'given2';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 import LoginFormContainer from '@containers/LoginFormContainer';
 
 describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
+  given('loginFields', () => ({ email: '123@naver.com', password: '5678' }));
 
   beforeEach(() => {
     dispatch.mockClear();
     useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({ loginFields: given.loginFields }));
   });
   it('change input fields value', () => {
     const { getByLabelText } = render(
@@ -21,12 +25,14 @@ describe('LoginFormContainer', () => {
     const emailInput = getByLabelText('email');
     const passwordInput = getByLabelText('password');
 
+    expect(emailInput.value).toBe('123@naver.com');
     fireEvent.change(emailInput, { target: { value: 'tester@example.com' } });
     expect(dispatch).toHaveBeenCalledWith({
       type: 'changeLoginFields',
       payload: { name: 'email', value: 'tester@example.com' },
     });
 
+    expect(passwordInput.value).toBe('5678');
     fireEvent.change(passwordInput, { target: { value: 'test' } });
     expect(dispatch).toHaveBeenCalledWith({
       type: 'changeLoginFields',
