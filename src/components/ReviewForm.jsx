4e864f19f5f0f0451ed645d3ee98a84f18c20fb8
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 23:06:13 +0900

    :construction: Green-Working: ReviewForm
    - input name 수정

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -4,10 +4,18 @@ export default function ReviewForm() {
   return (
     <form>
       <label htmlFor="score">평점</label>
-      <input type="number" id="score" />
+      <input
+        type="number"
+        id="score"
+        name="score"
+      />
 
       <label htmlFor="review">리뷰</label>
-      <input type="text" id="review" />
+      <input
+        type="text"
+        id="review"
+        name="description"
+      />
 
       <button type="submit">리뷰 남기기</button>
     </form>
