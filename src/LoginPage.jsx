a3ef9c9c225cc60aad3e92d116416e3bf5eac3df
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-09 20:53:17 +0900

    LoginPage LoginFormContainer로 컴포넌트 분리

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
-import { requestLogin } from './actions';
+import LoginFormContainer from './LoginFormContainer';
 
 export default function LoginPage() {
-  const dispatch = useDispatch();
-
-  function handleClick() {
-    dispatch(requestLogin());
-  }
   return (
     <div>
       <h2>Log In</h2>
-      <div>
-        <label htmlFor="login-email">E-mail</label>
-        <input type="email" id="login-email"></input>
-      </div>
-      <div>
-        <label htmlFor="login-password">Password</label>
-        <input type="password" id="login-password"></input>
-      </div>
-      <button type="button" onClick={handleClick}>Log-In</button>
+      <LoginFormContainer />
     </div>
   );
 }
