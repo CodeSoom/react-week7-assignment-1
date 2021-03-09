ddcdb182127cd87e7b7dd6a3f4b0838a6f2bd9dc
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 11:44:31 +0900

    :construction: Green-Working: reducer
    - changeLoginFields

--- src/reducer.js
+++ src/reducer.js
@@ -7,6 +7,7 @@ const initialState = {
   restaurant: null,
   selectedRegion: null,
   selectedCategory: null,
+  loginFields: { email: '', password: '' },
 };
 
 const reducers = {
@@ -51,6 +52,13 @@ const reducers = {
     return {
       ...state,
       selectedCategory: categories.find(equal('id', categoryId)),
+    };
+  },
+
+  changeLoginFields(state, { payload: { name, value } }) {
+    return {
+      ...state,
+      loginFields: { [name]: value },
     };
   },
 };
