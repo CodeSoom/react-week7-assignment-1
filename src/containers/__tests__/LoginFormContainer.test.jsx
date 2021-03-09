da1f062b4828ef1ee16a1c68af668c60aed7f08e
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 16:18:41 +0900

    :white_check_mark: Red-Test: LoginFormContainer
    - with accessToken without accessToken 분리

--- src/containers/__tests__/LoginFormContainer.test.jsx
+++ src/containers/__tests__/LoginFormContainer.test.jsx
@@ -11,43 +11,70 @@ import LoginFormContainer from '@containers/LoginFormContainer';
 describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
   given('loginFields', () => ({ email: '123@naver.com', password: '5678' }));
+  given('accessToken', () => null);
 
-  beforeEach(() => {
-    dispatch.mockClear();
-    useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) => selector({ loginFields: given.loginFields }));
-  });
-
-  it('change input fields value', () => {
-    const { getByLabelText } = render(
-      <LoginFormContainer />,
-    );
-
-    const emailInput = getByLabelText('email');
-    const passwordInput = getByLabelText('password');
-
-    expect(emailInput.value).toBe('123@naver.com');
-    fireEvent.change(emailInput, { target: { value: 'tester@example.com' } });
-    expect(dispatch).toHaveBeenCalledWith({
-      type: 'changeLoginFields',
-      payload: { name: 'email', value: 'tester@example.com' },
+  context('without accessToken', () => {
+    beforeEach(() => {
+      dispatch.mockClear();
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        loginFields: given.loginFields,
+        accessToken: given.accessToken,
+      }));
     });
 
-    expect(passwordInput.value).toBe('5678');
-    fireEvent.change(passwordInput, { target: { value: 'test' } });
-    expect(dispatch).toHaveBeenCalledWith({
-      type: 'changeLoginFields',
-      payload: { name: 'password', value: 'test' },
+    it('change input fields value', () => {
+      const { getByLabelText } = render(
+        <LoginFormContainer />,
+      );
+
+      const emailInput = getByLabelText('email');
+      const passwordInput = getByLabelText('password');
+
+      expect(emailInput.value).toBe('123@naver.com');
+      fireEvent.change(emailInput, { target: { value: 'tester@example.com' } });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'changeLoginFields',
+        payload: { name: 'email', value: 'tester@example.com' },
+      });
+
+      expect(passwordInput.value).toBe('5678');
+      fireEvent.change(passwordInput, { target: { value: 'test' } });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'changeLoginFields',
+        payload: { name: 'password', value: 'test' },
+      });
+    });
+
+    it('submit input fields values', () => {
+      const { getByRole } = render(
+        <LoginFormContainer />,
+      );
+
+      fireEvent.click(getByRole('button', { name: 'Log In' }));
+
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 
-  it('submit input fields values', () => {
-    const { getByRole } = render(
-      <LoginFormContainer />,
-    );
+  context('with accessToken', () => {
+    given('accessToken', () => '12346578');
 
-    fireEvent.click(getByRole('button', { name: 'Log In' }));
+    beforeEach(() => {
+      dispatch.mockClear();
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        loginFields: given.loginFields,
+        accessToken: given.accessToken,
+      }));
+    });
 
-    expect(dispatch).toHaveBeenCalled();
+    it('renders "Log out" button ', () => {
+      const { getByRole } = render(
+        <LoginFormContainer />,
+      );
+
+      expect(getByRole('button', { name: 'Log out' })).toBeInTheDocument();
+    });
   });
 });
