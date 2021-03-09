fe513768e8e0a81553b95202a594f1089e77a701
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-09 23:11:31 +0900

    :white_check_mark: Red-Test: ReviewForm
    - onChange test 추가

--- src/components/__tests__/ReviewForm.test.jsx
+++ src/components/__tests__/ReviewForm.test.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import ReviewForm from '@components/ReviewForm';
 
 describe('ReviewForm', () => {
+  const onChange = jest.fn();
+
   it('renders input fields and button', () => {
-    const { getByLabelText, getByRole } = render(<ReviewForm />);
+    const { getByLabelText, getByRole } = render(<ReviewForm onChange={onChange} />);
 
     const scoreInput = getByLabelText('평점');
     const reviewInput = getByLabelText('리뷰');
@@ -16,6 +18,10 @@ describe('ReviewForm', () => {
 
     expect(scoreInput).toHaveAttribute('type', 'number');
     expect(reviewInput).toHaveAttribute('type', 'text');
+
+    fireEvent.change(scoreInput, { target: { value: '3' } });
+    fireEvent.change(reviewInput, { target: { value: '그만큼 맜있다는 거지' } });
+    expect(onChange).toHaveBeenCalledTimes(2);
 
     expect(getByRole('button', { name: '리뷰 남기기' })).toBeInTheDocument();
   });
