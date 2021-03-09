a608c525ece7764c6abf7c53f1f1a4826ad46775
canary4651 <canary4651@gmail.com>
2021-03-09 09:28:08 +0900

    🟢 Create LoginPage component

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 
 export default function LoginPage() {
   return (
-    <div />
+    <div>
+      <h1>Login</h1>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="login-email"
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input
+          type="password"
+          id="login-password"
+        />
+      </div>
+    </div>
   );
 }
