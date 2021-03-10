2e86ea008d2afebf6c79151dec09aceaab01df2f
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-10 18:18:06 +0900

    LoginForm을 구현하여 email input에 대한 handelChange를 추가합니다.

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
-export default function LoginForm() {
+export default function LoginForm({ onChange }) {
+  function handleChange(event) {
+    const { target: { value } } = event;
+    onChange({ value });
+  }
+
   return (
     <>
       <h1>Log in</h1>
       <label htmlFor="login-email">E-mail</label>
-      <input type="email" id="login-email" />
+      <input
+        type="email"
+        id="login-email"
+        onChange={handleChange}
+      />
 
       <label htmlFor="login-password">Password</label>
-      <input type="password" id="login-password" />
+      <input
+        type="password"
+        id="login-password"
+      />
 
       <button type="button">Login</button>
     </>
