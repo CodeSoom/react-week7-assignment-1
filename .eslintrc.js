ce695fda8ac6b496124d3c4fe1606710c7acfeb9
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-10 14:34:48 +0900

    :wrench: Config: eslint 원래상태로 복구

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,16 +41,7 @@ module.exports = {
     'keyword-spacing': ['error', { before: true, after: true }],
     'comma-spacing': ['error', { before: false, after: true }],
     'comma-style': ['error', 'last'],
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
-      },
-    ],
+    'comma-dangle': ['error', 'always-multiline'],
     'space-in-parens': ['error', 'never'],
     'block-spacing': 'error',
     'array-bracket-spacing': ['error', 'never'],
