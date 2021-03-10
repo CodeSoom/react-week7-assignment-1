4b1e826ea64bad3b0c8358467549c57d7c14519f
yoonhe <yhehe228@gmail.com>
2021-03-10 19:37:11 +0900

    [green] LoginContainer를 구현합니다

--- src/LoginContainer.jsx
+++ src/LoginContainer.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import LoginForm from './LoginForm';
 
 import { changeLoginFields } from './actions';
+import { get } from './utils';
 
 export default function LoginContainer() {
   const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
 
   function handleChange({ value, name }) {
     dispatch(changeLoginFields({ value, name }));
   }
 
   return (
-    <LoginForm onChange={handleChange} />
+    <LoginForm onChange={handleChange} email={email} password={password} />
   );
 }
