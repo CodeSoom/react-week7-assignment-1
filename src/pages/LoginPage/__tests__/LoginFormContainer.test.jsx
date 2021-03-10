6e853db62bdb94f4e916377050082e3a69cbd741
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-10 19:48:48 +0900

    accessToken이 있을 경우 logout버튼을 보여준다는 test추가

--- src/pages/LoginPage/__tests__/LoginFormContainer.test.jsx
+++ src/pages/LoginPage/__tests__/LoginFormContainer.test.jsx
@@ -16,31 +16,44 @@ describe('LoginFormContainer', () => {
 
     useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
+      accessToken: given.accessToken,
       userLoginInputs: { email: 'test@naver', password: 'te' },
     }));
   });
 
-  it('input을 입력하면 변경된 입력을 반영하는 dispatch함수가 실행된다.', () => {
-    const { queryByLabelText } = render(<LoginFormContainer />);
-    const controls = [
-      { label: 'E-mail', origin: 'test@naver', value: 'test@naver.com' },
-      { label: 'Password', origin: 'te', value: 'test' },
-    ];
+  context('accessToken이 없을 때', () => {
+    given('accessToken', () => '');
+    it('input을 입력하면 변경된 입력을 반영하는 dispatch함수가 실행된다.', () => {
+      const { queryByLabelText } = render(<LoginFormContainer />);
+      const controls = [
+        { label: 'E-mail', origin: 'test@naver', value: 'test@naver.com' },
+        { label: 'Password', origin: 'te', value: 'test' },
+      ];
 
-    controls.forEach(({ label, origin, value }) => {
-      expect(queryByLabelText(label).value).toBe(origin);
+      controls.forEach(({ label, origin, value }) => {
+        expect(queryByLabelText(label).value).toBe(origin);
 
-      fireEvent.change(queryByLabelText(label), { target: { value } });
+        fireEvent.change(queryByLabelText(label), { target: { value } });
+
+        expect(dispatch).toBeCalled();
+      });
+    });
+
+    it('Login 버튼을 클릭하면 로그인하는 dispatch함수가 실행된다.', () => {
+      const { queryByText } = render(<LoginFormContainer />);
+
+      fireEvent.click(queryByText('Log In'));
 
       expect(dispatch).toBeCalled();
     });
   });
 
-  it('Login 버튼을 클릭하면 로그인하는 dispatch함수가 실행된다.', () => {
-    const { queryByText } = render(<LoginFormContainer />);
+  context('accessToken이 있을 때', () => {
+    given('accessToken', () => 'ACCESS_TOKEN');
+    it('로그아웃버튼을 보여준다.', () => {
+      const { queryByText } = render(<LoginFormContainer />);
 
-    fireEvent.click(queryByText('Log In'));
-
-    expect(dispatch).toBeCalled();
+      expect(queryByText('Log out')).toBeInTheDocument();
+    });
   });
 });
