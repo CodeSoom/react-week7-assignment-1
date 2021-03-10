886430220f8b6673c0d444931b8e61d2cc3dc979
yoonhe <yhehe228@gmail.com>
2021-03-10 15:58:55 +0900

    [red] reducers에 대한 테스트를 추가합니다 changeLoginFields reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,6 +7,7 @@ import {
   setRestaurant,
   selectRegion,
   selectCategory,
+  changeLoginFields,
 } from './actions';
 
 describe('reducer', () => {
@@ -18,6 +19,10 @@ describe('reducer', () => {
       restaurant: null,
       selectedRegion: null,
       selectedCategory: null,
+      loginFields: {
+        email: '',
+        password: '',
+      },
     };
 
     it('returns initialState', () => {
@@ -123,6 +128,25 @@ describe('reducer', () => {
         id: 1,
         name: '한식',
       });
+    });
+  });
+
+  describe('changeLoginFields', () => {
+    it('changes email fields', () => {
+      const initialState = {
+        loginFields: {
+          email: '',
+          password: '',
+        },
+      };
+
+      const state = reducer(initialState, changeLoginFields({
+        name: 'email',
+        value: 'test@test.com',
+      }));
+
+      expect(state.loginFields.email).toEqual('test@test.com');
+      expect(state.loginFields.password).toEqual('');
     });
   });
 });
