1c0545aad9c1d3a25146699b858a0c7aaa9431c9
yoonhe <yhehe228@gmail.com>
2021-03-10 19:42:00 +0900

    [refactoring] LoginPage를 리팩토링합니다

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
+
 describe('LoginPage', () => {
+  useSelector.mockImplementation((selector) => selector({
+    loginFields: {
+      email: 'test@test.com',
+      password: '1234',
+    },
+  }));
+
   function renderLoginPage() {
     return render(<LoginPage />);
   }
