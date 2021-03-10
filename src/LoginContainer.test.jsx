40f794bdc12be7d9f686497d9cf40029af2d2208
yoonhe <yhehe228@gmail.com>
2021-03-10 13:41:01 +0900

    [red] LoginContainer에 대한 테스트를 추가합니다 changeLoginFields action을 dispatch 하는지 테스트합니다.

--- src/LoginContainer.test.jsx
+++ src/LoginContainer.test.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
 
 import LoginContainer from './LoginContainer';
 
+jest.mock('react-redux');
+
 describe('LoginContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders email input', () => {
     const { queryByLabelText } = render(<LoginContainer />);
 
@@ -20,5 +29,23 @@ describe('LoginContainer', () => {
     const { queryByText } = render(<LoginContainer />);
 
     expect(queryByText('Log In')).not.toBeNull();
+  });
+
+  it('listens email input change event', () => {
+    const { queryByLabelText } = render(<LoginContainer />);
+
+    fireEvent.change(queryByLabelText('E-mail'), {
+      target: {
+        value: 'test@email.com',
+      },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeLoginFields',
+      payload: {
+        value: 'test@email.com',
+        name: 'email',
+      },
+    });
   });
 });
