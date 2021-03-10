39205d3f689448edc24aaaf352545fcd5d4d8c85
yoonhe <yhehe228@gmail.com>
2021-03-10 10:15:46 +0900

    [refactoring] LoginPage를 리팩토링합니다

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -4,20 +4,23 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
+  function renderLoginPage() {
+    return render(<LoginPage />);
+  }
   it('renders "Login" title', () => {
-    const { queryByText } = render(<LoginPage />);
+    const { queryByText } = renderLoginPage();
 
     expect(queryByText('Log In'));
   });
 
   it('renders email input', () => {
-    const { queryByLabelText } = render(<LoginPage />);
+    const { queryByLabelText } = renderLoginPage();
 
     expect(queryByLabelText('E-mail')).not.toBeNull();
   });
 
   it('renders password input', () => {
-    const { queryByLabelText } = render(<LoginPage />);
+    const { queryByLabelText } = renderLoginPage();
 
     expect(queryByLabelText('Password')).not.toBeNull();
   });
