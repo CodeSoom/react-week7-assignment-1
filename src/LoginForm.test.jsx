dd77fb60b1f3ffcee2b00f31f1e084657d4a3151
yoonhe <yhehe228@gmail.com>
2021-03-10 13:28:26 +0900

    [red] LoginForm에 대한 테스트를 추가합니다다

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 jest.mock('react-redux');
 
 describe('LoginForm', () => {
+  const handleChange = jest.fn();
+
+  function renderLoginForm() {
+    return render(<LoginForm onChange={handleChange} />);
+  }
+
   it('renders email input', () => {
-    const { queryByLabelText } = render(<LoginForm />);
+    const { queryByLabelText } = renderLoginForm();
 
     expect(queryByLabelText('E-mail')).not.toBeNull();
   });
 
   it('renders password input', () => {
-    const { queryByLabelText } = render(<LoginForm />);
+    const { queryByLabelText } = renderLoginForm();
 
     expect(queryByLabelText('Password')).not.toBeNull();
   });
 
   it('renders "Log In" button', () => {
-    const { queryByText } = render(<LoginForm />);
+    const { queryByText } = renderLoginForm();
 
     expect(queryByText('Log In')).not.toBeNull();
+  });
+
+  it('listens email input change event', () => {
+    const { queryByLabelText } = renderLoginForm();
+
+    fireEvent.change(queryByLabelText('E-mail'), {
+      target: {
+        value: 'test@email.com',
+      },
+    });
+
+    expect(handleChange).toBeCalled();
   });
 });
