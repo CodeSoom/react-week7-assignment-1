92e74e205aec6bf92170271ef618719da5cc260e
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-10 21:00:54 +0900

    LoginFormContainer를 구현하여 테스트를 통과합니다.

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import LoginForm from './LoginForm';
+
+import { get } from './utils';
 
 export default function LoginFormContainer() {
+  const disptach = useDispatch();
+
+  const loginInputs = useSelector(get('loginInputs'));
+
+  function handleChange() {
+    disptach();
+  }
+
+  function handleClick() {
+    disptach();
+  }
+
   return (
-    <>
-    </>
+    <LoginForm
+      loginInputs={loginInputs}
+      onChange={handleChange}
+      onClick={handleClick}
+    />
   );
 }
