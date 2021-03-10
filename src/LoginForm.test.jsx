61b901bf9ac17ad2c5eb7a1367fba17432033dd7
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-10 20:04:19 +0900

    LoginForm테스트를 리팩토링하여 중복되는 render함수를 하나의 함수 호출로 대체합니다.

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -5,13 +5,29 @@ import { fireEvent, render } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  function renderLoginForm({ loginInputs }) {
+    return render((
+      <LoginForm
+        loginInputs={loginInputs}
+        onChange={handleChange}
+        onClick={handleClick}
+      />));
+  }
+
   it('제목, 이메일 입력창, 암호 입력창, 버튼을 보여줍니다.', () => {
     const initialInputs = {
       email: '',
       password: '',
     };
 
-    const { queryByText, queryByLabelText } = render(<LoginForm loginInputs={initialInputs} />);
+    const { queryByText, queryByLabelText } = renderLoginForm({ loginInputs: initialInputs });
 
     expect(queryByText(/Log in/)).not.toBeNull();
 
@@ -25,18 +41,12 @@ describe('LoginForm', () => {
   });
 
   it('이메일, 암호 입력창에 값을 입력하면 입력값이 업데이트 됩니다.', () => {
-    const handleChange = jest.fn();
-
     const initialInputs = {
       email: 'previousEmail@example.com',
       password: 'previousPassword123',
     };
 
-    const { queryByLabelText } = render((
-      <LoginForm
-        loginInputs={initialInputs}
-        onChange={handleChange}
-      />));
+    const { queryByLabelText } = renderLoginForm({ loginInputs: initialInputs });
 
     expect(queryByLabelText(/E-mail/).value).toBe('previousEmail@example.com');
     expect(queryByLabelText(/Password/).value).toBe('previousPassword123');
@@ -51,12 +61,18 @@ describe('LoginForm', () => {
   });
 
   it('로그인하는 버튼을 눌러 아이디와 비밀번호를 제출합니다.', () => {
-    const handleClick = jest.fn();
+    const initialInputs = {
+      email: '',
+      password: '',
+    };
 
-    const { queryByText } = render((<LoginForm onClick={handleClick} />));
+    const { getByText, getByLabelText } = renderLoginForm({ loginInputs: initialInputs });
 
-    fireEvent.click(queryByText(/Login/));
+    fireEvent.click(getByText(/Login/));
 
     expect(handleClick).toBeCalled();
+
+    expect(getByLabelText(/E-mail/).value).toBe('');
+    expect(getByLabelText(/Password/).value).toBe('');
   });
 });
