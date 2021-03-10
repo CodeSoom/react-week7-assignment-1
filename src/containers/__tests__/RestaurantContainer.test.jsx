72a0f43b0a0a5e8ee6506cabdf85ecde6000427e
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-10 14:45:14 +0900

    :white_check_mark: Red-Test: RestaurantContainer
    - ReviewForm 적용

--- src/containers/__tests__/RestaurantContainer.test.jsx
+++ src/containers/__tests__/RestaurantContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -13,6 +13,12 @@ describe('RestaurantContainer', () => {
     return render(<RestaurantContainer restaurantId="1" />);
   }
 
+  given('restaurant', () => ({
+    id: 1,
+    name: '마법사주방',
+    address: '서울시 강남구',
+  }));
+
   beforeEach(() => {
     dispatch.mockClear();
     useDispatch.mockImplementation(() => dispatch);
@@ -24,17 +30,10 @@ describe('RestaurantContainer', () => {
 
   it('dispatches action', () => {
     renderRestaurantContainer();
-
     expect(dispatch).toBeCalled();
   });
 
   context('with restaurant', () => {
-    given('restaurant', () => ({
-      id: 1,
-      name: '마법사주방',
-      address: '서울시 강남구',
-    }));
-
     it('renders name and address', () => {
       const { container } = renderRestaurantContainer();
 
@@ -50,6 +49,42 @@ describe('RestaurantContainer', () => {
       const { container } = renderRestaurantContainer();
 
       expect(container).toHaveTextContent('Loading');
+    });
+  });
+
+  context('when logged in ', () => {
+    beforeEach(() => {
+      dispatch.mockClear();
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: given.restaurant,
+      }));
+    });
+
+    it('changes input fields value', () => {
+      const { getByLabelText } = render(<RestaurantContainer />);
+
+      const scoreInput = getByLabelText('평점');
+      const reviewInput = getByLabelText('리뷰');
+
+      fireEvent.change(scoreInput, { target: { value: '3' } });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'changeReviewFields',
+        payload: { name: 'score', value: '3' },
+      });
+
+      fireEvent.change(reviewInput, { target: { value: '그만큼 맜있다는 거지' } });
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'changeReviewFields',
+        payload: { name: 'description', value: '그만큼 맜있다는 거지' },
+      });
+    });
+
+    it('submits input fields values', () => {
+      const { getByRole } = render(<RestaurantContainer />);
+      fireEvent.click(getByRole('button', { name: '리뷰 남기기' }));
+
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 });
