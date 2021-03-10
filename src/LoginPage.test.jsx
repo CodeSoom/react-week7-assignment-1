72a8b01cc0f4d9866460682036aebf5e2324e544
윤해은 <yoon@yunhaeeun-ui-MacBookPro.local>
2021-03-10 10:07:46 +0900

    [red] LoginPage에 대한 테스트를 추가합니다 email, password input에 대한 테스트를 추가합니다.

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -4,9 +4,21 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
-  it('renders Login Page', () => {
+  it('renders "Login" title', () => {
     const { queryByText } = render(<LoginPage />);
 
     expect(queryByText('Log In'));
+  });
+
+  it('renders email input', () => {
+    const { queryByLabelText } = render(<LoginPage />);
+
+    expect(queryByLabelText('E-mail')).not.toBeNull();
+  });
+
+  it('renders password input', () => {
+    const { queryByLabelText } = render(<LoginPage />);
+
+    expect(queryByLabelText('Password')).not.toBeNull();
   });
 });
