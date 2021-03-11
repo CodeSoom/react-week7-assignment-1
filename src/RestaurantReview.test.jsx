6190e3df5695ef4d591759c572d86d481371a586
yoonhe <yhehe228@gmail.com>
2021-03-11 20:10:22 +0900

    [red] RestaurantReview에 대한 테스트를 추가합니다

--- src/RestaurantReview.test.jsx
+++ src/RestaurantReview.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import RestaurantReview from './RestaurantReview';
 
@@ -9,15 +9,53 @@ import REVIEWS from '../fixtures/reviews';
 const { score: SCORE, description: DESCRIPTION } = REVIEWS;
 
 describe('RestaurantReview', () => {
+  const handleChange = jest.fn();
+
   function renderRestaurantReview() {
-    return render(<RestaurantReview score={SCORE} description={DESCRIPTION} />);
+    return render(
+      <RestaurantReview
+        score=""
+        description=""
+        onChange={handleChange}
+      />,
+    );
   }
+
+  beforeEach(() => {
+    handleChange.mockClear();
+  });
 
   it('renders review form', () => {
     const { queryByLabelText, queryByText } = renderRestaurantReview();
 
-    expect(queryByLabelText('평점').value).toBe(SCORE);
-    expect(queryByLabelText('리뷰 내용').value).toBe(DESCRIPTION);
+    expect(queryByLabelText('평점').value).toBe('');
+    expect(queryByLabelText('리뷰 내용').value).toBe('');
     expect(queryByText('리뷰 남기기')).not.toBeNull();
+  });
+
+  it('listens change event', () => {
+    const { queryByLabelText } = renderRestaurantReview();
+
+    fireEvent.change(queryByLabelText('평점'), {
+      target: {
+        value: SCORE,
+      },
+    });
+
+    expect(handleChange).toBeCalledWith({
+      name: 'score',
+      value: SCORE,
+    });
+
+    fireEvent.change(queryByLabelText('리뷰 내용'), {
+      target: {
+        value: DESCRIPTION,
+      },
+    });
+
+    expect(handleChange).toBeCalledWith({
+      name: 'description',
+      value: DESCRIPTION,
+    });
   });
 });
