cb8f12449d3850fa81e6430ee0e438de677f3e68
yoonhe <yhehe228@gmail.com>
2021-03-11 18:51:50 +0900

    [refactoring] LoginContainer를 리팩토링합니다

--- src/LoginContainer.test.jsx
+++ src/LoginContainer.test.jsx
@@ -23,65 +23,63 @@ describe('LoginContainer', () => {
     }));
   });
 
-  it('renders email input', () => {
-    const { queryByLabelText } = render(<LoginContainer />);
+  const { email: EMAIL, password: PASSWORD } = LOGIN_FIELDS;
 
-    expect(queryByLabelText('E-mail')).not.toBeNull();
-  });
+  context('when logged out', () => {
+    it('renders email input', () => {
+      const { queryByLabelText } = render(<LoginContainer />);
 
-  it('renders password input', () => {
-    const { queryByLabelText } = render(<LoginContainer />);
-
-    expect(queryByLabelText('Password')).not.toBeNull();
-  });
-
-  it('listens email input change event', () => {
-    const { getByLabelText } = render(<LoginContainer />);
-
-    fireEvent.change(getByLabelText('E-mail'), {
-      target: {
-        value: LOGIN_FIELDS.email,
-      },
+      expect(queryByLabelText('E-mail')).not.toBeNull();
     });
 
-    expect(dispatch).toBeCalledWith({
-      type: 'changeLoginFields',
-      payload: {
+    it('renders password input', () => {
+      const { queryByLabelText } = render(<LoginContainer />);
+
+      expect(queryByLabelText('Password')).not.toBeNull();
+    });
+
+    it('listens change event', () => {
+      const { getByLabelText } = render(<LoginContainer />);
+
+      const controls = [{
+        label: 'E-mail',
         name: 'email',
-        value: LOGIN_FIELDS.email,
-      },
-    });
-  });
-
-  it('listens password input change event', () => {
-    const { queryByLabelText } = render(<LoginContainer />);
-
-    fireEvent.change(queryByLabelText('Password'), {
-      target: {
-        value: LOGIN_FIELDS.password,
-      },
-    });
-
-    expect(dispatch).toBeCalledWith({
-      type: 'changeLoginFields',
-      payload: {
+        value: EMAIL,
+      }, {
+        label: 'Password',
         name: 'password',
-        value: LOGIN_FIELDS.password,
-      },
+        value: PASSWORD,
+      }];
+
+      controls.forEach(({ lable, name, value }) => {
+        fireEvent.change(getByLabelText(lable), {
+          target: {
+            value,
+          },
+        });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'changeLoginFields',
+          payload: {
+            name,
+            value,
+          },
+        });
+      });
     });
-  });
 
-  it('renders "Log In" button', () => {
-    const { queryByText } = render(<LoginContainer />);
+    it('renders "Log In" button', () => {
+      const { queryByText } = render(<LoginContainer />);
 
-    expect(queryByText('Log In')).not.toBeNull();
-  });
+      expect(queryByText('Log In')).not.toBeNull();
+    });
 
-  it('listens click event', () => {
-    const { getByText } = render(<LoginContainer />);
+    it('listens click event', () => {
+      const { getByText } = render(<LoginContainer />);
 
-    fireEvent.submit(getByText('Log In'));
+      fireEvent.submit(getByText('Log In'));
 
-    expect(dispatch).toBeCalled();
+      expect(dispatch).toBeCalled();
+    });
   });
 });
