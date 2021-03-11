96acd264920268c623547ac2e44e07f27e60fbd5
yoonhe <yhehe228@gmail.com>
2021-03-11 20:34:41 +0900

    [red] RestaurantContainer에 대한 테스트를 추가합니다

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantContainer from './RestaurantContainer';
+
+import REVIEWS from '../fixtures/reviews';
+
+const { score: SCORE, description: DESCRIPTION } = REVIEWS;
 
 describe('RestaurantContainer', () => {
   const dispatch = jest.fn();
@@ -48,6 +52,35 @@ describe('RestaurantContainer', () => {
       expect(queryByLabelText('평점')).not.toBeNull();
       expect(queryByLabelText('리뷰 내용')).not.toBeNull();
       expect(queryByText('리뷰 남기기')).not.toBeNull();
+    });
+
+    it('listens change event', () => {
+      const { queryByLabelText } = renderRestaurantContainer();
+      const controls = [{
+        label: '평점',
+        name: 'score',
+        value: SCORE,
+      }, {
+        label: '리뷰 내용',
+        name: 'description',
+        value: DESCRIPTION,
+      }];
+
+      controls.forEach(({ label, name, value }) => {
+        fireEvent.change(queryByLabelText(label), {
+          target: {
+            value,
+          },
+        });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'changeReviewFields',
+          payload: {
+            name,
+            value,
+          },
+        });
+      });
     });
   });
 
