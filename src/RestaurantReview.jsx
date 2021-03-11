1fc97ab18b5d1eeb0f22a0f52c0181ab88344d4d
yoonhe <yhehe228@gmail.com>
2021-03-11 20:10:59 +0900

    [green] RestaurantReview를 구현합니다

--- src/RestaurantReview.jsx
+++ src/RestaurantReview.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
-export default function RestaurantReview({ score, description }) {
+export default function RestaurantReview({ score, description, onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+
+    onChange({ name, value });
+  }
+
   return (
     <>
       <div>
         <div>
           <label htmlFor="score-input">평점</label>
-          <input type="number" id="score-input" value={score} />
+          <input
+            type="number"
+            id="score-input"
+            name="score"
+            value={score}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="description-input">리뷰 내용</label>
-          <input type="text" id="description-input" value={description} />
+          <input
+            type="text"
+            id="description-input"
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
         </div>
 
         <button type="button">리뷰 남기기</button>
