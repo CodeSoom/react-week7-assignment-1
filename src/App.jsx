705d9e9c0ead3b42c9e0f3c332d42143f74c8710
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-12 15:04:27 +0900

    App test (localStorage관련) 통과

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {
   Link,
 } from 'react-router-dom';
 
+import { useDispatch } from 'react-redux';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
@@ -13,7 +15,19 @@ import RestaurantPage from './pages/RestaurantPage/RestaurantPage';
 import NotFoundPage from './NotFoundPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 
+import { setAccessToken } from './actions';
+
+import { loadItem } from './services/storage';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  const accessToken = loadItem('accessToken');
+
+  if (accessToken) {
+    dispatch(setAccessToken(accessToken));
+  }
+
   return (
     <div>
       <header>
