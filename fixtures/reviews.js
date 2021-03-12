a8d85ee49bca9d73624a3f5ad89f5463d9f62d46
yoonhe <yhehe228@gmail.com>
2021-03-12 20:46:29 +0900

    review, reviews fixtures 추가

--- fixtures/reviews.js
+++ fixtures/reviews.js
@@ -1,6 +1,10 @@
-const reviews = {
-  score: '5',
-  description: 'good!',
-};
+const reviews = [
+  {
+    id: 1,
+    name: '테스터',
+    score: '5',
+    description: '맛있어요',
+  },
+];
 
 export default reviews;
