3b4e7254815fa8ca263d135a1f260ccc47cec653
datalater <the7mincheol@gmail.com>
2021-03-12 14:57:32 +0900

    리뷰가 없는 상황에 대한 처리를 하라

--- src/RestaurantReviews.jsx
+++ src/RestaurantReviews.jsx
@@ -1,34 +1,44 @@
 import React from 'react';
 
-export default function RestaurantReviews({ restaurant }) {
-  const { reviews } = restaurant;
+function ReviewItems({ reviews }) {
+  if (!(reviews || []).length) {
+    return (
+      <p>등록된 리뷰가 없습니다.</p>
+    );
+  }
 
+  return (
+    <ul>
+      { reviews.map(({
+        id, name, score, description,
+      }) => (
+        <li key={id}>
+          <p>
+            작성자:
+            {' '}
+            {name}
+          </p>
+          <p>
+            평점:
+            {' '}
+            {score}
+          </p>
+          <p>
+            리뷰 내용:
+            {' '}
+            {description}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function RestaurantReviews({ reviews }) {
   return (
     <>
       <h3>리뷰</h3>
-      <ul>
-        { reviews.map(({
-          id, name, score, description,
-        }) => (
-          <li key={id}>
-            <p>
-              작성자:
-              {' '}
-              {name}
-            </p>
-            <p>
-              평점:
-              {' '}
-              {score}
-            </p>
-            <p>
-              리뷰 내용:
-              {' '}
-              {description}
-            </p>
-          </li>
-        ))}
-      </ul>
+      <ReviewItems reviews={reviews} />
     </>
   );
 }
--- src/RestaurantReviews.test.jsx
+++ src/RestaurantReviews.test.jsx
@@ -5,11 +5,8 @@ import { render } from '@testing-library/react';
 import RestaurantReviews from './RestaurantReviews';
 
 describe('RestaurantReviews', () => {
-  const restaurant = {
-    id: 1,
-    name: '마녀주방',
-    address: '서울시 강남구',
-    reviews: [
+  context('with reviews', () => {
+    const reviews = [
       {
         id: 1,
         restaurantId: 1,
@@ -17,26 +14,33 @@ describe('RestaurantReviews', () => {
         score: 5,
         description: '훌륭하다 훌륭하다 지구인놈들',
       },
-      {
-        id: 3,
-        restaurantId: 1,
-        name: '테스터',
-        score: 3,
-        description: 'Hi!',
-      },
-    ],
-  };
+    ];
 
-  it('renders reviews', () => {
-    const { container } = render((
-      <RestaurantReviews
-        restaurant={restaurant}
-      />
-    ));
+    it('renders reviews', () => {
+      const { container } = render((
+        <RestaurantReviews
+          reviews={reviews}
+        />
+      ));
 
-    expect(container).toHaveTextContent('리뷰');
-    expect(container).toHaveTextContent('작성자');
-    expect(container).toHaveTextContent('평점');
-    expect(container).toHaveTextContent('리뷰 내용');
+      expect(container).toHaveTextContent('리뷰');
+      expect(container).toHaveTextContent('작성자');
+      expect(container).toHaveTextContent('평점');
+      expect(container).toHaveTextContent('리뷰 내용');
+    });
+  });
+
+  context('without reviews', () => {
+    it('renders no reviews message', () => {
+      [[], undefined, null].forEach((reviews) => {
+        const { container } = render((
+          <RestaurantReviews
+            reviews={reviews}
+          />
+        ));
+
+        expect(container).toHaveTextContent('등록된 리뷰가 없습니다.');
+      });
+    });
   });
 });
