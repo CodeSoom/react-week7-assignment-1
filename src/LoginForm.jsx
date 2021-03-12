09c0cb21efd0470d2106a1f52eeae6c966997675
yoonhe <yhehe228@gmail.com>
2021-03-12 19:34:48 +0900

    [green] LoginForm을 구현합니다

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default function LoginForm({
-  email, password,
-  onChange, onSubmit,
+  accessToken, email, password,
+  onChange, onSubmit, onClick,
 }) {
   function handleChange(event) {
     const { target: { value, name } } = event;
@@ -11,30 +11,44 @@ export default function LoginForm({
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="input-email">E-mail</label>
-        <input
-          type="email"
-          name="email"
-          id="input-email"
-          value={email}
-          placeholder="email"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="input-password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="input-password"
-          value={password}
-          placeholder="password"
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit">Log In</button>
-    </form>
+    <>
+      {accessToken ? (
+        <div>
+          <button
+            type="button"
+            onClick={onClick}
+          >
+            Log Out
+
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="input-email">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              id="input-email"
+              value={email}
+              placeholder="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="input-password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="input-password"
+              value={password}
+              placeholder="password"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+      )}
+    </>
   );
 }
