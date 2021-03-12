f0fb0de4427f8a420bc5b3d2728c036103b2c938
dodoongtak <themighty.tak@gmail.com>
2021-03-12 16:01:27 +0900

    feat: log out upon clicking log out button

--- src/pages/LogInPage/LogInContainer.jsx
+++ src/pages/LogInPage/LogInContainer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 
-import { requestLogin } from '@actions';
+import { requestLogin, logOut } from '@actions';
+
+import { get } from '../../utils/utils';
+import { saveItem } from '../../services/storage';
+
 import LogInForm from './LogInForm';
 
 export default function LogInContainer() {
+  const accessToken = useSelector(get('accessToken'));
+
   const dispatch = useDispatch();
 
   const { register, handleSubmit } = useForm();
@@ -14,11 +20,18 @@ export default function LogInContainer() {
     dispatch(requestLogin({ logInFields: data }));
   };
 
+  function handleLogOut() {
+    dispatch(logOut());
+    saveItem('accessToken', '');
+  }
+
   return (
     <LogInForm
       register={register}
       handleSubmit={handleSubmit}
       onSubmit={onSubmit}
+      token={accessToken}
+      handleLogout={handleLogOut}
     />
   );
 }
--- src/pages/LogInPage/LogInForm.jsx
+++ src/pages/LogInPage/LogInForm.jsx
@@ -1,36 +1,56 @@
 import React from 'react';
 
-export default function LogInForm({ handleSubmit, onSubmit, register }) {
+export default function LogInForm({
+  handleSubmit,
+  onSubmit,
+  register,
+  token,
+  handleLogout,
+}) {
+  if (token) {
+    return (
+      <button
+        type="button"
+        onClick={handleLogout}
+      >
+        Log Out
+      </button>
+    );
+  }
+
   return (
     <div>
       <h1>Log In</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <fieldset>
-          <legend>식당 로그인:</legend>
-          <label htmlFor="email">
-            E-Mail
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            ref={register({ required: true })}
-          />
-          <label htmlFor="password">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            ref={register({ required: true })}
-          />
-          <button type="submit">
-            Log In
-          </button>
-          <br />
-        </fieldset>
-      </form>
+      {!token && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset>
+            <legend>식당 로그인:</legend>
+            <label htmlFor="email">
+              E-Mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              ref={register({ required: true })}
+            />
+            <label htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              ref={register({ required: true })}
+            />
+            <button type="submit">
+              Log In
+            </button>
+            <br />
+          </fieldset>
+        </form>
+      )}
+
     </div>
   );
 }
