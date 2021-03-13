8b5102e13b6d1bb9bc73e072e1817ba4201d39a7
yoonhe <yhehe228@gmail.com>
2021-03-13 09:14:51 +0900

    [refactoring] RestaurantReviews를 리팩토링합닝다

--- src/RestaurantReviews.test.jsx
+++ src/RestaurantReviews.test.jsx
@@ -5,20 +5,38 @@ import { render } from '@testing-library/react';
 import RestaurantReviews from './RestaurantReviews';
 
 describe('RestaurantReviews', () => {
-  const reviews = [
-    {
-      id: 1,
-      name: '테스터',
-      score: '5',
-      description: '맛있어요',
-    },
-  ];
+  function renderRestaurantReviews(reviews) {
+    return render(<RestaurantReviews reviews={reviews} />);
+  }
 
-  it('renders review list', () => {
-    const { queryByText } = render(<RestaurantReviews reviews={reviews} />);
+  context('with reviews', () => {
+    const reviews = [
+      {
+        id: 1,
+        name: '테스터',
+        score: '5',
+        description: '맛있어요',
+      },
+    ];
 
-    expect(queryByText('테스터')).not.toBeNull();
-    expect(queryByText('5점')).not.toBeNull();
-    expect(queryByText('맛있어요')).not.toBeNull();
+    it('renders review list', () => {
+      const { queryByText } = renderRestaurantReviews(reviews);
+
+      expect(queryByText('테스터')).not.toBeNull();
+      expect(queryByText('5점')).not.toBeNull();
+      expect(queryByText('맛있어요')).not.toBeNull();
+    });
+  });
+
+  context('without reviews', () => {
+    const reviews = null;
+
+    it('renders review list', () => {
+      const { queryByText } = renderRestaurantReviews(reviews);
+
+      expect(queryByText('테스터')).toBeNull();
+      expect(queryByText('5점')).toBeNull();
+      expect(queryByText('맛있어요')).toBeNull();
+    });
   });
 });
