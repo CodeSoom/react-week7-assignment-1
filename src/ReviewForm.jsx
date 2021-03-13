f552e8f3468ed3b99242d6805c9d3a83fbfdbc79
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-14 05:25:53 +0900

    ReviewForm을 구현합니다.

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 
-export default function ReviewForm() {
+const initialReviewInputs = {
+  rating: 4,
+  content: '맛있는 편이에요',
+};
+
+export default function ReviewForm({ reviewInputs = initialReviewInputs, onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
+
+  const { rating, content } = reviewInputs;
+
   return (
     <>
+      <div>
+        <label htmlFor="review-rating">평점</label>
+        <input
+          type="number"
+          id="review-rating"
+          name="rating"
+          value={rating}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="review-content">리뷰내용</label>
+        <input
+          type="text"
+          id="review-content"
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </div>
     </>
   );
 }
