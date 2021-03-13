58efb46ae5cd36cb74edbb00b41ce56bfa843260
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-14 06:07:41 +0900

    RestaurantContainer를 리팩토링하여 ReviewForm을 따로 분리하였기 때문에 import처리합니다.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -4,51 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
 
+import ReviewForm from './ReviewForm';
+
 import {
   loadRestaurant,
 } from './actions';
 
 import { get } from './utils';
-
-const initialReviewInputs = {
-  rating: 4,
-  content: '맛있는 편이에요',
-};
-
-function ReviewForm({ reviewInputs = initialReviewInputs, onChange }) {
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
-  const { rating, content } = reviewInputs;
-
-  return (
-    <>
-      <div>
-        <label htmlFor="review-rating">평점</label>
-        <input
-          type="number"
-          id="review-rating"
-          name="rating"
-          value={rating}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="review-content">리뷰내용</label>
-        <input
-          type="text"
-          id="review-content"
-          name="content"
-          value={content}
-          onChange={handleChange}
-        />
-      </div>
-    </>
-  );
-}
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
