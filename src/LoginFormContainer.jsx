923a5961def5bd9669fd86cc9e237b7d0b6300f7
seong-dodo <dyrnxm52@hanmail.net>
2021-06-14 21:26:03 +0900

    LoginFormContainer을 구현한다
    - Password input을 구현한다

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,15 +1,28 @@
 export default function LoginFormContainer() {
   return (
-    <div>
-      <label
-        htmlFor="login-email"
-      >
-        E-mail
-      </label>
-      <input
-        type="email"
-        id="login-email"
-      />
-    </div>
+    <>
+      <div>
+        <label
+          htmlFor="login-email"
+        >
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="login-email"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="login-password"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="login-password"
+        />
+      </div>
+    </>
   );
 }
