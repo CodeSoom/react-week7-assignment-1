0d55a660e31c5f971f4ea7d0d3384ee1df655982
dngwoodo <dngwoodo@gmail.com>
2021-06-14 23:52:39 +0900

    feat: Apply LoginFormContainer in LoginPage

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -1,8 +1,23 @@
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
 describe('LoginPage', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: '',
+        password: '',
+      },
+    }));
+  });
+
   it('renders title', () => {
     const { getByRole } = render((<LoginPage />));
 
