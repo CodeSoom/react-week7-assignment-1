fdeb2d1fc483fbd18cbc3ec8a0070a777e5e6dfc
yujonglee <yujonglee@g.skku.edu>
2021-06-14 15:21:32 +0900

    LoginPage를 작성하라

---
+++ src/LoginPage.jsx
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+  return (
+    <h2>LogIn</h2>
+  );
+}
---
+++ src/LoginPage.test.jsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  it('renders title', () => {
+    const { container } = render(<LoginPage />);
+    expect(container).toHaveTextContent('LogIn');
+  });
+});
