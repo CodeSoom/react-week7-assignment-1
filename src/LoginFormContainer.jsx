ab226a7280abe1c94fe95fe06e32135c785192ff
hxunz <guswl2843@gmail.com>
2021-06-16 00:36:02 +0900

    LoginFormContainer 생성

---
+++ src/LoginFormContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function LoginFormContainer() {
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input type="email" id="login-email" />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input type="password" id="login-password" />
+      </div>
+    </>
+  );
+}
--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { MemoryRouter } from 'react-router-dom';
-
 import { render } from '@testing-library/react';
 
 import LoginFormContainer from './LoginFormContainer';
@@ -9,12 +7,10 @@ import LoginFormContainer from './LoginFormContainer';
 describe('LoginFormContainer', () => {
   it('renders input controls', () => {
     const { getByLabelText } = render((
-      <MemoryRouter>
-        <LoginFormContainer />
-      </MemoryRouter>
+      <LoginFormContainer />
     ));
 
-    expect(getByLabelText('Username')).not.toBeNull();
+    expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
   });
 });
