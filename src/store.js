62a12309d5eed2dad673dd19bd211e30e7b76a3c
yujonglee <yujonglee@g.skku.edu>
2021-06-15 10:39:06 +0900

    redux-devtools를 설치하고 적용하라

--- src/store.js
+++ src/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 
 import thunk from 'redux-thunk';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
