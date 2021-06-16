4dfeb042fa7f73c9b5fdb2239f97f624648f1998
seong-dodo <dyrnxm52@hanmail.net>
2021-06-16 11:49:07 +0900

    ReviewForm에 대한 테스트를 추가한다
    - renders review write form에 대한 테스트를 추가한다

---
+++ src/ReviewForm.test.jsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import ReviewForm from './ReviewForm';
+
+describe('ReviewForm', () => {
+  it('renders review write form', () => {
+    const { queryByText } = render(<ReviewForm />);
+
+    expect(queryByText('평점')).not.toBeNull();
+    expect(queryByText('리뷰 내용')).not.toBeNull();
+  });
+});
