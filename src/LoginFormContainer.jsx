9918d369af665523b72b5b256594726fa0ec0670
최한울 <hanul.choi95@nts-corp.com>
2021-06-17 01:01:14 +0900

    accessToken 렌더링

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -13,6 +13,7 @@ export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   const { email, password } = useSelector(get('loginFields'));
+  const accessToken = useSelector(get('accessToken'));
 
   function handleChange({ name, value }) {
     dispatch(changeLoginField({ name, value }));
@@ -23,10 +24,13 @@ export default function LoginFormContainer() {
   }
 
   return (
-    <LoginForm
-      fields={{ email, password }}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-    />
+    <>
+      <LoginForm
+        fields={{ email, password }}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <p>{accessToken}</p>
+    </>
   );
 }
