8a1a7042fabe8757274105e727a3aff8fe541615
최한울 <hanul.choi95@nts-corp.com>
2021-06-16 22:01:09 +0900

    LoginFormContainer 파일에 대한 테스트를 작성한다
    - 로그인 폼 안에 레이블명에 대한 테스트를 작성한다.

---
+++ src/LoginFormContainer.test.jsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+
+import LoginFormContainer from './LoginFormContainer';
+
+describe('LoginFormContainer', () => {
+  it('renders input controls', () => {
+    const { getByLabelText } = render(
+      <LoginFormContainer />
+    );
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+});
