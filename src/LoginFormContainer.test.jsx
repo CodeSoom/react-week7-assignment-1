3da4585467a0f9be0f4a17b2bdab0f747a3f6c6f
최한울 <hanul.choi95@nts-corp.com>
2021-06-16 22:14:21 +0900

    로그인 버튼 렌더링에 대한 테스트를 작성한다
    - 로그인 버튼 렌더링과 버튼을 눌렀을 때 dispatch 함수 호출에 대한 테스트를 작성한다

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,8 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { useDispatch } from "react-redux";
 
 import LoginFormContainer from './LoginFormContainer';
 
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders input controls', () => {
     const { getByLabelText } = render(
       <LoginFormContainer />
@@ -10,5 +19,15 @@ describe('LoginFormContainer', () => {
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('renders "Log In" button', () => {
+    const { getByText } = render(
+      <LoginFormContainer />
+    );
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
