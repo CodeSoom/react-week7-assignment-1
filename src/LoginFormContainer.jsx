b92a22ea97fd1a71ee542c11cd7c96e197f56bbf
최한울 <hanul.choi95@nts-corp.com>
2021-06-17 00:05:27 +0900

    로그인 폼 변경 시 얻어낸 값을 input 엘리먼트에 넘기는 것을 구현한다

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,17 +1,32 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { requestLogin } from './actions';
+import {
+  requestLogin,
+  changeLoginField,
+} from './actions';
 
 import LoginForm from './LoginForm';
 
+import { get } from './utils';
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
+
+  function handleChange({ name, value }) {
+    dispatch(changeLoginField({ name, value }));
+  }
 
   function handleSubmit() {
     dispatch(requestLogin());
   }
 
   return (
-    <LoginForm onSubmit={handleSubmit} />
+    <LoginForm
+      fields={{ email, password }}
+      onChange={handleChange}
+      onSubmit={handleSubmit}
+    />
   );
 }
