4c7d834c8391f9ea1910050c6664c622b3050b04
최한울 <hanul.choi95@nts-corp.com>
2021-06-16 23:33:12 +0900

    로그인 폼에 값을 변경하는 이벤트핸들러에 대한 테스트를 작성한다

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -3,13 +3,24 @@ import { render, fireEvent } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  it('renders input controls', () => {
-    const { getByLabelText } = render(
-      <LoginForm />
-    );
+  it('renders input controls and listens change events', () => {
+    const handleChange = jest.fn();
 
+    const { getByLabelText } = render(
+      <LoginForm onChange={handleChange} />
+    );
+    
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'tester@example.com' },
+    });
+
+    expect(handleChange).toBeCalledWith({
+      name: 'email',
+      value: 'tester@example.com',
+    });
   });
 
   it('renders "Log In" button', () => {
