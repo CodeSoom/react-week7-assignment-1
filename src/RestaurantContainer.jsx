ad5ebf4226ef23a4a906bc5bd227d1d79859f0cb
seong-dodo <dyrnxm52@hanmail.net>
2021-06-16 16:55:27 +0900

    RestaurantContainer를 구현한다
    - 리뷰남기기에 대한 sendReview를 구현한다

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -8,6 +8,7 @@ import ReviewForm from './ReviewForm';
 import {
   loadRestaurant,
   changeReviewField,
+  sendReview,
 } from './actions';
 
 import { get } from './utils';
@@ -31,10 +32,17 @@ export default function RestaurantContainer({ restaurantId }) {
     dispatch(changeReviewField({ name, value }));
   }
 
+  function handleSubmit() {
+    dispatch(sendReview());
+  }
+
   return (
     <div>
       <RestaurantDetail restaurant={restaurant} />
-      <ReviewForm onChange={handleChange} />
+      <ReviewForm
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 }
