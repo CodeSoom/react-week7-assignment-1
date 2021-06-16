ecb412d58c89b8ea6526f4d852041ec757d70838
최한울 <hanul.choi95@nts-corp.com>
2021-06-16 22:31:36 +0900

    LoginForm을 구현한다

---
+++ src/LoginForm.jsx
@@ -0,0 +1,28 @@
+export default function LoginForm() {
+  function handleClick() {
+    //
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input type="email" id="login-email" />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input type="password" id="login-password" />
+      </div>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Log In
+      </button>
+    </>
+  );
+}
