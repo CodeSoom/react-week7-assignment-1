aa8764fa89750e6c73522a5be26cc69eaf15c365
yujonglee <yujonglee@g.skku.edu>
2021-06-17 20:40:12 +0900

    renderLoginForm의 불필요한 매개변수들을 삭제하라

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -4,7 +4,15 @@ import given from 'given2';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  function renderLoginForm({ form, handleChange, handleSubmit }) {
+  const form = {
+    email: 'email@email.com',
+    password: 'paXXword',
+  };
+
+  const handleChange = jest.fn();
+  const handleSubmit = jest.fn();
+
+  function renderLoginForm() {
     return render(
       <LoginForm
         form={form}
@@ -15,20 +23,12 @@ describe('LoginForm', () => {
     );
   }
 
-  const form = {
-    email: 'email@email.com',
-    password: 'paXXword',
-  };
-
-  const handleChange = jest.fn();
-  const handleSubmit = jest.fn();
-
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('renders input controls', () => {
-    const { getByRole } = renderLoginForm({ form, handleChange, handleSubmit });
+    const { getByRole } = renderLoginForm();
 
     expect(getByRole('textbox', { name: 'email' })).toHaveValue('email@email.com');
     expect(getByRole('textbox', { name: 'password' })).toHaveValue('paXXword');
@@ -38,7 +38,7 @@ describe('LoginForm', () => {
     given('isLoggedIn', () => true);
 
     it('renders log out button', () => {
-      const { getByRole } = renderLoginForm({ form, handleChange, handleSubmit });
+      const { getByRole } = renderLoginForm();
 
       expect(getByRole('button', { name: 'Log out' })).toBeInTheDocument();
     });
@@ -48,7 +48,7 @@ describe('LoginForm', () => {
     given('isLoggedIn', () => false);
 
     it('renders log in button', () => {
-      const { getByRole } = renderLoginForm({ form, handleChange, handleSubmit });
+      const { getByRole } = renderLoginForm();
 
       expect(getByRole('button', { name: 'Log In' })).toBeInTheDocument();
     });
