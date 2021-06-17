e86f2c82ba1e6cfc22bcd64532eaa17e5317c486
hanvit7 <hanvit7@gmail.com>
2021-06-17 12:23:54 +0900

    테스트 - LoginPage 테스트 추가
    - renders Log-in title 테스트 추가

---
+++ src/LoginPage.test.jsx
@@ -0,0 +1,19 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginPage from './LoginPage';
+
+describe('LoginPage', () => {
+  const renderLoginPage = () => render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>,
+  );
+
+  it('renders Log-in title', () => {
+    const { container } = renderLoginPage();
+
+    expect(container).toHaveTextContent('Log In');
+  });
+});
