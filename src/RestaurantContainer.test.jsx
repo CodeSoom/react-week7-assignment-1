0cc70bf8efac4d1341a6ece0d1761bbade99f63f
hxunz <guswl2843@gmail.com>
2021-06-17 16:22:26 +0900

    reviewForm function 생성

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -5,49 +7,34 @@ import { useDispatch, useSelector } from 'react-redux';
 import RestaurantContainer from './RestaurantContainer';
 
 describe('RestaurantContainer', () => {
-  const dispatch = jest.fn();
-
-  function renderRestaurantContainer() {
-    return render(<RestaurantContainer restaurantId="1" />);
-  }
-
   beforeEach(() => {
-    dispatch.mockClear();
+    const dispatch = jest.fn();
+
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((selector) => selector({
-      restaurant: given.restaurant,
+      restaurant: {
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+      },
     }));
   });
 
-  it('dispatches action', () => {
-    renderRestaurantContainer();
+  it('renders name and address', () => {
+    const { container } = render((
+      <RestaurantContainer restaurantId="1" />
+    ));
 
-    expect(dispatch).toBeCalled();
+    expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('서울시');
   });
 
-  context('with restaurant', () => {
-    given('restaurant', () => ({
-      id: 1,
-      name: '마법사주방',
-      address: '서울시 강남구',
-    }));
+  it('rensers review write form', () => {
+    const { queryByLabelText } = render((
+      <RestaurantContainer restaurantId="1" />
+    ));
 
-    it('renders name and address', () => {
-      const { container } = renderRestaurantContainer();
-
-      expect(container).toHaveTextContent('마법사주방');
-      expect(container).toHaveTextContent('서울시');
-    });
-  });
-
-  context('without restaurant', () => {
-    given('restaurant', () => null);
-
-    it('renders loading', () => {
-      const { container } = renderRestaurantContainer();
-
-      expect(container).toHaveTextContent('Loading');
-    });
+    expect(queryByLabelText('평점')).not.toBeNull();
   });
 });
