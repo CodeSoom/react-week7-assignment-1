e7ccc6260dcac0632d9fdb9304758a4370555d4c
seong-dodo <dyrnxm52@hanmail.net>
2021-06-18 21:44:13 +0900

    TextField에 대한 테스트를 추가한다
    - without type에 대한 테스트를 추가한다

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -3,16 +3,32 @@ import { render } from 'react-dom';
 import TextField from './TextField';
 
 describe('TextField', () => {
-  const handleChange = jest.fn();
+  context('with type', () => {
+    it('renders label and input control', () => {
+      const handleChange = jest.fn();
 
-  it('renders label and input control', () => {
-    const { queryByLabelText } = render(<TextField
-      label="평점"
-      type="number"
-      name="score"
-      onChange={handleChange}
-    />);
+      const { queryByLabelText } = render(<TextField
+        label="평점"
+        type="number"
+        name="score"
+        onChange={handleChange}
+      />);
 
-    expect(queryByLabelText('평점')).not.toBeNull();
+      expect(queryByLabelText('평점')).not.toBeNull();
+    });
+  });
+
+  context('without type', () => {
+    it('renders label and input control', () => {
+      const handleChange = jest.fn();
+
+      const { queryByLabelText } = render(<TextField
+        label="리뷰 내용"
+        name="description"
+        onChange={handleChange}
+      />);
+
+      expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+    });
   });
 });
