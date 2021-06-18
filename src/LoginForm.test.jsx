494f915293f3b260d6fecf45968b7c70f205b17f
hanvit7 <hanvit7@gmail.com>
2021-06-18 12:02:07 +0900

    테스트 - LoginForm 테스트 추가
    - input 값 변경에 따른 handleChange 테스트 추가

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -4,16 +4,23 @@ import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   const handleSubmit = jest.fn();
+  const handleChange = jest.fn();
 
   const renderLoginForm = () => render(
-    <LoginForm onSubmit={handleSubmit} />,
+    <LoginForm onSubmit={handleSubmit} onChange={handleChange} />,
   );
 
-  it('renders input controls', () => {
+  it('renders input controls and listens change events', () => {
     const { getByLabelText } = renderLoginForm();
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'tester@example.com' },
+    });
+
+    expect(handleChange).toBeCalled();
   });
 
   it('renders "Log In" button', () => {
