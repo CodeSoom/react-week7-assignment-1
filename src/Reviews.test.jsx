67c1b97ea0b6ec7d6293f3d2e7e0103724c7087d
seong-dodo <dyrnxm52@hanmail.net>
2021-06-18 10:17:43 +0900

    Reviews에 대한 테스트를 추가한다
    - 리뷰리스트에 대한 테스트를 추가한다

--- src/Reviews.test.jsx
+++ src/Reviews.test.jsx
@@ -2,10 +2,21 @@ import { render } from '@testing-library/react';
 
 import Reviews from './Reviews';
 
+import REVIEWS from '../fixtures/reviews';
+
 describe('Reviews', () => {
+  const reviews = REVIEWS;
+
   it('reders reivews title', () => {
-    const { container } = render(<Reviews />);
+    const { container } = render(<Reviews reviews={reviews} />);
 
     expect(container).toHaveTextContent('리뷰');
+  });
+
+  it('reders reivews list', () => {
+    const { container } = render(<Reviews reviews={reviews} />);
+
+    expect(container).toHaveTextContent('5');
+    expect(container).toHaveTextContent('훌륭하다 지구놈들');
   });
 });
