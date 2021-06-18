053f10bf85a7c4dbfa5666f0a14bde75cf83ae62
hanvit7 <hanvit7@gmail.com>
2021-06-18 22:22:47 +0900

    구현 - actions에 requestLogin 관련 추가
    actions
    - requestLogin 구현
    - setAccessToken 구현
    
    api
    - postLogin 추가

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,7 @@ import {
   fetchCategories,
   fetchRestaurants,
   fetchRestaurant,
+  postLogin,
 } from './services/api';
 
 export function setRegions(regions) {
@@ -93,10 +94,17 @@ export function changeLoginField({ name, value }) {
   };
 }
 
+export function setAccessToken(accesToken) {
+  return {
+    type: 'setAccessToken',
+    payload: { accesToken },
+  };
+}
 export function requestLogin() {
   return async (dispatch, getState) => {
-    // state = email, password
-    // HTTP POST < email, password
-    // dispatch(setAccessToken(accessToken));
+    const { loginFields: { email, password } } = getState();
+
+    const accessToken = postLogin({ email, password });
+    dispatch(setAccessToken(accessToken));
   };
 }
