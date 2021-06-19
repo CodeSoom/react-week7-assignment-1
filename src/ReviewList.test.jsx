8077abcb192e3694c471f317db382da0b67c4f82
hxunz <guswl2843@gmail.com>
2021-06-19 18:21:40 +0900

    ✅ ReviewList 컴포넌트 생성

--- src/ReviewList.test.jsx
+++ src/ReviewList.test.jsx
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 
+import ReviewList from './ReviewList';
+
 describe('ReviewList', () => {
   describe('when has reviews', () => {
     it('renders reviews', () => {
