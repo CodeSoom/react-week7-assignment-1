ee46a51dff66473b7400a962b159822038ad4198
hanvit7 <hanvit7@gmail.com>
2021-06-19 10:53:17 +0900

    테스트 - reducer에 changeReviewField 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -9,6 +9,7 @@ import {
   selectCategory,
   changeLoginField,
   setAccessToken,
+  changeReviewField,
 } from './actions';
 
 describe('reducer', () => {
@@ -22,6 +23,10 @@ describe('reducer', () => {
       selectedCategory: null,
       loginFields: {},
       accessToken: '',
+      reviewFields: {
+        score: '',
+        description: '',
+      },
     };
 
     it('returns initialState', () => {
@@ -155,5 +160,21 @@ describe('reducer', () => {
     const state = reducer(initialState, setAccessToken('TOKEN'));
 
     expect(state.accessToken).toBe('TOKEN');
+  });
+
+  describe('changeReviewField', () => {
+    const initialState = {
+      reviewFields: {
+        score: '0',
+        description: '',
+      },
+    };
+
+    const state = reducer(
+      initialState,
+      changeReviewField({ name: 'score', value: '5' }),
+    );
+
+    expect(state.reviewFields.score).toBe('5');
   });
 });
