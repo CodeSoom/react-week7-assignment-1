a4afedaf817354acdc58bceb57bc3a925e1a9a07
hxunz <guswl2843@gmail.com>
2021-06-19 18:12:25 +0900

    🧪 ReviewList 컴포넌트에 대한 테스트 코드 추가

--- src/ReviewList.test.jsx
+++ src/ReviewList.test.jsx
@@ -1,1 +1,13 @@
+import { render } from '@testing-library/react';
 
+describe('ReviewList', () => {
+  describe('when has reviews', () => {
+    it('renders reviews', () => {
+      render(<ReviewList />);
+    });
+  });
+
+  // describe('when has no review', () => {
+
+  // });
+});
