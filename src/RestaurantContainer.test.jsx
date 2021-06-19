59394f177f2779cb15a63da16b4c9ecc65c3fb0a
hanvit7 <hanvit7@gmail.com>
2021-06-19 11:41:46 +0900

    리팩토링 - RestaurantContainer 테스트 리팩토링

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -43,28 +43,23 @@ describe('RestaurantContainer', () => {
     it('renders review write form', () => {
       const { getByLabelText } = renderRestaurantContainer();
 
-      fireEvent.change(getByLabelText('평점'), {
-        target: { value: '5' },
-      });
+      const controls = [
+        { label: '평점', name: 'score', value: '5' },
+        { label: '리뷰 내용', name: 'description', value: '존맛탱' },
+      ];
 
-      expect(dispatch).toBeCalledWith({
-        type: 'changeReviewField',
-        payload: {
-          name: 'score',
-          value: '5',
-        },
-      });
+      controls.forEach(({ label, name, value }) => {
+        fireEvent.change(getByLabelText(label), {
+          target: { value },
+        });
 
-      fireEvent.change(getByLabelText('리뷰 내용'), {
-        target: { value: '존맛탱' },
-      });
-
-      expect(dispatch).toBeCalledWith({
-        type: 'changeReviewField',
-        payload: {
-          name: 'description',
-          value: '존맛탱',
-        },
+        expect(dispatch).toBeCalledWith({
+          type: 'changeReviewField',
+          payload: {
+            name,
+            value,
+          },
+        });
       });
     });
   });
