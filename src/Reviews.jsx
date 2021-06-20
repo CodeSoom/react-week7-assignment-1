ab26ee69d5f7d95be004d0204470e0308cbd288f
seong-dodo <dyrnxm52@hanmail.net>
2021-06-20 09:32:10 +0900

    Reviews 코드 수정한다
    - 코드 수정 필요부분 피드백 반영하여 수정한다
    - 코드 수정 필요부분 강의 참조하여 반영한다

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,15 +1,17 @@
 export default function Reviews({ reviews }) {
-  if (!(reviews || []).length) {
+  if (!reviews || !reviews.length) {
     return (
-      <p>리뷰가 없어요!</p>
+      <p>등록된 리뷰가 없어요!</p>
     );
   }
+
+  const sortedReviews = [...reviews].sort((a, b) => (b.id - a.id));
 
   return (
     <div>
       <h3>리뷰</h3>
       <ul>
-        {reviews.sort((a, b) => (b.id - a.id)).map(({
+        {sortedReviews.map(({
           id, name, score, description,
         }) => (
           <li key={id}>
