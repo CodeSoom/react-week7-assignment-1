ce3d243e2f40f2359e77b7e555f3eaa83b503281
lazy-sky <1cielo@naver.com>
2021-09-27 21:47:28 +0900

    feat: Login
    - httpie
    - handling form
    - separating tests

---
+++ src/LoginFormContainer.jsx
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import LoginForm from './LoginForm';
+
+import {
+  changeLoginField,
+  requestLogin,
+} from './actions';
+
+import { get } from './utils';
+
+export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
+  const accessToken = useSelector(get('accessToken'));
+
+  function handleChange({ name, value }) {
+    dispatch(changeLoginField({ name, value }));
+  }
+
+  function handleSubmit() {
+    dispatch(requestLogin());
+  }
+
+  return (
+    <div>
+      <LoginForm
+        fields={{ email, password }}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <p>
+        {accessToken}
+      </p>
+    </div>
+  );
+}
---
+++ src/LoginFormContainer.test.jsx
@@ -0,0 +1,56 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import LoginFormContainer from './LoginFormContainer';
+
+jest.mock('react-redux');
+
+describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: 'test@test',
+        password: '1234',
+      },
+    }));
+  });
+
+  const renderLoginFormContainer = () => render(
+    <LoginFormContainer />,
+  );
+
+  it('renders input controls', () => {
+    const { getByLabelText } = renderLoginFormContainer();
+
+    expect(getByLabelText('E-mail').value).toBe('test@test');
+    expect(getByLabelText('Password').value).toBe('1234');
+  });
+
+  it('listens change events', () => {
+    const { getByLabelText } = renderLoginFormContainer();
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'new email' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeLoginField',
+      payload: { name: 'email', value: 'new email' },
+    });
+  });
+
+  it('renders "Log In" button', () => {
+    const { getByText } = renderLoginFormContainer();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
+  });
+});
