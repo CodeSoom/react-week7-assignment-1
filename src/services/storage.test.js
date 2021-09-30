e982853f925b36488b470dc4c126c0127457aa49
seilylook <seilylook@naver.com>
2021-09-30 15:31:00 +0900

    refac: Green: storage.test - loadItem, saveItem 테스트 통과

--- src/services/storage.test.js
+++ src/services/storage.test.js
@@ -1,10 +1,14 @@
-import React from 'react';
-
 import {
   saveItem,
   loadItem,
 } from './storage';
 
 test('storage', () => {
-  
+  const key = 'user';
+
+  const value = 'accessToken';
+
+  saveItem(key, value);
+
+  expect(loadItem(key)).toBe(value);
 });
