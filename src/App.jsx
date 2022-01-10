903fd5a929df064ba151e00589f074a20ae3701f
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-10 19:29:56 +0900

    [refactor] 로그인 페이지 연동

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
 import RestaurantsPage from './pages/RestaurantsPage';
 import RestaurantPage from './pages/RestaurantPage';
 import NotFoundPage from './pages/NotFoundPage';
@@ -21,6 +22,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/login" component={LoginPage} />
         <Route exact path="/restaurants" component={RestaurantsPage} />
         <Route path="/restaurants/:id" component={RestaurantPage} />
         <Route component={NotFoundPage} />
