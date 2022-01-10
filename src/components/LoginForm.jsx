6a3da7ccd505d5a9c483e32e359f6c0536229b68
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-10 17:42:10 +0900

    [feat] 로그인폼 컴포넌트 기능 구현

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,39 @@
+export default function LoginForm({ onChange, onSubmit }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="login-email"
+          name="email"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+          onChange={handleChange}
+        />
+      </div>
+      <button
+        type="button"
+        onClick={onSubmit}
+      >
+        Log In
+      </button>
+    </>
+  );
+}
