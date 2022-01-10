51340c9ad3121fe6538e621fe1faa95783ae8ec8
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-10 17:04:56 +0900

    [test] 로그인 컨테이너 입력시 loginField 변환 기능 테스트

--- src/containers/LoginContainer.test.jsx
+++ src/containers/LoginContainer.test.jsx
@@ -1,8 +1,20 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import LoginContainer from './LoginContainer';
 
+jest.mock('react-redux');
+
 describe('LoginContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   // 근데 이거는 컴포넌트 테스트 아닌가?
   it('renders email and password inputs and "Log In" button', () => {
     const { queryByLabelText, queryByRole } = render((
@@ -14,4 +26,38 @@ describe('LoginContainer', () => {
 
     expect(queryByRole('button', { name: 'Log In' })).toBeInTheDocument();
   });
+
+  // TODO: 이메일 입력 => 리덕스 상태 변경 액션 호출(dispatch)
+  it('types E-mail and Password, calls dispatch', () => {
+    const { getByLabelText } = render((
+      <LoginContainer />
+    ));
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'changed email' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeLoginField',
+      payload: {
+        name: 'email',
+        value: 'changed email',
+      },
+    });
+
+    fireEvent.change(getByLabelText('Password'), {
+      target: { value: 'changed password' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeLoginField',
+      payload: {
+        name: 'password',
+        value: 'changed password',
+      },
+    });
+  });
+
+  // TODO: 입력하면 input 값이 입력한 값으로 변함
+  // TODO: 로그인 버튼 클릭하면 로그인 이벤트 호출(dispatch login 기능)
 });
