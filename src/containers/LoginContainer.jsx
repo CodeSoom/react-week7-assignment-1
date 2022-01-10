c9faa79d6de2a176acc7e857390351b0755df848
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-10 17:44:33 +0900

    [refactor] 로그인폼 컴포넌트 적용

--- src/containers/LoginContainer.jsx
+++ src/containers/LoginContainer.jsx
@@ -2,43 +2,16 @@ import { useDispatch } from 'react-redux';
 
 import { changeLoginField } from '../modules/actions';
 
+import LoginForm from '../components/LoginForm';
+
 export default function LoginContainer() {
   const dispatch = useDispatch();
 
-  function handleChangeLoginField(event) {
-    const { target: { name, value } } = event;
+  function handleChangeLoginField({ name, value }) {
     dispatch(changeLoginField({ name, value }));
   }
 
   return (
-    <>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input
-          type="email"
-          id="login-email"
-          name="email"
-          onChange={handleChangeLoginField}
-        />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          Password
-        </label>
-        <input
-          type="password"
-          id="login-password"
-          name="password"
-          onChange={handleChangeLoginField}
-        />
-      </div>
-      <button
-        type="button"
-      >
-        Log In
-      </button>
-    </>
+    <LoginForm onChange={handleChangeLoginField} />
   );
 }
