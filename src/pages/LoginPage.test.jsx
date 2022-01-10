52546ebc07e514b70d2c949a4a1811b3f629c962
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-10 19:28:00 +0900

    [test] 로그인 page 렌더 테스트 추가

--- src/pages/LoginPage.test.jsx
+++ src/pages/LoginPage.test.jsx
@@ -9,6 +9,17 @@ describe('LoginPage', () => {
     expect(container).toHaveTextContent('Log In');
   });
 
+  it('renders email and password inputs and "Log In" button', () => {
+    const { queryByLabelText, queryByRole } = render((
+      <LoginPage />
+    ));
+
+    expect(queryByLabelText('E-mail')).toBeInTheDocument();
+    expect(queryByLabelText('Password')).toBeInTheDocument();
+
+    expect(queryByRole('button', { name: 'Log In' })).toBeInTheDocument();
+  });
+
   // TODO: Login Container?
   // Login Input, 및 Log In 버튼 테스트
   // e2e 테스트를 Page 에서 해도 될듯
