e742843e65534a92adf4297eda109ca11e9b7f95
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-10 19:28:17 +0900

    [refactor] 로그인 페이지 로그인 컨테이너 연동 기능 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,10 @@
+import LoginContainer from '../containers/LoginContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <h2>Log In</h2>
+      <LoginContainer />
     </div>
   );
 }
