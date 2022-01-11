13b1b7d711fd93cb4c6eae67de734e52b83b9798
AutowiniFront <autowiniFront@naver.com>
2022-01-11 22:52:29 +0900

    구현: LoginForm
    email, password input의 changeEvent에 대한 함수 handleChange 구현

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,13 +1,19 @@
-export default function LoginForm({ onSubmit }) {
+export default function LoginForm({ onSubmit, onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+
+    onChange({ name, value });
+  }
+
   return (
     <div>
       <div>
         <label htmlFor="login-email">E-mail</label>
-        <input id="login-email" type="text" name="E-mail" />
+        <input id="login-email" type="text" name="email" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="login-password">Password</label>
-        <input id="login-password" type="password" name="password" />
+        <input id="login-password" type="password" name="password" onChange={handleChange} />
       </div>
       <div>
         <button type="button" onClick={onSubmit}>Log In</button>
