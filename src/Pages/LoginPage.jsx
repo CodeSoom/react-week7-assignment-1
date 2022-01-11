1447c09bd2af666ee531dac6d4fe2d2661be1ea1
jeongseop.lee <sjl0614@naver.com>
2022-01-11 21:22:38 +0900

    LoginPage input 중복 제거

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,11 +1,10 @@
+import LoginFormContainer from "../Components/LoginFormContainer";
+
 export default function LoginPage() {
   return (
     <div>
       <h2>Login 페이지</h2>
-      <label htmlFor="login-email">
-        Username
-      </label>
-      <input type="email" id="login-email" />
+      <LoginFormContainer />
     </div>
   );
 }
