2a24c380c5e3268c159aea741b23023fb434a894
jeongseop.lee <sjl0614@naver.com>
2022-01-11 21:43:53 +0900

    LoginForm button 테스트 작성

--- src/Components/LoginForm.test.jsx
+++ src/Components/LoginForm.test.jsx
@@ -1,12 +1,24 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  const handleSubmit = jest.fn();
+
+  const renderLoginForm = () => render(<LoginForm />);
+
   it('renders input controls', () => {
-    const { getByLabelText } = render(<LoginForm />);
+    const { getByLabelText } = renderLoginForm();
 
     expect(getByLabelText('Username')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('renders "Log in" button', () => {
+    const { getByText } = renderLoginForm();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(handleSubmit).toBeCalled();
   });
 });
