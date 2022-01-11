59250461ca91f50ea23d128e68d6f287076f49c9
jeongseop.lee <sjl0614@naver.com>
2022-01-11 21:21:20 +0900

    LoginPage input 테스트 통과

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,6 +2,10 @@ export default function LoginPage() {
   return (
     <div>
       <h2>Login 페이지</h2>
+      <label htmlFor="login-email">
+        Username
+      </label>
+      <input type="email" id="login-email" />
     </div>
   );
 }
