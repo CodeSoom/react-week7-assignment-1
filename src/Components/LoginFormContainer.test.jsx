e16a7d59ff8a022fd62e78a707d1a8379cb8e30f
jeongseop.lee <sjl0614@naver.com>
2022-01-11 21:27:04 +0900

    LoginFormContainer Log In 버튼 테스트 작성

--- src/Components/LoginFormContainer.test.jsx
+++ src/Components/LoginFormContainer.test.jsx
@@ -1,12 +1,30 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  const renderLoginFormContainer = () => render(<LoginFormContainer />);
+
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders input controls', () => {
-    const { getByLabelText } = render(<LoginFormContainer />);
+    const { getByLabelText } = renderLoginFormContainer();
 
     expect(getByLabelText('Username')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('renders "Log in" button', () => {
+    const { getByText } = renderLoginFormContainer();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
