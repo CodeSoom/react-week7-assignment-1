c1d3fe3158c16bc7687d52cb77310c424d2f6b16
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-12 02:02:41 +0900

    [test] LoginForm 이벤트 변화 감지 테스트 작성

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -3,13 +3,20 @@ import { render, fireEvent } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
-  it('input control들을 렌더링한다.', () => {
+  it('input control들을 렌더링하고, 이벤트 변화를 감지한다.', () => {
+    const handleChange = jest.fn();
+
     const { getByLabelText } = render((
-      <LoginForm />
+      <LoginForm onChange={handleChange} />
     ));
 
     expect(getByLabelText('E-mail')).not.toBeNull();
     expect(getByLabelText('Password')).not.toBeNull();
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'tester@example.com' },
+    });
+    expect(handleChange).toBeCalled();
   });
 
   it('Login 버튼을 렌더링한다.', () => {
