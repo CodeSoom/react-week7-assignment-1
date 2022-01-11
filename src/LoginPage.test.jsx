e3508f4a02d45aa6c925e872b27d795b09fc6505
Superduper-India <sunyoung1542@gmail.com>
2022-01-11 19:20:55 +0900

    Update: HomePage/ Link to "/login" 추가

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -3,12 +3,8 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
-  const renderLoginPage = () => render(
-    <LoginPage />,
-  );
-
-  it('renders title', () => {
-    const { container } = renderLoginPage();
+  it('renders "Log In" title', () => {
+    const { container } = render((<LoginPage />));
 
     expect(container).toHaveTextContent('Log In');
   });
