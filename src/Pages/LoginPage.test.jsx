bad5a3cf85e88faa5b56d9225d3fc34594fc4672
jeongseop.lee <sjl0614@naver.com>
2022-01-11 21:20:42 +0900

    LoginPage에 input test 추가

--- src/Pages/LoginPage.test.jsx
+++ src/Pages/LoginPage.test.jsx
@@ -3,9 +3,16 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
+  const renderLoginPage = () => render(<LoginPage />);
   it('renders title', () => {
-    const { container } = render(<LoginPage />);
+    const { container } = renderLoginPage();
 
     expect(container).toHaveTextContent('Login 페이지');
+  });
+
+  it('renders input controls', () => {
+    const { getByLabelText } = renderLoginPage();
+
+    expect(getByLabelText('Username')).not.toBeNull();
   });
 });
