7fe283784509d660c337cb95da12cc7a29d0c17f
jeongseop.lee <sjl0614@naver.com>
2022-01-11 21:35:47 +0900

    LoginForm 테스트 통과

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,6 +1,23 @@
 export default function LoginFormContainer() {
+  function handleClick() {
+  }
+
   return ((
     <>
+      <label htmlFor="login-email">
+        Username
+      </label>
+      <input type="email" id="login-email" />
+      <label htmlFor="login-password">
+        Password
+      </label>
+      <input type="password" id="login-password" />
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Log In
+      </button>
     </>
   ));
 }
