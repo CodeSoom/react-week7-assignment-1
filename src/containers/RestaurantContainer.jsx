fe7df2cb0f4a7dfcb12c0742a024e230d703c0bd
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-11 20:05:14 +0900

    [feat] restaurnat 컨테이너 리뷰form 기능 구현

--- src/containers/RestaurantContainer.jsx
+++ src/containers/RestaurantContainer.jsx
@@ -3,15 +3,20 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from '../components/RestaurantDetail';
+import ReviewForm from '../components/ReviewForm';
 
 import {
   loadRestaurant,
+  changeReviewField,
+  sendReview,
 } from '../modules/actions';
 
 import { get } from '../utils';
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
+
+  const accessToken = useSelector(get('accessToken'));
 
   useEffect(() => {
     dispatch(loadRestaurant({ restaurantId }));
@@ -25,9 +30,25 @@ export default function RestaurantContainer({ restaurantId }) {
     );
   }
 
+  function handleChangeReviewField({ name, value }) {
+    dispatch(changeReviewField({ name, value }));
+  }
+
+  function handleSubmitReview() {
+    dispatch(sendReview({ restaurantId }));
+  }
+
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
+      {accessToken
+        ? (
+          <ReviewForm
+            onChange={handleChangeReviewField}
+            onSubmit={handleSubmitReview}
+          />
+        )
+        : null}
     </>
   );
 }
