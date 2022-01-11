2680b6c7f00ad2163ca16e4be54f885e15f6a547
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-12 00:53:19 +0900

    [test] LoginFormContainer의 input label 유무를 테스트 한다.

---
+++ src/LoginFormContainer.test.jsx
@@ -0,0 +1,18 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginFormContainer from './LoginFormContainer';
+
+describe('LoginFormContainer', () => {
+  it('input control들을 렌더링한다.', () => {
+    const { getByLabelText } = render((
+      <MemoryRouter>
+        <LoginFormContainer />
+      </MemoryRouter>
+    ));
+
+    expect(getByLabelText('Username')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+});
