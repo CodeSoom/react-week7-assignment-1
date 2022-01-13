df8edfe9b794e05fa949ff69fcf16a16b720eb84
jeongseop.lee <sjl0614@naver.com>
2022-01-13 21:07:23 +0900

    RevieForm에 TextField 적용

--- src/Components/ReviewForm.jsx
+++ src/Components/ReviewForm.jsx
@@ -1,32 +1,19 @@
+import TextField from './TextField';
+
 export default function ReviewForm({ onChange }) {
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
   return (
     <>
-      <div>
-        <label htmlFor="review-store">
-          평점
-        </label>
-        <input
-          type="number"
-          id="review-store"
-          name="score"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="review-description">
-          리뷰 내용
-        </label>
-        <input
-          type="text"
-          id="review-description"
-          name="description"
-          onChange={handleChange}
-        />
-      </div>
+      <TextField
+        label="평점"
+        type="number"
+        name="score"
+        onChange={onChange}
+      />
+      <TextField
+        label="리뷰 내용"
+        name="description"
+        onChange={onChange}
+      />
     </>
   );
 }
