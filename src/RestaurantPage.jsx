6c8c012a8890f61f36f4071bb2850511a224d117
Superduper-India <sunyoung1542@gmail.com>
2022-01-13 14:22:20 +0900

    Green: RestaurantPage test/ 리뷰라벨 인풋 렌더 구현

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -8,14 +8,26 @@ export default function RestaurantPage({ params }) {
   return (
     <div>
       <RestaurantContainer restaurantId={id} />
-      <label htmlFor="rating-input">
-        평점
-      </label>
-      <input
-        id="rating-input"
-        type="number"
-        name="rating"
-      />
+      <div>
+        <label htmlFor="rating-input">
+          평점
+        </label>
+        <input
+          id="rating-input"
+          type="number"
+          name="rating"
+        />
+      </div>
+      <div>
+        <label htmlFor="review-input">
+          리뷰
+        </label>
+        <input
+          id="review-input"
+          type="number"
+          name="review"
+        />
+      </div>
     </div>
   );
 }
