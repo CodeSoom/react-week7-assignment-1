cf70ea384f5aedc8551fc90b76b5688d3999283a
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-13 22:02:32 +0900

    [구현/테스트] LogoutForm

---
+++ src/components/LogoutForm.test.jsx
@@ -0,0 +1,27 @@
+import { fireEvent, render } from '@testing-library/react';
+
+import LogoutForm from './LogoutForm';
+
+describe('LogoutForm', () => {
+  const onClick = jest.fn();
+
+  const rendersLogoutForm = () => (render((
+    <LogoutForm
+      onClick={onClick}
+    />
+  )));
+
+  it('renders "Log out" button', () => {
+    const { getByRole } = rendersLogoutForm();
+
+    expect(getByRole('button', { name: 'Log out' })).not.toBeNull();
+  });
+
+  it('"Log out" button works', () => {
+    const { getByRole } = rendersLogoutForm();
+
+    fireEvent.click(getByRole('button', { name: 'Log out' }));
+
+    expect(onClick).toBeCalled();
+  });
+});
