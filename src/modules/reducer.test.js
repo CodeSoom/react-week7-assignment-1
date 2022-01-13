452b483570a2ecc22641ae8f67077c17c8de514c
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-13 18:38:38 +0900

    [구현/테스트] reducer - changeLoginField 구현

--- src/modules/reducer.test.js
+++ src/modules/reducer.test.js
@@ -7,6 +7,7 @@ import {
   setRestaurant,
   selectRegion,
   selectCategory,
+  changeLoginField,
 } from './actions';
 
 describe('reducer', () => {
@@ -18,6 +19,7 @@ describe('reducer', () => {
       restaurant: null,
       selectedRegion: null,
       selectedCategory: null,
+      loginField: {},
     };
 
     it('returns initialState', () => {
@@ -123,6 +125,35 @@ describe('reducer', () => {
         id: 1,
         name: '한식',
       });
+    });
+  });
+
+  describe('changeLoginField', () => {
+    const initialState = {
+      loginField: {
+        email: '',
+        password: '',
+      },
+    };
+
+    it('changes email', () => {
+      const state = reducer(initialState,
+        changeLoginField({
+          name: 'email',
+          value: 'tester@example.com',
+        }));
+
+      expect(state.loginField.email).toBe('tester@example.com');
+    });
+
+    it('changes password', () => {
+      const state = reducer(initialState,
+        changeLoginField({
+          name: 'password',
+          value: 'test',
+        }));
+
+      expect(state.loginField.password).toBe('test');
     });
   });
 });
