ebce461ae993fb3c7da0f42bf8488a1dbf32367e
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-13 20:32:02 +0900

    [구현/테스트] LoginPage - 로그아웃 기능추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,12 @@
-import { useDispatch } from 'react-redux';
-import { changeLoginField, requestLogin } from '../modules/actions';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { changeLoginField, requestLogin, logout } from '../modules/actions';
+import { get } from '../modules/utils';
 
 export default function LoginPage() {
   const dispatch = useDispatch();
+
+  const accessToken = useSelector(get('accessToken'));
 
   function handleChagne(event) {
     const { target: { name, value } } = event;
@@ -13,22 +17,38 @@ export default function LoginPage() {
     dispatch(requestLogin());
   }
 
+  function handleClickLogout() {
+    dispatch(logout());
+  }
+
   return (
     <>
       <h2>Log In</h2>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input type="email" id="login-email" name="email" onChange={handleChagne} />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          Password
-        </label>
-        <input type="password" id="login-password" name="password" onChange={handleChagne} />
-      </div>
-      <button type="button" onClick={handleSubmit}>Log in</button>
+      {accessToken
+        ? (
+          <div>
+            <button type="button" onClick={handleClickLogout}>Log out</button>
+          </div>
+        )
+        : (
+          <div>
+            <div>
+              <label htmlFor="login-email">
+                E-mail
+              </label>
+              <input type="email" id="login-email" name="email" onChange={handleChagne} />
+            </div>
+            <div>
+              <label htmlFor="login-password">
+                Password
+              </label>
+              <input type="password" id="login-password" name="password" onChange={handleChagne} />
+            </div>
+            <button type="button" onClick={handleSubmit}>Log in</button>
+          </div>
+        )}
+
+      <p>{accessToken}</p>
     </>
   );
 }
--- src/pages/LoginPage.test.jsx
+++ src/pages/LoginPage.test.jsx
@@ -1,11 +1,11 @@
 import { fireEvent, render } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import given from 'given2';
 
 import LoginPage from './LoginPage';
-import { changeLoginField } from '../modules/actions';
+import { changeLoginField, logout } from '../modules/actions';
 
 jest.mock('react-redux');
 
@@ -13,49 +13,68 @@ describe('LoginPage', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    accessToken: given.accessToken,
+  }));
 
   beforeEach(() => {
     dispatch.mockClear();
   });
+  context('when not logged in', () => {
+    it('renders "Log in" button', () => {
+      const { container, getByRole } = render(<LoginPage />);
 
-  it('renders "Log in" button', () => {
-    const { container, getByRole } = render(<LoginPage />);
+      expect(container).toHaveTextContent('Log In');
 
-    expect(container).toHaveTextContent('Log In');
-
-    expect(getByRole('button', { name: 'Log in' })).not.toBeNull();
-  });
-
-  it(' "Log in" button works', () => {
-    const { getByRole } = render(<LoginPage />);
-
-    fireEvent.click(getByRole('button', { name: 'Log in' }));
-    expect(dispatch).toBeCalled();
-  });
-
-  it('email input works', () => {
-    given('email', () => 'tester@example.com');
-    const { getByLabelText } = render(<LoginPage />);
-
-    fireEvent.change(getByLabelText('E-mail'), {
-      target: { value: given.email },
+      expect(getByRole('button', { name: 'Log in' })).not.toBeNull();
     });
 
-    expect(dispatch).toBeCalledWith(
-      changeLoginField({ name: 'email', value: given.email }),
-    );
-  });
+    it(' "Log in" button works', () => {
+      const { getByRole } = render(<LoginPage />);
 
-  it('password input works', () => {
-    given('password', () => 'test');
-
-    const { getByLabelText } = render(<LoginPage />);
-    fireEvent.change(getByLabelText('Password'), {
-      target: { value: given.password },
+      fireEvent.click(getByRole('button', { name: 'Log in' }));
+      expect(dispatch).toBeCalled();
     });
 
-    expect(dispatch).toBeCalledWith(
-      changeLoginField({ name: 'password', value: given.password }),
-    );
+    it('email input works', () => {
+      given('email', () => 'tester@example.com');
+      const { getByLabelText } = render(<LoginPage />);
+
+      fireEvent.change(getByLabelText('E-mail'), {
+        target: { value: given.email },
+      });
+
+      expect(dispatch).toBeCalledWith(
+        changeLoginField({ name: 'email', value: given.email }),
+      );
+    });
+
+    it('password input works', () => {
+      given('password', () => 'test');
+
+      const { getByLabelText } = render(<LoginPage />);
+      fireEvent.change(getByLabelText('Password'), {
+        target: { value: given.password },
+      });
+
+      expect(dispatch).toBeCalledWith(
+        changeLoginField({ name: 'password', value: given.password }),
+      );
+    });
+  });
+
+  context('when logged in', () => {
+    given('accessToken', () => 'ACCESS_TOKEN');
+    it('renders "Log out" button', () => {
+      const { getByRole } = render(<LoginPage />);
+
+      expect(getByRole('button', { name: 'Log out' })).not.toBeNull();
+    });
+    it('"Log out" button works', () => {
+      const { getByRole } = render(<LoginPage />);
+      fireEvent.click(getByRole('button', { name: 'Log out' }));
+
+      expect(dispatch).toBeCalledWith(logout());
+    });
   });
 });
