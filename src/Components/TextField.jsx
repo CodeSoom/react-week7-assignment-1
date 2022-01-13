02a2248c86832455cc8d73da059dee96af325ba0
jeongseop.lee <sjl0614@naver.com>
2022-01-13 20:52:02 +0900

    TextField 테스트 통과

--- src/Components/TextField.jsx
+++ src/Components/TextField.jsx
@@ -1,6 +1,19 @@
-export default function ReviewForm() {
+export default function TextField({ onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
   return (
-    <>
-    </>
+    <div>
+      <label htmlFor="review-store">
+        평점
+      </label>
+      <input
+        type="number"
+        id="review-store"
+        name="score"
+        onChange={handleChange}
+      />
+    </div>
   );
 }
