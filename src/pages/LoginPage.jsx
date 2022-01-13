cc5f8aeeca2cf46f382c6c3cd277a85e1dfd42a5
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-13 18:37:34 +0900

    [구현/테스트] LoginPage - 입력한 값이 state에 저장 구현 , 그러기위해서 changeLoginField action 활용

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { changeLoginField } from '../modules/actions';
+// import { get } from "../modules/utils";
+
 export default function LoginPage() {
+  const dispatch = useDispatch();
+
+  // const { email, password } = useSelector(get('loginField'))
+
+  function handleChagne(event) {
+    const { target: { name, value } } = event;
+    dispatch(changeLoginField({ name, value }));
+  }
+
   return (
     <>
       <h2>Log In</h2>
@@ -6,13 +19,13 @@ export default function LoginPage() {
         <label htmlFor="login-email">
           E-mail
         </label>
-        <input type="email" id="login-emali" />
+        <input type="email" id="login-email" name="email" onChange={handleChagne} />
       </div>
       <div>
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="email" id="login-password" />
+        <input type="password" id="login-password" name="password" onChange={handleChagne} />
       </div>
       <button type="button">Log in</button>
 
--- src/pages/LoginPage.test.jsx
+++ src/pages/LoginPage.test.jsx
@@ -1,12 +1,54 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
+
+import given from 'given2';
 
 import LoginPage from './LoginPage';
+import { changeLoginField } from '../modules/actions';
+
+jest.mock('react-redux');
 
 describe('LoginPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
+
   it('renders "Log in" button', () => {
     const { container, getByRole } = render(<LoginPage />);
 
     expect(container).toHaveTextContent('Log In');
+
     expect(getByRole('button', { name: 'Log in' })).not.toBeNull();
+  });
+
+  it('email input works', () => {
+    given('email', () => 'tester@example.com');
+    const { getByLabelText } = render(<LoginPage />);
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: given.email },
+    });
+
+    expect(dispatch).toBeCalledWith(
+      changeLoginField({ name: 'email', value: given.email }),
+    );
+  });
+
+  it('password input works', () => {
+    given('password', () => 'test');
+
+    const { getByLabelText } = render(<LoginPage />);
+    fireEvent.change(getByLabelText('Password'), {
+      target: { value: given.password },
+    });
+
+    expect(dispatch).toBeCalledWith(
+      changeLoginField({ name: 'password', value: given.password }),
+    );
   });
 });
