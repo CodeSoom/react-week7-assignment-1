4ad2418ab6cd991a8e953810597b45011452e497
Superduper-India <sunyoung1542@gmail.com>
2022-01-14 21:26:59 +0900

    Green: LogoutForm/ onClick 이벤트 콜 구현

--- src/LogoutForm.jsx
+++ src/LogoutForm.jsx
@@ -1,8 +1,9 @@
 // 관심사: 로그아웃 폼을 화면에 나타내기
-export default function LogoutForm() {
+export default function LogoutForm({ onClickLogout }) {
   return (
     <button
       type="button"
+      onClick={onClickLogout}
     >
       Log Out
     </button>
