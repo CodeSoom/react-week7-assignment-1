e0b4c8a8cdf71603e14bd2a828618224432fa3eb
Superduper-India <sunyoung1542@gmail.com>
2022-01-14 20:21:07 +0900

    Green: LogoutForm test/ Log Out 버튼 렌더 구현

--- src/LogoutForm.jsx
+++ src/LogoutForm.jsx
@@ -1,6 +1,10 @@
 // 관심사: 로그아웃 폼을 화면에 나타내기
 export default function LogoutForm() {
   return (
-    <><button type="button" /></>
+    <button
+      type="button"
+    >
+      Log Out
+    </button>
   );
 }
