a7d0e520a333d5ffd0b7deac9caa46e538c720f3
padawanR0k <padawanR0k@gmail.com>
2022-01-15 15:28:27 +0900

    Login.jsx 구현

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,5 +3,30 @@ export default function LoginForm({
   pw,
   onChange,
 }) {
-  return (null);
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="id">
+          아이디
+        </label>
+        <input
+          type="text"
+          onChange={({ target: { name, value } }) => onChange(name, value)}
+          value={id}
+          id="id"
+          name="id"
+        />
+        <label htmlFor="pw">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          onChange={({ target: { name, value } }) => onChange(name, value)}
+          value={pw}
+          id="pw"
+          name="pw"
+        />
+      </form>
+    </div>
+  );
 }
