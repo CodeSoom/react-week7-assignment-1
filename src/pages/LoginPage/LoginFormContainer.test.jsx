9de5e83e17f966b698c1ffd643fc1ed2c8156f9c
Ravi <ravi@inflearn.com>
2022-01-15 18:41:09 +0900

    LoginForm: login

--- src/pages/LoginPage/LoginFormContainer.test.jsx
+++ src/pages/LoginPage/LoginFormContainer.test.jsx
@@ -2,13 +2,17 @@ import { fireEvent, render } from '@testing-library/react';
 import { useDispatch } from 'react-redux';
 
 import useForm from '../../hooks/useForm';
+import { postLogin } from '../../store/actions';
 
 import LoginFormContainer from './LoginFormContainer';
 
 jest.mock('../../hooks/useForm');
+jest.mock('../../store/actions');
 
 describe('LoginFormContainer', () => {
   useForm.mockImplementation(() => ({ values: { email: 'test@email.com', password: 'pass' } }));
+  postLogin.mockImplementation(() => jest.fn());
+
   const dispatch = jest.fn();
   const renderLoginFormContainer = () => render(<LoginFormContainer />);
 
@@ -19,12 +23,13 @@ describe('LoginFormContainer', () => {
   useDispatch.mockImplementation(() => dispatch);
 
   context('when clicks submit button', () => {
-    it('calls dispatch', () => {
+    it('calls dispatch and postLogin with email and password', () => {
       const { container } = renderLoginFormContainer();
 
       fireEvent.click(container.querySelector('button[type="submit"]'));
 
       expect(dispatch).toBeCalled();
+      expect(postLogin).toBeCalledWith({ email: 'test@email.com', password: 'pass' });
     });
   });
 });
