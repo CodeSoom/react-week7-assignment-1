02bad1053b456dd6ad955764430763043192d98a
Ravi <ravi@inflearn.com>
2022-01-15 18:09:45 +0900

    LoginForm: changes email & password test

--- src/pages/LoginPage/LoginForm.test.jsx
+++ src/pages/LoginPage/LoginForm.test.jsx
@@ -4,10 +4,12 @@ import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   const handleSubmit = jest.fn();
+  const handleChange = jest.fn();
 
   const renderLoginForm = ({ values }) => render(
     <LoginForm
       values={values}
+      onChange={handleChange}
       onSubmit={handleSubmit}
     />,
   );
@@ -24,6 +26,23 @@ describe('LoginForm', () => {
 
     expect(getByLabelText('E-mail')).toHaveValue(email);
     expect(getByLabelText('Password')).toHaveValue(password);
+  });
+
+  context('when inputs are changed', () => {
+    it('calls onChange', () => {
+      const email = 'test@test.com';
+      const password = 'pass';
+
+      const { getByLabelText } = renderLoginForm({ values: { email, password } });
+
+      const newEmail = 'new@new.com';
+      fireEvent.change(getByLabelText('E-mail'), { target: { value: newEmail } });
+      expect(handleChange).toBeCalledWith({ email: newEmail, password });
+
+      const newPassword = 'newpass';
+      fireEvent.change(getByLabelText('Password'), { target: { value: newPassword } });
+      expect(handleChange).toBeCalledWith({ email, password: newPassword });
+    });
   });
 
   context('when the "Log In" button is clicked', () => {
