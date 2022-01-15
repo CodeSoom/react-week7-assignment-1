d5c2cde0e88bf556e16b086dd20226a8477b757a
Ravi <ravi@inflearn.com>
2022-01-15 22:18:42 +0900

    useForm test

--- src/hooks/useForm.test.js
+++ src/hooks/useForm.test.js
@@ -3,14 +3,44 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useForm from './useForm';
 
 describe('useForm', () => {
-  it('', () => {
-    const handleSubmit = jest.fn();
-    const { result } = renderHook(() => useForm({ onSubmit: handleSubmit }));
+  const handleSubmit = jest.fn();
+
+  const renderUseForm = ({ initialValues }) => renderHook(() => useForm({
+    initialValues,
+    onSubmit: handleSubmit,
+  }));
+
+  beforeEach(() => {
+    handleSubmit.mockClear();
+  });
+
+  context('when submit is called', () => {
+    it('calls onSubmit', () => {
+      const { result } = renderUseForm({});
+
+      act(() => {
+        result.current.submit();
+      });
+
+      expect(handleSubmit).toBeCalled();
+    });
+  });
+
+  it('returns values from initialValues', () => {
+    const initialValues = { a: 1, b: 2 };
+
+    const { result } = renderUseForm({ initialValues });
+
+    expect(result.current.values).toEqual(initialValues);
+  });
+
+  it('changeValues changes value', () => {
+    const { result } = renderUseForm({ initialValues: { a: 1 } });
 
     act(() => {
-      result.current.submit();
+      result.current.changeValues({ a: 2 });
     });
 
-    expect(handleSubmit).toBeCalled();
+    expect(result.current.values.a).toBe(2);
   });
 });
