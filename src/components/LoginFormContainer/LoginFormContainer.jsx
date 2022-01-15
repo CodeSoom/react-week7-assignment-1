b69079634cad31513e514db1cb759e5dc65e63ad
padawanR0k <padawanR0k@gmail.com>
2022-01-15 16:08:59 +0900

    LoginFormContainer.jsx 초안 구현

--- src/components/LoginFormContainer/LoginFormContainer.jsx
+++ src/components/LoginFormContainer/LoginFormContainer.jsx
@@ -1,3 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import LoginForm from '../LoginForm/LoginForm';
+
 export default function LoginFormContainer() {
-  return (null);
+  const dispatch = useDispatch();
+  const {
+    id,
+    pw,
+  } = useSelector((state) => state.loginForm);
+
+  return (
+    <LoginForm
+      id={id}
+      pw={pw}
+      onChange={() => dispatch()}
+      onSubmit={() => dispatch()}
+    />
+  );
 }
