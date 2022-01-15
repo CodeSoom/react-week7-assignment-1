422b7486e85872c8e7f1f3eb3b8115ff8edabca5
Ravi <ravi@inflearn.com>
2022-01-15 22:56:37 +0900

    Form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,11 +1,27 @@
-export default function Form({ onSubmit, children }) {
+import FormContext from './FormContext';
+
+export default function Form({ form, onSubmit, children }) {
+  if (!form) {
+    return (
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit(event);
+      }}
+      >
+        {children}
+      </form>
+    );
+  }
+
   return (
-    <form onSubmit={(event) => {
-      event.preventDefault();
-      onSubmit(event);
-    }}
-    >
-      {children}
-    </form>
+    <FormContext.Provider value={{ form, onSubmit }}>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit(event);
+      }}
+      >
+        {children}
+      </form>
+    </FormContext.Provider>
   );
 }
