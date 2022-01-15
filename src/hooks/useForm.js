0e6ca105658e30824493cff36e1cab2004082fd7
Ravi <ravi@inflearn.com>
2022-01-15 22:18:58 +0900

    useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
-export default function useForm({ initialValues, onSubmit }) {
-  const [values, setValues] = useState(initialValues);
+export default function useForm({ initialValues = {}, onSubmit }) {
+  const keys = Object.keys(initialValues);
+  const stateAndSetterEntries = keys.map((key) => [key, useState(initialValues[key])]);
+  const stateAndSettersMap = Object.fromEntries(stateAndSetterEntries);
+
+  const values = Object.fromEntries(stateAndSetterEntries.map(([key, [state]]) => [key, state]));
 
   const changeValues = (newValues) => {
-    setValues({ ...values, ...newValues });
+    Object.keys(newValues).forEach((key) => {
+      stateAndSettersMap[key][1](newValues[key]);
+    });
+  };
+
+  const submit = () => {
+    onSubmit(values);
   };
 
   return {
     values,
-    submit: onSubmit,
+    submit,
     changeValues,
   };
 }
