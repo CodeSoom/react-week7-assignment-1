e3649f91ff7c96901a2314a6e3eb0982f16e786c
Superduper-India <sunyoung1542@gmail.com>
2022-01-15 20:27:24 +0900

    Refactoring: Review/ 강의보고 리뷰목록 내림차순 정렬

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,10 +1,12 @@
 // 관심사: 상태그려주기
 export default function Review({ reviews }) {
+  const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
+
   return (
     <>
       <h2>리뷰</h2>
       <ul>
-        {reviews.map((review) => (
+        {sortedReviews.map((review) => (
           <li key={review.id}>
             {`${review.name} | ${review.score} | ${review.description}`}
           </li>
