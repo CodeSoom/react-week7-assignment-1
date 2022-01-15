8b18655561c1a86008b144f0e733404fa51603f2
padawanR0k <padawanR0k@gmail.com>
2022-01-15 17:10:29 +0900

    logout시 쿠키 삭제하는 기능 추가

--- src/LogoutPage.jsx
+++ src/LogoutPage.jsx
@@ -1,8 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { setAccessToken } from './actions';
+
 export default function LogoutPage() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleClick = () => {
+    dispatch(setAccessToken(''));
+    history.push('/login');
+  };
+
   return (
     <div>
       <h1>Logout</h1>
-      <button type="button">logout</button>
+      <button onClick={() => handleClick()} type="button">logout</button>
     </div>
   );
 }
