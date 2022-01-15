811453ebb1c8b87e68133b8ade31a335bc1469ac
padawanR0k <padawanR0k@gmail.com>
2022-01-15 14:47:24 +0900

    Reviews.test.jsx 리뷰가 0개일   분기처리 추가

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,20 +3,22 @@ export default function Reviews({ reviews }) {
     <div>
       <h2>리뷰</h2>
       <ul>
-        {reviews.map(({
-          name, score, description, id,
-        }) => (
-          <li key={id}>
-            <ul>
-              <li>{name}</li>
-              <li>
-                {score}
-                점
-              </li>
-              <li>{description}</li>
-            </ul>
-          </li>
-        ))}
+        {!reviews.length
+          ? '리뷰가 존재하지 않습니다.'
+          : reviews.map(({
+            name, score, description, id,
+          }) => (
+            <li key={id}>
+              <ul>
+                <li>{name}</li>
+                <li>
+                  {score}
+                  점
+                </li>
+                <li>{description}</li>
+              </ul>
+            </li>
+          ))}
       </ul>
     </div>
   );
--- src/components/Reviews/Reviews.test.jsx
+++ src/components/Reviews/Reviews.test.jsx
@@ -4,14 +4,22 @@ import Reviews from './Reviews';
 import restaurant from '../../../fixtures/restaurant';
 
 describe('Reviews', () => {
-  it('리뷰 목록을 보여준다.', () => {
-    const { reviews } = restaurant;
-    const { queryAllByText } = render(<Reviews reviews={reviews} />);
+  context('리뷰 목록이', () => {
+    it('있으면 리뷰들을 보여준다.', () => {
+      const { reviews } = restaurant;
+      const { queryAllByText } = render(<Reviews reviews={reviews} />);
 
-    reviews.forEach(({ name, description, score }) => {
-      expect(queryAllByText(name)).not.toBeNull();
-      expect(queryAllByText(description)).not.toBeNull();
-      expect(queryAllByText(score)).not.toBeNull();
+      reviews.forEach(({ name, description, score }) => {
+        expect(queryAllByText(name)).not.toBeNull();
+        expect(queryAllByText(description)).not.toBeNull();
+        expect(queryAllByText(score)).not.toBeNull();
+      });
+    });
+
+    it('있으면 "리뷰가 존재하지 않습니다."를 보여준다', () => {
+      const { getByText } = render(<Reviews reviews={[]} />);
+
+      expect(getByText(/리뷰가 존재하지 않습니다\./)).not.toBeNull();
     });
   });
 });
