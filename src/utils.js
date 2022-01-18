33ffe57f3f3d5bd9deb6350d448d4612571b9d53
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-18 17:53:09 +0900

    [구현] utils - sort하는 함수를 지웠다가 테스트 coverage 때문에 다시 만듬 tenOfRecentReviews

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,12 @@ export function get(key) {
 export function equal(key, value) {
   return (obj) => obj[key] === value;
 }
+
+export function tenOfRecentReviews(reviews) {
+  const sortedRieviews = [...reviews].sort(
+    (a, b) => (b.id - a.id),
+  );
+  const tenOftheReviews = sortedRieviews.slice(0, 10);
+
+  return tenOftheReviews;
+}
