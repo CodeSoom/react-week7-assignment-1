2544633679306f6c46566844f971ec08bbb0eb24
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-18 14:31:03 +0900

    [리팩토링] utils - 사용하지 않은 함수 reviewsTo10 제거

--- src/utils.js
+++ src/utils.js
@@ -5,11 +5,3 @@ export function get(key) {
 export function equal(key, value) {
   return (obj) => obj[key] === value;
 }
-
-export function reviewsTo10(reviews) {
-  const slicedReviews = reviews.slice(-10);
-  const sortedReviewsComparedWithIds = slicedReviews.sort(
-    (a, b) => (b.id - a.id),
-  );
-  return sortedReviewsComparedWithIds;
-}
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,4 @@
-import { get, equal, reviewsTo10 } from './utils';
+import { get, equal } from './utils';
 
 test('get', () => {
   const state = {
@@ -22,10 +22,4 @@ test('equal', () => {
 
   expect(f(state)).toBeTruthy();
   expect(g(state)).toBeFalsy();
-});
-
-test('reviewsTo10', () => {
-  const state = [{ id: 1 }, { id: 2 }, { id: 3 }];
-
-  expect(reviewsTo10(state)).toEqual([{ id: 3 }, { id: 2 }, { id: 1 }]);
 });
