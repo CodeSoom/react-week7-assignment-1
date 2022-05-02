1a2456a72292f19b193482e2e6524fbf7044da8c
이찬호 <0407chan@naver.com>
2022-05-02 11:15:47 +0900

    [강의1] App에 Login 라우트 추가

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,7 +6,9 @@ export default function HomePage() {
       <h2>Home</h2>
       <ul>
         <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
         <li><Link to="/restaurants">Restaurants</Link></li>
+        <li><Link to="/xxx">멸망의 길</Link></li>
       </ul>
     </div>
   );
