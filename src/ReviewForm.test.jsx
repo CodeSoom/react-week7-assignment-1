ba098ff7d3f9b4a0d3f75a6dc23288a8466fb185
이찬호 <0407chan@naver.com>
2022-05-03 00:20:12 +0900

    [강의2] ReviewForm 테스트 추가

---
+++ src/ReviewForm.test.jsx
@@ -0,0 +1,54 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import ReviewForm from './ReviewForm';
+
+describe('ReviewForm', () => {
+  const handleChange = jest.fn();
+  const handleSubmit = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+    handleSubmit.mockClear();
+  });
+
+  function renderReviewForm() {
+    return render(
+      <ReviewForm
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />,
+    );
+  }
+
+  it('리뷰 폼을 렌더한다.', () => {
+    const { queryByLabelText } = renderReviewForm();
+
+    expect(queryByLabelText('평점')).not.toBeNull();
+    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('리뷰 폼에 입력 이벤트가 동작한다.', () => {
+    const { getByLabelText } = renderReviewForm();
+
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: '정말 최고!' },
+    ];
+
+    controls.forEach(({
+      label, name, value,
+    }) => {
+      fireEvent.change(getByLabelText(label), { target: { value } });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
+  });
+
+  it('"리뷰 남기기"버튼을 렌더한다.', () => {
+    const { getByText } = renderReviewForm();
+
+    fireEvent.click(getByText('리뷰 남기기'));
+
+    expect(handleSubmit).toBeCalled();
+  });
+});
