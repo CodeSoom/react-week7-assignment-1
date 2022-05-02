888ea12c05ac63b25789b859663451a0ac2e9926
이찬호 <0407chan@naver.com>
2022-05-03 00:20:48 +0900

    [강의2] ReviewForm 컴포넌트 RestaurantContainer에 적용

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,43 +3,15 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import ReviewForm from './ReviewForm';
 
 import {
   loadRestaurant,
   changeReviewField,
+  sendReview,
 } from './actions';
 
 import { get } from './utils';
-
-function ReviewForm({ onChange }) {
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
-  return (
-    <>
-      <div>
-        <label htmlFor="review-score">평점</label>
-        <input
-          id="review-score"
-          type="number"
-          name="score"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="review-description">리뷰 내용</label>
-        <input
-          id="review-description"
-          type="text"
-          name="description"
-          onChange={handleChange}
-        />
-      </div>
-    </>
-  );
-}
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
@@ -49,6 +21,7 @@ export default function RestaurantContainer({ restaurantId }) {
   }, []);
 
   const restaurant = useSelector(get('restaurant'));
+  const accessToken = useSelector(get('accessToken'));
 
   if (!restaurant) {
     return (
@@ -60,10 +33,20 @@ export default function RestaurantContainer({ restaurantId }) {
     dispatch(changeReviewField({ name, value }));
   }
 
+  function handleSubmit() {
+    dispatch(sendReview({ restaurantId }));
+  }
+
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
-      <ReviewForm onChange={handleChange} />
+      {accessToken
+        ? (
+          <ReviewForm
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        ) : null}
     </>
   );
 }
