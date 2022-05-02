461a64fa40f06984d2f1d367091b65b9c9887010
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-05-02 22:27:58 +0900

    Make test work
    Make test work.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,18 @@ import {
 } from 'react-router-dom';
 
 import HomePage from './HomePage';
+// Todo: separate loginpage
+// import LoginPage from './LoginPage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 import RestaurantPage from './RestaurantPage';
 import NotFoundPage from './NotFoundPage';
+
+function LoginPage() {
+  return (
+    <h2>Log In</h2>
+  );
+}
 
 export default function App() {
   return (
@@ -20,6 +28,7 @@ export default function App() {
       </header>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
         <Route path="/about" component={AboutPage} />
         <Route exact path="/restaurants" component={RestaurantsPage} />
         <Route path="/restaurants/:id" component={RestaurantPage} />
