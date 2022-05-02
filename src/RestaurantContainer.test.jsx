49c5a4724f958729207727e320e6406bd3af26cd
이찬호 <0407chan@naver.com>
2022-05-02 22:48:31 +0900

    [강의2] 리뷰폼에 대한 테스트 작성

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -48,6 +48,49 @@ describe('RestaurantContainer', () => {
       const { container } = renderRestaurantContainer();
 
       expect(container).toHaveTextContent('Loading');
+    });
+  });
+
+  it('리뷰 폼을 렌더한다.', () => {
+    given('restaurant', () => ({
+      id: 1,
+      name: '마법사주방',
+      address: '서울시 강남구',
+    }));
+
+    const { queryByLabelText } = render(<RestaurantContainer
+      restaurantId="1"
+    />);
+
+    expect(queryByLabelText('평점')).not.toBeNull();
+    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('리뷰 폼에 입력 이벤트가 동작한다.', () => {
+    given('restaurant', () => ({
+      id: 1,
+      name: '마법사주방',
+      address: '서울시 강남구',
+    }));
+
+    const { getByLabelText } = render(<RestaurantContainer
+      restaurantId="1"
+    />);
+
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: '정말 최고!' },
+    ];
+
+    controls.forEach(({
+      label, name, value,
+    }) => {
+      fireEvent.change(getByLabelText(label), { target: { value } });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeReviewField',
+        payload: { name, value },
+      });
     });
   });
 });
