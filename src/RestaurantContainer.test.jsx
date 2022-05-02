1d9fc516f43732adf10df8416ef9ebe48d8a0a38
이찬호 <0407chan@naver.com>
2022-05-03 00:20:45 +0900

    [강의2] ReviewForm 컴포넌트 RestaurantContainer에 적용

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -17,6 +17,11 @@ describe('RestaurantContainer', () => {
 
     useSelector.mockImplementation((selector) => selector({
       restaurant: given.restaurant,
+      reviewFields: {
+        score: '',
+        description: '',
+      },
+      accessToken: given.accessToken,
     }));
   });
 
@@ -51,46 +56,85 @@ describe('RestaurantContainer', () => {
     });
   });
 
-  it('리뷰 폼을 렌더한다.', () => {
-    given('restaurant', () => ({
-      id: 1,
-      name: '마법사주방',
-      address: '서울시 강남구',
-    }));
+  context('로그아웃 했을 때', () => {
+    it('리뷰 폼을 렌더하지 않는다', () => {
+      given('restaurant', () => ({
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+      }));
 
-    const { queryByLabelText } = render(<RestaurantContainer
-      restaurantId="1"
-    />);
+      const { queryByLabelText } = render(<RestaurantContainer
+        restaurantId="1"
+      />);
 
-    expect(queryByLabelText('평점')).not.toBeNull();
-    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+      expect(queryByLabelText('평점')).toBeNull();
+      expect(queryByLabelText('리뷰 내용')).toBeNull();
+    });
   });
 
-  it('리뷰 폼에 입력 이벤트가 동작한다.', () => {
-    given('restaurant', () => ({
-      id: 1,
-      name: '마법사주방',
-      address: '서울시 강남구',
-    }));
+  context('로그인 했을 때', () => {
+    // TODO: accessToken 세팅
+    given('accessToken', () => 'ACCESS_TOEKEN');
+    it('리뷰 폼을 렌더한다.', () => {
+      given('restaurant', () => ({
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+      }));
 
-    const { getByLabelText } = render(<RestaurantContainer
-      restaurantId="1"
-    />);
+      const { queryByLabelText } = render(<RestaurantContainer
+        restaurantId="1"
+      />);
 
-    const controls = [
-      { label: '평점', name: 'score', value: '5' },
-      { label: '리뷰 내용', name: 'description', value: '정말 최고!' },
-    ];
+      expect(queryByLabelText('평점')).not.toBeNull();
+      expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+    });
 
-    controls.forEach(({
-      label, name, value,
-    }) => {
-      fireEvent.change(getByLabelText(label), { target: { value } });
+    it('리뷰 폼에 입력 이벤트가 동작한다.', () => {
+      given('restaurant', () => ({
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+      }));
 
-      expect(dispatch).toBeCalledWith({
-        type: 'changeReviewField',
-        payload: { name, value },
+      const { getByLabelText } = render(<RestaurantContainer
+        restaurantId="1"
+      />);
+
+      const controls = [
+        { label: '평점', name: 'score', value: '5' },
+        { label: '리뷰 내용', name: 'description', value: '정말 최고!' },
+      ];
+
+      controls.forEach(({
+        label, name, value,
+      }) => {
+        fireEvent.change(getByLabelText(label), { target: { value } });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'changeReviewField',
+          payload: { name, value },
+        });
       });
+    });
+
+    it('"리뷰 남기기"버튼을 출력한다.', () => {
+      given('restaurant', () => ({
+        id: 1,
+        name: '마법사주방',
+        address: '서울시 강남구',
+      }));
+
+      const { getByText } = render(
+        <RestaurantContainer
+          restaurantId="1"
+        />,
+      );
+
+      fireEvent.change(getByText('리뷰 남기기'));
+
+      expect(dispatch).toBeCalledTimes(1);
     });
   });
 });
