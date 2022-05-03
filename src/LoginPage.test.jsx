337d213d96fd4d190964b271df37aded0b8d9c15
hdpyo <hudongpyo.dev@gmail.com>
2022-05-03 15:30:12 +0900

    Refactor test : <LoginPage />
    - Add "import LoginPage from './LoginPage';"

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -2,6 +2,8 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
+import LoginPage from './LoginPage';
+
 describe('LoginPage', () => {
   const renderLoginPage = () => render(
     <MemoryRouter>
