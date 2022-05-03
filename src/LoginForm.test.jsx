c71864c516d2abd57eff3a34cee1eef4bd81c4ba
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-05-03 19:59:32 +0900

    Add LoginForm test
    Add LoginForm test.

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,13 +1,19 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  const handleClick = jest.fn();
+  const handleChange = jest.fn();
+
   const renderLoginForm = () => render(
     <MemoryRouter initialEntries={['/login']}>
-      <LoginForm />
+      <LoginForm
+        onSubmit={handleClick}
+        onChange={handleChange}
+      />
     </MemoryRouter>,
   );
 
@@ -22,5 +28,30 @@ describe('LoginForm', () => {
 
     expect(queryByLabelText('E-mail')).not.toBeNull();
     expect(queryByLabelText('Password')).not.toBeNull();
+  });
+
+  it('listens click event', () => {
+    const { getByText } = renderLoginForm();
+
+    fireEvent.click(getByText('login'));
+    expect(handleClick).toBeCalled();
+  });
+
+  it('listens change event', () => {
+    const { getByLabelText } = renderLoginForm();
+
+    fireEvent.change(getByLabelText('E-mail'), { target: { value: 'test@email.com' } });
+
+    expect(handleChange).toBeCalledWith({
+      name: 'email',
+      value: 'test@email.com',
+    });
+
+    fireEvent.change(getByLabelText('Password'), { target: { value: '1234' } });
+
+    expect(handleChange).toBeCalledWith({
+      name: 'password',
+      value: '1234',
+    });
   });
 });
