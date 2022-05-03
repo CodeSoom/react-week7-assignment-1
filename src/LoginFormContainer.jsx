f47710c8c23183a7c7b4d42b1449feec60409692
kjoobin <imkj1148@gmail.com>
2022-05-03 20:50:39 +0900

    사용하지 않는 코드 삭제

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,7 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setLoginField } from './actions';
 import LoginForm from './LoginForm';
-import { get } from './utils';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
