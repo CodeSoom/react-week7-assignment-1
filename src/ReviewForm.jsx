fd16f9cfc91128b4ebc53fd81cd2e43b82d293a3
이찬호 <0407chan@naver.com>
2022-05-03 20:26:31 +0900

    refactor. 리뷰남기기 후 input값을 지우기 위해 ReviewForm에 reviewFields받고, TextField에 각 값 전달

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,17 +1,19 @@
 import TextField from './TextField';
 
-export default function ReviewForm({ onChange, onSubmit }) {
+export default function ReviewForm({ reviewFields, onChange, onSubmit }) {
   return (
     <>
       <TextField
         label="평점"
         name="score"
         type="number"
+        inputValue={reviewFields.score}
         onChange={onChange}
       />
       <TextField
         label="리뷰 내용"
         name="description"
+        inputValue={reviewFields.description}
         onChange={onChange}
       />
       <button
