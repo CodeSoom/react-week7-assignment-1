c0aa65d9a8ae61ecec902cc9b294f38aef076516
kjoobin <imkj1148@gmail.com>
2022-05-03 19:08:37 +0900

    LoginFormContainer 와 LoginForm 연결

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,8 +1,7 @@
+import LoginForm from './LoginForm';
+
 export default function LoginFormContainer() {
   return (
-    <div>
-      <h2>Login 페이지</h2>
-      <p>Login 페이지입니다.</p>
-    </div>
+    <LoginForm />
   );
 }
