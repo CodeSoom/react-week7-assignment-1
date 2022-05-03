dd00e5f1c561b9a76688e63e891bafe338b46625
hdpyo <hudongpyo.dev@gmail.com>
2022-05-03 19:07:29 +0900

    Pass failing test : <LoginForm />
    - renders input controls

---
+++ src/LoginForm.jsx
@@ -0,0 +1,32 @@
+export default function LoginForm() {
+  function handleClick() {
+    // TODO
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">email</label>
+        <input
+          type="email"
+          placeholder="email"
+          id="login-email"
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">password</label>
+        <input
+          type="password"
+          placeholder="password"
+          id="login-password"
+        />
+      </div>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Log In
+      </button>
+    </>
+  );
+}
