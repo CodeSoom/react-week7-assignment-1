a6c031b677406f6022f5378ae5823f60d261bed6
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-05-04 02:15:57 +0900

    Make test run
    Make test run.

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -5,26 +5,28 @@ import TextField from './TextField';
 describe('TextField', () => {
   const handleChange = jest.fn();
 
-  const renderTextField = ({ label, name }) => render((
+  const label = 'E-mail';
+  const type = 'email';
+  const name = 'email';
+  const value = 'test@email.com';
+
+  const renderTextField = () => render((
     <TextField
       label={label}
+      type={type}
       name={name}
       onChange={handleChange}
     />
   ));
 
   it('renders label', () => {
-    const { queryByLabelText } = renderTextField({ label: 'E-mail', name: 'email' });
+    const { queryByLabelText } = renderTextField();
 
-    expect(queryByLabelText('E-mail').value).toBe('test@example.com');
+    expect(queryByLabelText('E-mail')).not.toBeNull();
   });
 
   it('listens change event', () => {
-    const label = 'E-mail';
-    const name = 'email';
-    const value = 'test@email.com';
-
-    const { getByLabelText } = renderTextField({ label, name });
+    const { getByLabelText } = renderTextField();
 
     const input = getByLabelText(label);
 
