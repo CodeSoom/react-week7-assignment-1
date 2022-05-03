0aa491051d1ed35204336a0b937c2094d2ab0407
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-05-03 09:06:22 +0900

    Refactor
    Refactor.

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,22 +2,26 @@ export default function LoginPage() {
   return (
     <>
       <h2>Log In</h2>
-      <label htmlFor="email-input">
-        E-mail
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="email-input"
-      />
-      <label htmlFor="password-input">
-        Password
-      </label>
-      <input
-        type="password"
-        name="password"
-        id="password-input"
-      />
+      <div>
+        <label htmlFor="email-input">
+          E-mail
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email-input"
+        />
+      </div>
+      <div>
+        <label htmlFor="password-input">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password-input"
+        />
+      </div>
     </>
   );
 }
