5caf4b3397eb49d9979dde1452495b9e0fb3ae5b
이찬호 <0407chan@naver.com>
2022-05-03 20:12:28 +0900

    [red] RestaurantContainer에 리뷰 있을때, 없을때 테스트 추가

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -137,4 +137,54 @@ describe('RestaurantContainer', () => {
       expect(dispatch).toBeCalledTimes(1);
     });
   });
+
+  context('리뷰가 있을 때', () => {
+    given('restaurant', () => ({
+      id: 1,
+      name: '마법사주방',
+      address: '서울시 강남구',
+      reviews: [{
+        id: 1,
+        restaurantId: 1,
+        name: '피카츄',
+        score: 5,
+        description: '피카츄가 더 최고야!',
+      },
+      {
+        id: 3,
+        restaurantId: 1,
+        name: '라이츄',
+        score: 3,
+        description: '라이츄가 최고야!',
+      }],
+    }));
+
+    it('리뷰를 출력한다.', () => {
+      const { queryByText } = render(<RestaurantContainer
+        restaurantId="1"
+      />);
+
+      expect(queryByText('리뷰')).not.toBeNull();
+      expect(queryByText('피카츄')).not.toBeNull();
+      expect(queryByText(5)).not.toBeNull();
+      expect(queryByText('피카츄가 더 최고야!')).not.toBeNull();
+    });
+  });
+
+  context('리뷰가 없을 때', () => {
+    given('restaurant', () => ({
+      id: 1,
+      name: '마법사주방',
+      address: '서울시 강남구',
+      reviews: [],
+    }));
+
+    it('"리뷰가 없어요!"를 출력한다.', () => {
+      const { queryByText } = render(<RestaurantContainer
+        restaurantId="1"
+      />);
+
+      expect(queryByText('리뷰가 없어요!')).not.toBeNull();
+    });
+  });
 });
