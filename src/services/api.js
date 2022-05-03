31ee8f32f6aabae734c711e5fff72dfcb69c24ab
kjoobin <imkj1148@gmail.com>
2022-05-04 08:36:26 +0900

    push login api method POST 로 수정

--- src/services/api.js
+++ src/services/api.js
@@ -32,6 +32,7 @@ export async function postLogin({ email, password }) {
   const url = 'https://eatgo-login-api.ahastudio.com/session';
 
   const response = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
