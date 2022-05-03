8d37a95bc0e53acf986ed1dc7444f8b110860a93
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-05-03 20:15:22 +0900

    Add LoginFormContainer test
    Add LoginFormContainer test.

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,15 +1,25 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import { render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+
+import { render, fireEvent } from '@testing-library/react';
 
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   const renderLoginPage = () => render(
     <MemoryRouter initialEntries={['/login']}>
       <LoginPage />
     </MemoryRouter>,
   );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('renders title', () => {
     const { container } = renderLoginPage();
@@ -22,5 +32,19 @@ describe('LoginPage', () => {
 
     expect(queryByLabelText('E-mail')).not.toBeNull();
     expect(queryByLabelText('Password')).not.toBeNull();
+  });
+
+  it('calls dispatch', () => {
+    const { getByText, getByLabelText } = renderLoginPage();
+
+    fireEvent.click(getByText('login'));
+
+    expect(dispatch).toBeCalled();
+
+    fireEvent.change(getByLabelText('E-mail'), { target: { value: 'test@email.com' } });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'setLoginField',
+    });
   });
 });
