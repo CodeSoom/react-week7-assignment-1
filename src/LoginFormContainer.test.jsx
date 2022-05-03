d4b0d5bc099b8d673c5aba362ee3c2295aa4785d
hdpyo <hudongpyo.dev@gmail.com>
2022-05-03 18:57:12 +0900

    Add failing test : <LoginFormContainer />
    - it renders login button

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,8 +1,18 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
 describe('LoginFormContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   const renderLoginFormContainer = () => render(
     <LoginFormContainer />,
   );
@@ -12,5 +22,13 @@ describe('LoginFormContainer', () => {
 
     expect(getByLabelText('email')).not.toBeNull();
     expect(getByLabelText('password')).not.toBeNull();
+  });
+
+  it('renders login button', () => {
+    const { getByText } = renderLoginFormContainer();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
