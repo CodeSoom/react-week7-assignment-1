4fbb79db342faa476c5043f272df57e65495ee32
kjoobin <imkj1148@gmail.com>
2022-05-03 19:12:10 +0900

    LoginForm onChange 테스트

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,12 +1,42 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const onChange = jest.fn();
+
+  const renderLoginForm = render((
+    <LoginForm onChange={onChange} />
+  ));
+
   it('renders login form', () => {
     const { queryByLabelText } = render(<LoginForm />);
 
     expect(queryByLabelText('E-mail')).not.toBeNull();
     expect(queryByLabelText('Password')).not.toBeNull();
+  });
+
+  it('listens for change event', () => {
+    const { getByLabelText } = render(<LoginForm />);
+
+    const controls = [
+      { label: 'E-mail', name: 'email', value: 'tester@example.com' },
+      { label: 'Password', name: 'password', value: 'tester' },
+    ];
+
+    controls.forEach(({ label, name, value }) => {
+      fireEvent.change(
+        getByLabelText(label),
+        { tartget: { value } },
+      );
+
+      expect(onChange).toBeCalledWith({
+        target: { name, label },
+      });
+    });
   });
 });
