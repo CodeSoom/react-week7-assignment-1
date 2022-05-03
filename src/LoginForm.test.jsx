5ccee33380f2f8175ef0abc1ecbe48b604ebfd6a
hdpyo <hudongpyo.dev@gmail.com>
2022-05-03 21:03:19 +0900

    Add failing test : <LoginForm />
    - renders login button
      - login button click

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
@@ -10,5 +10,17 @@ describe('LoginForm', () => {
 
     expect(getByLabelText('email')).not.toBeNull();
     expect(getByLabelText('password')).not.toBeNull();
+  });
+
+  it('renders login button', () => {
+    const handleSubmit = jest.fn();
+
+    const { getByText } = render((
+      <LoginForm onSubmit={handleSubmit} />
+    ));
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(handleSubmit).toBeCalled();
   });
 });
