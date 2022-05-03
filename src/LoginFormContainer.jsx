fcc952c3fcf4024d8cbbb3e9925fe94f7adc0a29
kjoobin <imkj1148@gmail.com>
2022-05-03 21:02:21 +0900

    LoginFormContainer 에 log in event 구현

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,16 +1,27 @@
-import { useDispatch } from 'react-redux';
-import { setLoginField } from './actions';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { requestSession, setLoginField } from './actions';
+
 import LoginForm from './LoginForm';
+
+import { get } from './utils';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
+  const { loginField } = useSelector(get('loginField'));
 
   function handleChange({ name, value }) {
     dispatch(setLoginField({ name, value }));
   }
+
+  function handleLoginClick() {
+    dispatch(requestSession(loginField));
+  }
+
   return (
     <LoginForm
       onChange={handleChange}
+      onSubmit={handleLoginClick}
     />
   );
 }
