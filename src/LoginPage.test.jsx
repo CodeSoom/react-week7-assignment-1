f69c89366452aa4000fb12b95d18892fb4557096
hdpyo <hudongpyo.dev@gmail.com>
2022-05-04 11:39:01 +0900

    Add beforeEach in <LoginPage /> test
    - add mockImplementation for useSelector
      - loginFields

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -2,9 +2,22 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
+
 describe('LoginPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: '',
+        password: '',
+      },
+    }));
+  });
+
   const renderLoginPage = () => render(
     <MemoryRouter>
       <LoginPage />
