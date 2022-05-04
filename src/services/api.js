36afbff303fedbb7bba5e287a054aa90f7b41b99
hdpyo <hudongpyo.dev@gmail.com>
2022-05-04 15:37:27 +0900

    Update function postLogin in api
    - api 서버에 로그인을 요청하고, 로그인에 성공하면 액세스 토큰을 얻는 동작을 구현한다.
    - 이것을 간편하게 해주는 axios 라는 라이브러리가 있다고 한다.

--- src/services/api.js
+++ src/services/api.js
@@ -28,7 +28,16 @@ export async function fetchRestaurant({ restaurantId }) {
   return data;
 }
 
-export async function postLogin() {
+export async function postLogin({ email, password }) {
   // TODO : api 서버에 로그인 요청
-  return {};
+  const url = 'https://eatgo-login-api.ahastudio.com/session';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const { accessToken } = await response.json();
+  return accessToken;
 }
