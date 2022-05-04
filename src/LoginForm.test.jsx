a13d4dc2143ceee0dd3ddaae20ce6d53327f69f3
hdpyo <hudongpyo.dev@gmail.com>
2022-05-04 11:58:48 +0900

    Refactor <LoginForm /> test
    - <LoginForm /> 에 loginFields props 를 전달한다.
    - input 의 onChange 이전에 사용할 email, password 값을 설정한다. (previousValue)

--- src/LoginForm.test.jsx
+++ src/LoginForm.test.jsx
@@ -6,17 +6,36 @@ describe('LoginForm', () => {
   it('renders input controls and change events', () => {
     const handleChange = jest.fn();
 
+    // - 변경 이전값으로 사용할 email, password (previousValue)
+    const email = 'test@test';
+    const password = '1234';
+
     const { queryByLabelText } = render((
-      <LoginForm onChange={handleChange} />
+      <LoginForm
+        loginFiels={{ email, password }}
+        onChange={handleChange}
+      />
     ));
 
     // - 강의[1] 29:21 : 테스트할 input 이 여러 개일 때, 아래와 같이 반복문으로 테스트할 수 있다.
     const controls = [
-      { label: 'email', name: 'email', value: 'tester@example.com' },
-      { label: 'password', name: 'password', value: 'test' },
+      {
+        label: 'email',
+        name: 'email',
+        previousValue: { email },
+        value: 'tester@example.com',
+      },
+      {
+        label: 'password',
+        name: 'password',
+        previousValue: { password },
+        value: 'test',
+      },
     ];
 
-    controls.forEach(({ label, name, value }) => {
+    controls.forEach(({
+      label, name, previousValue, value,
+    }) => {
       const input = queryByLabelText(label);
 
       expect(input).not.toBeNull();
