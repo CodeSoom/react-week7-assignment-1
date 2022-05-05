0ca8722419047d80427dc07cd9df7e7bf23c4ddb
이찬호 <0407chan@naver.com>
2022-05-05 22:44:42 +0900

    refactor. Storage가 호출되는가로 테스트 수정.
    - 지우님 코드 참고함

--- src/services/storage.test.js
+++ src/services/storage.test.js
@@ -1,36 +1,28 @@
 import { loadItem, saveItem } from './storage';
 
-jest.mock('./storage');
-
-const mockStorage = {};
-
-const localStorageMock = {
-  getItem: jest.fn((key) => mockStorage[key]),
-  setItem: jest.fn((key, value) => { mockStorage[key] = value; }),
-};
-global.localStorage = localStorageMock;
-
 describe('storage', () => {
   beforeEach(() => {
-    saveItem.mockImplementation((key, value) => { localStorage.setItem(key, value); });
-    saveItem('피카츄', '전기 포켓몬');
-    saveItem('파이리', '불 포켓몬');
-    loadItem.mockImplementation((key) => localStorage.getItem(key));
+    const mockStorage = {};
+    global.Storage.prototype.setItem = jest.fn(({ key, value }) => {
+      mockStorage[key] = value;
+    });
+
+    global.Storage.prototype.getItem = jest.fn((key) => mockStorage[key]);
   });
 
-  context('storage에 값이 존재 할 때', () => {
-    it('해당 값을 가져온다', () => {
-      const result = loadItem('피카츄');
+  describe('saveItem', () => {
+    it('"localStorage의 setItem 함수를 호출한다.', () => {
+      saveItem('피카츄', '전기 포켓몬');
 
-      expect(result).toBe('전기 포켓몬');
+      expect(global.Storage.prototype.setItem).toHaveBeenCalled();
     });
   });
 
-  context('storage에 값이 없을 때', () => {
-    it('빈 값을 가져온다', () => {
-      const result = loadItem('라이츄');
+  describe('loadItem', () => {
+    it('"localStorage의 getItem 함수를 호출한다.', () => {
+      loadItem('피카츄');
 
-      expect(result).toBe(null);
+      expect(global.Storage.prototype.getItem).toHaveBeenCalled();
     });
   });
 });
