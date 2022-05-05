9aa24e18909413da4d73f77cf4fd9afcdc813fc8
hdpyo <hudongpyo.dev@gmail.com>
2022-05-05 15:10:37 +0900

    Update test : <RestaurantForm />
    - it renders review write fields

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -3,12 +3,19 @@ import { render } from '@testing-library/react';
 import ReviewForm from './ReviewForm';
 
 describe('ReviewForm', () => {
-  it('renders <ReviewForm /> ', () => {
-    const { container } = render((
-      <ReviewForm />
+  it('renders review write fields ', () => {
+    const handleChange = jest.fn();
+
+    const { container, queryByLabelText } = render((
+      <ReviewForm
+        onChange={handleChange}
+      />
     ));
 
     expect(container).toHaveTextContent('평점');
     expect(container).toHaveTextContent('리뷰 내용');
+
+    expect(queryByLabelText('평점')).not.toBeNull();
+    expect(queryByLabelText('리뷰 내용')).not.toBeNull();
   });
 });
