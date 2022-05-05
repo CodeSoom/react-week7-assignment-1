b100898b04ca132a2b0aef7b2df2ae26b9014d2e
hdpyo <hudongpyo.dev@gmail.com>
2022-05-05 15:15:37 +0900

    Add test : <RestaurantForm />
    - it listens input change event

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
@@ -17,5 +17,26 @@ describe('ReviewForm', () => {
 
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('listens input change event', () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render((
+      <ReviewForm
+        onChange={handleChange}
+      />
+    ));
+
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: '맛있어요' },
+    ];
+
+    controls.forEach(({ label, name, value }) => {
+      fireEvent.change(getByLabelText(label), { target: { value } });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
   });
 });
