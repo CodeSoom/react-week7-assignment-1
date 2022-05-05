7ee1e42291f3735089c389328c85f23c6874d3ca
이찬호 <0407chan@naver.com>
2022-05-05 22:07:33 +0900

    refactor. 컴포넌트로 분리

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,37 +1,22 @@
+import TextField from './TextField';
+
 export default function LoginForm({ fields, onChange, onSubmit }) {
   const { email, password } = fields;
 
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
   return (
     <>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input
-          id="login-email"
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          Password
-        </label>
-        <input
-          id="login-password"
-          type="password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </div>
+      <TextField
+        label="E-mail"
+        name="email"
+        inputValue={email}
+        onChange={onChange}
+      />
+      <TextField
+        label="Password"
+        name="password"
+        inputValue={password}
+        onChange={onChange}
+      />
       <button
         type="button"
         onClick={onSubmit}
