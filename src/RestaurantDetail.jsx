612e21f528713e9b918f2c3d206f5bf6707345ee
kjoobin <imkj1148@gmail.com>
2022-05-06 07:54:46 +0900

    reder reviews 구현

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -1,7 +1,9 @@
 import MenuItems from './MenuItems';
 
 export default function RestaurantDetail({ restaurant }) {
-  const { name, address, menuItems } = restaurant;
+  const {
+    name, address, menuItems, reviews = [],
+  } = restaurant;
 
   return (
     <div>
@@ -13,6 +15,18 @@ export default function RestaurantDetail({ restaurant }) {
       </p>
       <h3>메뉴</h3>
       <MenuItems menuItems={menuItems} />
+      <h3>리뷰</h3>
+      <ul>
+        {reviews.map(({
+          id, name: reviewerName, score, description,
+        }) => (
+          <li key={id}>
+            {`${reviewerName}
+            ${score}
+            ${description}`}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
