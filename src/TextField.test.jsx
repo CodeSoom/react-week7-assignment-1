853560e6c703196378c606121dac84e393415721
hdpyo <hudongpyo.dev@gmail.com>
2022-05-05 15:49:20 +0900

    Add failing test : <TextField />
    - <TextField /> 의 input 에 type 을 전달해서 명시적으로 전달받지 않으면 기본값으로 "text" 를 사용하도록 테스트 한다.

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -3,17 +3,38 @@ import { render } from '@testing-library/react';
 import TextField from './TextField';
 
 describe('TextField', () => {
-  it('renders label and input control ', () => {
-    const handleChange = jest.fn();
+  context('with type', () => {
+    it('renders label and input control ', () => {
+      const handleChange = jest.fn();
 
-    const { queryByLabelText } = render((
-      <TextField
-        label="평점"
-        name="score"
-        onChange={handleChange}
-      />
-    ));
+      const { container, queryByLabelText } = render((
+        <TextField
+          label="평점"
+          type="number"
+          name="score"
+          onChange={handleChange}
+        />
+      ));
 
-    expect(queryByLabelText('평점')).not.toBeNull();
+      expect(queryByLabelText('평점')).not.toBeNull();
+      expect(container).toContainHTML('type="number"');
+    });
+  });
+
+  context('without type', () => {
+    it('renders label and input control ', () => {
+      const handleChange = jest.fn();
+
+      const { container, queryByLabelText } = render((
+        <TextField
+          label="리뷰 내용"
+          name="description"
+          onChange={handleChange}
+        />
+      ));
+
+      expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+      expect(container).toContainHTML('type="text"');
+    });
   });
 });
