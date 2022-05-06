3cad2e93816d5c56c4146750f2a39f0c879ff110
kjoobin <imkj1148@gmail.com>
2022-05-06 18:12:42 +0900

    review form change event 테스트

--- src/ReviewForm.test.jsx
+++ src/ReviewForm.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
@@ -8,10 +8,35 @@ describe('ReviewForm', () => {
    * 리뷰 내용
    * 리뷰 남기기
    */
+
+  const onChange = jest.fn();
+
+  beforeEach(() => {
+    onChange.mockClear();
+  });
+
   it('renders labels', () => {
-    const { queryByLabelText } = render(<ReviewForm />);
+    const { queryByLabelText } = render(<ReviewForm onChange={onChange} />);
 
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('listens for change events', () => {
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: '훌륭하다 훌륭해!' },
+    ];
+
+    const { getByLabelText } = render(<ReviewForm onChange={onChange} />);
+
+    controls.forEach(({ label, name, value }) => {
+      fireEvent.change(
+        getByLabelText(label),
+        { target: { name, value } },
+      );
+
+      expect(onChange).toBeCalledWith({ name, value });
+    });
   });
 });
