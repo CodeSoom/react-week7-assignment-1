d5adc12cacb7e132f60b9b108afee045182cf744
kjoobin <imkj1148@gmail.com>
2022-05-06 18:09:28 +0900

    review input fields 구현

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,5 +1,22 @@
-export default function ReviewForm() {
+import InputField from './InputField';
+
+export default function ReviewForm({ onChange, fields = {} }) {
+  const { score, description } = fields;
   return (
-    <></>
+    <div>
+      <InputField
+        onChange={onChange}
+        label="평점"
+        name="score"
+        type="number"
+        value={score}
+      />
+      <InputField
+        onChange={onChange}
+        label="리뷰 내용"
+        name="description"
+        value={description}
+      />
+    </div>
   );
 }
