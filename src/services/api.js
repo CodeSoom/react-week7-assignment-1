dfa82dc082b9e1ae3f052a073edd2e679c4555c6
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-15 08:55:54 +0900

    반복되는 코드, url 리팩토링
    이전 코드에서 data를 fetch 해오는 과정과 API 주소의 url이 상당히 많이 중복되어 있었기 때문에
    이를 리팩토링하여 코드를 간결하게 만들었습니다.

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,33 @@
-export async function fetchRegions() {
-  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+const BASE_URL = 'https://eatgo-customer-api.ahastudio.com';
+
+async function fetchData(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data;
+}
+
+export async function fetchRegions() {
+  const url = `${BASE_URL}/regions`;
+  return fetchData(url);
 }
 
 export async function fetchCategories() {
-  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const url = `${BASE_URL}/categories`;
+  return fetchData(url);
 }
 
 export async function fetchRestaurants({ regionName, categoryId }) {
-  const url = 'https://eatgo-customer-api.ahastudio.com/restaurants'
-    + `?region=${regionName}&category=${categoryId}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const url = `${BASE_URL}/restaurants?region=${regionName}&category=${categoryId}`;
+  return fetchData(url);
 }
 
 export async function fetchRestaurant({ restaurantId }) {
-  const url = 'https://eatgo-customer-api.ahastudio.com' + `/restaurants/${restaurantId}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const url = `${BASE_URL}/restaurants/${restaurantId}`;
+  return fetchData(url);
 }
 
 export async function postLogin({ email, password }) {
-  const url = 'https://eatgo-login-api.ahastudio.com/session';
+  const url = `${BASE_URL}/session`;
   const response = await fetch(url, {
     method: 'POST',
     headers: {
@@ -46,8 +45,7 @@ export async function postReview({
   score,
   description,
 }) {
-  const url = 'https://eatgo-customer-api.ahastudio.com'
-    + `/restaurants/${restaurantId}/reviews`;
+  const url = `${BASE_URL}/restaurants/${restaurantId}/reviews`;
 
   const response = await fetch(url, {
     method: 'POST',
