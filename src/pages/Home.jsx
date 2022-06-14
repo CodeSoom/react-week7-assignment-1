691d9aa4d61f2b673cd9f80b71103efb7f9bd720
rukeni <rukeni@naver.com>
2022-06-14 17:12:48 +0900

    Home에서 Login 라우팅 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ export default function HomePage() {
       <h1>Home</h1>
       <ul>
         <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
         <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </div>
