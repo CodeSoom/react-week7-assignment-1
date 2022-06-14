8de1044237260d7e53d683c56a11bdef5b11f0ba
rukeni <rukeni@naver.com>
2022-06-14 15:29:06 +0900

    Email, Password 텍스트박스, Login버튼을 추가한다.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,16 @@
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+      </div>
+      <button type="button">Login</button>
+    </div>
   );
 }
