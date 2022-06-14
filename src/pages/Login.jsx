2cc05d74463f2c335148028f18a4c6cb395023c0
rukeni <rukeni@naver.com>
2022-06-14 17:13:43 +0900

    Login 페이지 redux 연동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setLoginFields } from '../apps/store/actions';
+
+import { get } from '../apps/utils';
+
 export default function Login() {
+  const { email, password } = useSelector(get('loginFields'));
+
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    dispatch(setLoginFields({ [name]: value }));
+  };
   return (
     <div>
       <h2>Login</h2>
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" name="email" value={email} onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" name="password" id="password" value={password} onChange={handleChange} />
       </div>
       <button type="button">Login</button>
     </div>
