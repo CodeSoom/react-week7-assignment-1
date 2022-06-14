27880b7dcc6aa167d3433a47ed5a20145cbc3cf8
rukeni <rukeni@naver.com>
2022-06-14 17:03:37 +0900

    로그인을 위한 리듀서 기능 구현
    1. setAccessToken : 토큰 삽입
    
    2. setLoginFields : 인풋박스 value 갱신
    
    3. initialState에 loginFields, auth추가

--- src/apps/store/reducer.js
+++ src/apps/store/reducer.js
@@ -27,9 +27,16 @@ const initialState = {
   },
   selectedRegion: null,
   selectedCategory: null,
-  isLoading: false,
-  isError: false,
-  errorMessage: '',
+  loginFields: {
+    email: '',
+    password: '',
+  },
+  auth: {
+    accessToken: null,
+    isLoading: false,
+    isError: false,
+    errorMessage: '',
+  },
 };
 
 const reducers = {
@@ -130,6 +137,29 @@ const reducers = {
         isLoading,
         isError,
         errorMessage,
+      },
+    };
+  },
+
+  setAccessToken(state, { payload: { accessToken } }) {
+    return {
+      ...state,
+      auth: {
+        ...state.auth,
+        accessToken,
+        isLoading: false,
+        isError: false,
+        errorMessage: '',
+      },
+    };
+  },
+
+  setLoginFields(state, { payload: { name, value } }) {
+    return {
+      ...state,
+      loginFields: {
+        ...state.loginFields,
+        [name]: value,
       },
     };
   },
