0813db4bdeb93014748d6a52aedf22a70e5bff14
Mika <solinepik@gmail.com>
2022-06-16 02:48:39 +0900

    LoginPage에 email, password input과 submit 버튼을 구현합니다

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,7 +1,20 @@
 export default function LoginPage() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
-    </div>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input id="email" type="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <button type="submit">Log in</button>
+    </form>
   );
 }
