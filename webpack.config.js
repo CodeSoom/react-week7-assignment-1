0a1fd62ff00a4d6bcb0c87f99833a360edc35e54
rukeni <rukeni@naver.com>
2022-06-16 00:30:54 +0900

    개발환경일때는 .env.local을 바라보게하고 프로덕션일때는 .env를 바라볼 수 있도록 웹팩에 분기문 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: {
-      index: 'index.html',
+module.exports = () => {
+  const isDevelopment = process.env.NODE_ENV !== 'development';
+  if (!isDevelopment) {
+    dotenv.config();
+  }
+  if (isDevelopment) {
+    dotenv.config({ path: path.join(__dirname, './.env.local') });
+  }
+  return {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
     },
-  },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL),
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: {
+        index: 'index.html',
+      },
+    },
+  };
 };
