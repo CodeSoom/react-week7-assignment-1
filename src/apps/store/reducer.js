c117f061bf50a8be19e2cd03ff3ddf1571c6a39d
rukeni <rukeni@naver.com>
2022-06-17 15:29:09 +0900

    root reducer에 reviewState, reviewReducer 추가

--- src/apps/store/reducer.js
+++ src/apps/store/reducer.js
@@ -5,12 +5,14 @@ import { categoryState, categoryReducers } from '../../features/category/categor
 import { restaurantState, restaurantReducer } from '../../features/restaurant/restaurantReducer';
 
 import { authState, authReducer } from '../../features/auth/authReducer';
+import { reviewReducer, reviewState } from '../../features/review/reviewReducer';
 
 const initialState = {
   ...regionState,
   ...categoryState,
   ...restaurantState,
   ...authState,
+  ...reviewState,
 };
 
 const reducers = {
@@ -18,6 +20,7 @@ const reducers = {
   ...categoryReducers,
   ...restaurantReducer,
   ...authReducer,
+  ...reviewReducer,
 
   setLoading(state, {
     payload: {
