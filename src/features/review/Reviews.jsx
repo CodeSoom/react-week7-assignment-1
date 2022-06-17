e01ab53785fc149365b6dac56930cc991ac70e73
rukeni <rukeni@naver.com>
2022-06-17 19:54:06 +0900

    Reviews 컴포넌트 역순정렬하여 새로추가시 맨위로 노출되도록 수정

--- src/features/review/Reviews.jsx
+++ src/features/review/Reviews.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 export default function Reviews({ reviews }) {
   return (
-    <ul>
-      {reviews.map(({
-        id, name, score, description,
-      }) => (
-        <li key={id}>
-          <div>{name}</div>
-          <div>{score}</div>
-          <div>{description}</div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>리뷰</h2>
+      <ul>
+        {reviews.sort((a, b) => b.id - a.id).map(({
+          id, name, score, description,
+        }) => (
+          <li key={id}>
+            <div>{name}</div>
+            <div>
+              {score}
+              점
+            </div>
+            <div>{description}</div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
