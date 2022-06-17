0d50888bd12a6098c0047acb5ac6776304115501
rukeni <rukeni@naver.com>
2022-06-17 18:21:48 +0900

    reviews fixture생성

---
+++ fixtures/reviews.js
@@ -0,0 +1,18 @@
+const reviews = [
+  {
+    id: 32,
+    restaurantId: 1,
+    name: '테스터',
+    score: 5,
+    description: '누군가가 테스트하고 있나보네요 ㅋㅋㅋㅋ',
+  },
+  {
+    id: 33,
+    restaurantId: 1,
+    name: '테스터',
+    score: 5,
+    description: '반가워요~~~',
+  },
+];
+
+export default reviews;
--- src/features/restaurant/RestaurantsContainer.test.jsx
+++ src/features/restaurant/RestaurantsContainer.test.jsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
+
 import { MemoryRouter } from 'react-router-dom';
 
 import RestaurantsContainer from './RestaurantsContainer';
