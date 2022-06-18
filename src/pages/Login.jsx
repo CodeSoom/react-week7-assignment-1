aa335ee2be9cefd457db7ebbe3f29f168853219d
rukeni <rukeni@naver.com>
2022-06-18 14:24:09 +0900

    로그인 페이지에 있는 기능들 각 관심사별로 분리

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { login, setLoginFields } from '../features/auth/authActions';
-
-import { get } from '../apps/utils';
+import LoginFormContainer from '../features/auth/LoginFormContainer';
 
 export default function Login() {
-  const { email, password } = useSelector(get('loginFields'));
-  const {
-    isLogin, isLoading, isError, errorMessage,
-  } = useSelector(get('auth'));
-
-  const dispatch = useDispatch();
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    dispatch(setLoginFields(name, value));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(login());
-  };
-
-  if (isLoading) return <div>로딩중...</div>;
-  if (isError) return <div>{errorMessage}</div>;
-
-  if (isLogin) {
-    return <button type="button">로그아웃</button>;
-  }
   return (
-    <div>
-      <h2>Login</h2>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" value={email} onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" value={password} onChange={handleChange} />
-      </div>
-      <button type="button" onClick={handleSubmit}>Login</button>
-    </div>
+    <>
+      <LoginFormContainer />
+    </>
   );
 }
