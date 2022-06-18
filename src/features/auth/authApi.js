f247ddbf9579c5fa9f3595e51a53d3cf21cbf7fb
rukeni <rukeni@naver.com>
2022-06-18 16:11:21 +0900

    authroize할때 에서 try catch 제거

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -5,15 +5,11 @@ export const request = axios.create({
 });
 
 export async function authorize(email, password) {
-  try {
-    const response = await request.post('/session', {
-      email,
-      password,
-    });
+  const response = await request.post('/session', {
+    email,
+    password,
+  });
 
-    const { data } = await response;
-    return data;
-  } catch (error) {
-    throw new Error('Invalid email or password');
-  }
+  const { data } = await response;
+  return data;
 }
