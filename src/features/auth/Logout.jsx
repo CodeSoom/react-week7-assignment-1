30a60245a798bdbb81cd72e34691610e1fea627a
rukeni <rukeni@naver.com>
2022-06-18 16:18:32 +0900

    로그아웃 버튼 Log out으로 수정

--- src/features/auth/Logout.jsx
+++ src/features/auth/Logout.jsx
@@ -2,6 +2,6 @@ import Button from '../shared/Button';
 
 export default function Logout({ onClick }) {
   return (
-    <Button onClick={onClick}>로그아웃</Button>
+    <Button onClick={onClick}>Log out</Button>
   );
 }
