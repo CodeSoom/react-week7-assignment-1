919d33938d85b1c9e8791d049f53411ec964c7e4
rukeni <rukeni@naver.com>
2022-06-18 16:00:25 +0900

    isLogin이 true일때 로그아웃버튼이 보이는지 테스트코드에 추가

--- src/features/auth/LoginFormContainer.test.jsx
+++ src/features/auth/LoginFormContainer.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -9,27 +9,60 @@ describe('LoginFormContainer', () => {
 
   useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    loginFields: {
-      email: '',
-      password: '',
-    },
-    auth: {
-      isLogin: false,
-      isLoading: false,
-      isError: false,
-      errorMessage: '',
-    },
-  }));
+  const mockSelector = (isLoggedIn) => {
+    useSelector.mockImplementation((selector) => selector({
+      loginFields: {
+        email: '',
+        password: '',
+      },
+      auth: {
+        isLogin: isLoggedIn,
+        isLoading: false,
+        isError: false,
+        errorMessage: '',
+      },
+    }));
+  };
 
-  it('renders input controls', () => {
-    const { getByLabelText, getByRole } = render((
+  const renderWithContext = (isLoggedIn) => {
+    mockSelector(isLoggedIn);
+    return render((
       <LoginFormContainer />
     ));
+  };
 
-    expect(getByLabelText('Email')).toBeInTheDocument();
-    expect(getByLabelText('Password')).toBeInTheDocument();
+  context('when isLogin is true', () => {
+    it('renders Logout', () => {
+      renderWithContext(true);
 
-    expect(getByRole('button')).toBeInTheDocument();
+      expect(screen.getByRole('button')).toHaveTextContent('로그아웃');
+    });
+  });
+
+  context('when the form is submitted', () => {
+    it('calls the dispatch function', () => {
+      renderWithContext(false);
+
+      const loginButton = screen.getByRole('button');
+
+      fireEvent.click(loginButton);
+
+      expect(dispatch).toBeCalled();
+
+      expect(loginButton).toHaveTextContent('Login');
+    });
+  });
+
+  context('when textbox is changed', () => {
+    it('calls the dispatch function', () => {
+      renderWithContext(false);
+
+      fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'tester@example.com' } });
+
+      expect(dispatch).toBeCalled();
+
+      expect(screen.getByLabelText('Email')).toBeInTheDocument();
+      expect(screen.getByLabelText('Password')).toBeInTheDocument();
+    });
   });
 });
