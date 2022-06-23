a37e796d2b3f048400f0ab95e10d7937b13cf5a0
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-23 12:02:55 +0900

    오류 수정
    지금까지 dotenv 설정을 잘못 하고 있어서 그와 관련된 코드를 전면 수정했습니다.

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,3 @@
-const BASE_URL = 'https://eatgo-customer-api.ahastudio.com';
-
 async function fetchData(url) {
   const response = await fetch(url);
   const data = await response.json();
@@ -7,27 +5,27 @@ async function fetchData(url) {
 }
 
 export async function fetchRegions() {
-  const url = `${BASE_URL}/regions`;
+  const url = `${process.env.CUSTOMER_API_SERVER}/regions`;
   return fetchData(url);
 }
 
 export async function fetchCategories() {
-  const url = `${BASE_URL}/categories`;
+  const url = `${process.env.CUSTOMER_API_SERVER}/categories`;
   return fetchData(url);
 }
 
 export async function fetchRestaurants({ regionName, categoryId }) {
-  const url = `${BASE_URL}/restaurants?region=${regionName}&category=${categoryId}`;
+  const url = `${process.env.CUSTOMER_API_SERVER}/restaurants?region=${regionName}&category=${categoryId}`;
   return fetchData(url);
 }
 
 export async function fetchRestaurant({ restaurantId }) {
-  const url = `${BASE_URL}/restaurants/${restaurantId}`;
+  const url = `${process.env.CUSTOMER_API_SERVER}/restaurants/${restaurantId}`;
   return fetchData(url);
 }
 
 export async function postLogin({ email, password }) {
-  const url = 'https://eatgo-login-api.ahastudio.com/session';
+  const url = `${process.env.AUTH_API_SERVER}/session`;
   const response = await fetch(url, {
     method: 'POST',
     headers: {
@@ -45,7 +43,7 @@ export async function postReview({
   score,
   description,
 }) {
-  const url = `${BASE_URL}/restaurants/${restaurantId}/reviews`;
+  const url = `${process.env.CUSTOMER_API_SERVER}/restaurants/${restaurantId}/reviews`;
 
   const response = await fetch(url, {
     method: 'POST',
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: {
-      index: 'index.html',
+module.exports = () => {
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
+  return {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
     },
-  },
+    plugins: [
+      new Dotenv({
+        path: `./.env.${isDevelopment ? 'development' : 'production'}`,
+        systemvars: true,
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
 };
