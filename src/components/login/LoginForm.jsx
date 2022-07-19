abb630df093d18f4b4b608e61aeb6fd3727eccbf
kyh196201 <kyh196201@gmail.com>
2022-07-19 20:19:00 +0900

    LoginForm 테스트 통과
    - email, password props 구현

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,17 +1,17 @@
-export default function LoginForm() {
+export default function LoginForm({ email, password }) {
   return (
     <form>
       <div>
         <label htmlFor="email">
           E-mail
         </label>
-        <input type="text" id="email" />
+        <input type="text" id="email" value={email} />
       </div>
       <div>
         <label htmlFor="password">
           Password
         </label>
-        <input type="password" id="password" />
+        <input type="password" id="password" value={password} />
       </div>
 
       <button type="submit">Log In</button>
