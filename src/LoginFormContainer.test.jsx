60ffab1aa61377eed60ddc95214f1c8250974ba6
minsuk <alstjr0183@naver.com>
2022-07-19 21:02:42 +0900

    feat: LoginFormContainer.test - 추가 로그인 버튼을 테스트하는 코드를 추가했습니다

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -1,8 +1,17 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
 describe('LoginFormContainer', () => {
+  const dispatch = jest.Fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   const renderLoginFormContainer = () => render(
     <LoginFormContainer />,
   );
@@ -17,5 +26,13 @@ describe('LoginFormContainer', () => {
     const { getByLabelText } = renderLoginFormContainer();
 
     expect(getByLabelText('Password')).not.toBeNull();
+  });
+
+  it('login - button이 렌더링된다', () => {
+    const { getByText } = renderLoginFormContainer();
+
+    fireEvent.click(getByText('Log In'));
+
+    expect(dispatch).toBeCalled();
   });
 });
