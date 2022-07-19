d13ad6883909eb163505c6510c14ca5f7e499625
kyh196201 <kyh196201@gmail.com>
2022-07-19 19:47:29 +0900

    LoginPage 테스트 통과

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,11 @@
+import LoginFormContainer from '../../containers/login/LoginFormContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <h1>Log In</h1>
+
+      <LoginFormContainer />
     </div>
   );
 }
