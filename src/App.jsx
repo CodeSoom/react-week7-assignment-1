e9dfde4953ab7a7114b92058fc9fb71baad9c1e0
kyh196201 <kyh196201@gmail.com>
2022-07-19 19:25:41 +0900

    App 테스트 통과
    - /login path에 해당하는 라우터를 설정했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 
 import HomePage from './HomePage';
+import LoginPage from './pages/LoginPage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 import RestaurantPage from './RestaurantPage';
@@ -20,6 +21,7 @@ export default function App() {
       </header>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
         <Route path="/about" component={AboutPage} />
         <Route exact path="/restaurants" component={RestaurantsPage} />
         <Route path="/restaurants/:id" component={RestaurantPage} />
