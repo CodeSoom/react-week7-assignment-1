3f0ea25e1e2b1d339a6b4020b0978ca76ee6f905
Woo Dy <kjop7419@naver.com>
2022-07-19 20:41:17 +0900

    test(reducer) logout

--- src/reducer/reducer.test.js
+++ src/reducer/reducer.test.js
@@ -10,6 +10,7 @@ import {
   changeLoginField,
   setAccessToken,
   changeReviewField,
+  logout,
 } from '../actions/actions';
 
 describe('reducer', () => {
@@ -181,6 +182,16 @@ describe('reducer', () => {
     const state = reducer(initialState, setAccessToken('TOKEN'));
 
     expect(state.accessToken).toBe('TOKEN');
+  });
+
+  describe('logout', () => {
+    const initialState = {
+      accessToken: 'ACCESS_TOKEN',
+    };
+
+    const state = reducer(initialState, logout());
+
+    expect(state.accessToken).toBe('');
   });
 
   describe('changeReviewField', () => {
