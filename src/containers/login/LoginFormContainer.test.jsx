1307c09be75f11f97f444aea1cc117a910c88722
kyh196201 <kyh196201@gmail.com>
2022-07-19 20:46:05 +0900

    LoginFormContainer 테스트 수정
    - 스토어에있는 email, password를 LoginForm에 전달하는지 확인하는 테스트를 추가했습니다.

--- src/containers/login/LoginFormContainer.test.jsx
+++ src/containers/login/LoginFormContainer.test.jsx
@@ -1,8 +1,25 @@
 import { render } from '@testing-library/react';
 
+import given from 'given2';
+
+import { useSelector } from 'react-redux';
+
+import { EMAIL, PASSWORD } from '../../../fixtures/login';
+
 import LoginFormContainer from './LoginFormContainer';
 
+jest.mock('react-redux');
+
 describe('<LoginFormContainer />', () => {
+  given('loginFields', () => ({
+    email: '',
+    password: '',
+  }));
+
+  useSelector.mockImplementation((selector) => selector({
+    loginFields: given.loginFields,
+  }));
+
   const renderLoginFormContainer = () => render((<LoginFormContainer />));
 
   it('renders input controls', () => {
@@ -19,5 +36,33 @@ describe('<LoginFormContainer />', () => {
 
     expect(button).toBeInTheDocument();
     expect(button).toHaveAttribute('type', 'submit');
+  });
+
+  context('with email and password', () => {
+    given('loginFields', () => ({
+      email: EMAIL,
+      password: PASSWORD,
+    }));
+
+    it('it renders current email and password', () => {
+      const { getByLabelText } = renderLoginFormContainer();
+
+      expect(getByLabelText('E-mail')).toHaveDisplayValue(EMAIL);
+      expect(getByLabelText('Password')).toHaveValue(PASSWORD);
+    });
+  });
+
+  context('without email and password', () => {
+    given('loginFields', () => ({
+      email: '',
+      password: '',
+    }));
+
+    it('it renders empty email and password', () => {
+      const { getByLabelText } = renderLoginFormContainer();
+
+      expect(getByLabelText('E-mail')).toHaveValue('');
+      expect(getByLabelText('Password')).toHaveValue('');
+    });
   });
 });
