738bd326eaf8199f516fcbab4ab81c6f0df5efa1
kyh196201 <kyh196201@gmail.com>
2022-07-19 20:28:41 +0900

    LoginForm 테스트 통과
    - onChange를 props로 전달받고, 내부에서 handleChange를 구현했습니다.

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,17 +1,35 @@
-export default function LoginForm({ email, password }) {
+export default function LoginForm({ email, password, onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+
+    onChange({ name, value });
+  }
+
   return (
     <form>
       <div>
         <label htmlFor="email">
           E-mail
         </label>
-        <input type="text" id="email" value={email} />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="password">
           Password
         </label>
-        <input type="password" id="password" value={password} />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
       </div>
 
       <button type="submit">Log In</button>
