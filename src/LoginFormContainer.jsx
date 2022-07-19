03684e26ffd723db7161a34f1f1bd484d6a9cbb9
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-19 20:44:06 +0900

    add: (green) LoginFormContainer 테스트코드를 통과해라
    정상 렌더와 버튼 클릭시 dispatch 호출 통과를 위한 화면 구현

---
+++ src/LoginFormContainer.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux';
+
+import { requestLogin } from './actions';
+
+export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    dispatch(requestLogin());
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input type="email" id="login-email" />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input type="password" id="login-password" />
+      </div>
+      <button type="button" onClick={handleClick}>Log In</button>
+    </>
+  );
+}
