3b882a4799fa9597028d3102003389fc95519af8
kyh196201 <kyh196201@gmail.com>
2022-07-19 20:46:14 +0900

    LoginFormContainer 테스트 통과

--- src/containers/login/LoginFormContainer.jsx
+++ src/containers/login/LoginFormContainer.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux';
+
+import { get } from '../../utils';
+
 import LoginForm from '../../components/login/LoginForm';
 
 export default function LoginFormContainer() {
+  const { email, password } = useSelector(get('loginFields'));
+
   return (
-    <LoginForm />
+    <LoginForm
+      email={email}
+      password={password}
+    />
   );
 }
