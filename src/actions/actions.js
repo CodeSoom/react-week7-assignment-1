147f43a7982c6ec6136c901ae9421c5c8527a00f
Woo Dy <kjop7419@naver.com>
2022-07-19 20:40:32 +0900

    fix(logout) logout action create

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -7,6 +7,8 @@ import {
   postReview,
 } from '../services/api';
 
+import { saveItem } from '../services/storage';
+
 export function setRegions(regions) {
   return {
     type: 'setRegions',
@@ -95,6 +97,12 @@ export function changeLoginField({ name, value }) {
   };
 }
 
+export function logout() {
+  return {
+    type: 'logout',
+  };
+}
+
 export function changeReviewField({ name, value }) {
   return {
     type: 'changeReviewField',
@@ -112,7 +120,11 @@ export function setAccessToken(accessToken) {
 export function requestLogin() {
   return async (dispatch, getState) => {
     const { loginFields: { email, password } } = getState();
+
     const accessToken = await postLogin({ email, password });
+
+    saveItem('accessToken', accessToken);
+
     dispatch(setAccessToken(accessToken));
   };
 }
