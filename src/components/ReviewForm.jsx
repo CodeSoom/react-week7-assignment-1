ecb4853eced3a3604bacb6bdf356bfe9666ba3ea
Woo Dy <kjop7419@naver.com>
2022-07-19 19:21:20 +0900

    refactor(ReviewForm) TextField에 value값 전달

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,6 +1,11 @@
 import TextField from './TextField';
 
-export default function ReviewForm({ onChange, onSubmit }) {
+export default function ReviewForm({
+  onChange,
+  onSubmit,
+  score,
+  description,
+}) {
   return (
     <div>
       <TextField
@@ -8,11 +13,13 @@ export default function ReviewForm({ onChange, onSubmit }) {
         name="score"
         type="number"
         onChange={onChange}
+        targetValue={score}
       />
       <TextField
         label="리뷰 내용"
         name="description"
         onChange={onChange}
+        targetValue={description}
       />
       <button
         type="button"
