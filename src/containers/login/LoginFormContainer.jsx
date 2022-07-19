a364327ed071a1a60b51e45b8b65c7626b2c9b9f
kyh196201 <kyh196201@gmail.com>
2022-07-19 19:58:32 +0900

    LoginFormContainer 리팩토링
    - LoginFormContainer 내부의 UI에 관련된 로직을 LoginForm 컴포넌트를 이용해서 분리했습니다.

--- src/containers/login/LoginFormContainer.jsx
+++ src/containers/login/LoginFormContainer.jsx
@@ -1,20 +1,7 @@
+import LoginForm from '../../components/login/LoginForm';
+
 export default function LoginFormContainer() {
   return (
-    <form>
-      <div>
-        <label htmlFor="email">
-          E-mail
-        </label>
-        <input type="text" id="email" />
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password
-        </label>
-        <input type="password" id="password" />
-      </div>
-
-      <button type="submit">Log In</button>
-    </form>
+    <LoginForm />
   );
 }
