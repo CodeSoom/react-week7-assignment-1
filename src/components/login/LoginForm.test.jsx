de7c72d2331cd1ec8f921e2ca33e563abeb430ad
kyh196201 <kyh196201@gmail.com>
2022-07-19 20:17:45 +0900

    LoginForm 테스트 추가
    - email, password props에 따라서 렌더링이 달라지는 것에대한 테스트를 추가했습니다.

--- src/components/login/LoginForm.test.jsx
+++ src/components/login/LoginForm.test.jsx
@@ -3,7 +3,12 @@ import { render } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
 describe('<LoginForm />', () => {
-  const renderLoginForm = () => render((<LoginForm />));
+  const renderLoginForm = ({ email = '', password = '' } = {}) => render((
+    <LoginForm
+      email={email}
+      password={password}
+    />
+  ));
 
   it('renders input controls', () => {
     const { getByLabelText } = renderLoginForm();
@@ -19,5 +24,41 @@ describe('<LoginForm />', () => {
 
     expect(button).toBeInTheDocument();
     expect(button).toHaveAttribute('type', 'submit');
+  });
+
+  context('with email and password values', () => {
+    const loginFields = {
+      email: 'test@example.com',
+      password: 'test',
+    };
+
+    it('it renders current email and password', () => {
+      const { getByLabelText } = renderLoginForm(loginFields);
+
+      expect(getByLabelText('E-mail')).toHaveDisplayValue(loginFields.email);
+      expect(getByLabelText('Password')).toHaveValue(loginFields.password);
+    });
+  });
+
+  context('without email and password values', () => {
+    const loginFields = [{
+      email: undefined,
+      password: undefined,
+    }, {
+      email: '',
+      password: '',
+    }];
+
+    loginFields.forEach(({ email, password }) => {
+      it('it renders empty email and password', () => {
+        const { getByLabelText } = renderLoginForm({
+          email,
+          password,
+        });
+
+        expect(getByLabelText('E-mail')).toHaveValue('');
+        expect(getByLabelText('Password')).toHaveValue('');
+      });
+    });
   });
 });
