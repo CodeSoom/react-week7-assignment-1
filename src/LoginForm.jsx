c0ecbc622fdd8aa10c83b67c97701d877fdda4e1
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-19 22:04:57 +0900

    refactor: 테스트 코드 통과를 위한 코드 구현
    input에 onChange 핸들러 함수 handleChange 추가

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,17 +1,22 @@
-export default function LoginForm({ onSubmit }) {
+export default function LoginForm({ onSubmit, onChange }) {
+  const handleChange = (e) => {
+    const { target: { name, value } } = e;
+    onChange({ name, value });
+  };
+
   return (
     <>
       <div>
         <label htmlFor="login-email">
           E-mail
         </label>
-        <input type="email" id="login-email" />
+        <input type="email" id="login-email" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="login-password">
           Password
         </label>
-        <input type="password" id="login-password" />
+        <input type="password" id="login-password" onChange={handleChange} />
       </div>
       <button type="button" onClick={onSubmit}>Log In</button>
     </>
