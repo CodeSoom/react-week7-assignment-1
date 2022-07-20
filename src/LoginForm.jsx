d7c094280bbbfa65577cec912e292c19aa06aaff
minsuk <alstjr0183@naver.com>
2022-07-20 20:36:35 +0900

    fix: LoginFormContainer 분리작업 ui와 기능을 분리했습니다

---
+++ src/LoginForm.jsx
@@ -0,0 +1,19 @@
+export default function LoginForm({ onSubmit }) {
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">E-mail</label>
+        <input type="email" id="login-email" />
+      </div>
+
+      <div>
+        <label htmlFor="login-password">Password</label>
+        <input type="password" id="login-password" />
+      </div>
+
+      <button onClick={onSubmit} type="button">
+        Log In
+      </button>
+    </>
+  );
+}
--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
+import { requestLogin } from './actions';
+
+import LoginForm from './LoginForm';
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
-  const handleClickLogin = () => {
-    dispatch();
+  const handleSubmit = () => {
+    dispatch(requestLogin());
   };
 
   return (
-    <>
-      <div>
-        <label htmlFor="login-email">E-mail</label>
-        <input type="email" id="login-email" />
-      </div>
-
-      <div>
-        <label htmlFor="login-password">Password</label>
-        <input type="password" id="login-password" />
-      </div>
-
-      <button onClick={handleClickLogin} type="button">
-        Log In
-      </button>
-    </>
+    <LoginForm onSubmit={handleSubmit} />
   );
 }
