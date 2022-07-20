243941b55e3ca591bcdafc932a472040048b30ac
kyh196201 <kyh196201@gmail.com>
2022-07-20 19:51:13 +0900

    email, password input에 대한 fixture를 추가했습니다.

--- fixtures/login.js
+++ fixtures/login.js
@@ -1,3 +1,15 @@
 export const EMAIL = 'tester@example.com';
 
 export const PASSWORD = 'test';
+
+export const EMAIL_INPUT = {
+  value: 'tester@example.com',
+  label: 'E-mail',
+  name: 'email',
+};
+
+export const PASSWORD_INPUT = {
+  value: 'test',
+  label: 'Password',
+  name: 'password',
+};
