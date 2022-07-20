2ce8f8cbc1859bb3b7f305f72ff29150c5b80b61
2scent <ad7549@gmail.com>
2022-07-20 18:15:06 +0900

    Edit LoginFormContainer to break line

--- src/pages/LoginPage/LoginFormConatiner/LoginFormContainer.jsx
+++ src/pages/LoginPage/LoginFormConatiner/LoginFormContainer.jsx
@@ -1,11 +1,17 @@
 export default function LoginFormContainer() {
   return (
     <form>
-      <label htmlFor="email">E-mail</label>
-      <input id="email" type="text" />
-      <label htmlFor="password">Password</label>
-      <input id="password" type="text" />
-      <button type="submit">Log In</button>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input id="email" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="text" />
+      </div>
+      <div>
+        <button type="submit">Log In</button>
+      </div>
     </form>
   );
 }
