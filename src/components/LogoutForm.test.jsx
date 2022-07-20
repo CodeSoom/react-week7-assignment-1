cd8ce114024a4256412b6036706def29c135115c
Woo Dy <kjop7419@naver.com>
2022-07-20 19:08:24 +0900

    refactor(LogoutForm.test) 중복제거 및 mockClear

--- src/components/LogoutForm.test.jsx
+++ src/components/LogoutForm.test.jsx
@@ -5,14 +5,24 @@ import LogoutForm from './LogoutForm';
 describe('LogoutForm', () => {
   const handleClick = jest.fn();
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  function renderLogoutForm() {
+    return render((
+      <LogoutForm onClick={handleClick} />
+    ));
+  }
+
   it('renders "Log out" button', () => {
-    const { container } = render(<LogoutForm />);
+    const { container } = renderLogoutForm();
 
     expect(container).toHaveTextContent('Log out');
   });
 
   it('listens click event', () => {
-    const { getByText } = render(<LogoutForm onClick={handleClick} />);
+    const { getByText } = renderLogoutForm();
 
     fireEvent.click(getByText('Log out'));
 
