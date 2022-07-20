0e098e27a89be05add6d6704453da3b5ef077401
kyh196201 <kyh196201@gmail.com>
2022-07-20 20:23:39 +0900

    api 테스트 추가
    - postLogin에 대한 테스트를 추가했습니다.

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -3,6 +3,7 @@ import {
   fetchCategories,
   fetchRestaurants,
   fetchRestaurant,
+  postLogin,
 } from './api';
 
 import REGIONS from '../../fixtures/regions';
@@ -65,6 +66,20 @@ describe('api', () => {
       const restaurant = await fetchRestaurant({ restaurantId: 1 });
 
       expect(restaurant).toEqual(RESTAURANT);
+    });
+  });
+
+  describe('postLogin', () => {
+    beforeEach(() => {
+      mockFetch({
+        accessToken: 'ACCESS_TOKEN',
+      });
+    });
+
+    it('returns accessToken', async () => {
+      const { accessToken } = await postLogin({ email: '', password: '' });
+
+      expect(accessToken).toEqual('ACCESS_TOKEN');
     });
   });
 });
