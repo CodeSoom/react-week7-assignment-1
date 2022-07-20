eb9ecacb1d5aed7d68cadf419d5d153545906dbc
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-20 22:44:22 +0900

    테스트 통과를 위해 useSelector 추가
    email과 password를 loginFields에서 가져옵니다

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,14 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import {
   requestLogin,
   changeLoginField,
 } from './actions';
 
+import { get } from './utils';
+
 import LoginForm from './LoginForm';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
 
   function handleSubmit() {
     dispatch(requestLogin());
@@ -20,6 +24,7 @@ export default function LoginFormContainer() {
 
   return (
     <LoginForm
+      fields={{ email, password }}
       onSubmit={handleSubmit}
       onChange={handleChange}
     />
