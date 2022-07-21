71e7cb1925bc48e01b3c29792d84755d17ca5856
minsuk <alstjr0183@naver.com>
2022-07-22 00:36:14 +0900

    fix: LoginForm , LoginFormContainer - 함수 ui 분리 함수랑 ui랑 분리했습니다

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,16 +1,11 @@
 export default function LoginForm({ onChange, onSubmit }) {
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
   return (
     <>
       <div>
         <label htmlFor="login-email">E-mail</label>
         <input
           name="email"
-          onChange={handleChange}
+          onChange={onChange}
           type="email"
           id="login-email"
         />
@@ -20,7 +15,7 @@ export default function LoginForm({ onChange, onSubmit }) {
         <label htmlFor="login-password">Password</label>
         <input
           name="password"
-          onChange={handleChange}
+          onChange={onChange}
           type="password"
           id="login-password"
         />
--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { requestLogin } from './actions';
+import { requestLogin, changeLoginFields } from './actions';
 
 import LoginForm from './LoginForm';
 
+import { get } from './utils';
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
 
   const handleSubmit = () => {
     dispatch(requestLogin());
   };
 
+  const handleChange = ({ name, value }) => {
+    dispatch(changeLoginFields({ name, value }));
+  };
+
   return (
-    <LoginForm onSubmit={handleSubmit} />
+    <LoginForm fields={{ email, password }} onSubmit={handleSubmit} onChange={handleChange} />
   );
 }
