0b4c6d506d3744c1b9c186ef1104834cb100b15a
minsuk <alstjr0183@naver.com>
2022-07-22 00:34:14 +0900

    feat: LoginPage.test - useSelector test 추가 useSelector 테스트코드를 추가해주었습니다

--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -2,9 +2,24 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import LoginPage from './LoginPage';
 
+jest.mock('react-redux');
+
 describe('LoginPage', () => {
+  useSelector.mockImplementation((selector) => selector({
+    loginFields: {
+      email: 'minsuk@naver.com',
+      password: '123123',
+    },
+  }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const renderLoginPage = () => render(
     <MemoryRouter>
       <LoginPage />
