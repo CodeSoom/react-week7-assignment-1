b19a3f88752e5b9aec4aec81167779ccb6648952
kyh196201 <kyh196201@gmail.com>
2022-07-21 19:34:03 +0900

    RestaurantDetail 테스트 통과
    - RestaurantDetail 내부에 review form을 구현했습니다.

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -13,6 +13,32 @@ export default function RestaurantDetail({ restaurant }) {
       </p>
       <h3>메뉴</h3>
       <MenuItems menuItems={menuItems} />
+
+      <form>
+        <div>
+          <label htmlFor="review-score">
+            평점
+          </label>
+          <input
+            id="review-score"
+            name="score"
+            type="number"
+          />
+        </div>
+        <div>
+          <label htmlFor="review-description">
+            리뷰 내용
+          </label>
+          <input
+            id="review-description"
+            name="description"
+            type="number"
+          />
+        </div>
+        <button type="submit">리뷰 남기기</button>
+      </form>
+
+      {/* reviews */}
     </div>
   );
 }
