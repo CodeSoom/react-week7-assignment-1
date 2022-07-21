fdc8b1ef9a8b92c4e533ae8469b4817e751c92c6
kyh196201 <kyh196201@gmail.com>
2022-07-21 20:19:55 +0900

    RestaurantDetail 테스트 수정
    - ReviewForm에서 발생한 change 이벤트를 수신하는지 확인하는 테스트를 추가했습니다.

--- src/RestaurantDetail.test.jsx
+++ src/RestaurantDetail.test.jsx
@@ -1,8 +1,14 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import RestaurantDetail from './RestaurantDetail';
 
 describe('RestaurantDetail', () => {
+  const handleChangeReviewForm = jest.fn();
+
+  beforeEach(() => {
+    handleChangeReviewForm.mockClear();
+  });
+
   const restaurant = {
     id: 1,
     name: '마법사주방',
@@ -10,7 +16,10 @@ describe('RestaurantDetail', () => {
   };
 
   const renderRestaurantDetail = () => render(
-    <RestaurantDetail restaurant={restaurant} />,
+    <RestaurantDetail
+      restaurant={restaurant}
+      onChangeReviewForm={handleChangeReviewForm}
+    />,
   );
 
   it('renders name and address', () => {
@@ -26,5 +35,23 @@ describe('RestaurantDetail', () => {
     expect(getByLabelText('평점')).toBeInTheDocument();
     expect(getByLabelText('리뷰 내용')).toBeInTheDocument();
     expect(getByText('리뷰 남기기')).toBeInTheDocument();
+  });
+
+  describe('changes input controls in review form', () => {
+    it('calls onChangeReviewForm', () => {
+      const { getByLabelText } = renderRestaurantDetail();
+
+      expect(handleChangeReviewForm).not.toBeCalled();
+
+      fireEvent.change(getByLabelText('평점'), {
+        target: { value: '5' },
+      });
+
+      fireEvent.change(getByLabelText('리뷰 내용'), {
+        target: { value: 'Good!' },
+      });
+
+      expect(handleChangeReviewForm).toBeCalledTimes(2);
+    });
   });
 });
