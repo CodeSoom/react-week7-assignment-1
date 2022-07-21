f3e66f039229d5345594ce85a16753c7439a8c5b
kyh196201 <kyh196201@gmail.com>
2022-07-21 19:54:30 +0900

    ReviewForm 테스트 수정
    - score, description props가 있을 경우, 없을 경우에 대한 테스트를 추가했습니다.

--- src/components/restaurant/ReviewForm.test.jsx
+++ src/components/restaurant/ReviewForm.test.jsx
@@ -3,18 +3,53 @@ import { render } from '@testing-library/react';
 import ReviewForm from './ReviewForm';
 
 describe('<ReviewForm />', () => {
-  const renderReviewForm = () => render(<ReviewForm />);
+  const renderReviewForm = ({ score, description }) => render((
+    <ReviewForm
+      score={score}
+      description={description}
+    />
+  ));
 
   it('renders score and description input controls', () => {
-    const { getByLabelText } = renderReviewForm();
+    const { getByLabelText } = renderReviewForm({
+      score: '',
+      description: '',
+    });
 
     expect(getByLabelText('평점')).toBeInTheDocument();
     expect(getByLabelText('리뷰 내용')).toBeInTheDocument();
   });
 
   it('renders "리뷰 남기기" button', () => {
-    const { getByText } = renderReviewForm();
+    const { getByText } = renderReviewForm({
+      score: '',
+      description: '',
+    });
 
     expect(getByText('리뷰 남기기')).toBeInTheDocument();
+  });
+
+  context('with score and description', () => {
+    it('it renders current score and description', () => {
+      const { getByLabelText } = renderReviewForm({
+        score: '5',
+        description: 'Good!',
+      });
+
+      expect(getByLabelText('평점')).toHaveDisplayValue('5');
+      expect(getByLabelText('리뷰 내용')).toHaveDisplayValue('Good!');
+    });
+  });
+
+  context('without score and description', () => {
+    it('it renders empty input controls', () => {
+      const { getByLabelText } = renderReviewForm({
+        score: '',
+        description: '',
+      });
+
+      expect(getByLabelText('평점')).toHaveDisplayValue('');
+      expect(getByLabelText('리뷰 내용')).toHaveDisplayValue('');
+    });
   });
 });
