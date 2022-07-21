5a87295c65112d736d333532c4bb19080ca91c2b
kyh196201 <kyh196201@gmail.com>
2022-07-21 19:33:39 +0900

    RestaurantDetail 테스트 추가
    - review form이 렌더링되었는지 확인하는 테스트를 추가했습니다.

--- src/RestaurantDetail.test.jsx
+++ src/RestaurantDetail.test.jsx
@@ -9,12 +9,22 @@ describe('RestaurantDetail', () => {
     address: '서울시 강남구',
   };
 
+  const renderRestaurantDetail = () => render(
+    <RestaurantDetail restaurant={restaurant} />,
+  );
+
   it('renders name and address', () => {
-    const { container } = render(
-      <RestaurantDetail restaurant={restaurant} />,
-    );
+    const { container } = renderRestaurantDetail();
 
     expect(container).toHaveTextContent('마법사주방');
     expect(container).toHaveTextContent('서울시');
+  });
+
+  it('renders review form', () => {
+    const { getByLabelText, getByText } = renderRestaurantDetail();
+
+    expect(getByLabelText('평점')).toBeInTheDocument();
+    expect(getByLabelText('리뷰 내용')).toBeInTheDocument();
+    expect(getByText('리뷰 남기기')).toBeInTheDocument();
   });
 });
