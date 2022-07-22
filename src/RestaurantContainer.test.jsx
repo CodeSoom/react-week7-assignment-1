7fe31afb1614239e38a5b66352a7f8e46b55e622
kyh196201 <kyh196201@gmail.com>
2022-07-22 20:04:07 +0900

    RestaurantContainer 테스트 수정
    - score, description props가 있을 경우, 없을 경우에 대한 테스트를 추가했습니다.

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -2,9 +2,16 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { DESCRIPTION, SCORE } from '../fixtures/review';
+
 import RestaurantContainer from './RestaurantContainer';
 
 describe('RestaurantContainer', () => {
+  given('reviewFields', () => ({
+    score: '',
+    description: '',
+  }));
+
   const dispatch = jest.fn();
 
   function renderRestaurantContainer() {
@@ -17,6 +24,7 @@ describe('RestaurantContainer', () => {
 
     useSelector.mockImplementation((selector) => selector({
       restaurant: given.restaurant,
+      reviewFields: given.reviewFields,
     }));
   });
 
@@ -46,6 +54,34 @@ describe('RestaurantContainer', () => {
       expect(getByLabelText('평점')).toBeInTheDocument();
       expect(getByLabelText('리뷰 내용')).toBeInTheDocument();
       expect(getByText('리뷰 남기기')).toBeInTheDocument();
+    });
+
+    context('with score and description', () => {
+      given('reviewFields', () => ({
+        score: SCORE,
+        description: DESCRIPTION,
+      }));
+
+      it('it renders current score and description', () => {
+        const { getByLabelText } = renderRestaurantContainer();
+
+        expect(getByLabelText('평점')).toHaveDisplayValue(SCORE);
+        expect(getByLabelText('리뷰 내용')).toHaveDisplayValue(DESCRIPTION);
+      });
+    });
+
+    context('without score and description', () => {
+      given('reviewFields', () => ({
+        score: '',
+        description: '',
+      }));
+
+      it('it renders empty score and description', () => {
+        const { getByLabelText } = renderRestaurantContainer();
+
+        expect(getByLabelText('평점')).toHaveDisplayValue('');
+        expect(getByLabelText('리뷰 내용')).toHaveDisplayValue('');
+      });
     });
   });
 
