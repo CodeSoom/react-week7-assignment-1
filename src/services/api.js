1c5f1d9fd7cf8b04c3bbb02318c67f33c97e38a0
kyh196201 <kyh196201@gmail.com>
2022-07-22 20:59:48 +0900

    api 테스트 통과
    - postReview api를 구현하고, mock api 함수를 추가했습니다.

--- src/services/api.js
+++ src/services/api.js
@@ -43,3 +43,23 @@ export async function postLogin({ email, password }) {
 
   return accessToken;
 }
+
+export async function postReview({
+  score, description, restaurantId, accessToken,
+}) {
+  const url = 'https://eatgo-customer-api.ahastudio.com/restaurants/'
+  + `${restaurantId}/reviews`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ score, description }),
+  });
+
+  await response.json();
+
+  return true;
+}
