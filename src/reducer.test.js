8b6abb4ded1a9e84abd19214b1ec85eb6b0a8ce8
minsuk <alstjr0183@naver.com>
2022-07-23 19:12:14 +0900

    fix: reducer.test - 초기값 추가 loginfields 초기값추가를했습니다

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -18,6 +18,11 @@ describe('reducer', () => {
       restaurant: null,
       selectedRegion: null,
       selectedCategory: null,
+
+      loginFields: {
+        email: '',
+        password: '',
+      },
     };
 
     it('returns initialState', () => {
