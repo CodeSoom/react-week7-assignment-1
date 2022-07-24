fef4fea4e13bc2c205651078a92159126e137c64
seungwoo Kim <kyh196201@gmail.com>
2022-07-24 23:09:51 +0900

    LoginFormContainer 테스트 수정
    - 로그인 했을 경우에 대한 테스트를 추가했습니다.

--- src/containers/login/LoginFormContainer.test.jsx
+++ src/containers/login/LoginFormContainer.test.jsx
@@ -33,87 +33,102 @@ describe('<LoginFormContainer />', () => {
 
   useSelector.mockImplementation((selector) => selector({
     loginFields: given.loginFields,
+    accessToken: given.accessToken,
   }));
 
   const renderLoginFormContainer = () => render((<LoginFormContainer />));
 
-  it('renders input controls', () => {
-    const { getByLabelText } = renderLoginFormContainer();
+  context('not logged in', () => {
+    given('accessToken', () => '');
 
-    expect(getByLabelText('E-mail')).toBeInTheDocument();
-    expect(getByLabelText('Password')).toBeInTheDocument();
-  });
-
-  it('renders submit button', () => {
-    const { getByText } = renderLoginFormContainer();
-
-    const button = getByText('Log In');
-
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveAttribute('type', 'submit');
-  });
-
-  context('with email and password', () => {
-    given('loginFields', () => ({
-      email: EMAIL,
-      password: PASSWORD,
-    }));
-
-    it('it renders current email and password', () => {
+    it('renders input controls', () => {
       const { getByLabelText } = renderLoginFormContainer();
 
-      expect(getByLabelText('E-mail')).toHaveDisplayValue(EMAIL);
-      expect(getByLabelText('Password')).toHaveValue(PASSWORD);
+      expect(getByLabelText('E-mail')).toBeInTheDocument();
+      expect(getByLabelText('Password')).toBeInTheDocument();
     });
-  });
 
-  context('without email and password', () => {
-    given('loginFields', () => ({
-      email: '',
-      password: '',
-    }));
-
-    it('it renders empty email and password', () => {
-      const { getByLabelText } = renderLoginFormContainer();
-
-      expect(getByLabelText('E-mail')).toHaveValue('');
-      expect(getByLabelText('Password')).toHaveValue('');
-    });
-  });
-
-  describe('changes email and password', () => {
-    it('dispatch setLoginFields', () => {
-      const { getByLabelText } = renderLoginFormContainer();
-
-      expect(dispatch).not.toBeCalled();
-
-      fireEvent.change(getByLabelText(EMAIL_INPUT.label), {
-        target: { value: EMAIL_INPUT.value },
-      });
-
-      expect(dispatch).toBeCalledWith(setLoginFields({
-        name: EMAIL_INPUT.name,
-        value: EMAIL_INPUT.value,
-      }));
-
-      fireEvent.change(getByLabelText(PASSWORD_INPUT.label), {
-        target: { value: PASSWORD_INPUT.value },
-      });
-
-      expect(dispatch).toBeCalledWith(setLoginFields({
-        name: PASSWORD_INPUT.name,
-        value: PASSWORD_INPUT.value,
-      }));
-    });
-  });
-
-  describe('clicks login button', () => {
-    it('dispatch requestLogin', () => {
+    it('renders submit button', () => {
       const { getByText } = renderLoginFormContainer();
 
-      fireEvent.click(getByText('Log In'));
+      const button = getByText('Log In');
 
-      expect(dispatch).toBeCalled();
+      expect(button).toBeInTheDocument();
+      expect(button).toHaveAttribute('type', 'submit');
+    });
+
+    context('with email and password', () => {
+      given('loginFields', () => ({
+        email: EMAIL,
+        password: PASSWORD,
+      }));
+
+      it('it renders current email and password', () => {
+        const { getByLabelText } = renderLoginFormContainer();
+
+        expect(getByLabelText('E-mail')).toHaveDisplayValue(EMAIL);
+        expect(getByLabelText('Password')).toHaveValue(PASSWORD);
+      });
+    });
+
+    context('without email and password', () => {
+      given('loginFields', () => ({
+        email: '',
+        password: '',
+      }));
+
+      it('it renders empty email and password', () => {
+        const { getByLabelText } = renderLoginFormContainer();
+
+        expect(getByLabelText('E-mail')).toHaveValue('');
+        expect(getByLabelText('Password')).toHaveValue('');
+      });
+    });
+
+    describe('changes email and password', () => {
+      it('dispatch setLoginFields', () => {
+        const { getByLabelText } = renderLoginFormContainer();
+
+        expect(dispatch).not.toBeCalled();
+
+        fireEvent.change(getByLabelText(EMAIL_INPUT.label), {
+          target: { value: EMAIL_INPUT.value },
+        });
+
+        expect(dispatch).toBeCalledWith(setLoginFields({
+          name: EMAIL_INPUT.name,
+          value: EMAIL_INPUT.value,
+        }));
+
+        fireEvent.change(getByLabelText(PASSWORD_INPUT.label), {
+          target: { value: PASSWORD_INPUT.value },
+        });
+
+        expect(dispatch).toBeCalledWith(setLoginFields({
+          name: PASSWORD_INPUT.name,
+          value: PASSWORD_INPUT.value,
+        }));
+      });
+    });
+
+    describe('clicks login button', () => {
+      it('dispatch requestLogin', () => {
+        const { getByText } = renderLoginFormContainer();
+
+        fireEvent.click(getByText('Log In'));
+
+        expect(dispatch).toBeCalled();
+      });
+    });
+  });
+
+  context('is logged in', () => {
+    given('accessToken', () => 'ACCESS_TOKEN');
+
+    it('renders "Log out" button', () => {
+      const { getByText } = renderLoginFormContainer();
+
+      expect(getByText('Log out')).toBeInTheDocument();
     });
   });
 });
