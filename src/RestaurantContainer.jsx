471e53e3ebead284850e78737370a092ebef7f4e
minsuk <alstjr0183@naver.com>
2022-07-24 23:42:15 +0900

    feat: RestaurantContainer - 수정 리뷰폼과 handleChange 함수를 생성해주었습니다

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,15 +3,21 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import ReviewForm from './ReviewForm';
 
 import {
   loadRestaurant,
+  changeReviewField,
 } from './actions';
 
 import { get } from './utils';
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
+
+  const handleChange = ({ name, value }) => {
+    dispatch(changeReviewField({ name, value }));
+  };
 
   useEffect(() => {
     dispatch(loadRestaurant({ restaurantId }));
@@ -28,6 +34,8 @@ export default function RestaurantContainer({ restaurantId }) {
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
+      <ReviewForm onChange={handleChange} title="평점" name="score" />
+      <ReviewForm onChange={handleChange} title="리뷰 내용" name="description" />
     </>
   );
 }
