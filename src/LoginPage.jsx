ac0d748a27a8d6cef6779554bdf4c2cde4f87f3e
hanna244 <shhn0509@gmail.com>
2022-08-16 19:54:58 +0900

    LoginPage test - E-mail input

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,7 +1,10 @@
+import LoginFromContainer from './LoginFromContainer';
+
 export default function LoginPage() {
   return (
     <div>
       <h2>Login</h2>
+      <LoginFromContainer />
     </div>
   );
 }
--- src/LoginPage.test.jsx
+++ src/LoginPage.test.jsx
@@ -5,15 +5,22 @@ import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
-  it('renders title', () => {
-    const renderLoginPage = () => render(
-      <MemoryRouter>
-        <LoginPage />
-      </MemoryRouter>,
-    );
+  const renderLoginPage = () => render(
+    <MemoryRouter>
+      <LoginPage />
+    </MemoryRouter>,
+  );
 
+  it('renders title', () => {
     const { container } = renderLoginPage();
 
     expect(container).toHaveTextContent('Login');
+  });
+
+  // LoginFromContainer.test에도 동일하게 작성되어 있는 코드이다. 이중으로 테스트 장치를 만든것.
+  it('renders input control', () => {
+    const { getByLabelText } = renderLoginPage();
+
+    expect(getByLabelText('E-mail')).not.toBeNull();
   });
 });
