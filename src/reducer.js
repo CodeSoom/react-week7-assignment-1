bc3d10d9dae08c4954d42380444db3982367621b
Heejae Chang <heejae0704@gmail.com>
2022-08-18 15:34:26 +0900

    Reducer setAccessToken 구현 (Green)

--- src/reducer.js
+++ src/reducer.js
@@ -43,6 +43,13 @@ const reducers = {
     };
   },
 
+  setAccessToken(state, { payload: { accessToken } }) {
+    return {
+      ...state,
+      accessToken,
+    };
+  },
+
   selectRegion(state, { payload: { regionId } }) {
     const { regions } = state;
     return {
