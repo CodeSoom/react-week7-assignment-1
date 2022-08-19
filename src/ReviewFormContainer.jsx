2d1ad62620e548bd95fb50ac591512bae444e5aa
Heejae Chang <heejae0704@gmail.com>
2022-08-19 15:58:32 +0900

    ReviewFormContainer 테스트 작성 (Red)

---
+++ src/ReviewFormContainer.jsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { get } from './utils';
+
+export default function ReviewFormContainer() {
+  const dispatch = useDispatch();
+
+  const { email, password } = useSelector(get('loginFields'));
+  const accessToken = useSelector(get('accessToken'));
+
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    dispatch(changeLoginField({ name, value }));
+  }
+
+  function handleSubmit() {
+    dispatch(requestLogin());
+  }
+
+  function handleClickLogout() {
+    dispatch(clearAccessToken());
+    localStorage.removeItem('accessToken');
+  }
+
+  if (accessToken) {
+    return (
+      <LogoutForm onClick={handleClickLogout} />
+    );
+  }
+
+  return (
+    <LoginForm
+      email={email}
+      password={password}
+      onChange={handleChange}
+      onClick={handleSubmit}
+      accessToken={accessToken}
+    />
+  );
+}
