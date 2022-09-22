024f06b9e783b25fa5cfa9dc359a31b8c8d6c762
0-Chan <kyc4421194@gmail.com>
2022-09-22 20:32:04 +0900

    :green_circle: Add LoginForm to pass the test

---
+++ src/LoginForm.jsx
@@ -0,0 +1,31 @@
+export default function LoginForm() {
+  return (
+    <>
+      <div>
+        <label htmlFor="login-email">
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="login-email"
+          name="email"
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">
+          Password
+        </label>
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+        />
+      </div>
+      <button
+        type="button"
+      >
+        Log In
+      </button>
+    </>
+  );
+}
