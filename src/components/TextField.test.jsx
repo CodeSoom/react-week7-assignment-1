913a908c0cc5121d208880e8cdd5fff1bf378dda
ChoJinmok <jjmok1127@gmail.com>
2022-09-24 20:33:57 +0900

    Red: TextField 테스트

---
+++ src/components/TextField.test.jsx
@@ -0,0 +1,63 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import TextField from './TextField';
+
+import { reviewFormcontrols } from '../../fixtures/controls';
+
+describe('TextField', () => {
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+  });
+
+  function renderTextField({
+    label, type, name, value,
+  }) {
+    return render(
+      <TextField
+        label={label}
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+      />,
+    );
+  }
+
+  it('renders label and input control', () => {
+    const { queryByLabelText } = renderTextField({
+      label: '평점',
+    });
+
+    expect(queryByLabelText('평점')).not.toBeNull();
+  });
+
+  context('with type', () => {
+    it('renders setting input control', () => {
+      const { container } = renderTextField({
+        type: 'number',
+      });
+
+      expect(container).toContainHTML('type="number"');
+    });
+  });
+
+  context('without type', () => {
+    it('renders \'text\'input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="text"');
+    });
+  });
+
+  it('listens change event', () => {
+    const { label, name, value } = renderTextField[1];
+
+    const { getByLabelText } = renderTextField({ label, name });
+
+    fireEvent.change(getByLabelText(label), { target: { value } });
+
+    expect(handleChange).toBeCalledWith({ name, value });
+  });
+});
