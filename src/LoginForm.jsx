ca5aefc53a74c182dd1325b2e60d08b4dd0cb82c
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-22 18:21:58 +0900

    LoginForm 컴포넌트 생성 및 input 출력
    input에 관한 form은 LoginForm 컴포넌트에서 전체적으로 관리할 수 있도록 분리하였습니다

---
+++ src/LoginForm.jsx
@@ -0,0 +1,42 @@
+export default function LoginForm({ fields, onSubmit, onChange }) {
+  const { email, password } = fields;
+
+  function handleChange(e) {
+    e.preventDefault();
+
+    const { target: { name, value } } = e;
+
+    onChange({ name, value });
+  }
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="login-email">E-mail</label>
+        <input
+          type="text"
+          id="login-email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="login-password">Password</label>
+        <input
+          type="password"
+          id="login-password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+      </div>
+      <button
+        type="button"
+        onClick={onSubmit}
+      >
+        Log in
+      </button>
+    </form>
+  );
+}
