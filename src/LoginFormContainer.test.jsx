719fd76cc5ef7c893e2d0a48f4514bd9629ab2cb
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-22 21:09:17 +0900

    LoginFormContainer 테스트에 로그인 여부에 따른 조건 추가
    로그인을 하지 않았거나 혹은 로그아웃을 했을 경우, 로그인을 하였을 경우에 따른 테스트 조건을 추가하였습니다

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import LoginFormContainer from './LoginFormContainer';
 
+import loginControls from '../fixtures/loginControls';
+
 describe('LoginFormContainer', () => {
   const dispatch = jest.fn();
 
@@ -16,43 +18,58 @@ describe('LoginFormContainer', () => {
         email: 'test@test',
         password: '1234',
       },
+      accessToken: given.accessToken,
     }));
   });
 
   const renderLoginFormContainer = () => render((<LoginFormContainer />));
 
-  it('로그인 input이 랜더링된다', () => {
-    renderLoginFormContainer();
+  context('로그인을 하지 않았거나 로그아웃일 경우', () => {
+    given('accessToken', () => '')
 
-    expect(screen.getByLabelText('E-mail').value).toBe('test@test');
-    expect(screen.getByLabelText('Password').value).toBe('1234');
-  });
-
-  it('로그인 input에 onChange 이벤트가 호출된다', () => {
-    renderLoginFormContainer();
-
-    const controls = [
-      { label: 'E-mail', name: 'email', value: 'new E-mail' },
-      { label: 'Password', name: 'password', value: 'new Password' },
-    ];
-
-    controls.forEach(({ label, name, value }) => {
-      const input = screen.getByLabelText(label);
-
-      fireEvent.change(input, { target: { value } });
-
-      expect(dispatch).toBeCalledWith({
-        type: 'changeLoginField',
-        payload: { name, value },
+    it('로그인 input이 랜더링된다', () => {
+      renderLoginFormContainer();
+  
+      expect(screen.getByLabelText('E-mail').value).toBe('test@test');
+      expect(screen.getByLabelText('Password').value).toBe('1234');
+    });
+  
+    it('로그인 input에 onChange 이벤트가 호출된다', () => {
+      renderLoginFormContainer();
+  
+      loginControls.forEach(({ label, name, value }) => {
+        const input = screen.getByLabelText(label);
+  
+        fireEvent.change(input, { target: { value } });
+  
+        expect(dispatch).toBeCalledWith({
+          type: 'changeLoginField',
+          payload: { name, value },
+        });
       });
     });
-  });
+  
+    it('로그인 버튼이 랜더링된다', () => {
+      renderLoginFormContainer();
+  
+      fireEvent.click(screen.getByText('Log in'));
+  
+      expect(dispatch).toBeCalled();
+    });
+  })
 
-  it('로그인 버튼이 랜더링된다', () => {
-    renderLoginFormContainer();
+  context('로그인을 하였을 경우', () => {
+    given('accessToken', () => 'ACCESS_TOKEN')
 
-    fireEvent.click(screen.getByText('Log in'));
+    it('로그아웃 페이지가 랜더링된다', () => {
+      const { container } = renderLoginFormContainer();
 
-    expect(dispatch).toBeCalled();
-  });
+      expect(container).toHaveTextContent('Log out');
+
+      fireEvent.click(screen.getByText('Log out'))
+
+      expect(dispatch).toBeCalledWith({ type: 'logout'})
+    })
+  })
+
 });
