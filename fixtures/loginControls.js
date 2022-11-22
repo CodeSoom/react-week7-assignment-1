bc15d8f5181e54423f55be2927d5cac7e3c80deb
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-22 20:52:46 +0900

    fixtures에 테스트용 데이터 loginControls, reviewControls, reviewFields 생성
    테스트에서 사용할 수 있도록 배열로 fixtures에 생성하였습니다

---
+++ fixtures/loginControls.js
@@ -0,0 +1,6 @@
+const loginControls = [
+  { label: 'E-mail', name: 'email', value: 'new E-mail' },
+  { label: 'Password', name: 'password', value: 'new Password' },
+];
+
+export default loginControls;
---
+++ fixtures/reviewControls.js
@@ -0,0 +1,11 @@
+const reviewControls = [
+  {
+    label: '평점',
+    name: 'score',
+    value: '5',
+    type: 'number',
+  },
+  { label: '리뷰 내용', name: 'description', value: '맛있어요👍' },
+];
+
+export default reviewControls;
