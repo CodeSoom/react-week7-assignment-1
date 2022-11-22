bd2408fbbccbfa8cec97008d57a21ce46a0b72e2
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-22 20:47:30 +0900

    RestaurantContaienr 컴포넌트 ReviewForm 컴포넌트 연결
    accessToken이 있을 시에만 ReveiwForm이 보이도록 삼항연산자를 활용하여 설정하였습니다.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -2,13 +2,15 @@ import { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import RestaurantDetail from './RestaurantDetail';
-
 import {
-  loadRestaurant,
+  loadRestaurant, changeReviewField, sendReview,
 } from './actions';
 
 import { get } from './utils';
+
+import RestaurantDetail from './RestaurantDetail';
+
+import ReviewForm from './ReviewForm';
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
@@ -19,15 +21,35 @@ export default function RestaurantContainer({ restaurantId }) {
 
   const restaurant = useSelector(get('restaurant'));
 
+  const accessToken = useSelector(get('accessToken'));
+
   if (!restaurant) {
     return (
       <p>Loading...</p>
     );
   }
 
+  function handleChange({ name, value }) {
+    dispatch(changeReviewField({ name, value }));
+  }
+
+  function handleSubmit() {
+    dispatch(sendReview({ restaurantId }));
+  }
+
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
+      {
+        accessToken
+          ? (
+            <ReviewForm
+              onChange={handleChange}
+              onSubmit={handleSubmit}
+            />
+          )
+          : null
+      }
     </>
   );
 }
