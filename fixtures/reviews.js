4a83b2c6e6a11b7d0c59cea6017f677a9c64090b
sejineeee <sejinee0918@gmail.com>
2022-11-25 02:31:15 +0900

    fixture 테스트용 데이터 reviews 생성

---
+++ fixtures/reviews.js
@@ -0,0 +1,10 @@
+const reviews = [
+  {
+    id: 1, name: '테스터', score: 3, description: '맛있어요',
+  },
+  {
+    id: 2, name: '테스터', score: 5, description: '최고예요',
+  },
+];
+
+export default reviews;
