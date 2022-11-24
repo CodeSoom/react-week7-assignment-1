fe26cba4012d65e8447da2c51bc277ca125937db
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-24 21:13:38 +0900

    테스트용 데이터 reviews 생성
    리뷰에 관한 내용을 테스트할 경우 사용할 수 있게 fixtures에 테스트용 리뷰 데이터를 생성하였습니다

---
+++ fixtures/reviews.js
@@ -0,0 +1,14 @@
+const reviews = [
+  {
+    name: '테스터',
+    score: '5',
+    description: '맛있어요👍',
+  },
+  {
+    name: '테스터',
+    score: '1',
+    description: '우욱',
+  },
+];
+
+export default reviews;
