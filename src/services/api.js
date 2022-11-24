e291e0023560ec58dbc816966910e237e5e637d2
sejineeee <sejinee0918@gmail.com>
2022-11-24 20:32:56 +0900

    postLogin, postReview api 호출 함수 구현

--- src/services/api.js
+++ src/services/api.js
@@ -27,3 +27,35 @@ export async function fetchRestaurant({ restaurantId }) {
   const data = await response.json();
   return data;
 }
+
+export async function postLogin({ email, password }) {
+  const url = 'https://eatgo-login-api.ahastudio.com/session';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const { accessToken } = await response.json();
+  return accessToken;
+}
+
+export async function postReview({
+  accessToken,
+  restaurantId,
+  score,
+  description,
+}) {
+  const url = `https://eatgo-customer-api.ahastudio.com/restaurants/${restaurantId}/reviews`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ score, description }),
+  });
+  await response.json();
+}
