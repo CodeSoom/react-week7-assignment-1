cf33e867328697a462af00168f8e01a7a497ced2
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-26 15:03:53 +0900

    eslint로 코드 수정

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -25,51 +25,50 @@ describe('LoginFormContainer', () => {
   const renderLoginFormContainer = () => render((<LoginFormContainer />));
 
   context('로그인을 하지 않았거나 로그아웃일 경우', () => {
-    given('accessToken', () => '')
+    given('accessToken', () => '');
 
     it('로그인 input이 랜더링된다', () => {
       renderLoginFormContainer();
-  
+
       expect(screen.getByLabelText('E-mail').value).toBe('test@test');
       expect(screen.getByLabelText('Password').value).toBe('1234');
     });
-  
+
     it('로그인 input에 onChange 이벤트가 호출된다', () => {
       renderLoginFormContainer();
-  
+
       loginControls.forEach(({ label, name, value }) => {
         const input = screen.getByLabelText(label);
-  
+
         fireEvent.change(input, { target: { value } });
-  
+
         expect(dispatch).toBeCalledWith({
           type: 'changeLoginField',
           payload: { name, value },
         });
       });
     });
-  
+
     it('로그인 버튼이 랜더링된다', () => {
       renderLoginFormContainer();
-  
+
       fireEvent.click(screen.getByText('Log in'));
-  
+
       expect(dispatch).toBeCalled();
     });
-  })
+  });
 
   context('로그인을 하였을 경우', () => {
-    given('accessToken', () => 'ACCESS_TOKEN')
+    given('accessToken', () => 'ACCESS_TOKEN');
 
     it('로그아웃 페이지가 랜더링된다', () => {
       const { container } = renderLoginFormContainer();
 
       expect(container).toHaveTextContent('Log out');
 
-      fireEvent.click(screen.getByText('Log out'))
+      fireEvent.click(screen.getByText('Log out'));
 
-      expect(dispatch).toBeCalledWith({ type: 'logout'})
-    })
-  })
-
+      expect(dispatch).toBeCalledWith({ type: 'logout' });
+    });
+  });
 });
