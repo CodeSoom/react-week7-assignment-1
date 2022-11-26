1b0eaa6ba3fdd72b5a13dec198fff7aee79980cc
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-26 20:34:59 +0900

    TextField 테스트에 입력 값 랜더링 여부 테스트 추가
    컴포넌트에서 입력한 값을 랜더링하는지 여부 테스트를 진행하였습니다

--- src/TextField.test.jsx
+++ src/TextField.test.jsx
@@ -35,6 +35,19 @@ describe('TextField', () => {
     });
   });
 
+  it('입력한 값을 랜더링한다', () => {
+    const name = 'score';
+    const value = '5';
+
+    const { getByLabelText } = renderTextField({
+      label: '평점', name, value, type: 'number',
+    });
+
+    fireEvent.change(getByLabelText('평점'), { target: { value } });
+
+    expect(getByLabelText('평점').value).toBe(value);
+  });
+
   it('input 창에 입력하면 handleChange가 호출된다', () => {
     renderTextField({
       label: '평점', name: 'score', type: 'number',
