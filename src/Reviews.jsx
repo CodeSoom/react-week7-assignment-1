c83225733b5153d441a647e8221ec3081fa926d9
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-26 14:55:41 +0900

    리뷰를 입력할 때마다 계속해서 리뷰들이 재로딩되는 문제 해결
    api에서 받은 리뷰를 그대로 reverse 사용해서 일어난 에러로 spread 연산자를 이용해서 풀어서 사용할 수 있도록 수정하여 에러 해결하였습니다

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -2,10 +2,11 @@ export default function Reviews({ reviews }) {
   if (!reviews || reviews.length === 0) {
     return <p>리뷰가 없어요!</p>;
   }
+
   return (
     <ul>
       {
-        reviews.reverse().map((review) => (
+        [...reviews].reverse().map((review) => (
           <li key={review.id}>
             <p>{review.name}</p>
             <p>{review.score}</p>
