8fcf2f52429e47b6c84f0b1df8ba7addf572e4d5
hyeji07 <zindex_ann@naver.com>
2022-11-27 09:36:03 +0900

    App.jsx LoginPage 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import LoginPage from './LoginPage/LoginPage';
 import RestaurantsPage from './RestaurantsPage';
 import RestaurantPage from './RestaurantPage';
 import NotFoundPage from './NotFoundPage';
@@ -21,6 +22,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/login" component={LoginPage} />
         <Route exact path="/restaurants" component={RestaurantsPage} />
         <Route path="/restaurants/:id" component={RestaurantPage} />
         <Route component={NotFoundPage} />
