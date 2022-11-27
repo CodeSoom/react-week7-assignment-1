91611db4d39fa9e35282d9d062b0807a8e1efab1
sejineeee <sejinee0918@gmail.com>
2022-11-27 20:52:11 +0900

    refactor: input의 value를 비워주도록 구현
    props로 fields를 받음
    디스트럭처링을 통해서 TextField 컴포넌트에 각각 props 전달

--- src/ReviewForm.jsx
+++ src/ReviewForm.jsx
@@ -1,18 +1,26 @@
 import TextField from './TextField';
 
-export default function ReviewForm({ onChange, onSubmit }) {
+export default function ReviewForm({
+  fields,
+  onChange,
+  onSubmit,
+}) {
+  const { score, description } = fields;
+
   return (
     <>
       <TextField
         label="평점"
         name="score"
         type="number"
+        value={score}
         onChange={onChange}
       />
       <TextField
         label="리뷰 내용"
         name="description"
         type="text"
+        value={description}
         onChange={onChange}
       />
       <button type="button" onClick={onSubmit}>
