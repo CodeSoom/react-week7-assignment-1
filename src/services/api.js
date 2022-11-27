004fc8a66a3d247dd9454e5166e0724dce9e3d01
hyeji07 <zindex_ann@naver.com>
2022-11-27 14:31:38 +0900

    api.js postReview추가

--- src/services/api.js
+++ src/services/api.js
@@ -43,3 +43,25 @@ export async function postLogin({ email, password }) {
 
   return accessToken;
 }
+
+export async function postReview({
+  restaurantId,
+  accessToken,
+  score,
+  description,
+}) {
+  const url = `https://eatgo-customer-api.ahastudio.com/restaurants/${restaurantId}/reviews`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ score, description }),
+  });
+
+  const data = await response.json();
+
+  return data;
+}
