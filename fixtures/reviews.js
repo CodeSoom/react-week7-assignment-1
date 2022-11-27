77e8f6202374493d26464f7ea01d0d44bfd266fb
hyeji07 <zindex_ann@naver.com>
2022-11-27 14:51:16 +0900

    fixtures reviews.js생성

---
+++ fixtures/reviews.js
@@ -0,0 +1,16 @@
+const reviews = [
+  {
+    id: 1,
+    name: '테스터',
+    score: '5',
+    description: '맛나다',
+  },
+  {
+    id: 2,
+    name: '테스터',
+    score: '1',
+    description: '밍밍',
+  },
+];
+
+export default reviews;
