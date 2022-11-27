ef2e407953c4d8277ce05c8d17969c8824bdae9f
hyeji07 <zindex_ann@naver.com>
2022-11-27 14:52:38 +0900

    RestaurantContainer test추가

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantContainer from './RestaurantContainer';
+
+import reviewControls from '../fixtures/reviewControls';
 
 describe('RestaurantContainer', () => {
   const dispatch = jest.fn();
@@ -13,10 +15,16 @@ describe('RestaurantContainer', () => {
 
   beforeEach(() => {
     dispatch.mockClear();
+
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((selector) => selector({
       restaurant: given.restaurant,
+      reviewFields: {
+        score: '',
+        description: '',
+      },
+      accessToken: given.accessToken,
     }));
   });
 
@@ -48,6 +56,54 @@ describe('RestaurantContainer', () => {
       const { container } = renderRestaurantContainer();
 
       expect(container).toHaveTextContent('Loading');
+    });
+  });
+
+  context('로그아웃일 경우', () => {
+    it('리뷰 form이 랜더링되지 않는다', () => {
+      renderRestaurantContainer();
+
+      expect(screen.queryByLabelText('평점')).toBeNull();
+      expect(screen.queryByLabelText('리뷰 내용')).toBeNull();
+    });
+  });
+
+  context('로그인일 경우', () => {
+    given('accessToken', () => 'ACCESS_TOKEN');
+    given('restaurant', () => ({
+      id: 1,
+      name: '마법사주방',
+      address: '서울시 강남구',
+    }));
+
+    it('리뷰 작성하는 form이 랜더링된다', () => {
+      renderRestaurantContainer();
+
+      expect(screen.queryByLabelText('평점')).not.toBeNull();
+      expect(screen.queryByLabelText('리뷰 내용')).not.toBeNull();
+    });
+
+    it('리뷰 작성하는 form에 changeReviewField가 호출된다', () => {
+      renderRestaurantContainer();
+
+      reviewControls.forEach(({ label, name, value }) => {
+        const input = screen.getByLabelText(label);
+
+        fireEvent.change(input, { target: { value } });
+
+        expect(dispatch).toBeCalledWith({
+          type: 'changeReviewField',
+          payload: { name, value },
+        });
+      });
+    });
+
+    it('"리뷰 남기기"버튼이 랜더링된다', () => {
+      renderRestaurantContainer();
+
+      fireEvent.click(screen.getByText('리뷰 남기기'));
+
+      expect(dispatch).toBeCalledTimes(2);
     });
   });
 });
