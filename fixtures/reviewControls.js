6072c7423d424f7fdbfc6e279106c951b67d9abb
hyeji07 <zindex_ann@naver.com>
2022-11-27 14:49:46 +0900

    fixtures reviewControls.js 생성

---
+++ fixtures/reviewControls.js
@@ -0,0 +1,11 @@
+const reviewControls = [
+  {
+    label: '평점',
+    name: 'score',
+    value: '5',
+    type: 'number',
+  },
+  { label: '리뷰 내용', name: 'description', value: '맛나다' },
+];
+
+export default reviewControls;
